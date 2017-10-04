724d102ff569d41fce718c06302e75b688b456c2
Vincent Laviron <vincent.laviron@gmail.com>
2017-10-04 18:31:27 +0200

    Add an optional check for invariants on the Cmm representation
    Port cmm invariants (ocaml PR 1400)

--- ocaml/utils/config.mli
+++ ocaml/utils/config.mli
@@ -207,6 +207,9 @@ val flambda_backend : bool
 val with_flambda_invariants : bool
 (** Whether the invariants checks for flambda are enabled *)
 
+val with_cmm_invariants : bool
+(** Whether the invariants checks for Cmm are enabled *)
+
 val profinfo : bool
 (** Whether the compiler was configured for profiling *)
 
