bf3773daec43a637ccc374fdde421765b02bf373
Mark Shinwell <mshinwell@gmail.com>
2021-02-10 17:34:55 +0000

    flambda-backend: To upstream: dune build fixes (depends on previous to-upstream patches)

--- ocamltest/ocamltest_unix.dummy.ml
+++ ocamltest/ocamltest_unix.dummy.ml
@@ -2,10 +2,9 @@
 (*                                                                        *)
 (*                                 OCaml                                  *)
 (*                                                                        *)
-(*             Xavier Leroy, projet Cristal, INRIA Rocquencourt           *)
+(*                 David Allsopp, OCaml Labs, Cambridge.                  *)
 (*                                                                        *)
-(*   Copyright 2002 Institut National de Recherche en Informatique et     *)
-(*     en Automatique.                                                    *)
+(*   Copyright 2020 David Allsopp Ltd.                                    *)
 (*                                                                        *)
 (*   All rights reserved.  This file is distributed under the terms of    *)
 (*   the GNU Lesser General Public License version 2.1, with the          *)
@@ -13,4 +12,7 @@
 (*                                                                        *)
 (**************************************************************************)
 
-let _ = Stdlib.exit (Opttopmain.main())
+(* Dummy implementations for when the Unix library isn't built *)
+let has_symlink () = false
+let symlink ?to_dir:_ _ _ = invalid_arg "symlink not available"
+let chmod _ _ = invalid_arg "chmod not available"
--- ocamltest/ocamltest_unix.real.ml
+++ ocamltest/ocamltest_unix.real.ml
@@ -2,10 +2,9 @@
 (*                                                                        *)
 (*                                 OCaml                                  *)
 (*                                                                        *)
-(*             Xavier Leroy, projet Cristal, INRIA Rocquencourt           *)
+(*                 David Allsopp, OCaml Labs, Cambridge.                  *)
 (*                                                                        *)
-(*   Copyright 2002 Institut National de Recherche en Informatique et     *)
-(*     en Automatique.                                                    *)
+(*   Copyright 2020 David Allsopp Ltd.                                    *)
 (*                                                                        *)
 (*   All rights reserved.  This file is distributed under the terms of    *)
 (*   the GNU Lesser General Public License version 2.1, with the          *)
@@ -13,4 +12,18 @@
 (*                                                                        *)
 (**************************************************************************)
 
-let _ = Stdlib.exit (Opttopmain.main())
+(* Unix.has_symlink never raises *)
+let has_symlink = Unix.has_symlink
+
+(* Convert Unix_error to Sys_error *)
+let wrap f x =
+  try f x
+  with Unix.Unix_error(err, fn_name, arg) ->
+    let msg =
+      Printf.sprintf "%s failed on %S with %s"
+                     fn_name arg (Unix.error_message err)
+    in
+      raise (Sys_error msg)
+
+let symlink ?to_dir source = wrap (Unix.symlink ?to_dir source)
+let chmod file = wrap (Unix.chmod file)
