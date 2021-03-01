d0676b8730def1f7a3d28ca268c70394f6f00db1
Mark Shinwell <mshinwell@gmail.com>
2021-03-01 15:36:18 +0000

    To upstream: fix dependency problem with Instruct

--- ocaml/bytecomp/instruct.mli
+++ ocaml/bytecomp/instruct.mli
@@ -19,49 +19,37 @@ open Lambda
 
 (* Structure of compilation environments *)
 
-type compilation_env =
-  { ce_stack: int Ident.tbl; (* Positions of variables in the stack *)
-    ce_heap: int Ident.tbl;  (* Structure of the heap-allocated env *)
-    ce_rec: int Ident.tbl }  (* Functions bound by the same let rec *)
-
-(* The ce_stack component gives locations of variables residing
-   in the stack. The locations are offsets w.r.t. the origin of the
-   stack frame.
-   The ce_heap component gives the positions of variables residing in the
-   heap-allocated environment.
-   The ce_rec component associates offsets to identifiers for functions
-   bound by the same let rec as the current function.  The offsets
-   are used by the OFFSETCLOSURE instruction to recover the closure
-   pointer of the desired function from the env register (which
-   points to the closure for the current function). *)
+type compilation_env = Debug_event.compilation_env =
+  { ce_stack: int Ident.tbl;
+    ce_heap: int Ident.tbl;
+    ce_rec: int Ident.tbl }
 
 (* Debugging events *)
 
-(* Warning: when you change these types, check runtime/backtrace_byt.c *)
-type debug_event =
-  { mutable ev_pos: int;                (* Position in bytecode *)
-    ev_module: string;                  (* Name of defining module *)
-    ev_loc: Location.t;                 (* Location in source file *)
-    ev_kind: debug_event_kind;          (* Before/after event *)
-    ev_defname: string;                 (* Enclosing definition *)
-    ev_info: debug_event_info;          (* Extra information *)
-    ev_typenv: Env.summary;             (* Typing environment *)
-    ev_typsubst: Subst.t;               (* Substitution over types *)
-    ev_compenv: compilation_env;        (* Compilation environment *)
-    ev_stacksize: int;                  (* Size of stack frame *)
-    ev_repr: debug_event_repr }         (* Position of the representative *)
+type debug_event = Debug_event.debug_event =
+  { mutable ev_pos: int;
+    ev_module: string;
+    ev_loc: Location.t;
+    ev_kind: debug_event_kind;
+    ev_defname: string;
+    ev_info: debug_event_info;
+    ev_typenv: Env.summary;
+    ev_typsubst: Subst.t;
+    ev_compenv: compilation_env;
+    ev_stacksize: int;
+    ev_repr: debug_event_repr }
 
-and debug_event_kind =
+and debug_event_kind = Debug_event.debug_event_kind =
     Event_before
   | Event_after of Types.type_expr
   | Event_pseudo
 
-and debug_event_info =
+and debug_event_info = Debug_event.debug_event_info =
     Event_function
   | Event_return of int
   | Event_other
 
-and debug_event_repr =
+and debug_event_repr = Debug_event.debug_event_repr =
     Event_none
   | Event_parent of int ref
   | Event_child of int ref
