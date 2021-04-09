7c73cdc95fda529dedba5e429251c41cad0922c4
poechsel <pierre.oechsel@gmail.com>
2021-04-09 17:33:10 +0200

    Copy files from ocaml/ by executing scripts/copy-ocaml-subset.sh

---
+++ scripts/copy-ocaml-subset.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# To be run from the root of the Flambda backend repo
+
+set -eu -o pipefail
+
+# Delete folders if they already exists:
+rm -rf backend driver file_formats middle_end native_toplevel
+
+# Import them from ocaml
+cp -r ocaml/asmcomp backend
+cp -r ocaml/driver driver
+cp -r ocaml/file_formats file_formats
+cp -r ocaml/middle_end middle_end
+cp -r ocaml/toplevel native_toplevel
