93622e1b28fc59b81327fa94c44e3ee0c23b2a09
Greta Yorsh <gyorsh@janestreet.com>
2021-04-28 09:41:56 +0100

    Remove the constraint builtin=true
    Use effects and coeffects in Selectgen for all Cextcall
    and not only for builtins.

--- backend/selectgen.ml
+++ backend/selectgen.ml
@@ -414,9 +414,7 @@ method is_simple_expr = function
       begin match op with
       (* inlined ops without effects and coeffects are simple
          if their arguments are *)
-      | Cextcall { builtin = true;  (* CR gyorsh: don't require builtin? *)
-                   effects = No_effects;
-                   coeffects = No_coeffects; } ->
+      | Cextcall { effects = No_effects; coeffects = No_coeffects; } ->
         List.for_all self#is_simple_expr args
         (* The following may have side effects *)
       | Capply _ | Cextcall _ | Calloc | Cstore _ | Craise _ | Cprobe _
@@ -461,11 +459,9 @@ method effects_of exp =
   | Cop (op, args, _) ->
     let from_op =
       match op with
-      | Cextcall { builtin = true; effects = e; coeffects = ce; } ->
-        (* CR gyorsh for mshinwell:
-           should we remove the constraint builtin=true? *)
+      | Cextcall { effects = e; coeffects = ce; } ->
         EC.effect_and_coeffect (select_effects e) (select_coeffects ce)
-      | Capply _ | Cextcall _ | Cprobe _ | Copaque -> EC.arbitrary
+      | Capply _ | Cprobe _ | Copaque -> EC.arbitrary
       | Calloc -> EC.none
       | Cstore _ -> EC.effect_only Effect.Arbitrary
       | Cprefetch _ -> EC.arbitrary
