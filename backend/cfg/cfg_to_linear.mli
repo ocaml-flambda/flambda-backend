e4b4eee1638ee7d5bbc21312587b77450a0f3bfa
xclerc <xclerc@janestreet.com>
2021-06-22 14:56:59 +0100

    Update the `fun_tailrec_entry_point_label` field when we are back to linear.

--- backend/cfg/cfg_to_linear.mli
+++ backend/cfg/cfg_to_linear.mli
@@ -27,6 +27,6 @@
 
 [@@@ocaml.warning "+a-30-40-41-42"]
 
-val run : Cfg_with_layout.t -> Linear.instruction
+val run : Cfg_with_layout.t -> Linear.instruction * Label.t option
 
 val print_assembly : Cfg.basic_block list -> unit
