d19c211aa5cd743d831fdbf92933dca42d35c466
Mark Shinwell <mshinwell@gmail.com>
2021-07-20 14:01:35 +0100

    menhir version change in middle_end/flambda2/parser/flambda_parser.ml

--- middle_end/flambda2/parser/flambda_parser.ml
+++ middle_end/flambda2/parser/flambda_parser.ml
@@ -2,7 +2,7 @@
 (* This generated code requires the following version of CamlinternalMenhirLib: *)
 
 let () =
-  CamlinternalMenhirLib.StaticVersion.require_20200211
+  CamlinternalMenhirLib.StaticVersion.require_20190924
 
 module MenhirBasics = struct
   
