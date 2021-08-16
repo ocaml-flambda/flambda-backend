8e29370765363e7e7376d9caa0fb97fc0ceba6cf
basimkhajwal <basimkhajwal@gmail.com>
2021-08-16 09:57:17 +0100

    Implement asm_symbol

---
+++ backend/debug/dwarf/dwarf_deps/asm_symbol.ml
@@ -0,0 +1,11 @@
+module Thing = struct
+  type t = string
+  let compare = String.compare
+  let equal = String.equal
+  let hash = Hashtbl.hash
+  let output chan t = Printf.fprintf chan "%s" t
+  let print = Format.pp_print_string
+end
+
+include Thing
+include Identifiable.Make(Thing)
