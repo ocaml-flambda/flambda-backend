16b1a7bbaafd260cd189142893d55a0e028c098d
Vincent Laviron <vincent.laviron@gmail.com>
2021-08-19 12:27:14 +0200

    Add example

---
+++ middle_end/flambda2/tests/mlexamples/float_and_int_mix.ml
@@ -0,0 +1,17 @@
+type 'a ref = { mutable contents : 'a }
+external ref : 'a -> 'a ref = "%makemutable"
+external ( ! ) : 'a ref -> 'a = "%field0"
+external ( := ) : 'a ref -> 'a -> unit = "%setfield0"
+
+external magic : 'a -> 'b = "%identity"
+external opaque_identity : 'a -> 'a = "%opaque"
+
+let[@inline never] read_int () = 42
+
+let gasp_in_horror () =
+  let r = ref (magic 0.0) in
+  if opaque_identity true then begin
+    r := read_int ()
+  end;
+  !r
+
