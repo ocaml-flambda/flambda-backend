a17494451422bb0c413abdc0f7fe682f661e4773
Pierre Oechsel <pierre.oechsel@gmail.com>
2021-09-03 15:45:51 +0100

    ocaml/ is now a submodule

--- ocaml/testsuite/tests/lib-threads/test-runtime-cleanup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-case "$OCAMLRUNPARAM" in
-  c=1|c=1,*|*,c=1|*,c=1,*)
-    echo "runtime cleans up at exit" > ${ocamltest_response};
-    exit ${TEST_SKIP};;
-  *) exit ${TEST_PASS};;
-esac
--- ocaml/testsuite/tests/tool-ocamlopt-save-ir/save_ir_after_typing.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-grep "wrong argument 'typing'" compiler-output.raw | grep "save-ir-after" | sed 's/^.*: wrong argument/wrong argument/'
