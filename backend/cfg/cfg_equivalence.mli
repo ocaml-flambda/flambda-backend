ab20d3967f7c72f56f77e2dc3380b0c6f9f9be17
xclerc <xclerc@janestreet.com>
2021-09-07 15:20:15 +0100

    Better description of the CFG equivalence check.

--- backend/cfg/cfg_equivalence.mli
+++ backend/cfg/cfg_equivalence.mli
@@ -5,8 +5,17 @@ val check_cfg_with_layout : Mach.fundecl -> Cfg_with_layout.t -> Cfg_with_layout
 
     The passed CFGs are deemed equivalent iff:
     - the graphs are isomorphic;
-    - the layouts are equal, up to the function mapping vertices in the
-      isomorphism above.
+    - the layouts are equal, up to the function mapping the vertices in the
+      isomorphism above;
+    and blocks are deemed equivalent iff:
+    - they hold equivalent instruction lists;
+    - they end with equivalent terminators;
+    - they have the same value for their `trap_depth`, `can_raise`, `dead`,
+      and `is_trap_handler` fields;
+    - their successor and predecessor sets are equivalent up to the function
+      mapping the vertices in the isomorphism above.
+    (Instruction identifiers are ignored when comparing both mere instructions,
+    and terminators.)
 
     Caveat: the check currently ignores the live sets and debug information
     on all terminators, and the input registers on `Always _` terminators. *)
