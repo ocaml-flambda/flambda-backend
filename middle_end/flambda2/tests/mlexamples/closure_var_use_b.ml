b6906bb27eb06542dac58e1bab5729e3785d87e4
Vincent Laviron <vincent.laviron@gmail.com>
2021-10-18 14:13:36 +0200

    Allow Code_id join to return imported code IDs
    The tests show a bug with that: inlining of functions from A
    create closures that have a closure variable 'x', but the code
    associated doesn't mention the variable after simplification.
    However, the common ancestor of the closures still needs the
    variable, so we must not remove it.

---
+++ middle_end/flambda2/tests/mlexamples/closure_var_use_b.ml
@@ -0,0 +1,9 @@
+external op : 'a -> 'a = "%opaque"
+
+module A = Closure_var_use_a
+
+let _ =
+  let g =
+    if op true then (A.f[@inlined always]) 0 else (A.f[@inlined always]) 1
+  in
+  (g[@inlined never]) 2
