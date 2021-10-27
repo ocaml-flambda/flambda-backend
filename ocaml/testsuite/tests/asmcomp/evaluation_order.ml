238b0bc735f468bee5b615f0319cf38f4bbabcdb
Greta Yorsh <gyorsh@janestreet.com>
2021-10-27 16:02:15 +0100

    Fix test failure - add test references

--- ocaml/testsuite/tests/asmcomp/evaluation_order.ml
+++ ocaml/testsuite/tests/asmcomp/evaluation_order.ml
@@ -75,24 +75,4 @@ let () =
   in
   test_arrayref_unsafe [| "1";"2";"3" |] 0 Fun.id;
 
-  (* CR gyorsh: fix bigarray_get  *)
-  let[@inline never] test_bigarray_get
-       (a : (Complex.t, complex32_elt, c_layout) Array1.t)
-       (i : int) =
-    print_float (Array1.unsafe_get (print_endline "A"; a)
-                   (print_endline "B"; i)).Complex.re;
-    print_newline ()
-  in
-  test_bigarray_get (Array1.init complex32 c_layout 3 (fun _ -> Complex.one)) 0;
-
-  (* CR gyorsh: fix bigarray_set  *)
-  let[@inline never] test_bigarray_set
-       (a : (Complex.t, complex32_elt, c_layout) Array1.t)
-       (i : int) =
-    Array1.unsafe_set (print_endline "A"; a) (print_endline "B"; i)
-      (print_endline "C"; Complex.i);
-    print_endline "?"
-  in
-  test_bigarray_set (Array1.create complex32 c_layout 3) 0;
-
   ()
---
+++ ocaml/testsuite/tests/asmcomp/evaluation_order_bigarray.ml
@@ -0,0 +1,24 @@
+(* TEST
+ * bytecode
+*)
+open Bigarray
+let () =
+(* CR gyorsh: fix bigarray_get  *)
+  let[@inline never] test_bigarray_get
+       (a : (Complex.t, complex32_elt, c_layout) Array1.t)
+       (i : int) =
+    print_float (Array1.unsafe_get (print_endline "A"; a)
+                   (print_endline "B"; i)).Complex.re;
+    print_newline ()
+  in
+  test_bigarray_get (Array1.init complex32 c_layout 3 (fun _ -> Complex.one)) 0;
+
+  (* CR gyorsh: fix bigarray_set  *)
+  let[@inline never] test_bigarray_set
+       (a : (Complex.t, complex32_elt, c_layout) Array1.t)
+       (i : int) =
+    Array1.unsafe_set (print_endline "A"; a) (print_endline "B"; i)
+      (print_endline "C"; Complex.i);
+    print_endline "?"
+  in
+  test_bigarray_set (Array1.create complex32 c_layout 3) 0;
