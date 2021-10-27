6c6d8df49344125ab6d4b6cf523299770eb097ca
Greta Yorsh <gyorsh@janestreet.com>
2021-10-27 15:32:01 +0100

    More tests

--- ocaml/testsuite/tests/asmcomp/evaluation_order.ml
+++ ocaml/testsuite/tests/asmcomp/evaluation_order.ml
@@ -2,12 +2,16 @@
 *)
 external unsafe_get : 'a array -> int -> 'a = "%array_unsafe_get"
 external caml_bytes_get_16 : bytes -> int -> int = "%caml_bytes_get16"
+external caml_bytes_set_16 : bytes -> int -> int -> unit = "%caml_bytes_set16"
 
 open Bigarray
 type bigstring = (char, int8_unsigned_elt, c_layout) Array1.t
 
 external caml_bigstring_get_16 :
   bigstring -> int -> int = "%caml_bigstring_get16"
+
+external caml_bigstring_set_16 :
+  bigstring -> int -> int -> unit = "%caml_bigstring_set16"
 
 let bigstring_of_string s =
   let a = Array1.create char c_layout (String.length s) in
@@ -31,4 +35,64 @@ let () =
                          bigstring_of_string (String.make 10 '\x00'))
     (print_endline "world"; 0)
   |> Printf.printf "%x\n";
+
+  (* bytes_set *)
+  caml_bytes_set_16 (print_endline "a"; Bytes.make 10 '\x00')
+    (print_endline "b"; 0)
+    (print_endline "c"; 0xFF);
+
+  (* bigstring_set *)
+  caml_bigstring_set_16 (print_endline "a";
+                         bigstring_of_string (String.make 10 '\x00'))
+    (print_endline "b"; 0)
+    (print_endline "c"; 0xFF);
+
+  (* mk_compare_ints_untagged *)
+  print_int (compare (print_endline "A"; Sys.opaque_identity (2))
+               (print_endline "B"; Sys.opaque_identity (3)));
+  print_newline ();
+
+  (* mk_compare_floats *)
+  print_int (compare (print_endline "A"; Sys.opaque_identity (2.0))
+               (print_endline "B"; Sys.opaque_identity (3.5)));
+  print_newline ();
+
+  (* bytesset_safe *)
+  Bytes.set (print_endline "a"; Bytes.make 10 '\x00')
+    (print_endline "b"; 0)
+    (print_endline "c"; 'c');
+
+  (* safe_div_bi *)
+  Printf.printf "%nd\n"
+    (Nativeint.div (print_endline "A"; Sys.opaque_identity (6n))
+               (print_endline "B"; Sys.opaque_identity (3n)));
+
+  (* arrayref_unsafe *)
+  let[@inline never] test_arrayref_unsafe
+    : type t . t array -> int -> (t -> string) -> unit =
+    fun a i c ->
+      print_endline (c (Array.unsafe_get (print_endline "A"; a) (print_endline "B"; i)))
+  in
+  test_arrayref_unsafe [| "1";"2";"3" |] 0 Fun.id;
+
+  (* CR gyorsh: fix bigarray_get  *)
+  let[@inline never] test_bigarray_get
+       (a : (Complex.t, complex32_elt, c_layout) Array1.t)
+       (i : int) =
+    print_float (Array1.unsafe_get (print_endline "A"; a)
+                   (print_endline "B"; i)).Complex.re;
+    print_newline ()
+  in
+  test_bigarray_get (Array1.init complex32 c_layout 3 (fun _ -> Complex.one)) 0;
+
+  (* CR gyorsh: fix bigarray_set  *)
+  let[@inline never] test_bigarray_set
+       (a : (Complex.t, complex32_elt, c_layout) Array1.t)
+       (i : int) =
+    Array1.unsafe_set (print_endline "A"; a) (print_endline "B"; i)
+      (print_endline "C"; Complex.i);
+    print_endline "?"
+  in
+  test_bigarray_set (Array1.create complex32 c_layout 3) 0;
+
   ()
