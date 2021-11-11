5c453465da22f79ec5be6c42187f5007de295414
Guillaume Bury <guillaume.bury@gmail.com>
2021-11-11 11:28:28 +0100

    Inline generative effects in cmm (#378)

--- middle_end/flambda2/to_cmm/to_cmm.ml
+++ middle_end/flambda2/to_cmm/to_cmm.ml
@@ -630,10 +630,35 @@ let decide_inline_let effs
          flushed by the env. *)
   end
   | One -> begin
-    match Env.classify effs with
-    | Effect when not (Flambda_features.Expert.inline_effects_in_cmm ()) ->
-      Regular
-    | _ -> Inline
+    match (effs : Effects_and_coeffects.t) with
+    (* The decision here is whether to consider the binding for inlining or not.
+       It is always correct to consider an effectful expression for inlining, as
+       the environment is going to handle the details of preserving the effects
+       and coeffects ordering (if inlining without reordering is impossible then
+       the expressions will be bound at some safe place instead).
+
+       So the decision here is about readability of the generated Cmm code
+       (effectful expressions as arguments to other primitives make it hard to
+       follow the order of evaluation) and locality (for deeply nested
+       expressions, binding the sub-expressions outside can keep them alive for
+       longer than strictly necessary).
+
+       The current choice of always inlining pure expressions and expressions
+       with only generative effects is guided by the relatively common case of
+       initialisation of huge static structures (including arrays). Without
+       inlining, all intermediate results would be live for long periods of time
+       and the default register allocator would have trouble dealing with that
+       (it's quadratic in the number of registers live at the same time).
+
+       Deep expressions involving arbitrary effects are less common, so inlining
+       for these expressions is controlled by the global [inline_effects_in_cmm]
+       setting. *)
+    | Only_generative_effects _, _ -> Inline
+    | Arbitrary_effects, _ ->
+      if Flambda_features.Expert.inline_effects_in_cmm ()
+      then Inline
+      else Regular
+    | No_effects, _ -> Inline
   end
   | More_than_one -> Regular
 
