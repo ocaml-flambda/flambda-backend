stedolan/ocaml

Switch to a system with 3 modes for values

typing/env.ml
@@ -214,6 +214,14 @@ module TycompTbl =
 
 type empty = |
 
+type value_lock =
+  | Lock of { mode : alloc_mode; }
+  | Region_lock
+
+let is_region_lock = function
+  | Lock _ -> false
+  | Region_lock -> true
+
 module IdTbl =
   struct
     (** This module is used to store all kinds of components except
@@ -423,7 +431,7 @@ type type_descriptions =
 let in_signature_flag = 0x01
 
 type t = {
-  values: (alloc_mode, value_entry, value_data) IdTbl.t;
+  values: (value_lock, value_entry, value_data) IdTbl.t;
   constrs: constructor_data TycompTbl.t;
   labels: label_data TycompTbl.t;
   types: (empty, type_data, type_data) IdTbl.t;
@@ -495,7 +503,7 @@ and address_lazy = (address_unforced, address) EnvLazy.t
 and value_data =
   { vda_description : value_description;
     vda_address : address_lazy;
-    vda_mode : alloc_mode }
+    vda_mode : Value_mode.t }
 
 and value_entry =
   | Val_bound of value_data
@@ -564,7 +572,7 @@ type lookup_error =
   | Generative_used_as_applicative of Longident.t
   | Illegal_reference_to_recursive_module
   | Cannot_scrape_alias of Longident.t * Path.t
-  | Local_value_escapes of Longident.t
+  | Local_value_escapes of Longident.t * Value_mode.error
   | Local_value_used_in_closure of Longident.t
 
 type error =
@@ -1578,7 +1586,7 @@ let rec components_of_module_maker
             in
             let vda = { vda_description = decl';
                         vda_address = addr;
-                        vda_mode = Alloc_mode.heap } in
+                        vda_mode = Value_mode.global } in
             c.comp_values <- NameMap.add (Ident.name id) vda c.comp_values;
         | Sig_type(id, decl, _, _) ->
             let fresh_decl =
@@ -1912,7 +1920,7 @@ let add_functor_arg id env =
    functor_args = Ident.add id () env.functor_args;
    summary = Env_functor_arg (env.summary, id)}
 
-let add_value ?check ?(mode = Alloc_mode.heap) id desc env =
+let add_value ?check ?(mode = Value_mode.global) id desc env =
   let addr = value_declaration_address env id desc in
   store_value ?check mode id addr desc env
 
@@ -1959,7 +1967,7 @@ let add_local_type path info env =
 let enter_value ?check name desc env =
   let id = Ident.create_local name in
   let addr = value_declaration_address env id desc in
-  let env = store_value ?check Alloc_mode.heap id addr desc env in
+  let env = store_value ?check Value_mode.global id addr desc env in
   (id, env)
 
 let enter_type ~scope name info env =
@@ -1997,7 +2005,10 @@ let enter_module ~scope ?arg s presence mty env =
   enter_module_declaration ~scope ?arg s presence (md mty) env
 
 let add_lock mode env =
-  { env with values = IdTbl.add_lock mode env.values }
+  { env with values = IdTbl.add_lock (Lock {mode}) env.values }
+
+let add_region_lock env =
+  { env with values = IdTbl.add_lock Region_lock env.values }
 
 (* Insertion of all components of a signature *)
 
@@ -2414,21 +2425,31 @@ let lookup_ident_module (type a) (load : a load) ~errors ~use ~loc s env =
         end
     end
 
-let constrain_modes ~errors ~loc env id vmode locks mode =
-  let constrain m n err =
-    match Alloc_mode.submode m n with
-    | Ok () -> ()
-    | Error () -> may_lookup_error errors loc env err in
-  constrain vmode mode (Local_value_escapes id);
-  locks |> List.iter (fun l ->
-    constrain vmode l (Local_value_used_in_closure id))
+let lock_mode ~errors ~loc env id vmode locks =
+  let vmode =
+    if List.exists is_region_lock locks then Value_mode.nonlocal vmode
+    else vmode
+  in
+  let vmode_nonlocal = Value_mode.to_alloc_nonlocal vmode in
+  List.iter
+    (function
+      | Region_lock -> ()
+      | Lock { mode } -> begin
+          match Alloc_mode.submode vmode_nonlocal mode with
+          | Ok () -> ()
+          | Error () ->
+            may_lookup_error errors loc env
+              (Local_value_used_in_closure id)
+        end)
+    locks;
+  vmode
 
-let lookup_ident_value ~errors ~use ~loc name mode env =
+let lookup_ident_value ~errors ~use ~loc name env =
   match IdTbl.find_name_and_modes wrap_value ~mark:use name env.values with
   | (path, locks, Val_bound vda) ->
-      constrain_modes ~errors ~loc env (Lident name) vda.vda_mode locks mode;
+      let mode = lock_mode ~errors ~loc env (Lident name) vda.vda_mode locks in
       use_value ~use ~loc path vda;
-      path, vda.vda_description
+      path, vda.vda_description, mode
   | (_, _, Val_unbound reason) ->
       report_value_unbound ~errors ~loc env reason (Lident name)
   | exception Not_found ->
@@ -2660,10 +2681,13 @@ let lookup_module_path ~errors ~use ~loc ~load lid env : Path.t =
       check_functor_appl ~errors ~loc env p1 f arg p2 md2;
       Papply(p1, p2)
 
-let lookup_value ~errors ~use ~loc lid mode env =
+let lookup_value ~errors ~use ~loc lid env =
   match lid with
-  | Lident s -> lookup_ident_value ~errors ~use ~loc s mode env
-  | Ldot(l, s) -> lookup_dot_value ~errors ~use ~loc l s env
+  | Lident s -> lookup_ident_value ~errors ~use ~loc s env
+  | Ldot(l, s) ->
+    let path, desc = lookup_dot_value ~errors ~use ~loc l s env in
+    let mode = Value_mode.global in
+    path, desc, mode
   | Lapply _ -> assert false
 
 let lookup_type_full ~errors ~use ~loc lid env =
@@ -2748,7 +2772,8 @@ let find_module_by_name lid env =
 
 let find_value_by_name lid env =
   let loc = Location.(in_file !input_name) in
-  lookup_value ~errors:false ~use:false ~loc lid Alloc_mode.max_mode env
+  let path, desc, _ = lookup_value ~errors:false ~use:false ~loc lid env in
+  path, desc
 
 let find_type_by_name lid env =
   let loc = Location.(in_file !input_name) in
@@ -2824,9 +2849,7 @@ let lookup_all_labels_from_type ?(use=true) ~loc ty_path env =
 
 let lookup_instance_variable ?(use=true) ~loc name env =
   match IdTbl.find_name_and_modes wrap_value ~mark:use name env.values with
-  | (path, locks, Val_bound vda) -> begin
-      constrain_modes ~errors:true ~loc env (Lident name)
-        vda.vda_mode locks Alloc_mode.heap;
+  | (path, _, Val_bound vda) -> begin
       let desc = vda.vda_description in
       match desc.val_kind with
       | Val_ivar(mut, cl_num) ->
@@ -3245,15 +3268,22 @@ let report_lookup_error _loc env ppf = function
       fprintf ppf
         "The module %a is an alias for module %a, which %s"
         !print_longident lid !print_path p cause
-  | Local_value_escapes lid ->
+  | Local_value_escapes(lid, reason) ->
+      let mode =
+        match reason with
+        | Regionality -> ""
+        | Locality -> "local "
+      in
       fprintf ppf
-        "@[The value %a is local, so cannot be used here as it might escape@]"
-        !print_longident lid
+        "@[The %svalue %a cannot be used here@ \
+           as it escapes its region@]"
+        mode !print_longident lid
   | Local_value_used_in_closure lid ->
       fprintf ppf
         "@[The value %a is local, so cannot be used \
            inside a closure that might escape@]"
         !print_longident lid
+
 
 let report_error ppf = function
   | Missing_module(_, path1, path2) ->

typing/parmatch.ml
@@ -38,7 +38,7 @@ let omega_list = Patterns.omega_list
 let extra_pat =
   make_pat
     (Tpat_var (Ident.create_local "+", mknoloc "+"))
-    Ctype.none Alloc_mode.local Env.empty
+    Ctype.none Value_mode.min_mode Env.empty
 
 
 (*******************)
@@ -710,7 +710,7 @@ let set_last a =
 (* mark constructor lines for failure when they are incomplete *)
 let mark_partial =
   let zero =
-    make_pat (`Constant (Const_int 0)) Ctype.none Alloc_mode.local Env.empty
+    make_pat (`Constant (Const_int 0)) Ctype.none Value_mode.min_mode Env.empty
   in
   List.map (fun ((hp, _), _ as ps) ->
     match hp.pat_desc with
@@ -946,7 +946,7 @@ let build_other ext env =
           make_pat
             (Tpat_var (Ident.create_local "*extension*",
                        {txt="*extension*"; loc = d.pat_loc}))
-            Ctype.none Alloc_mode.local Env.empty
+            Ctype.none Value_mode.min_mode Env.empty
       | Construct _ ->
           begin match ext with
           | Some ext ->
@@ -1956,7 +1956,7 @@ type ppat_of_type =
       (string, label_description) Hashtbl.t
 
 let ppat_of_type env ty =
-  match pats_of_type env ty Alloc_mode.local with
+  match pats_of_type env ty Value_mode.min_mode with
   | [] -> PT_empty
   | [{pat_desc = Tpat_any}] -> PT_any
   | [pat] ->

typing/typemod.ml
@@ -1957,7 +1957,7 @@ and type_module_aux ~alias sttn funct_body anchor env smod =
           in
           Named (id, param, mty), Types.Named (id, mty.mty_type), newenv, true
       in
-      let newenv = Env.add_lock Alloc_mode.heap newenv in
+      let newenv = Env.add_lock Alloc_mode.global newenv in
       let body = type_module sttn funct_body None newenv sbody in
       { mod_desc = Tmod_functor(t_arg, body);
         mod_type = Mty_functor(ty_arg, body.mod_type);

ocaml-flambda/flambda-backend

typing/env.ml
@@ -214,6 +214,10 @@ module TycompTbl =
 
 type empty = |
 
+type value_lock =
+  | Lock of { mode : Value_mode.t; }
+  | Region_lock
+
 module IdTbl =
   struct
     (** This module is used to store all kinds of components except
@@ -423,7 +427,7 @@ type type_descriptions =
 let in_signature_flag = 0x01
 
 type t = {
-  values: (alloc_mode, value_entry, value_data) IdTbl.t;
+  values: (value_lock, value_entry, value_data) IdTbl.t;
   constrs: constructor_data TycompTbl.t;
   labels: label_data TycompTbl.t;
   types: (empty, type_data, type_data) IdTbl.t;
@@ -495,7 +499,7 @@ and address_lazy = (address_unforced, address) EnvLazy.t
 and value_data =
   { vda_description : value_description;
     vda_address : address_lazy;
-    vda_mode : alloc_mode }
+    vda_mode : Value_mode.t }
 
 and value_entry =
   | Val_bound of value_data
@@ -564,7 +568,7 @@ type lookup_error =
   | Generative_used_as_applicative of Longident.t
   | Illegal_reference_to_recursive_module
   | Cannot_scrape_alias of Longident.t * Path.t
-  | Local_value_escapes of Longident.t
+  | Local_value_escapes of Longident.t * Value_mode.error
   | Local_value_used_in_closure of Longident.t
 
 type error =
@@ -1578,7 +1582,7 @@ let rec components_of_module_maker
             in
             let vda = { vda_description = decl';
                         vda_address = addr;
-                        vda_mode = Alloc_mode.heap } in
+                        vda_mode = Value_mode.global } in
             c.comp_values <- NameMap.add (Ident.name id) vda c.comp_values;
         | Sig_type(id, decl, _, _) ->
             let fresh_decl =
@@ -1912,7 +1916,7 @@ let add_functor_arg id env =
    functor_args = Ident.add id () env.functor_args;
    summary = Env_functor_arg (env.summary, id)}
 
-let add_value ?check ?(mode = Alloc_mode.heap) id desc env =
+let add_value ?check ?(mode = Value_mode.global) id desc env =
   let addr = value_declaration_address env id desc in
   store_value ?check mode id addr desc env
 
@@ -1959,7 +1963,7 @@ let add_local_type path info env =
 let enter_value ?check name desc env =
   let id = Ident.create_local name in
   let addr = value_declaration_address env id desc in
-  let env = store_value ?check Alloc_mode.heap id addr desc env in
+  let env = store_value ?check Value_mode.global id addr desc env in
   (id, env)
 
 let enter_type ~scope name info env =
@@ -1997,7 +2001,10 @@ let enter_module ~scope ?arg s presence mty env =
   enter_module_declaration ~scope ?arg s presence (md mty) env
 
 let add_lock mode env =
-  { env with values = IdTbl.add_lock mode env.values }
+  { env with values = IdTbl.add_lock (Lock {mode}) env.values }
+
+let add_region_lock env =
+  { env with values = IdTbl.add_lock Region_lock env.values }
 
 (* Insertion of all components of a signature *)
 
@@ -2414,21 +2421,25 @@ let lookup_ident_module (type a) (load : a load) ~errors ~use ~loc s env =
         end
     end
 
-let constrain_modes ~errors ~loc env id vmode locks mode =
-  let constrain m n err =
-    match Alloc_mode.submode m n with
-    | Ok () -> ()
-    | Error () -> may_lookup_error errors loc env err in
-  constrain vmode mode (Local_value_escapes id);
-  locks |> List.iter (fun l ->
-    constrain vmode l (Local_value_used_in_closure id))
+let lock_mode ~errors ~loc env id vmode locks =
+  List.fold_left
+    (fun vmode lock ->
+      match lock with
+      | Region_lock -> Value_mode.local_to_regional vmode
+      | Lock {mode} ->
+          match Value_mode.submode vmode mode with
+          | Ok () -> vmode
+          | Error _ ->
+              may_lookup_error errors loc env
+                (Local_value_used_in_closure id))
+    vmode locks
 
-let lookup_ident_value ~errors ~use ~loc name mode env =
+let lookup_ident_value ~errors ~use ~loc name env =
   match IdTbl.find_name_and_modes wrap_value ~mark:use name env.values with
   | (path, locks, Val_bound vda) ->
-      constrain_modes ~errors ~loc env (Lident name) vda.vda_mode locks mode;
+      let mode = lock_mode ~errors ~loc env (Lident name) vda.vda_mode locks in
       use_value ~use ~loc path vda;
-      path, vda.vda_description
+      path, vda.vda_description, mode
   | (_, _, Val_unbound reason) ->
       report_value_unbound ~errors ~loc env reason (Lident name)
   | exception Not_found ->
@@ -2660,10 +2671,13 @@ let lookup_module_path ~errors ~use ~loc ~load lid env : Path.t =
       check_functor_appl ~errors ~loc env p1 f arg p2 md2;
       Papply(p1, p2)
 
-let lookup_value ~errors ~use ~loc lid mode env =
+let lookup_value ~errors ~use ~loc lid env =
   match lid with
-  | Lident s -> lookup_ident_value ~errors ~use ~loc s mode env
-  | Ldot(l, s) -> lookup_dot_value ~errors ~use ~loc l s env
+  | Lident s -> lookup_ident_value ~errors ~use ~loc s env
+  | Ldot(l, s) ->
+    let path, desc = lookup_dot_value ~errors ~use ~loc l s env in
+    let mode = Value_mode.global in
+    path, desc, mode
   | Lapply _ -> assert false
 
 let lookup_type_full ~errors ~use ~loc lid env =
@@ -2748,7 +2762,8 @@ let find_module_by_name lid env =
 
 let find_value_by_name lid env =
   let loc = Location.(in_file !input_name) in
-  lookup_value ~errors:false ~use:false ~loc lid Alloc_mode.max_mode env
+  let path, desc, _ = lookup_value ~errors:false ~use:false ~loc lid env in
+  path, desc
 
 let find_type_by_name lid env =
   let loc = Location.(in_file !input_name) in
@@ -2824,9 +2839,7 @@ let lookup_all_labels_from_type ?(use=true) ~loc ty_path env =
 
 let lookup_instance_variable ?(use=true) ~loc name env =
   match IdTbl.find_name_and_modes wrap_value ~mark:use name env.values with
-  | (path, locks, Val_bound vda) -> begin
-      constrain_modes ~errors:true ~loc env (Lident name)
-        vda.vda_mode locks Alloc_mode.heap;
+  | (path, _, Val_bound vda) -> begin
       let desc = vda.vda_description in
       match desc.val_kind with
       | Val_ivar(mut, cl_num) ->
@@ -3245,15 +3258,22 @@ let report_lookup_error _loc env ppf = function
       fprintf ppf
         "The module %a is an alias for module %a, which %s"
         !print_longident lid !print_path p cause
-  | Local_value_escapes lid ->
+  | Local_value_escapes(lid, reason) ->
+      let mode =
+        match reason with
+        | Regionality -> ""
+        | Locality -> "local "
+      in
       fprintf ppf
-        "@[The value %a is local, so cannot be used here as it might escape@]"
-        !print_longident lid
+        "@[The %svalue %a cannot be used here@ \
+           as it escapes its region@]"
+        mode !print_longident lid
   | Local_value_used_in_closure lid ->
       fprintf ppf
         "@[The value %a is local, so cannot be used \
            inside a closure that might escape@]"
         !print_longident lid
+
 
 let report_error ppf = function
   | Missing_module(_, path1, path2) ->

typing/parmatch.ml
@@ -38,7 +38,7 @@ let omega_list = Patterns.omega_list
 let extra_pat =
   make_pat
     (Tpat_var (Ident.create_local "+", mknoloc "+"))
-    Ctype.none Alloc_mode.local Env.empty
+    Ctype.none Value_mode.max_mode Env.empty
 
 
 (*******************)
@@ -710,7 +710,7 @@ let set_last a =
 (* mark constructor lines for failure when they are incomplete *)
 let mark_partial =
   let zero =
-    make_pat (`Constant (Const_int 0)) Ctype.none Alloc_mode.local Env.empty
+    make_pat (`Constant (Const_int 0)) Ctype.none Value_mode.max_mode Env.empty
   in
   List.map (fun ((hp, _), _ as ps) ->
     match hp.pat_desc with
@@ -946,7 +946,7 @@ let build_other ext env =
           make_pat
             (Tpat_var (Ident.create_local "*extension*",
                        {txt="*extension*"; loc = d.pat_loc}))
-            Ctype.none Alloc_mode.local Env.empty
+            Ctype.none Value_mode.max_mode Env.empty
       | Construct _ ->
           begin match ext with
           | Some ext ->
@@ -1956,7 +1956,7 @@ type ppat_of_type =
       (string, label_description) Hashtbl.t
 
 let ppat_of_type env ty =
-  match pats_of_type env ty Alloc_mode.local with
+  match pats_of_type env ty Value_mode.max_mode with
   | [] -> PT_empty
   | [{pat_desc = Tpat_any}] -> PT_any
   | [pat] ->

typing/typemod.ml
@@ -1957,7 +1957,7 @@ and type_module_aux ~alias sttn funct_body anchor env smod =
           in
           Named (id, param, mty), Types.Named (id, mty.mty_type), newenv, true
       in
-      let newenv = Env.add_lock Alloc_mode.heap newenv in
+      let newenv = Env.add_lock Value_mode.global newenv in
       let body = type_module sttn funct_body None newenv sbody in
       { mod_desc = Tmod_functor(t_arg, body);
         mod_type = Mty_functor(ty_arg, body.mod_type);