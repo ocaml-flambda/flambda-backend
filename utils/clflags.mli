a7960dd604ee71880f479f8f56939d901fbd26a0
Greta Yorsh <45005955+gretay-js@users.noreply.github.com>
2021-11-17 14:50:39 +0000

    flambda-backend: Move flambda-backend specific flags out of ocaml/ subdirectory (#382)

--- utils/clflags.mli
+++ utils/clflags.mli
@@ -27,6 +27,7 @@ module Int_arg_helper : sig
   val parse_no_error : string -> parsed ref -> parse_result
 
   val get : key:int -> parsed -> int
+  val default : int -> parsed
 end
 
 (** Optimization parameters represented as floats indexed by round number. *)
@@ -41,7 +42,12 @@ module Float_arg_helper : sig
   val parse_no_error : string -> parsed ref -> parse_result
 
   val get : key:int -> parsed -> float
+  val default : float -> parsed
 end
+val set_int_arg :
+    int option -> Int_arg_helper.parsed ref -> int -> int option -> unit
+val set_float_arg :
+    int option -> Float_arg_helper.parsed ref -> float -> float option -> unit
 
 val objfiles : string list ref
 val ccobjs : string list ref
@@ -56,7 +62,6 @@ val debug : bool ref
 val debug_full : bool ref
 val unsafe : bool ref
 val use_linscan : bool ref
-val use_ocamlcfg : bool ref
 val link_everything : bool ref
 val custom_runtime : bool ref
 val no_check_prims : bool ref
@@ -222,106 +227,6 @@ val unboxed_types : bool ref
 
 val insn_sched : bool ref
 val insn_sched_default : bool
-
-module Flambda2 : sig
-  module Default : sig
-    val classic_mode : bool
-    val join_points : bool
-    val unbox_along_intra_function_control_flow : bool
-    val backend_cse_at_toplevel : bool
-    val cse_depth : int
-    val treat_invalid_code_as_unreachable : bool
-
-    val unicode : bool
-  end
-
-  val classic_mode : bool ref
-  val join_points : bool ref
-  val unbox_along_intra_function_control_flow : bool ref
-  val backend_cse_at_toplevel : bool ref
-  val cse_depth : int ref
-  val treat_invalid_code_as_unreachable : bool ref
-
-  val unicode : bool ref
-
-  module Dump : sig
-    val rawfexpr : bool ref
-    val fexpr : bool ref
-    val flexpect : bool ref
-    val closure_offsets : bool ref
-    val freshen : bool ref
-  end
-
-  module Expert : sig
-    module Default : sig
-      val code_id_and_symbol_scoping_checks : bool
-      val fallback_inlining_heuristic : bool
-      val inline_effects_in_cmm : bool
-      val phantom_lets : bool
-      val max_block_size_for_projections : int option
-      val max_unboxing_depth : int
-      val can_inline_recursive_functions : bool
-    end
-
-    val code_id_and_symbol_scoping_checks : bool ref
-    val fallback_inlining_heuristic : bool ref
-    val inline_effects_in_cmm : bool ref
-    val phantom_lets : bool ref
-    val max_block_size_for_projections : int option ref
-    val max_unboxing_depth : int ref
-    val can_inline_recursive_functions : bool ref
-  end
-
-  module Debug : sig
-    module Default : sig
-      val permute_every_name : bool
-      val concrete_types_only_on_canonicals : bool
-    end
-
-    val permute_every_name : bool ref
-    val concrete_types_only_on_canonicals : bool ref
-  end
-
-  module Inlining : sig
-    module Default : sig
-      val max_depth : int
-      val max_rec_depth : int
-
-      val call_cost : float
-      val alloc_cost : float
-      val prim_cost : float
-      val branch_cost : float
-      val indirect_call_cost : float
-      val poly_compare_cost : float
-
-      val small_function_size : int
-      val large_function_size : int
-
-      val threshold : float
-
-      val speculative_inlining_only_if_arguments_useful : bool
-    end
-
-    val max_depth : Int_arg_helper.parsed ref
-    val max_rec_depth : Int_arg_helper.parsed ref
-
-    val call_cost : Float_arg_helper.parsed ref
-    val alloc_cost : Float_arg_helper.parsed ref
-    val prim_cost : Float_arg_helper.parsed ref
-    val branch_cost : Float_arg_helper.parsed ref
-    val indirect_call_cost : Float_arg_helper.parsed ref
-    val poly_compare_cost : Float_arg_helper.parsed ref
-
-    val small_function_size : Int_arg_helper.parsed ref
-    val large_function_size : Int_arg_helper.parsed ref
-
-    val threshold : Float_arg_helper.parsed ref
-
-    val speculative_inlining_only_if_arguments_useful : bool ref
-
-    val report_bin : bool ref
-  end
-end
 
 val set_oclassic : unit -> unit
 val set_o2 : unit -> unit
