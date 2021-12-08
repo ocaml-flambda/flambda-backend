3a5d4f4ae5dc952beb5ef5ac4e17407fb536edcc
Keryan Didier <keryan.didier@ocamlpro.com>
2021-12-08 17:33:36 +0100

    hide failure on invalid code

--- middle_end/flambda2/from_lambda/closure_conversion.ml
+++ middle_end/flambda2/from_lambda/closure_conversion.ml
@@ -1509,10 +1509,13 @@ let close_apply acc env (apply : IR.apply) : Acc.t * Expr_with_acc.t =
           Code_metadata.result_arity metadata )
     | Value_unknown -> None
     | _ ->
-      Misc.fatal_errorf
-        "Unexpected approximation for callee %a in [Closure_conversion], \
-         expected a closure approximation."
-        Simple.print callee
+      if Flambda_features.check_invariants ()
+      then
+        Misc.fatal_errorf
+          "Unexpected approximation for callee %a in [Closure_conversion], \
+           expected a closure approximation."
+          Simple.print callee
+      else None
   in
   match code_info with
   | None -> close_exact_or_unknown_apply acc env apply None
