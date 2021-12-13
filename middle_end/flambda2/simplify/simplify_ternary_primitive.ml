69a2ffaada983d684a2304c44d9ea99ceb4fad2b
Mark Shinwell <mshinwell@pm.me>
2021-12-13 15:24:55 +0000

    Tidying up in Simplify_ternary_primitive

--- middle_end/flambda2/simplify/simplify_ternary_primitive.ml
+++ middle_end/flambda2/simplify/simplify_ternary_primitive.ml
@@ -18,8 +18,8 @@
 
 open! Simplify_import
 
-let simplify_array_set (array_kind : P.Array_kind.t) init_or_assign dacc dbg
-    ~arg1 ~arg1_ty:array_ty ~arg2 ~arg2_ty:_ ~arg3 ~arg3_ty:_ ~result_var =
+let simplify_array_set original_prim (array_kind : P.Array_kind.t) dacc dbg
+    ~array_ty ~result_var =
   let elt_kind = P.Array_kind.element_kind array_kind |> K.With_subkind.kind in
   let array_kind =
     Simplify_common.specialise_array_kind dacc array_kind ~array_ty
@@ -35,20 +35,17 @@ let simplify_array_set (array_kind : P.Array_kind.t) init_or_assign dacc dbg
       P.Array_kind.element_kind array_kind |> K.With_subkind.kind
     in
     assert (K.equal elt_kind elt_kind');
-    let prim : P.t =
-      Ternary (Array_set (array_kind, init_or_assign), arg1, arg2, arg3)
-    in
-    let named = Named.create_prim prim dbg in
-    let ty = T.unknown (P.result_kind' prim) in
+    let named = Named.create_prim original_prim dbg in
+    let ty = T.unknown (P.result_kind' original_prim) in
     let dacc = DA.add_variable dacc result_var ty in
     Simplified_named.reachable named ~try_reify:false, dacc
 
-let simplify_ternary_primitive dacc _original_prim (prim : P.ternary_primitive)
-    ~arg1 ~arg1_ty ~arg2 ~arg2_ty ~arg3 ~arg3_ty dbg ~result_var =
+let simplify_ternary_primitive dacc original_prim (prim : P.ternary_primitive)
+    ~arg1 ~arg1_ty ~arg2 ~arg2_ty:_ ~arg3 ~arg3_ty:_ dbg ~result_var =
   match prim with
-  | Array_set (array_kind, init_or_assign) ->
-    simplify_array_set array_kind init_or_assign dacc dbg ~arg1 ~arg1_ty ~arg2
-      ~arg2_ty ~arg3 ~arg3_ty ~result_var
+  | Array_set (array_kind, _init_or_assign) ->
+    simplify_array_set original_prim array_kind dacc dbg ~array_ty:arg1_ty
+      ~result_var
   | Block_set _ | Bytes_or_bigstring_set _ | Bigarray_set _ ->
     let prim : P.t = Ternary (prim, arg1, arg2, arg3) in
     let named = Named.create_prim prim dbg in
