7b6e39e458ad524eabc570f8bf6a14841de1619b
Vincent Laviron <vincent.laviron@gmail.com>
2021-12-14 11:54:49 +0100

    Update comments in test

--- middle_end/flambda2/tests/mlexamples/array_specialisation_examples.ml
+++ middle_end/flambda2/tests/mlexamples/array_specialisation_examples.ml
@@ -1,29 +1,25 @@
-(* Should be simplified to a Values array load, but isn't because of the missing
-   representation for Values arrays in Flambda kinds *)
+(* Generic get primitive, on an array of Values. Should be simplified to
+   a Values primitive. *)
 let f (arr : int option array) =
   let[@local] get a = Array.get a 0 in
   get arr
 
-(* Same as above, but the Make_array primitive computes a reasonable type for
-   the array so the array load is correctly simplified to a Values load *)
+(* Same as above, with the additional length information that removes the
+   bounds check. *)
 let g (x : int option) =
   let[@local] get a = Array.get a 0 in
   get [| x |]
 
-(* Example of a specialised primitive (on Immediates arrays) being called on an
-   array of type Values. With the correct information from kinds, we could have
-   refined the type of [a] and the array load primitive would have seen an
-   argument of kind Immediates. The simplification of [Is_boxed_float x] means
-   that we know we're not dealing with a float array, at least. *)
+(* Example of a specialised primitive (on Immediates arrays) being called on a
+   generic array. The specialised primitive should be kept. *)
 let h (x : int) =
   let[@local] make v = [| v |] in
   let[@local] get (a : int array) = a.(0) in
   let a = make x in
   get a
 
-(* Same as above, but the use of a GADT means that even with the right handling
-   of array kinds the type of [a] at the load point would still be a generic
-   array. *)
+(* Same as above, but with a GADT to ensure that the array creation is truly
+   generic. *)
 type _ wit =
   | Int : int wit
   | Other : _ wit
