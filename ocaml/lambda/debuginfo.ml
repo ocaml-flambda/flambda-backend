a918a3286c028c14228054d61b6a02bbd87eaee6
Mark Shinwell <mshinwell@gmail.com>
2022-01-14 15:43:39 +0000

    Fix some Debuginfo.t scopes in the frontend (#248)
    * Add Debuginfo scopes for partial/eta expansion wrappers and "lazy"; use in the frontend.
    
    * Fix test
    
    As bytecode and native differs in how they handle partial application
    the check for partial applications has been split in a new separate
    testcase.
    
    Co-authored-by: Pierre Oechsel <pierre.oechsel@gmail.com>

--- ocaml/lambda/debuginfo.ml
+++ ocaml/lambda/debuginfo.ml
@@ -24,10 +24,16 @@ module Scoped_location = struct
     | Sc_module_definition
     | Sc_class_definition
     | Sc_method_definition
+    | Sc_partial_or_eta_wrapper
+    | Sc_lazy
 
   type scopes =
     | Empty
     | Cons of {item: scope_item; str: string; str_fun: string}
+
+  let str = function
+    | Empty -> ""
+    | Cons r -> r.str
 
   let str_fun = function
     | Empty -> "(fun)"
@@ -45,8 +51,12 @@ module Scoped_location = struct
        | 'a'..'z' | 'A'..'Z' | '_' | '0'..'9' -> s
        | _ -> "(" ^ s ^ ")"
 
-  let dot ?(sep = ".") scopes s =
-    let s = add_parens_if_symbolic s in
+  let dot ?(sep = ".") ?no_parens scopes s =
+    let s =
+      match no_parens with
+      | None -> add_parens_if_symbolic s
+      | Some () -> s
+    in
     match scopes with
     | Empty -> s
     | Cons {str; _} -> str ^ sep ^ s
@@ -71,6 +81,11 @@ module Scoped_location = struct
       | _ -> dot scopes s
     in
     cons Sc_method_definition str
+
+  let enter_lazy ~scopes = cons Sc_lazy (str scopes)
+
+  let enter_partial_or_eta_wrapper ~scopes =
+    cons Sc_partial_or_eta_wrapper (dot ~no_parens:() scopes "(partial)")
 
   let string_of_scopes = function
     | Empty -> "<unknown>"
@@ -106,6 +121,11 @@ module Scoped_location = struct
   let string_of_scoped_location = function
     | Loc_unknown -> "??"
     | Loc_known { loc = _; scopes } -> string_of_scopes scopes
+
+  let map_scopes f t =
+    match t with
+    | Loc_unknown -> Loc_unknown
+    | Loc_known { loc; scopes } -> Loc_known { loc; scopes = f ~scopes }
 end
 
 type item = {
