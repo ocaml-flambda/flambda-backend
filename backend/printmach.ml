9f368d2c566ce1ae396fcbdb717bfe004a2b5f92
Greta Yorsh <gyorsh@janestreet.com>
2022-02-17 13:13:06 +0000

    Remove unnecessary type annotation

--- backend/printmach.ml
+++ backend/printmach.ml
@@ -207,9 +207,7 @@ let operation op arg ppf res =
   | Ibeginregion -> fprintf ppf "beginregion"
   | Iendregion -> fprintf ppf "endregion %a" operand arg.(0)
   | Ispecific op ->
-    (Arch.print_specific_operation : (Format.formatter -> Mach.operand -> unit) ->
-         Arch.specific_operation -> Format.formatter -> Mach.operand array -> unit)
-      operand op ppf arg
+    Arch.print_specific_operation operand op ppf arg
   | Iprobe {name;handler_code_sym} ->
     fprintf ppf "probe \"%s\" %s %a" name handler_code_sym operands arg
   | Iprobe_is_enabled {name} -> fprintf ppf "probe_is_enabled \"%s\"" name
