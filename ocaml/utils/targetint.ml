e869887d5baad0342b1daf5d248dcbe156bd969c
Pierre Oechsel <pierre.oechsel@gmail.com>
2022-02-21 13:19:23 +0000

    Add two new methods to targetint for dwarf

--- ocaml/utils/targetint.ml
+++ ocaml/utils/targetint.ml
@@ -102,3 +102,11 @@ include (val
            | 64 -> (module Int64)
            | _ -> assert false
           ) : S)
+
+let size_in_bytes_as_targetint =
+  match size with 32 -> of_int32 4l | 64 -> of_int64 8L | _ -> assert false
+
+let to_uint64_exn t =
+  match repr t with
+  | Int32 t -> Numbers.Uint64.of_nonnegative_int32_exn t
+  | Int64 t -> Numbers.Uint64.of_nonnegative_int64_exn t
--- ocaml/utils/targetint.mli
+++ ocaml/utils/targetint.mli
@@ -205,3 +205,9 @@ val repr : t -> repr
 
 val print : Format.formatter -> t -> unit
 (** Print a target integer to a formatter. *)
+
+val size_in_bytes_as_targetint : t
+(** The width of a target integer in bytes, expressed as a value of type [t]. *)
+
+val to_uint64_exn : t -> Numbers.Uint64.t
+(** Convert the given target integer to an unsigned 64-bit integer. *)
