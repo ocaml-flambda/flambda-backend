e315b2afdb9180d06b4016b30290dfe159e0e8cc
Mark Shinwell <mshinwell@pm.me>
2022-03-04 11:28:03 +0000

    Increase max inlining depth at -O3 (#575)

--- driver/flambda_backend_flags.ml
+++ driver/flambda_backend_flags.ml
@@ -286,7 +286,7 @@ module Flambda2 = struct
       threshold = Some 100.;
     }
 
-    let o3_arguments = o2_arguments
+    let o3_arguments = { o2_arguments with max_depth = Some 6 }
   end
 end
 
