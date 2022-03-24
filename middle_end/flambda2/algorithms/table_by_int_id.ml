e71e42f53b52884886551be414c9fae1e328743e
Vincent Laviron <vincent.laviron@gmail.com>
2022-03-24 10:43:35 +0100

    Small fixes to Table_by_int_id (#593)
    * Table_by_int_id.Id: shift inputs instead of ignoring low bits
    
    * Table_by_int_id.add: more efficient increment

--- middle_end/flambda2/algorithms/table_by_int_id.ml
+++ middle_end/flambda2/algorithms/table_by_int_id.ml
@@ -21,22 +21,20 @@ module Int = Numbers.Int
 module Id = struct
   include Int
 
-  let num_empty_bottom_bits = 3
+  let flags_size_in_bits = 3
 
-  let mask_selecting_top_bits = -1 lsl num_empty_bottom_bits
+  let create t flags =
+    if flags < 0 || flags >= 1 lsl (flags_size_in_bits + 1)
+    then Misc.fatal_errorf "Flags value 0x%x out of range" flags;
+    (t lsl flags_size_in_bits) lor flags
+
+  let mask_selecting_top_bits = -1 lsl flags_size_in_bits
 
   let mask_selecting_bottom_bits = lnot mask_selecting_top_bits
 
-  let flags_size_in_bits = num_empty_bottom_bits
-
   let flags t = t land mask_selecting_bottom_bits
 
-  let without_flags t = t land mask_selecting_top_bits
-
-  let[@inline always] with_flags t flags =
-    if flags < 0 || flags >= 1 lsl (flags_size_in_bits + 1)
-    then Misc.fatal_errorf "Flags value 0x%x out of range" flags;
-    without_flags t lor flags
+  let next t = t + (1 lsl flags_size_in_bits)
 end
 
 module Make (E : sig
@@ -71,7 +69,7 @@ struct
   exception Already_added of int
 
   let add t elt =
-    let id = Id.with_flags (E.hash elt) E.flags in
+    let id = Id.create (E.hash elt) E.flags in
     match HT.find t id with
     | exception Not_found ->
       HT.add t id elt;
@@ -82,20 +80,17 @@ struct
       else
         try
           let starting_id = id in
-          let id = ref (starting_id + 1) in
+          let id = ref (Id.next starting_id) in
           (* If there is a collision, we search for another slot, but take care
              not to alter the flags bits. *)
           while !id <> starting_id do
-            (* CR mshinwell: performance could be improved *)
-            while Id.flags !id <> E.flags do
-              incr id
-            done;
+            assert (Id.flags !id = E.flags);
             match HT.find t !id with
             | exception Not_found -> raise (Can_add !id)
             | existing_elt ->
               if E.equal elt existing_elt
               then raise (Already_added !id)
-              else incr id
+              else id := Id.next !id
           done;
           Misc.fatal_errorf "No hash values left for@ %a" E.print elt
         with
