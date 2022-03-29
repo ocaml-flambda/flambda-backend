9d1a4dfb98d79da32b378a58a495deada36d75de
Vincent Laviron <vincent.laviron@gmail.com>
2022-03-29 11:19:48 +0200

    Fix destroyed registers for pushtrap instructions

--- backend/amd64/proc.ml
+++ backend/amd64/proc.ml
@@ -313,6 +313,9 @@ let destroyed_at_alloc =
   else
     [| r11 |]
 
+let destroyed_at_pushtrap =
+  [| r11 |]
+
 let destroyed_at_oper = function
     Iop(Icall_ind | Icall_imm _ | Iextcall { alloc = true; }) ->
     all_phys_regs
@@ -324,7 +327,9 @@ let destroyed_at_oper = function
   | Iop(Iintop(Imulh _ | Icomp _) | Iintop_imm((Icomp _), _))
         -> [| rax |]
   | Iswitch(_, _) -> [| rax; rdx |]
-  | Itrywith _ -> [| r11 |]
+  | Itrywith _ -> destroyed_at_pushtrap
+  | Iexit (_, traps) when List.exists (function Cmm.Push _ -> true | Pop -> false) traps ->
+    destroyed_at_pushtrap
   | Iop(Ispecific (Irdtsc | Irdpmc)) -> [| rax; rdx |]
   | Iop(Ispecific(Isqrtf | Isextend32 | Izextend32 | Icrc32q | Ilea _
                  | Istore_int (_, _, _) | Ioffset_loc (_, _)
