d793c3b49491cff610bb48065d70b5405497e703
Mark Shinwell <mshinwell@pm.me>
2022-04-15 15:13:20 +0100

    Introduce To_cmm_expr

---
+++ middle_end/flambda2/to_cmm/to_cmm_expr.mli
@@ -0,0 +1,21 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                 OCaml                                  *)
+(*                                                                        *)
+(*                        Guillaume Bury, OCamlPro                        *)
+(*                                                                        *)
+(*   Copyright 2019--2019 OCamlPro SAS                                    *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
+(*   special exception on linking described in the file LICENSE.          *)
+(*                                                                        *)
+(**************************************************************************)
+
+(** Translation of Flambda expressions to Cmm. *)
+
+val expr :
+  To_cmm_env.t ->
+  To_cmm_result.t ->
+  Flambda.Expr.t ->
+  Cmm.expression * To_cmm_result.t
