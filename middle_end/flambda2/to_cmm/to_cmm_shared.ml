b75756cffdbbaf823b59fc8510026a7ef5e24d2f
Mark Shinwell <mshinwell@pm.me>
2022-06-09 17:09:01 +0100

    Cache symbols for Invalid messages

--- middle_end/flambda2/to_cmm/to_cmm_shared.ml
+++ middle_end/flambda2/to_cmm/to_cmm_shared.ml
@@ -133,22 +133,32 @@ let bound_parameters env l =
 
 let invalid res ~message =
   let dbg = Debuginfo.none in
-  let message_sym =
-    Symbol.create
-      (Compilation_unit.get_current_exn ())
-      (Linkage_name.create (Variable.unique_name (Variable.create "invalid")))
-  in
-  let data_items =
-    Cmm_helpers.emit_string_constant
-      (Symbol.linkage_name_as_string message_sym, Global)
-      message []
-    |> To_cmm_result.add_archive_data_items res
+  let message_sym, res =
+    match To_cmm_result.invalid_message_symbol res ~message with
+    | None ->
+      let message_sym =
+        Symbol.create
+          (Compilation_unit.get_current_exn ())
+          (Linkage_name.create
+             (Variable.unique_name (Variable.create "invalid")))
+      in
+      let res =
+        Cmm_helpers.emit_string_constant
+          (Symbol.linkage_name_as_string message_sym, Global)
+          message []
+        |> To_cmm_result.add_archive_data_items res
+      in
+      let res =
+        To_cmm_result.add_invalid_message_symbol res message_sym ~message
+      in
+      message_sym, res
+    | Some message_sym -> message_sym, res
   in
   let call_expr =
     extcall ~dbg ~alloc:false ~is_c_builtin:false ~returns:false ~ty_args:[XInt]
       "caml_flambda2_invalid" Cmm.typ_void [symbol ~dbg message_sym]
   in
-  call_expr, data_items
+  call_expr, res
 
 let make_update env dbg kind ~symbol var ~index ~prev_updates =
   let e, env, _ece = To_cmm_env.inline_variable env var in
