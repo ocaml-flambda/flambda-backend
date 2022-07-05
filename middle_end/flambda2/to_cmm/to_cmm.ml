0de4fb2d6497bd5fd7018f3f8d4361b2f9996ba7
Mark Shinwell <mshinwell@pm.me>
2022-07-05 11:27:26 +0100

    Add PR URL

--- middle_end/flambda2/to_cmm/to_cmm.ml
+++ middle_end/flambda2/to_cmm/to_cmm.ml
@@ -63,8 +63,8 @@ let flush_cmm_helpers_state () =
 
 let unit0 ~offsets ~make_symbol flambda_unit ~all_code =
   (* If someone wants to add 32-bit support in the future there will be a
-     (merged) PR on ocaml-flambda/flambda-backend which can be used as a
-     guide. *)
+     (merged) PR on ocaml-flambda/flambda-backend which can be used as a guide:
+     https://github.com/ocaml-flambda/flambda-backend/pull/685 *)
   if Target_system.is_32_bit
   then
     Misc.fatal_error
