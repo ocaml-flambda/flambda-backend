bb5bd0275683ea32e6175ff20576bf76e7f9a67b
azewierzejew <36162539+azewierzejew@users.noreply.github.com>
2022-09-15 14:34:14 +0100

    CFG: Simplify liveness computation (#783)
    * Simplify liveness transfer function.
    
    * Add comment about Reloadretaddr.
    
    * Apply suggestions from code review
    
    Co-authored-by: Xavier Clerc <xclerc@users.noreply.github.com>
    
    Co-authored-by: Xavier Clerc <xclerc@users.noreply.github.com>

--- backend/cfg/cfg_liveness.ml
+++ backend/cfg/cfg_liveness.ml
@@ -51,69 +51,67 @@ module Transfer :
 
   type nonrec error = error
 
+  let[@inline always] instruction { before; across = _ } ~can_raise ~exn
+      (instr : _ Cfg.instruction) =
+    let across = Reg.diff_set_array before instr.res in
+    let across =
+      if can_raise then Reg.Set.union across exn.before else across
+    in
+    let before = Reg.add_set_array across instr.arg in
+    { before; across }
+
   let basic :
       domain ->
       exn:domain ->
       Cfg.basic Cfg.instruction ->
       (domain, error) result =
-   fun { before; across = _ } ~exn instr ->
+   fun ({ before; across = _ } as domain) ~exn instr ->
     Result.ok
     @@
     match instr.desc with
-    | Op _ | Call _ ->
+    | Op _ | Call _ | Reloadretaddr | Pushtrap _ | Poptrap | Prologue ->
       if Cfg.is_pure_basic instr.desc
          && Reg.disjoint_set_array before instr.res
          && (not (Proc.regs_are_volatile instr.arg))
          && not (Proc.regs_are_volatile instr.res)
-      then { before; across = before }
+      then
+        (* If the operation is without side-effects and the result is unused
+           then don't mark the arguments as used because this instruction could
+           be removed. *)
+        { before; across = before }
       else
-        let across = Reg.diff_set_array before instr.res in
-        let across =
-          if Cfg.can_raise_basic instr.desc && instr.stack_offset > 0
-          then Reg.Set.union across exn.before
-          else across
-        in
-        let before = Reg.add_set_array across instr.arg in
-        { before; across }
-    | Reloadretaddr ->
-      { before = Reg.diff_set_array before Proc.destroyed_at_reloadretaddr;
-        across = Reg.Set.empty
-      }
-    | Pushtrap _ -> { before; across = before }
-    | Poptrap -> { before; across = before }
-    | Prologue -> { before; across = before }
+        instruction
+          ~can_raise:(Cfg.can_raise_basic instr.desc)
+          ~exn domain instr
 
   let terminator :
       domain ->
       exn:domain ->
       Cfg.terminator Cfg.instruction ->
       (domain, error) result =
-   fun { before; across = _ } ~exn instr ->
+   fun domain ~exn instr ->
     Result.ok
     @@
     match instr.desc with
     | Never -> assert false
-    | Always _ ->
-      { before = Reg.add_set_array before instr.arg; across = before }
-    | Parity_test _ ->
-      { before = Reg.add_set_array before instr.arg; across = before }
-    | Truth_test _ ->
-      { before = Reg.add_set_array before instr.arg; across = before }
-    | Float_test _ ->
-      { before = Reg.add_set_array before instr.arg; across = before }
-    | Int_test _ ->
-      { before = Reg.add_set_array before instr.arg; across = before }
-    | Switch _ ->
-      { before = Reg.add_set_array before instr.arg; across = before }
-    | Return -> { before = Reg.set_of_array instr.arg; across = Reg.Set.empty }
     | Tailcall (Self _) ->
-      { before = Reg.set_of_array instr.arg; across = Reg.Set.empty }
-    | Raise _ ->
-      { before = Reg.add_set_array exn.before instr.arg; across = exn.before }
-    | Tailcall (Func _) ->
-      { before = Reg.set_of_array instr.arg; across = Reg.Set.empty }
+      (* CR-someday azewierzejew: If the stamps for the tail call DomainState
+         argument and parameter were the same and Tailcall (Self _) had
+         [instr.arg = instr.res] (either by removing the args or adding results
+         because currently there is a nonempty array in args but empty in res)
+         then it would behave exactly the same as every other instruction. *)
+      (* We have to handle this as a special case because the registers for a
+         given DomainState argument and parameter pair have different stamps. *)
+      instruction
+        ~can_raise:(Cfg.can_raise_terminator instr.desc)
+        ~exn Domain.bot instr
+    | Always _ | Parity_test _ | Truth_test _ | Float_test _ | Int_test _
+    | Switch _ | Return | Raise _
+    | Tailcall (Func _)
     | Call_no_return _ ->
-      { before = Reg.add_set_array exn.before instr.arg; across = exn.before }
+      instruction
+        ~can_raise:(Cfg.can_raise_terminator instr.desc)
+        ~exn domain instr
 
   let exception_ : domain -> (domain, error) result =
    fun { before; across = _ } ->
