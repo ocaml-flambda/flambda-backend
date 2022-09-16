91fd57a62df0c811fe6bbefd3b8634e693f8924f
Keryan Didier <keryan.didier@ocamlpro.com>
2022-09-16 11:42:21 +0200

    Reduce switch with statically known scrutinee to a single branch in from_lambda (#824)

--- middle_end/flambda2/from_lambda/closure_conversion_aux.ml
+++ middle_end/flambda2/from_lambda/closure_conversion_aux.ml
@@ -162,18 +162,19 @@ module Env = struct
               "Closure_conversion: approximation loader returned a Symbol \
                approximation (%a) for symbol %a"
               Symbol.print sym Symbol.print symbol
-          | Value_unknown | Closure_approximation _ | Block_approximation _ ->
+          | Value_unknown | Value_int _ | Closure_approximation _
+          | Block_approximation _ ->
             ());
         let rec filter_inlinable approx =
           match (approx : value_approximation) with
-          | Value_unknown | Value_symbol _
-          | Closure_approximation (_, _, Metadata_only _) ->
+          | Value_unknown | Value_symbol _ | Value_int _
+          | Closure_approximation { code = Metadata_only _; _ } ->
             approx
           | Block_approximation (approxs, alloc_mode) ->
             let approxs = Array.map filter_inlinable approxs in
             Value_approximation.Block_approximation (approxs, alloc_mode)
-          | Closure_approximation (code_id, function_slot, Code_present code)
-            -> (
+          | Closure_approximation
+              { code_id; function_slot; code = Code_present code; _ } -> (
             match[@ocaml.warning "-fragile-match"]
               Inlining.definition_inlining_decision (Code.inline code)
                 (Code.cost_metrics code)
@@ -181,9 +182,11 @@ module Env = struct
             | Attribute_inline | Small_function _ -> approx
             | _ ->
               Value_approximation.Closure_approximation
-                ( code_id,
-                  function_slot,
-                  Code_or_metadata.(remember_only_metadata (create code)) ))
+                { code_id;
+                  function_slot;
+                  code = Code_or_metadata.(remember_only_metadata (create code));
+                  symbol = None
+                })
         in
         let approx = filter_inlinable approx in
         externals := Symbol.Map.add symbol approx !externals;
@@ -339,8 +342,8 @@ module Env = struct
   let add_approximation_alias t name alias =
     match find_value_approximation t (Simple.name name) with
     | Value_unknown -> t
-    | (Value_symbol _ | Closure_approximation _ | Block_approximation _) as
-      approx ->
+    | ( Value_symbol _ | Value_int _ | Closure_approximation _
+      | Block_approximation _ ) as approx ->
       add_value_approximation t alias approx
 
   let set_path_to_root t path_to_root =
