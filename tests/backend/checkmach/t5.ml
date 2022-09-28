7e0a7090d445797df4fc0a7c4889496fbb1c1c98
Greta Yorsh <gyorsh@janestreet.com>
2022-09-28 13:04:09 +0100

    Test [@@noalloc] on external

--- tests/backend/checkmach/t5.ml
+++ tests/backend/checkmach/t5.ml
@@ -1,1 +1,3 @@
 let[@noalloc assume][@inline never] test x = [x;x+1]
+(* The test below to make sure "noalloc" on external is still handled correctly. *)
+external external_test : unit -> unit = "test" [@@noalloc]
