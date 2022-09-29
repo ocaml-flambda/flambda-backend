7660cc065b992cc046476ce00d60ddd5d51766d2
Greta Yorsh <gyorsh@janestreet.com>
2022-09-29 17:34:24 +0100

    Copyright on the two new files under flambda2

--- middle_end/flambda2/terms/check_attribute.mli
+++ middle_end/flambda2/terms/check_attribute.mli
@@ -1,3 +1,14 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                 OCaml                                  *)
+(*                                                                        *)
+(*   Copyright 2022 Jane Street Group LLC                                 *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
+(*   special exception on linking described in the file LICENSE.          *)
+(*                                                                        *)
+(**************************************************************************)
 (** Annotations on function declaration (not call sites) *)
 module Property : sig
   type t = Noalloc
