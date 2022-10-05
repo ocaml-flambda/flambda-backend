8d416b4f19e8841c8d2fe8343939f814197e5fe2
Xavier Clerc <xclerc@users.noreply.github.com>
2022-10-05 13:16:45 +0100

    Reload: check stack slot and register class when deciding whether a move is a no-op (#865)

--- backend/cfg/cfg.ml
+++ backend/cfg/cfg.ml
@@ -453,10 +453,11 @@ let is_pure_basic : basic -> bool = function
 
 let is_noop_move instr =
   match instr.desc with
-  | Op (Move | Spill | Reload) -> (
-    match instr.arg.(0).loc with
+  | Op (Move | Spill | Reload) ->
+    (match instr.arg.(0).loc with
     | Unknown -> false
     | Reg _ | Stack _ -> Reg.same_loc instr.arg.(0) instr.res.(0))
+    && Proc.register_class instr.arg.(0) = Proc.register_class instr.res.(0)
   | Op
       ( Const_int _ | Const_float _ | Const_symbol _ | Stackoffset _ | Load _
       | Store _ | Intop _ | Intop_imm _ | Negf | Absf | Addf | Subf | Mulf
