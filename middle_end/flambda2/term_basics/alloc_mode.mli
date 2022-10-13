162d4bd11610ad9d154741480d8f235cc5def2ac
Vincent Laviron <vincent.laviron@gmail.com>
2022-10-13 17:44:38 +0200

    More documentation comments

--- middle_end/flambda2/term_basics/alloc_mode.mli
+++ middle_end/flambda2/term_basics/alloc_mode.mli
@@ -13,6 +13,7 @@
 (**************************************************************************)
 
 module For_types : sig
+  (** Constraints on the location of allocated values *)
   type t = private
     | Heap  (** Normal allocation on the OCaml heap. *)
     | Local  (** Allocation on the local allocation stack. *)
@@ -24,16 +25,22 @@ module For_types : sig
 
   val heap : t
 
+  (** Returns [Heap] if stack allocation is disabled! *)
   val local : unit -> t
 
+  (** Returns [Heap] if stack allocation is disabled! *)
   val unknown : unit -> t
 
+  (** Maps [Alloc_local] to [Heap_or_local], as all Lambda annotations that we
+      transform into constraints have this semantics *)
   val from_lambda : Lambda.alloc_mode -> t
 
+  (** Symmetric to [from_lambda], so [Heap_or_local] is mapped to [Alloc_local] *)
   val to_lambda : t -> Lambda.alloc_mode
 end
 
 module For_allocations : sig
+  (** Decisions on allocation locations *)
   type t = private
     | Heap  (** Normal allocation on the OCaml heap. *)
     | Local of { region : Variable.t }
