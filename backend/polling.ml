9fedbcca82c457d59dc37e949d8dc523bebd6abe
Mark Shinwell <mshinwell@pm.me>
2022-10-17 17:18:44 +0100

    Add missing cases in polling.ml

--- backend/polling.ml
+++ backend/polling.ml
@@ -247,7 +247,7 @@ let find_poll_alloc_or_calls instr =
             Iintoffloat | Inegf | Iabsf | Iaddf | Isubf | Imulf | Idivf |
             Iopaque | Ispecific _ | Ibeginregion | Iendregion |
             Icompf _ | Iname_for_debugger _ | Iprobe _ |
-            Iprobe_is_enabled _ )-> None
+            Iprobe_is_enabled _ | Ivalueofint | Iintofvalue)-> None
       | Iend | Ireturn _ | Iifthenelse _ | Iswitch _ | Icatch _ | Iexit _ |
         Itrywith _ | Iraise _ -> None
     in
