49d508ea76199117a5b5345af3bbe5ce2aa2aec7
Guillaume Bury <guillaume.bury@gmail.com>
2022-10-19 17:10:50 +0200

    Add a type for the return value of Block_set (and similar prims)

--- middle_end/flambda2/simplify/simplify_primitive_result.ml
+++ middle_end/flambda2/simplify/simplify_primitive_result.ml
@@ -31,6 +31,13 @@ let create_simplified simplified_named ~try_reify dacc =
 let create_invalid dacc =
   { simplified_named = Invalid; try_reify = false; dacc }
 
+let create_unit dacc ~result_var ~original_term =
+  (* CR gbury: would it make sense to have a Flambda2_types.unit instead
+     of this ? *)
+  let ty = Flambda2_types.this_tagged_immediate Targetint_31_63.zero in
+  let dacc = Downwards_acc.add_variable dacc result_var ty in
+  create original_term ~try_reify:false dacc
+
 let create_unknown dacc ~result_var kind ~original_term =
   let ty = Flambda2_types.unknown kind in
   let dacc = Downwards_acc.add_variable dacc result_var ty in
