05e1614ca9060de6cdd142e5ca406d6caa9df020
Nathanaëlle Courant <nathanaelle.courant@ocamlpro.com>
2022-10-28 12:23:29 +0200

    Add test for another failure

---
+++ middle_end/flambda2/tests/ref_to_var/unboxed_invariant_ref_2.ml
@@ -0,0 +1,6 @@
+type t = { mutable x : int }
+
+let rec go b r =
+  if Sys.opaque_identity false then r else if b > 0 then go b { x = 0 } else (r.x <- r.x + 1; go b r)
+
+let f x = ((go [@inlined]) 0 { x = 0 }).x
