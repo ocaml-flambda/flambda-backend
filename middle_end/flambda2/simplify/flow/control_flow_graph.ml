fee59341c8b6e41782927bdd6695740eb43f784c
Nathanaëlle Courant <nathanaelle.courant@ocamlpro.com>
2022-10-28 14:17:24 +0200

    Make fmt

--- middle_end/flambda2/simplify/flow/control_flow_graph.ml
+++ middle_end/flambda2/simplify/flow/control_flow_graph.ml
@@ -176,19 +176,18 @@ let compute_continuation_extra_args_for_aliases ~speculative ~required_names
         let s =
           List.fold_left
             (fun acc param ->
-               match Variable.Map.find param doms with
-               | exception Not_found ->
-                 if Name.Set.mem (Name.var param) required_names
-                 then
-                   Misc.fatal_errorf "Dom not found for: %a@." Variable.print
-                     param
-                 else
-                   acc
-               | dom ->
-                 if Variable.equal param dom || Variable.Set.mem dom unboxed_blocks then
-                   acc
-                 else
-                   Variable.Set.add dom acc)
+              match Variable.Map.find param doms with
+              | exception Not_found ->
+                if Name.Set.mem (Name.var param) required_names
+                then
+                  Misc.fatal_errorf "Dom not found for: %a@." Variable.print
+                    param
+                else acc
+              | dom ->
+                if Variable.equal param dom
+                   || Variable.Set.mem dom unboxed_blocks
+                then acc
+                else Variable.Set.add dom acc)
             Variable.Set.empty
             (Bound_parameters.vars elt.T.Continuation_info.params)
         in
@@ -276,10 +275,9 @@ let compute_continuation_extra_args_for_aliases ~speculative ~required_names
                 | None ->
                   let removed = Variable.Set.add param removed in
                   let lets_to_introduce =
-                    if Variable.Set.mem alias unboxed_blocks then
-                      lets_to_introduce
-                    else
-                      Variable.Map.add param alias lets_to_introduce
+                    if Variable.Set.mem alias unboxed_blocks
+                    then lets_to_introduce
+                    else Variable.Map.add param alias lets_to_introduce
                   in
                   removed, lets_to_introduce
                 | Some (aliased_to, exception_param) ->
@@ -296,10 +294,9 @@ let compute_continuation_extra_args_for_aliases ~speculative ~required_names
                     in
                     let removed = Variable.Set.add param removed in
                     let lets_to_introduce =
-                      if Variable.Set.mem alias unboxed_blocks then
-                        lets_to_introduce
-                      else
-                        Variable.Map.add param alias lets_to_introduce
+                      if Variable.Set.mem alias unboxed_blocks
+                      then lets_to_introduce
+                      else Variable.Map.add param alias lets_to_introduce
                     in
                     removed, lets_to_introduce))
           (Variable.Set.empty, Variable.Map.empty)
--- middle_end/flambda2/tests/ref_to_var/unboxed_invariant_ref_2.ml
+++ middle_end/flambda2/tests/ref_to_var/unboxed_invariant_ref_2.ml
@@ -1,6 +1,12 @@
 type t = { mutable x : int }
 
 let rec go b r =
-  if Sys.opaque_identity false then r else if b > 0 then go b { x = 0 } else (r.x <- r.x + 1; go b r)
+  if Sys.opaque_identity false
+  then r
+  else if b > 0
+  then go b { x = 0 }
+  else (
+    r.x <- r.x + 1;
+    go b r)
 
-let f x = ((go [@inlined]) 0 { x = 0 }).x
+let f () = ((go [@inlined]) 0 { x = 0 }).x
