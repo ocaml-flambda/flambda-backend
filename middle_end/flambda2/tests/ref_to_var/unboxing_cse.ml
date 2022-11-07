d2658ff03abc00558cd34c59bd27fbc7f33b1067
Nathanaëlle Courant <Ekdohibs@users.noreply.github.com>
2022-11-07 16:01:27 +0100

    Fix comment

--- middle_end/flambda2/tests/ref_to_var/unboxing_cse.ml
+++ middle_end/flambda2/tests/ref_to_var/unboxing_cse.ml
@@ -5,5 +5,5 @@ external opaque : 'a -> 'a = "%opaque"
 let f x z =
   let[@inline] test (_, y) = if y < z then opaque 0 else opaque 0 in
   let _ = test (0, x) in
-  (* will work if you delete this line *)
+  (* will correctly be unboxed if you delete the above line *)
   test (if opaque true then 0, x else 1, x)
