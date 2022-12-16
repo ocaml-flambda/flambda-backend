0b1b1d442eb7a1dd4c8e4ed8a5c7b37b9fc612da
Luke Maurer <lmaurer@janestreet.com>
2022-12-16 15:06:36 +0000

    Add test

--- ocaml/testsuite/tests/tool-ocamlobjinfo/question.ml
+++ ocaml/testsuite/tests/tool-ocamlobjinfo/question.ml
@@ -8,6 +8,11 @@ program = "question.cmxs"
 **** check-ocamlopt.byte-output
 ***** ocamlobjinfo
 ****** check-program-output
+
+***** ocamlobjinfo
+program = "question.cmx"
+(* The cmx output varies too much to check. We're just happy it didn't
+   segfault on us. *)
 *)
 
 let answer = 42
