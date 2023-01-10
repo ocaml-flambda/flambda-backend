97611977d600faa8417696bab7946c903b0b6102
Mark Shinwell <mshinwell@pm.me>
2023-01-10 16:27:08 +0000

    Fix DE.find_code_exn

--- middle_end/flambda2/simplify/env/downwards_env.ml
+++ middle_end/flambda2/simplify/env/downwards_env.ml
@@ -427,18 +427,11 @@ let mem_code t id =
 let find_code_exn t id =
   match Code_id.Map.find id t.all_code with
   | code -> Code_or_metadata.create code
-  | exception Not_found -> (
-    (* This [find_exn] call doesn't load any .cmx files, but in the majority of
-       cases will succeed. *)
-    match Exported_code.find_exn (t.get_imported_code ()) id with
-    | code_or_metadata -> code_or_metadata
-    | exception Not_found -> (
-      (* In this case either the code ID isn't bound due to a compiler error or
-         we haven't yet loaded the relevant .cmx file. Make sure the .cmx is
-         loaded and try again. *)
-      match TE.resolver t.typing_env (Code_id.get_compilation_unit id) with
-      | None -> raise Not_found
-      | Some _typing_env -> Exported_code.find_exn (t.get_imported_code ()) id))
+  | exception Not_found ->
+    let (_ : TE.Serializable.t option) =
+      TE.resolver t.typing_env (Code_id.get_compilation_unit id)
+    in
+    Exported_code.find_exn (t.get_imported_code ()) id
 
 let define_code t ~code_id ~code =
   if not
