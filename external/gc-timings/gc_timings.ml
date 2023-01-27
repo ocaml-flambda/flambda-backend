4a692630c9395cf412a2d80c6c4046671988d84d
Pierre Oechsel <pierre.oechsel@gmail.com>
2023-01-27 11:12:15 +0000

    Match gc timing output with dtiming format

--- external/gc-timings/gc_timings.ml
+++ external/gc-timings/gc_timings.ml
@@ -6,5 +6,9 @@ let start_collection () =
   collect_gc_timings ()
 
 let print ?(precision=3) ppf =
-  Format.fprintf ppf "%0.*fs gc_minor\n" precision (gc_minor_ns () *. 1e-9);
-  Format.fprintf ppf "%0.*fs gc_major\n" precision (gc_major_ns () *. 1e-9)
+  let minor = gc_minor_ns () in
+  let major = gc_major_ns () in
+  let secs x = x *. 1e-9 in
+  Format.fprintf ppf "%0.*fs gc\n" precision (secs (minor +. major));
+  Format.fprintf ppf "  %0.*fs minor\n" precision (secs minor);
+  Format.fprintf ppf "  %0.*fs major\n" precision (secs major)
