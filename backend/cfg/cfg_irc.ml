b3dc887db2e5f15f28224e51167838f64db20447
Xavier Clerc <xclerc@janestreet.com>
2023-01-31 13:06:33 +0000

    More IRC metrics

--- backend/cfg/cfg_irc.ml
+++ backend/cfg/cfg_irc.ml
@@ -444,7 +444,11 @@ let rewrite : State.t -> Cfg_with_liveness.t -> Reg.t list -> reset:bool -> bool
         log_body_and_terminator ~indent:3 block.body block.terminator liveness);
       Cfg.BasicInstructionList.iter_cell block.body ~f:(fun cell ->
           let instr = Cfg.BasicInstructionList.instr cell in
-          match Cfg_stack_operands.basic spilled_map instr with
+          match
+            Profile.record ~accumulate:true "stack_operands"
+              (fun () -> Cfg_stack_operands.basic spilled_map instr)
+              ()
+          with
           | All_spilled_registers_rewritten -> ()
           | May_still_have_spilled_registers ->
             let sharing = Reg.Tbl.create 8 in
@@ -452,7 +456,11 @@ let rewrite : State.t -> Cfg_with_liveness.t -> Reg.t list -> reset:bool -> bool
               instr;
             rewrite_instruction ~direction:(Store_after_cell cell) ~sharing
               instr);
-      match Cfg_stack_operands.terminator spilled_map block.terminator with
+      match
+        Profile.record ~accumulate:true "stack_operands"
+          (fun () -> Cfg_stack_operands.terminator spilled_map block.terminator)
+          ()
+      with
       | All_spilled_registers_rewritten -> ()
       | May_still_have_spilled_registers ->
         (let sharing = Reg.Tbl.create 8 in
@@ -509,7 +517,9 @@ let rec main : round:int -> State.t -> Cfg_with_liveness.t -> unit =
   let log_work_list_desc prefix =
     if irc_debug then log ~indent:1 "%s -- %s" prefix (work_lists_desc state)
   in
-  build state cfg_with_liveness;
+  Profile.record ~accumulate:true "build"
+    (fun () -> build state cfg_with_liveness)
+    ();
   let cfg_with_layout = Cfg_with_liveness.cfg_with_layout cfg_with_liveness in
   if irc_debug
   then (
@@ -524,35 +534,45 @@ let rec main : round:int -> State.t -> Cfg_with_liveness.t -> unit =
       RegisterStamp.PairSet.iter adj_set ~f:(fun p ->
           log ~indent:1 "(%d, %d) <- adj_set" (RegisterStamp.fst p)
             (RegisterStamp.snd p)));
-  make_work_list state;
+  Profile.record ~accumulate:true "make_work_list"
+    (fun () -> make_work_list state)
+    ();
   State.invariant state;
   if irc_debug then log_work_list_desc "before loop";
   let spill_cost_is_up_to_date = ref false in
   let continue = ref true in
   while !continue do
     if not (State.is_empty_simplify_work_list state)
-    then simplify state
+    then
+      Profile.record ~accumulate:true "simplify" (fun () -> simplify state) ()
     else if not (State.is_empty_work_list_moves state)
-    then coalesce state
+    then
+      Profile.record ~accumulate:true "coalesce" (fun () -> coalesce state) ()
     else if not (State.is_empty_freeze_work_list state)
-    then freeze state
+    then Profile.record ~accumulate:true "freeze" (fun () -> freeze state) ()
     else if not (State.is_empty_spill_work_list state)
-    then (
-      if not !spill_cost_is_up_to_date
-      then (
-        (match Lazy.force Spilling_heuristics.env with
-        | Set_choose ->
-          (* note: `spill_cost` will not be used by the heuristics *) ()
-        | Flat_uses -> update_spill_cost cfg_with_layout ~flat:true ()
-        | Hierarchical_uses -> update_spill_cost cfg_with_layout ~flat:false ());
-        spill_cost_is_up_to_date := true);
-      select_spill state)
+    then
+      Profile.record ~accumulate:true "select_spill"
+        (fun () ->
+          if not !spill_cost_is_up_to_date
+          then (
+            (match Lazy.force Spilling_heuristics.env with
+            | Set_choose ->
+              (* note: `spill_cost` will not be used by the heuristics *) ()
+            | Flat_uses -> update_spill_cost cfg_with_layout ~flat:true ()
+            | Hierarchical_uses ->
+              update_spill_cost cfg_with_layout ~flat:false ());
+            spill_cost_is_up_to_date := true);
+          select_spill state)
+        ()
     else continue := false;
     if irc_debug then log_work_list_desc "end of loop";
     State.invariant state
   done;
   if irc_debug then log ~indent:1 "(after loop)";
-  assign_colors state cfg_with_layout;
+  Profile.record ~accumulate:true "assign_colors"
+    (fun () -> assign_colors state cfg_with_layout)
+    ();
   State.invariant state;
   match State.spilled_nodes state with
   | [] -> if irc_debug then log ~indent:1 "(end of main)"
@@ -561,7 +581,11 @@ let rec main : round:int -> State.t -> Cfg_with_liveness.t -> unit =
     then
       List.iter spilled_nodes ~f:(fun reg ->
           log ~indent:1 "/!\\ register %a needs to be spilled" Printmach.reg reg);
-    match rewrite state cfg_with_liveness spilled_nodes ~reset:true with
+    match
+      Profile.record ~accumulate:true "rewrite"
+        (fun () -> rewrite state cfg_with_liveness spilled_nodes ~reset:true)
+        ()
+    with
     | false -> ()
     | true ->
       State.invariant state;
@@ -616,7 +640,9 @@ let run : Cfg_with_liveness.t -> Cfg_with_liveness.t =
     match rewrite state cfg_with_liveness spilling ~reset:false with
     | false -> ()
     | true -> Cfg_with_liveness.invalidate_liveness cfg_with_liveness));
-  main ~round:1 state cfg_with_liveness;
+  Profile.record ~accumulate:true "main"
+    (fun () -> main ~round:1 state cfg_with_liveness)
+    ();
   (* note: slots need to be updated before prologue removal *)
   if irc_debug
   then
