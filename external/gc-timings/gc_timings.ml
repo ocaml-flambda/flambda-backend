231e9c36d0e9a8da5af684595af5fd10bdd06305
Pierre Oechsel <pierre.oechsel@gmail.com>
2023-02-16 15:01:31 +0000

    spend -> spent

--- external/gc-timings/gc_timings.ml
+++ external/gc-timings/gc_timings.ml
@@ -1,6 +1,6 @@
 external collect_gc_timings : unit -> unit = "caml_timing_collect_gc" [@@noalloc]
-external gc_minor_ns : unit -> float = "caml_timing_gc_time_spend_minor"
-external gc_major_ns : unit -> float = "caml_timing_gc_time_spend_major"
+external gc_minor_ns : unit -> float = "caml_timing_gc_time_spent_minor"
+external gc_major_ns : unit -> float = "caml_timing_gc_time_spent_major"
 
 let start_collection () =
   collect_gc_timings ()
