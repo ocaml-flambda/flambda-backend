ca8e2bec8e228fda4d0670fa35bd2a860de72d7f
Pierre Oechsel <pierre.oechsel@gmail.com>
2023-02-16 15:01:52 +0000

    spend -> spent

--- external/gc-timings/gc_timings.mli
+++ external/gc-timings/gc_timings.mli
@@ -1,7 +1,7 @@
 val start_collection : unit -> unit
 
-(* Time spend doing a minor collection in nanoseconds *)
+(* Time spent doing a minor collection in nanoseconds *)
 val gc_minor_ns : unit -> float
 
-(* Time spend doing a major collection in nanoseconds *)
+(* Time spent doing a major collection in nanoseconds *)
 val gc_major_ns : unit -> float
