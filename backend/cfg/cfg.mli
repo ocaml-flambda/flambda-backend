16f829737a6debb0cbb001a82edc04946b66ae9f
Greta Yorsh <gyorsh@janestreet.com>
2023-02-23 19:35:10 +0000

    Move DoublyLinkedList out of Cfg

--- backend/cfg/cfg.mli
+++ backend/cfg/cfg.mli
@@ -31,67 +31,6 @@ include module type of struct
   include Cfg_intf.S
 end
 
-module DoublyLinkedList : sig
-  type 'a cell
-
-  val insert_before : 'a cell -> 'a -> unit
-
-  val insert_after : 'a cell -> 'a -> unit
-
-  val value : 'a cell -> 'a
-
-  val prev : 'a cell -> 'a cell option
-
-  type 'a t
-
-  val make_empty : unit -> _ t
-
-  val make_single : 'a -> 'a t
-
-  val of_list : 'a list -> 'a t
-
-  val hd : 'a t -> 'a option
-
-  val last : 'a t -> 'a option
-
-  val add_begin : 'a t -> 'a -> unit
-
-  val add_end : 'a t -> 'a -> unit
-
-  val is_empty : 'a t -> bool
-
-  val length : 'a t -> int
-
-  val remove_first : 'a t -> f:('a -> bool) -> unit
-
-  val filter_left : 'a t -> f:('a -> bool) -> unit
-
-  val filter_right : 'a t -> f:('a -> bool) -> unit
-
-  val iter : 'a t -> f:('a -> unit) -> unit
-
-  val iteri : 'a t -> f:(int -> 'a -> unit) -> unit
-
-  val iter_cell : 'a t -> f:('a cell -> unit) -> unit
-
-  val iter_right_cell : 'a t -> f:('a cell -> unit) -> unit
-
-  val iter2 : 'a t -> 'a t -> f:('a -> 'a -> unit) -> unit
-
-  val fold_left : 'a t -> f:('b -> 'a -> 'b) -> init:'b -> 'b
-
-  val fold_right : 'a t -> f:('a -> 'b -> 'b) -> init:'b -> 'b
-
-  val find_cell_opt : 'a t -> f:('a -> bool) -> 'a cell option
-
-  val find_opt : 'a t -> f:('a -> bool) -> 'a option
-
-  val to_list : 'a t -> 'a list
-
-  (* Adds all of the elements of `from` to `to_`, and clears `from`. *)
-  val transfer : to_:'a t -> from:'a t -> unit -> unit
-end
-
 type basic_instruction_list = basic instruction DoublyLinkedList.t
 
 type basic_block =
