001f10c47cc3f9b949ba604a589114348ec653ca
Greta Yorsh <gyorsh@janestreet.com>
2023-02-28 18:10:13 +0000

    Update tests

--- tests/backend/checkmach/t.ml
+++ tests/backend/checkmach/t.ml
@@ -1,6 +1,6 @@
 (* CR gyorsh: @assert are currently ignored by the compiler.
    Support for them will be handled in a separate PR. They are here to show
-   which functions we expect to pass the noalloc check. *)
+   which functions we expect to pass the zero_alloc check. *)
 let[@inline never] test1 n =
   let rec create n =
     if n = 0 then []
@@ -11,38 +11,34 @@ let[@inline never] test1 n =
 
 let[@inline never] test2 n  = List.length(test1 n)
 
-let[@noalloc][@inline never] test3 l  = List.length l
+let[@zero_alloc strict][@inline never] test3 l  = List.length l
 
-let[@noalloc][@inline never] test6 n = n + 37
+let[@zero_alloc strict][@inline never] test6 n = n + 37
 
-let[@noalloc][@inline never] test7 n m = (test6 n) * (test6 m)
+let[@zero_alloc strict][@inline never] test7 n m = (test6 n) * (test6 m)
 
 exception Exn_string of string
 exception Exn_int of int
 exception Exn
 exception Exn2
 
-let[@inline never] test8 n =
+let[@zero_alloc][@inline never] test8b n =
   match n with
-  | 1 -> raise_notrace Exn
-  | 2 -> raise_notrace (Exn_int 1)
-  | 3 -> raise_notrace (Exn_string "foo")
+  | 1 -> raise Exn
+  | 2 -> raise (Exn_int n)
+  | 3 -> raise (Exn_string "foo")
   | _ -> 10
 
-let[@inline never] test9 n =
-  try test8 n
-  with _ -> 0
-
-let[@noalloc][@inline never] test10 n =
+let[@zero_alloc strict][@inline never] test10 n =
   match n with
   | 1 -> raise_notrace Exn
   | _ -> 10
 
-let[@noalloc][@inline never] test11 n =
+let[@zero_alloc strict][@inline never] test11 n =
   try test10 n
   with Exn -> 10
 
-let[@noalloc][@inline never] test12 n =
+let[@zero_alloc strict][@inline never] test12 n =
   let test n =
     match n with
     | 1 -> raise Exn
@@ -57,10 +53,10 @@ let[@inline never] test13 n =
 
 let test14 n = Float.of_int n
 
-let[@noalloc][@inline never] test15 n = Int64.to_int (Int64.of_float (test14 n))
+let[@zero_alloc strict][@inline never] test15 n = Int64.to_int (Int64.of_float (test14 n))
 
-let[@noalloc] test16 n m = S.foo n m
-let[@noalloc] test17 n m = S.foo (S.foo n n) m
+let[@zero_alloc strict] test16 n m = S.foo n m
+let[@zero_alloc strict] test17 n m = S.foo (S.foo n n) m
 let test18 n m = S.foo n m
 
 exception Exn3 of (int * int)
@@ -68,9 +64,7 @@ type boo =
   | A
   | B of int
 
-(* CR gyorsh: analysis for noalloc_exn is not yet implemented.
-   It ignores allocations post-dominated by a raise. *)
-let(* [@noalloc_exn] *)[@inline never] test18 n boo =
+let[@zero_alloc][@inline never] test18 n boo =
   if n > 0 then n + n
   else begin
     let k = 45 in
@@ -90,7 +84,59 @@ let[@inline never] test19 n =
   in
   create n
 
-let(* [@noalloc_exn] *) rec foo n =
+let[@zero_alloc] rec foo n =
   bar (n-1)
-and(* [@noalloc_exn] *) bar n =
+and[@zero_alloc] bar n =
   foo (n-1)
+
+let[@zero_alloc] always_allocate_and_raise n = raise (Exn3 (4,n))
+
+let[@inline never] f n = (n,n)
+
+(* compiles to a recursive catch that the check can handle *)
+let[@inline never] test20 n = Sys.opaque_identity n
+let[@zero_alloc] test21 n =
+  let i = ref 0 in
+  let j = ref 0 in
+  while !j < n do
+    while !i < n do
+      i := test20 !i
+    done;
+    if n > 0 then raise (Exn_int !i);
+  done
+
+(*
+The check is too conservative about recursive catch to handle the following:
+
+let[@zero_alloc] test22 n =
+  let e = (Exn_int n) in
+  for i = 0 to n do
+    ();
+  done;
+  raise e
+*)
+
+let[@zero_alloc] test23 n =
+  let n =
+    try raise (Exn_int n) with Exn_int n -> n
+  in
+  raise (Exn_int (n*n))
+
+let[@zero_alloc] test24 n =
+  try raise (Exn_int n)
+  with Exn_int n -> raise (Exn_int (n+1))
+
+let[@zero_alloc] test25 n =
+  try
+    S.baz n
+  with
+  | _ -> raise (Exn3 (n,n))
+
+let[@zero_alloc strict] test26 n =
+  try raise Exn
+  with _ -> n
+
+(* The following is zero_alloc but we cannot prove it yet. *)
+let[@zero_alloc] test27 n =
+  try raise Exn
+  with _ -> n
--- tests/backend/checkmach/t5.ml
+++ tests/backend/checkmach/t5.ml
@@ -1,3 +1,8 @@
-let[@noalloc assume][@inline never] test x = [x;x+1]
+let[@zero_alloc strict assume][@inline never] test x = [x;x+1]
 (* The test below to make sure "noalloc" on external is still handled correctly. *)
 external external_test : unit -> unit = "test" [@@noalloc]
+
+let[@zero_alloc assume][@inline never] test4 x =
+  if x > (-100)
+  then x
+  else raise (Failure ("don't be so negative, "^(string_of_int x)))
