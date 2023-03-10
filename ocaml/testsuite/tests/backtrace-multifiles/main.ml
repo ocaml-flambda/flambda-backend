19510676880a5741f8947f16b7cdded883ed0a86
Guillaume Bury <guillaume.bury@gmail.com>
2023-03-10 18:04:13 +0100

    Add some explanation in the new test

--- ocaml/testsuite/tests/backtrace-multifiles/main.ml
+++ ocaml/testsuite/tests/backtrace-multifiles/main.ml
@@ -2,13 +2,22 @@
 
 let[@inline never] test s =
   let s' = (String.cat[@inlined never]) "! " s in
-  (Foo.h[@inlined]) s'; (* CR gbury: remove [@inlined] *)
-  (Foo.print_stack[@inlined]) (); (* CR gbury: remove [@inlined] *)
+  (Foo.h[@inlined]) s'; (* CR gbury: remove [@inlined], cf #1199 *)
+  (Foo.print_stack[@inlined]) (); (* CR gbury: remove [@inlined], cf #1199  *)
   (print_endline[@inlined never]) "end of test"
 
 let () =
   Printexc.record_backtrace true;
   test "foobar"
+
+(* This test aims at checking that backtraces are correct after inlining,
+   particularly after inlining of functions from other files, and
+   furthermore from other files compiled with different optimization options.
+   We therefore define quite a few functions (in `foo.ml`) that use one
+   another (with a lot of inlining). Additionally, we also print
+   stack/backtraces after an inlined function (in the `test` function above),
+   to ensure that we do not wrongly propagate debuginfos past the function
+   call. *)
 
 (* TEST
 
