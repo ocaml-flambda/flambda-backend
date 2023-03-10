5548835c5d073e389624ff1f61a75cfac114096f
Mark Shinwell <mshinwell@pm.me>
2023-03-10 19:17:09 +0000

    experiment

--- ocaml/testsuite/tests/afl-instrumentation/test.ml
+++ ocaml/testsuite/tests/afl-instrumentation/test.ml
@@ -60,12 +60,14 @@ let random () = opaque @@
   if Random.int 100 < 50 then print_string "a" else print_string "b";
   if Random.int 100 < 50 then print_string "a" else print_string "b"
 
+(*
 let already_forced = lazy (ref 42)
 let _ = Lazy.force already_forced
 
 let laziness () = opaque @@
   let _ = Lazy.force already_forced in
   Gc.major ()
+*)
 
 let tests =
   [| ("lists", fun () -> lists 42);
@@ -76,5 +78,5 @@ let tests =
      ("classes", fun () -> classes 42);
      ("obj_ordering", obj_ordering);
      (* ("random", random); *)
-     ("laziness", laziness);
+(*     ("laziness", laziness); *)
   |]
