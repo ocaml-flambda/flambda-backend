cdb2af6d6c947b0709cbf5edae7888fe969b5056
Nathanaëlle Courant <nathanaelle.courant@ocamlpro.com>
2023-03-10 10:48:25 +0100

    backport to ocaml/

--- ocaml/middle_end/clambda_primitives.mli
+++ ocaml/middle_end/clambda_primitives.mli
@@ -125,6 +125,10 @@ type primitive =
   | Popaque
   (* Probes *)
   | Pprobe_is_enabled of { name : string }
+  | Punbox_float
+  | Pbox_float of alloc_mode
+  | Punbox_int of boxed_integer
+  | Pbox_int of boxed_integer * alloc_mode
 
 and integer_comparison = Lambda.integer_comparison =
     Ceq | Cne | Clt | Cgt | Cle | Cge
@@ -147,6 +151,8 @@ and value_kind = Lambda.value_kind =
 and layout = Lambda.layout =
   | Ptop
   | Pvalue of value_kind
+  | Punboxed_float
+  | Punboxed_int of boxed_integer
   | Pbottom
 
 and block_shape = Lambda.block_shape
