7eb2db2825ddc5c6b03d12dbbc219d04e7b8c043
Pierre Oechsel <pierre.oechsel@gmail.com>
2023-03-28 11:15:19 +0100

    Remove trailing newlines from objinfo

--- ocaml/tools/objinfo.ml
+++ ocaml/tools/objinfo.ml
@@ -142,8 +142,7 @@ let print_cms_infos cms =
   let open Cms_format in
   printf "Cms unit name: %a\n" Compilation_unit.output cms.cms_modname;
   printf "Source file: %s\n"
-    (match cms.cms_sourcefile with None -> "(none)" | Some f -> f);
-  printf "\n"
+    (match cms.cms_sourcefile with None -> "(none)" | Some f -> f)
 
 let linkage_name comp_unit =
   Symbol.for_compilation_unit comp_unit
--- tools/flambda_backend_objinfo.ml
+++ tools/flambda_backend_objinfo.ml
@@ -141,8 +141,7 @@ let print_cms_infos cms =
   let open Cms_format in
   printf "Cms unit name: %a\n" Compilation_unit.output cms.cms_modname;
   printf "Source file: %s\n"
-    (match cms.cms_sourcefile with None -> "(none)" | Some f -> f);
-  printf "\n"
+    (match cms.cms_sourcefile with None -> "(none)" | Some f -> f)
 
 let print_general_infos print_name name crc defines iter_cmi iter_cmx =
   printf "Name: %a\n" print_name name;
