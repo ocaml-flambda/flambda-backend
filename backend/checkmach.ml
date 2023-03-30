689ed071b1f48b7f53754d0d0b412096f49bb131
Greta Yorsh <gyorsh@janestreet.com>
2023-03-30 19:49:32 +0100

    Update comment about init_lbl passed to backward dataflow analysis

--- backend/checkmach.ml
+++ backend/checkmach.ml
@@ -727,7 +727,7 @@ end = struct
 
        To check divergent loops, the initial value of "div" component of all
        Iexit labels is set to "Safe" instead of "Bot". This is conservative with
-       respect to non-recursive Icatch. *)
+       respect to non-recursive Icatch and Itrywith handlers. *)
     D.analyze ~exnescape:Value.exn_escape ~init_lbl:Value.diverges ~transfer
       body
     |> fst
