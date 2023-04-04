cf78b985e2d12eddbe7b4ee377d4d70de7c0774f
Greta Yorsh <gyorsh@janestreet.com>
2023-04-04 17:42:00 +0100

    Print source-level function name not only function symbol in errors

--- tests/backend/checkmach/filter.sh
+++ tests/backend/checkmach/filter.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sed -r 's/Error: Annotation check for zero_alloc( strict | )failed on function caml(.*)_[0-9]+(_[0-9]+_code)?$/Error: Annotation check for zero_alloc\1failed on function caml\2_HIDE_STAMP/'
+sed -r 's/Error: Annotation check for zero_alloc( strict | )failed on function ([^ ]*) \(caml(.*)_[0-9]+(_[0-9]+_code)?\)$/Error: Annotation check for zero_alloc\1failed on function \2 \(caml\3_HIDE_STAMP\)/'
