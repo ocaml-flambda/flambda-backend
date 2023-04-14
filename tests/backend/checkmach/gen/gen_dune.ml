24a5ed40e108a09acdd8c039a1dc4da4b8781f54
Greta Yorsh <gyorsh@janestreet.com>
2023-04-14 12:42:58 +0100

    Generate positive tests

--- tests/backend/checkmach/gen/gen_dune.ml
+++ tests/backend/checkmach/gen/gen_dune.ml
@@ -1,39 +1,44 @@
 let () =
-  for i = 1 to 12 do
-    let extra_dep =
-      match i with
-      | 3 | 4 -> true
-      | _ -> false
-    in
-    let flambda_only =
-      match i with
-      | 12 -> true
-      | _ -> false
-    in
-    let enabled_if =
-      if flambda_only then
-        (* what we really want to say if dune knew about flambda2:
-           (or %{ocaml-config:flambda} %{ocaml-config:flambda2}) *)
-        {|(enabled_if (and (= %{context_name} "main") %{ocaml-config:flambda}))|}
-      else
-        {|(enabled_if (= %{context_name} "main"))|}
-    in
+  let enabled_if flambda_only =
+    if flambda_only then
+      (* what we really want to say if dune knew about flambda2:
+         (or %{ocaml-config:flambda} %{ocaml-config:flambda2}) *)
+      {|(enabled_if (and (= %{context_name} "main") %{ocaml-config:flambda}))|}
+    else
+      {|(enabled_if (= %{context_name} "main"))|}
+  in
+  let print_test ~flambda_only ~deps =
+    let enabled_if = enabled_if flambda_only in
+    Printf.printf
+    {|
+(rule
+ (alias   runtest)
+ %s
+ (deps %s)
+ (action (run %%{bin:ocamlopt.opt} %%{deps} -g -c -zero-alloc-check -dcse -dcheckmach -dump-into-file -O3)))
+|}
+    enabled_if deps
+  in
+  let print_test_expected_output ~flambda_only ~extra_dep ~exit_code name =
+    let enabled_if = enabled_if flambda_only in
     let ml_deps =
       let s =
-        (if extra_dep then Printf.sprintf "t%d.ml " i else "")
+        match extra_dep with
+        | None -> ""
+        | Some s -> s^" "
       in
-      Printf.sprintf {|(:ml %sfail%d.ml)|} s i
+      Printf.sprintf {|(:ml %s%s.ml)|} s name
     in
   Printf.printf
     {|
 (rule
  %s
- (targets fail%d.output.corrected)
+ (targets %s.output.corrected)
  (deps %s filter.sh)
  (action
-   (with-outputs-to fail%d.output.corrected
+   (with-outputs-to %s.output.corrected
     (pipe-outputs
-    (with-accepted-exit-codes 2
+    (with-accepted-exit-codes %d
      (run %%{bin:ocamlopt.opt} %%{ml} -g -color never -error-style short -c -zero-alloc-check -O3))
     (run "./filter.sh")
    ))))
@@ -41,8 +46,31 @@ let () =
 (rule
  (alias   runtest)
  %s
- (deps fail%d.output fail%d.output.corrected)
- (action (diff fail%d.output fail%d.output.corrected)))
+ (deps %s.output %s.output.corrected)
+ (action (diff %s.output %s.output.corrected)))
 |}
-  enabled_if i ml_deps i enabled_if i i i i
-  done
+  enabled_if name ml_deps name exit_code enabled_if name name name name
+  in
+  print_test ~flambda_only:false ~deps:"s.ml t.ml";
+  print_test ~flambda_only:false ~deps:"t5.ml test_assume.ml";
+  print_test ~flambda_only:true ~deps:"test_flambda.ml";
+  for i = 1 to 12 do
+    let extra_dep =
+      match i with
+      | 3 | 4 -> Some (Printf.sprintf "t%d.ml" i)
+      | _ -> None
+    in
+    let flambda_only =
+      match i with
+      | 12 -> true
+      | _ -> false
+    in
+    let name = Printf.sprintf "fail%d" i in
+    print_test_expected_output ~flambda_only ~extra_dep ~exit_code:2 name
+  done;
+  print_test_expected_output ~flambda_only:false ~extra_dep:None
+    ~exit_code:2 "test_attribute_error_duplicate";
+  (* Closure does not optimize the function away, so the unchecked attribute
+     warning is only with flambda and flambda2. *)
+  print_test_expected_output ~flambda_only:true ~extra_dep:None
+    ~exit_code:0 "test_attr_unused"
