fe7e74cc9ee64cc167d2d76700cd88ffcad1ac18
Roman Leshchinskiy <rleshchinskiy@gmail.com>
2023-04-28 11:34:30 +0100

    Lazy deserialization of cmi files (#1322)
    * Lazy deserialization of cmi files
    
    * Preserve legacy API in cmi_format
    
    * make depend
    
    * bootstrap
    
    ---------
    
    Co-authored-by: Roman Leshchinskiy <rleshchinskiy@janestreet.com>

--- ocaml/file_formats/cmi_format.mli
+++ ocaml/file_formats/cmi_format.mli
@@ -21,21 +21,26 @@ type pers_flags =
   | Opaque
   | Unsafe_string
 
-type cmi_infos = {
+type 'sg cmi_infos_generic = {
     cmi_name : Compilation_unit.t;
-    cmi_sign : Types.signature_item list;
+    cmi_sign : 'sg;
     cmi_crcs : Import_info.t array;
     cmi_flags : pers_flags list;
 }
 
+type cmi_infos_lazy = Subst.Lazy.signature cmi_infos_generic
+type cmi_infos = Types.signature cmi_infos_generic
+
 (* write the magic + the cmi information *)
-val output_cmi : string -> out_channel -> cmi_infos -> Digest.t
+val output_cmi : string -> out_channel -> cmi_infos_lazy -> Digest.t
 
 (* read the cmi information (the magic is supposed to have already been read) *)
 val input_cmi : in_channel -> cmi_infos
+val input_cmi_lazy : in_channel -> cmi_infos_lazy
 
 (* read a cmi from a filename, checking the magic *)
 val read_cmi : string -> cmi_infos
+val read_cmi_lazy : string -> cmi_infos_lazy
 
 (* Error report *)
 
