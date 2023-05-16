dbbb0a0ec567ff0b3a1a4d7bc3000d9cb9086253
Greta Yorsh <gyorsh@janestreet.com>
2023-05-16 19:22:43 +0100

    Add a test for inconsistent enabled_at_init for %probe

--- tests/backend/probes/t2.ml
+++ tests/backend/probes/t2.ml
@@ -1,5 +1,5 @@
 let () =
-  [%probe "name" (Printf.printf "default\n")];
-  [%probe "name" ~enabled_at_init:true (Printf.printf "enabled at init\n")];
-  [%probe "name" ~enabled_at_init:false (Printf.printf "not enabled at init\n")];
+  [%probe "a" (Printf.printf "default\n")];
+  [%probe "b" ~enabled_at_init:true (Printf.printf "enabled at init\n")];
+  [%probe "c" ~enabled_at_init:false (Printf.printf "not enabled at init\n")];
   ()
