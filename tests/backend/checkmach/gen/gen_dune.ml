8f00b3b6c478f36683284a472d7e39f2bbf6169a
Greta Yorsh <gyorsh@janestreet.com>
2023-05-23 12:52:34 +0100

    Use the new flag in tests

--- tests/backend/checkmach/gen/gen_dune.ml
+++ tests/backend/checkmach/gen/gen_dune.ml
@@ -19,7 +19,7 @@ let () =
 |}
     enabled_if deps
   in
-  let print_test_expected_output ~flambda_only ~extra_dep ~exit_code name =
+  let print_test_expected_output ~cutoff ~flambda_only ~extra_dep ~exit_code name =
     let enabled_if = enabled_if flambda_only in
     let ml_deps =
       let s =
@@ -39,7 +39,8 @@ let () =
    (with-outputs-to %s.output.corrected
     (pipe-outputs
     (with-accepted-exit-codes %d
-     (run %%{bin:ocamlopt.opt} %%{ml} -g -color never -error-style short -c -zero-alloc-check -O3))
+     (run %%{bin:ocamlopt.opt} %%{ml} -g -color never -error-style short -c
+          -zero-alloc-check -checkmach-details-cutoff %d -O3))
     (run "./filter.sh")
    ))))
 
@@ -49,42 +50,46 @@ let () =
  (deps %s.output %s.output.corrected)
  (action (diff %s.output %s.output.corrected)))
 |}
-  enabled_if name ml_deps name exit_code enabled_if name name name name
+  enabled_if name ml_deps name exit_code cutoff enabled_if name name name name
   in
+  let default_cutoff = 20 in
   print_test ~flambda_only:false ~deps:"s.ml t.ml";
   print_test ~flambda_only:false ~deps:"t5.ml test_assume.ml";
   print_test ~flambda_only:true ~deps:"test_flambda.ml";
 
-  print_test_expected_output ~flambda_only:false ~extra_dep:None ~exit_code:2 "fail1";
-  print_test_expected_output ~flambda_only:false ~extra_dep:None ~exit_code:2 "fail2";
-  print_test_expected_output ~flambda_only:false ~extra_dep:(Some "t3.ml")
+  print_test_expected_output ~cutoff:0 ~flambda_only:false ~extra_dep:None ~exit_code:2 "fail1";
+  print_test_expected_output ~cutoff:0 ~flambda_only:false ~extra_dep:None ~exit_code:2 "fail2";
+  print_test_expected_output ~cutoff:0 ~flambda_only:false ~extra_dep:(Some "t3.ml")
     ~exit_code:2 "fail3";
-  print_test_expected_output ~flambda_only:false ~extra_dep:(Some "t4.ml")
+  print_test_expected_output ~cutoff:0 ~flambda_only:false ~extra_dep:(Some "t4.ml")
     ~exit_code:2 "fail4";
-  print_test_expected_output ~flambda_only:false ~extra_dep:None ~exit_code:2 "fail5";
-  print_test_expected_output ~flambda_only:false ~extra_dep:None ~exit_code:2 "fail6";
-  print_test_expected_output ~flambda_only:false ~extra_dep:None ~exit_code:2 "fail7";
-  print_test_expected_output ~flambda_only:false ~extra_dep:None ~exit_code:2 "fail8";
-  print_test_expected_output ~flambda_only:false ~extra_dep:None ~exit_code:2 "fail9";
-  print_test_expected_output ~flambda_only:false ~extra_dep:None ~exit_code:2 "fail10";
-  print_test_expected_output ~flambda_only:false ~extra_dep:None ~exit_code:2 "fail11";
-  print_test_expected_output ~flambda_only:true  ~extra_dep:None ~exit_code:2 "fail12";
-  print_test_expected_output ~flambda_only:false ~extra_dep:None ~exit_code:2 "fail13";
-  print_test_expected_output ~flambda_only:false ~extra_dep:None ~exit_code:2 "fail14";
-  print_test_expected_output ~flambda_only:false ~extra_dep:None ~exit_code:2 "fail15";
-  print_test_expected_output ~flambda_only:false ~extra_dep:None ~exit_code:2 "fail16";
-  print_test_expected_output ~flambda_only:false ~extra_dep:None ~exit_code:2 "fail17";
-  print_test_expected_output ~flambda_only:false ~extra_dep:None ~exit_code:2 "fail18";
-  print_test_expected_output ~flambda_only:false ~extra_dep:(Some "dep19.ml") ~exit_code:2 "fail19";
+  print_test_expected_output ~cutoff:0 ~flambda_only:false ~extra_dep:None ~exit_code:2 "fail5";
+  print_test_expected_output ~cutoff:0 ~flambda_only:false ~extra_dep:None ~exit_code:2 "fail6";
+  print_test_expected_output ~cutoff:0 ~flambda_only:false ~extra_dep:None ~exit_code:2 "fail7";
+  print_test_expected_output ~cutoff:0 ~flambda_only:false ~extra_dep:None ~exit_code:2 "fail8";
+  print_test_expected_output ~cutoff:0 ~flambda_only:false ~extra_dep:None ~exit_code:2 "fail9";
+  print_test_expected_output ~cutoff:0 ~flambda_only:false ~extra_dep:None ~exit_code:2 "fail10";
+  print_test_expected_output ~cutoff:0 ~flambda_only:false ~extra_dep:None ~exit_code:2 "fail11";
+  print_test_expected_output ~cutoff:default_cutoff ~flambda_only:true  ~extra_dep:None ~exit_code:2 "fail12";
+  print_test_expected_output ~cutoff:0 ~flambda_only:false ~extra_dep:None ~exit_code:2 "fail13";
+  print_test_expected_output ~cutoff:0 ~flambda_only:false ~extra_dep:None ~exit_code:2 "fail14";
+  print_test_expected_output ~cutoff:0 ~flambda_only:false ~extra_dep:None ~exit_code:2 "fail15";
+  print_test_expected_output ~cutoff:0 ~flambda_only:false ~extra_dep:None ~exit_code:2 "fail16";
+  print_test_expected_output ~cutoff:0 ~flambda_only:false ~extra_dep:None ~exit_code:2 "fail17";
+  print_test_expected_output ~cutoff:0 ~flambda_only:false ~extra_dep:None ~exit_code:2 "fail18";
+  (* test printing detailed error message only in flambda because the exact output depends
+     on optimization level. *)
+  print_test_expected_output ~cutoff:default_cutoff ~flambda_only:true ~extra_dep:(Some "dep19.ml") ~exit_code:2 "fail19";
+  print_test_expected_output ~cutoff:default_cutoff ~flambda_only:true ~extra_dep:None ~exit_code:2 "fail20";
 
-  print_test_expected_output ~flambda_only:false ~extra_dep:None
+  print_test_expected_output ~cutoff:0 ~flambda_only:false ~extra_dep:None
     ~exit_code:2 "test_attribute_error_duplicate";
   (* Closure does not optimize the function away, so the unchecked attribute
      warning is only with flambda and flambda2. *)
-  print_test_expected_output ~flambda_only:true ~extra_dep:None
+  print_test_expected_output ~cutoff:default_cutoff ~flambda_only:true ~extra_dep:None
     ~exit_code:0 "test_attr_unused";
   (* Checks that the warning is printed and compilation is successful. *)
-  print_test_expected_output ~flambda_only:false ~extra_dep:None
+  print_test_expected_output ~cutoff:default_cutoff ~flambda_only:false ~extra_dep:None
     ~exit_code:0 "t6";
   (* Check that entry function and functors are ignored with  [@@@zero_alloc all] *)
   print_test ~flambda_only:false ~deps:"t7.ml";
