df41daecc5bc7a965e1b2044f8de8b461196ad22
Richard Eisenberg <reisenberg@janestreet.com>
2023-05-30 07:37:10 -0400

    flambda-backend: Remove layout variables from [val]s (#1423)
    * Remove layout variables from [val]s
    
    * Actually update tests
    
    * bootstrap

--- testsuite/tests/typing-layouts/basics_beta.ml
+++ testsuite/tests/typing-layouts/basics_beta.ml
@@ -493,3 +493,15 @@ Error: Layout void is used here, but the appropriate layouts extension is not en
 
 (* CR layouts v2: This test moved to [basics_alpha.ml] as it needs a non-value
    sort.  Bring back here when we have one. *)
+
+(*******************************************)
+(* Test 29: [external]s default to [value] *)
+
+(* CR layouts v2: This test moved to [basics_alpha.ml] as it needs a non-value
+   sort.  Bring back here when we have one. *)
+
+(**************************************)
+(* Test 30: [val]s default to [value] *)
+
+(* CR layouts v2: This test moved to [basics_alpha.ml] as it needs a non-value
+   sort.  Bring back here when we have one. *)
