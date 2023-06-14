870c60de5130989d0da2513adf05de8a5f76c11a
Xavier Clerc <xclerc@janestreet.com>
2023-06-14 10:56:13 +0100

    Comments in `Cfg_dominators` interface.

--- backend/cfg/cfg_dominators.mli
+++ backend/cfg/cfg_dominators.mli
@@ -8,22 +8,32 @@ val compute_dominators : Cfg.t -> dominators
 
 val is_dominating : dominators -> Label.t -> Label.t -> bool
 (* [is_dominating doms x y] is [true] iff [x] is dominating [y] according to
-   [dominators]. That is, all paths from the entry node to [y] go through [x]. 
+   [dominators]. That is, all paths from the entry node to [y] go through [x].
    All edges, regular and exceptional are treated the same way. *)
 
 val is_strictly_dominating : dominators -> Label.t -> Label.t -> bool
 (* [is_strictly_dominating doms x y] is [true] iff [x] is strictly dominating
-   [y] according to [dominators]. That is, [is_dominating x y = true] and [x] is not equal [y]. 
-   All edges, regular and exceptional are treated the same way.*)
+   [y] according to [dominators]. That is, [is_dominating x y = true] and [x] is
+   not equal [y]. All edges, regular and exceptional are treated the same way.*)
 
 type immediate_dominators = Label.t Label.Map.t
 
 val compute_immediate_dominators : Cfg.t -> dominators -> immediate_dominators
+(* [compute_immediate_dominators cfg doms] returns a map from labels to their
+   immediate dominator, using the following definition: "the immediate dominator
+   a node n is the unique node that strictly dominates n but does not strictly
+   dominate any other node that strictly dominates n". By definition, the
+   returned map has an binding for each node of [cfg], except the entry
+   point. *)
 
 type dominance_frontiers = Label.Set.t Label.Map.t
 
 val compute_dominance_frontiers :
   Cfg.t -> dominators -> immediate_dominators -> dominance_frontiers
+(* [compute_dominance_frontiers cfg doms idoms] returns a map from labels to
+   their frontiers as sets of labels. The definition we use is the following:
+   "the dominance frontier of a node n is the set of all nodes m such that n
+   dominates a predecessor of m, but does not dominate m itself". *)
 
 type dominator_tree =
   { label : Label.t;
