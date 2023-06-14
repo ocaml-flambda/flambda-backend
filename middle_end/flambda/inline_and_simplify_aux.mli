5b57a4f5ac8d937e44a7dc04b9085db9150730ac
Luke Maurer <lmaurer@janestreet.com>
2023-06-14 18:52:35 +0100

    Better region tracking in flambda1
    Regions are currently not tracked very precisely in flambda1. In particular, it
    is assumed that nothing in a region block can affect the parent region, which is
    not true when there are explicit exclaves. Also, some old traversal code tends
    to ignore region borders entirely, which is almost certainly causing subtle bugs
    somewhere (and adding region lifting makes it cause unsubtle bugs everywhere).
    
    Adding an actual region stack makes it easy to be precise, and will also help
    remove regions that have enclaves (see upcoming PR).

--- middle_end/flambda/inline_and_simplify_aux.mli
+++ middle_end/flambda/inline_and_simplify_aux.mli
@@ -296,11 +296,29 @@ module Result : sig
   (** Mark that the given static exception has been used. *)
   val use_static_exception : t -> Static_exception.t -> t
 
-  (** Mark/clear whether local allocations may be made in
-      the nearest enclosing region *)
-  val set_region_use : t -> bool -> t
+  (** Enter the scope of a region.  A subsequent call to [set_region_used] will
+      affect this region. *)
+  val enter_region : t -> t
 
-  (** Whether [set_region_use _ true] has been called *)
+  (** Leave the scope of a region, restoring the previous one as the new
+      innermost region. *)
+  val leave_region : t -> t
+
+  type exclave
+
+  (** Enter an exclave.  A subsequent call to [set_region_used] will now affect
+      the outer region.  Returns a value that must be passed to the
+      corresponding [leave_exclave]. *)
+  val enter_exclave : t -> exclave * t
+
+  (** Leave an exclave, effectively re-entering the outer region. *)
+  val leave_exclave : t -> exclave -> t
+
+  (** Mark that local allocations may be made in
+      the nearest enclosing region *)
+  val set_region_used : t -> t
+
+  (** Whether [set_region_used _] has been called *)
   val may_use_region : t -> bool
 
   (** Mark that we are moving up out of the scope of a static-catch block
