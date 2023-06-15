ccasin/oxcaml

Delete sort_function_return

ocaml/typing/typecore.ml
@@ -5460,14 +5460,18 @@ and type_expect_
         in
         loop slet.pbop_pat (newvar initial_layout) sands
       in
+      let body_sort = Sort.new_var () in
       let ty_func_result =
-        newvar (Layout.of_new_sort_var ~why:Function_result)
+        newvar (Layout.of_sort ~why:Function_result body_sort)
       in
       let arrow_desc = Nolabel, Alloc_mode.global, Alloc_mode.global in
       let ty_func =
         newty (Tarrow(arrow_desc, newmono ty_params, ty_func_result, commu_ok))
       in
-      let ty_result = newvar (Layout.of_new_sort_var ~why:Function_result) in
+      let op_result_sort = Sort.new_var () in
+      let ty_result =
+        newvar (Layout.of_sort ~why:Function_result op_result_sort)
+      in
       let ty_andops = newvar (Layout.of_new_sort_var ~why:Function_argument) in
       let ty_op =
         newty (Tarrow(arrow_desc, newmono ty_andops,
@@ -5511,12 +5515,14 @@ and type_expect_
           bop_op_path = op_path;
           bop_op_val = op_desc;
           bop_op_type = op_type;
+          bop_op_result_sort = op_result_sort;
           bop_exp = exp;
           bop_loc = slet.pbop_loc; }
       in
       let warnings = Warnings.backup () in
       let desc =
-        Texp_letop{let_; ands; param; param_sort; body; partial; warnings}
+        Texp_letop{let_; ands; param; param_sort; body; body_sort; partial;
+                   warnings}
       in
       rue { exp_desc = desc;
             exp_loc = sexp.pexp_loc;
@@ -5700,7 +5706,7 @@ and type_function ?in_function loc attrs env (expected_mode : expected_mode)
     | _ -> false
   in
   let ty_expected' = instance ty_expected in
-  let { ty_arg; arg_mode; arg_sort; ty_ret; ret_mode } =
+  let { ty_arg; arg_mode; arg_sort; ty_ret; ret_mode; ret_sort } =
     let force_tpoly =
       (* If [has_poly] is true then we rely on the later call to
          type_pat to enforce the invariant that the parameter type
@@ -5836,7 +5842,7 @@ and type_function ?in_function loc attrs env (expected_mode : expected_mode)
     exp_desc =
       Texp_function
         { arg_label; param; cases; partial; region; curry; warnings;
-          arg_mode; arg_sort; alloc_mode };
+          arg_mode; arg_sort; alloc_mode; ret_sort };
     exp_loc = loc; exp_extra = [];
     exp_type =
       instance (newgenty (Tarrow((arg_label,arg_mode,ret_mode),
@@ -6321,6 +6327,7 @@ and type_argument ?explanation ?recarg env (mode : expected_mode) sarg
       let eta_mode = Value_mode.local_to_regional (Value_mode.of_alloc marg) in
       let eta_pat, eta_var = var_pair ~mode:eta_mode "eta" ty_arg in
       let arg_sort = type_sort_exn env ~why:Function_argument ty_arg in
+      let ret_sort = type_sort_exn env ~why:Function_argument ty_res in
       let func texp =
         let ret_mode = Value_mode.of_alloc mret in
         let e =
@@ -6340,7 +6347,8 @@ and type_argument ?explanation ?recarg env (mode : expected_mode) sarg
             exp_desc = Texp_function { arg_label = Nolabel; param; cases;
                                        partial = Total; region = false; curry;
                                        warnings = Warnings.backup ();
-                                       arg_mode = marg; arg_sort; alloc_mode } }
+                                       arg_mode = marg; arg_sort; ret_sort;
+                                       alloc_mode } }
       in
       Location.prerr_warning texp.exp_loc
         (Warnings.Eliminated_optional_arguments
@@ -7210,7 +7218,10 @@ and type_andops env sarg sands expected_ty =
         let op_type = op_desc.val_type in
         let ty_arg = newvar (Layout.of_new_sort_var ~why:Function_argument) in
         let ty_rest = newvar (Layout.of_new_sort_var ~why:Function_argument) in
-        let ty_result = newvar (Layout.of_new_sort_var ~why:Function_result) in
+        let op_result_sort = Sort.new_var () in
+        let ty_result =
+          newvar (Layout.of_sort ~why:Function_result op_result_sort)
+        in
         let arrow_desc = (Nolabel,Alloc_mode.global,Alloc_mode.global) in
         let ty_rest_fun =
           newty (Tarrow(arrow_desc, newmono ty_arg, ty_result, commu_ok))
@@ -7243,6 +7254,7 @@ and type_andops env sarg sands expected_ty =
             bop_op_path = op_path;
             bop_op_val = op_desc;
             bop_op_type = op_type;
+            bop_op_result_sort = op_result_sort;
             bop_exp = exp;
             bop_loc = loc }
         in

ocaml/typing/typedtree.ml
@@ -113,7 +113,8 @@ and expression_desc =
       region : bool; curry : fun_curry_state;
       warnings : Warnings.state;
       arg_mode : Types.alloc_mode;
-      arg_sort : Layouts.sort;
+      arg_sort : sort;
+      ret_sort : sort;
       alloc_mode : Types.alloc_mode }
   | Texp_apply of expression * (arg_label * apply_arg) list * apply_position * Types.alloc_mode
   | Texp_match of expression * sort * computation case list * partial
@@ -148,7 +149,7 @@ and expression_desc =
       for_to   : expression;
       for_dir  : direction_flag;
       for_body : expression;
-      for_body_layout : Layouts.layout;
+      for_body_layout : layout;
     }
   | Texp_send of expression * meth * apply_position * Types.alloc_mode
   | Texp_new of
@@ -168,8 +169,9 @@ and expression_desc =
       let_ : binding_op;
       ands : binding_op list;
       param : Ident.t;
-      param_sort : Layouts.sort;
+      param_sort : sort;
       body : value case;
+      body_sort : sort;
       partial : partial;
       warnings : Warnings.state;
     }
@@ -231,6 +233,7 @@ and binding_op =
     bop_op_name : string loc;
     bop_op_val : Types.value_description;
     bop_op_type : Types.type_expr;
+    bop_op_result_sort : sort;
     bop_exp : expression;
     bop_loc : Location.t;
   }
@@ -243,9 +246,9 @@ and omitted_parameter =
   { mode_closure : alloc_mode;
     mode_arg : alloc_mode;
     mode_ret : alloc_mode;
-    sort_arg : Layouts.sort }
+    sort_arg : sort }
 
-and apply_arg = (expression * Layouts.sort, omitted_parameter) arg_or_omitted
+and apply_arg = (expression * sort, omitted_parameter) arg_or_omitted
 
 and apply_position =
   | Tail

ocaml/typing/typedtree.mli
@@ -215,6 +215,7 @@ and expression_desc =
       warnings : Warnings.state;
       arg_mode : Types.alloc_mode;
       arg_sort : Layouts.sort;
+      ret_sort : Layouts.sort;
       alloc_mode : Types.alloc_mode}
         (** [Pexp_fun] and [Pexp_function] both translate to [Texp_function].
             See {!Parsetree} for more details.
@@ -346,6 +347,7 @@ and expression_desc =
       param : Ident.t;
       param_sort : Layouts.sort;
       body : value case;
+      body_sort : Layouts.sort;
       partial : partial;
       warnings : Warnings.state;
     }
@@ -418,6 +420,7 @@ and binding_op =
     bop_op_type : Types.type_expr;
     (* This is the type at which the operator was used.
        It is always an instance of [bop_op_val.val_type] *)
+    bop_op_result_sort : Layouts.sort;
     bop_exp : expression;
     bop_loc : Location.t;
   }

ocaml-flambda/flambda-backend

ocaml/typing/typecore.ml
@@ -5496,14 +5496,18 @@ and type_expect_
         in
         loop slet.pbop_pat (newvar initial_layout) sands
       in
+      let body_sort = Sort.new_var () in
       let ty_func_result =
-        newvar (Layout.of_new_sort_var ~why:Function_result)
+        newvar (Layout.of_sort ~why:Function_result body_sort)
       in
       let arrow_desc = Nolabel, Alloc_mode.global, Alloc_mode.global in
       let ty_func =
         newty (Tarrow(arrow_desc, newmono ty_params, ty_func_result, commu_ok))
       in
-      let ty_result = newvar (Layout.of_new_sort_var ~why:Function_result) in
+      let op_result_sort = Sort.new_var () in
+      let ty_result =
+        newvar (Layout.of_sort ~why:Function_result op_result_sort)
+      in
       let ty_andops = newvar (Layout.of_new_sort_var ~why:Function_argument) in
       let ty_op =
         newty (Tarrow(arrow_desc, newmono ty_andops,
@@ -5547,12 +5551,14 @@ and type_expect_
           bop_op_path = op_path;
           bop_op_val = op_desc;
           bop_op_type = op_type;
+          bop_op_return_sort = op_result_sort;
           bop_exp = exp;
           bop_loc = slet.pbop_loc; }
       in
       let warnings = Warnings.backup () in
       let desc =
-        Texp_letop{let_; ands; param; param_sort; body; partial; warnings}
+        Texp_letop{let_; ands; param; param_sort; body; body_sort; partial;
+                   warnings}
       in
       rue { exp_desc = desc;
             exp_loc = sexp.pexp_loc;
@@ -5736,7 +5742,7 @@ and type_function ?in_function loc attrs env (expected_mode : expected_mode)
     | _ -> false
   in
   let ty_expected' = instance ty_expected in
-  let { ty_arg; arg_mode; arg_sort; ty_ret; ret_mode } =
+  let { ty_arg; arg_mode; arg_sort; ty_ret; ret_mode; ret_sort } =
     let force_tpoly =
       (* If [has_poly] is true then we rely on the later call to
          type_pat to enforce the invariant that the parameter type
@@ -5872,7 +5878,7 @@ and type_function ?in_function loc attrs env (expected_mode : expected_mode)
     exp_desc =
       Texp_function
         { arg_label; param; cases; partial; region; curry; warnings;
-          arg_mode; arg_sort; alloc_mode };
+          arg_mode; arg_sort; alloc_mode; ret_sort };
     exp_loc = loc; exp_extra = [];
     exp_type =
       instance (newgenty (Tarrow((arg_label,arg_mode,ret_mode),
@@ -6355,6 +6361,7 @@ and type_argument ?explanation ?recarg env (mode : expected_mode) sarg
       let eta_mode = Value_mode.local_to_regional (Value_mode.of_alloc marg) in
       let eta_pat, eta_var = var_pair ~mode:eta_mode "eta" ty_arg in
       let arg_sort = type_sort_exn env ~why:Function_argument ty_arg in
+      let ret_sort = type_sort_exn env ~why:Function_argument ty_res in
       let func texp =
         let ret_mode = Value_mode.of_alloc mret in
         let e =
@@ -6374,7 +6381,8 @@ and type_argument ?explanation ?recarg env (mode : expected_mode) sarg
             exp_desc = Texp_function { arg_label = Nolabel; param; cases;
                                        partial = Total; region = false; curry;
                                        warnings = Warnings.backup ();
-                                       arg_mode = marg; arg_sort; alloc_mode } }
+                                       arg_mode = marg; arg_sort; ret_sort;
+                                       alloc_mode } }
       in
       Location.prerr_warning texp.exp_loc
         (Warnings.Eliminated_optional_arguments
@@ -7253,7 +7261,10 @@ and type_andops env sarg sands expected_ty =
         let op_type = op_desc.val_type in
         let ty_arg = newvar (Layout.of_new_sort_var ~why:Function_argument) in
         let ty_rest = newvar (Layout.of_new_sort_var ~why:Function_argument) in
-        let ty_result = newvar (Layout.of_new_sort_var ~why:Function_result) in
+        let op_result_sort = Sort.new_var () in
+        let ty_result =
+          newvar (Layout.of_sort ~why:Function_result op_result_sort)
+        in
         let arrow_desc = (Nolabel,Alloc_mode.global,Alloc_mode.global) in
         let ty_rest_fun =
           newty (Tarrow(arrow_desc, newmono ty_arg, ty_result, commu_ok))
@@ -7286,6 +7297,7 @@ and type_andops env sarg sands expected_ty =
             bop_op_path = op_path;
             bop_op_val = op_desc;
             bop_op_type = op_type;
+            bop_op_return_sort = op_result_sort;
             bop_exp = exp;
             bop_loc = loc }
         in

ocaml/typing/typedtree.ml
@@ -113,7 +113,8 @@ and expression_desc =
       region : bool; curry : fun_curry_state;
       warnings : Warnings.state;
       arg_mode : Types.alloc_mode;
-      arg_sort : Layouts.sort;
+      arg_sort : sort;
+      ret_sort : sort;
       alloc_mode : Types.alloc_mode }
   | Texp_apply of expression * (arg_label * apply_arg) list * apply_position * Types.alloc_mode
   | Texp_match of expression * sort * computation case list * partial
@@ -148,7 +149,7 @@ and expression_desc =
       for_to   : expression;
       for_dir  : direction_flag;
       for_body : expression;
-      for_body_layout : Layouts.layout;
+      for_body_layout : layout;
     }
   | Texp_send of expression * meth * apply_position * Types.alloc_mode
   | Texp_new of
@@ -168,8 +169,9 @@ and expression_desc =
       let_ : binding_op;
       ands : binding_op list;
       param : Ident.t;
-      param_sort : Layouts.sort;
+      param_sort : sort;
       body : value case;
+      body_sort : sort;
       partial : partial;
       warnings : Warnings.state;
     }
@@ -231,6 +233,7 @@ and binding_op =
     bop_op_name : string loc;
     bop_op_val : Types.value_description;
     bop_op_type : Types.type_expr;
+    bop_op_return_sort : sort;
     bop_exp : expression;
     bop_loc : Location.t;
   }
@@ -243,9 +246,9 @@ and omitted_parameter =
   { mode_closure : alloc_mode;
     mode_arg : alloc_mode;
     mode_ret : alloc_mode;
-    sort_arg : Layouts.sort }
+    sort_arg : sort }
 
-and apply_arg = (expression * Layouts.sort, omitted_parameter) arg_or_omitted
+and apply_arg = (expression * sort, omitted_parameter) arg_or_omitted
 
 and apply_position =
   | Tail

ocaml/typing/typedtree.mli
@@ -215,6 +215,7 @@ and expression_desc =
       warnings : Warnings.state;
       arg_mode : Types.alloc_mode;
       arg_sort : Layouts.sort;
+      ret_sort : Layouts.sort;
       alloc_mode : Types.alloc_mode}
         (** [Pexp_fun] and [Pexp_function] both translate to [Texp_function].
             See {!Parsetree} for more details.
@@ -346,6 +347,7 @@ and expression_desc =
       param : Ident.t;
       param_sort : Layouts.sort;
       body : value case;
+      body_sort : Layouts.sort;
       partial : partial;
       warnings : Warnings.state;
     }
@@ -418,6 +420,7 @@ and binding_op =
     bop_op_type : Types.type_expr;
     (* This is the type at which the operator was used.
        It is always an instance of [bop_op_val.val_type] *)
+    bop_op_return_sort : Layouts.sort;
     bop_exp : expression;
     bop_loc : Location.t;
   }