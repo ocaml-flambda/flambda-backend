36175645aba939f766f0f3d490380b1ecfcb4697
Greta Yorsh <gyorsh@janestreet.com>
2023-06-21 10:51:08 +0100

    tmp

--- tests/backend/checkmach/t6.ml
+++ tests/backend/checkmach/t6.ml
@@ -12,16 +12,16 @@ let[@zero_alloc strict] pass3 x y = x + y
 let[@zero_alloc off] test1 x = (x, x)
 
 (* assume still works *)
-let[@zero_alloc assume][@inline never][@specialise never] fail_loud x = [ x; x+1 ]
+let[@zero_alloc assume][@inline never][@specialise never][@local never] fail_loud x = [ x; x+1 ]
 
 let call_loud x = fail_loud (x*2)
 
 (* assume strict still works *)
-let[@inline never][@specialise never][@zero_alloc strict assume] fail_loud2 x = raise (Exn (x,x))
+let[@inline never][@specialise never][@local never][@zero_alloc strict assume] fail_loud2 x = raise (Exn (x,x))
 
 let[@zero_alloc strict] call_loud2 x = fail_loud2 (x+1)
 
 (* For duplicate attributes we get a warning and the first one takes effect. *)
-let[@inline never][@specialise never][@zero_alloc strict assume][@zero_alloc off] fail_loud1 x = raise (Exn (x,x))
+let[@inline never][@specialise never][@zero_alloc strict assume][@zero_alloc off][@local never] fail_loud1 x = raise (Exn (x,x))
 
 let[@zero_alloc strict] call_loud1 x = fail_loud1 (x+1)
