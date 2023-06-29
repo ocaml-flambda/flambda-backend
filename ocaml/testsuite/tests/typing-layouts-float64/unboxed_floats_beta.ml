202f2799104c33f9d28b9f5a9c00a5ccc280e044
Chris Casinghino <ccasinghino@janestreet.com>
2023-06-28 23:30:25 -0400

    Add some [@inline always]s, and correct a test flag

--- ocaml/testsuite/tests/typing-layouts-float64/unboxed_floats_beta.ml
+++ ocaml/testsuite/tests/typing-layouts-float64/unboxed_floats_beta.ml
@@ -36,12 +36,12 @@ module Float_u : Float_u = struct
   (* We may in the future add primitives for these, but for now this has proven
      to be good enough - the boxing/unboxing is eliminated on all
      middle-ends. *)
-  let ( + ) x y = of_float ((to_float x) +. (to_float y))
-  let ( - ) x y = of_float ((to_float x) -. (to_float y))
-  let ( * ) x y = of_float ((to_float x) *. (to_float y))
-  let ( / ) x y = of_float ((to_float x) /. (to_float y))
-  let ( ** ) x y = of_float ((to_float x) ** (to_float y))
-  let ( > ) x y = (to_float x) > (to_float y)
+  let[@inline always] ( + ) x y = of_float ((to_float x) +. (to_float y))
+  let[@inline always] ( - ) x y = of_float ((to_float x) -. (to_float y))
+  let[@inline always] ( * ) x y = of_float ((to_float x) *. (to_float y))
+  let[@inline always] ( / ) x y = of_float ((to_float x) /. (to_float y))
+  let[@inline always] ( ** ) x y = of_float ((to_float x) ** (to_float y))
+  let[@inline always] ( > ) x y = (to_float x) > (to_float y)
 end
 
 (* CR layouts v2: move this into a stand-alone [Float_u] module *)
