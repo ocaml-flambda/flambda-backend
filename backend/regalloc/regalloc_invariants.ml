6649ebb9652d4ffc8093f29281d47e6e0034ccf9
TheNumbat <max@thenumb.at>
2023-07-12 12:21:12 -0400

    format

--- backend/regalloc/regalloc_invariants.ml
+++ backend/regalloc/regalloc_invariants.ml
@@ -147,7 +147,9 @@ let postcondition_layout : Cfg_with_layout.t -> unit =
       unit =
     match reg.Reg.loc with
     | Reg phys_reg -> (
-      try let _ : Reg.t = Proc.phys_reg reg.typ phys_reg in ()
+      try
+        let (_ : Reg.t) = Proc.phys_reg reg.typ phys_reg in
+        ()
       with Invalid_argument _ ->
         fatal
           "instruction %d assigned %a to register %i, which has an \
