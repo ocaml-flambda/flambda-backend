117c11647dd4785d56b1660d2a861f0dcb100506
Vivian <vivianyyd@gmail.com>
2023-07-18 15:20:46 -0400

    Test for fn defns

--- ocaml/testsuite/tests/typing-implicit-source-positions/fn_type_decl.ml
+++ ocaml/testsuite/tests/typing-implicit-source-positions/fn_type_decl.ml
@@ -2,8 +2,35 @@
    * expect
 *)
 
-type f = (src_pos:[%src_pos] -> unit) -> unit
+type t = src_pos:[%src_pos] -> unit -> unit
 
 [%%expect {|
-type f = (src_pos:lexing_position -> unit) -> unit
+type t = src_pos:lexing_position -> unit -> unit
+|}]
+
+(*  TODO: Desired behavior below  *)
+
+(* This fails because the type expression thinks t has a Labeled argument,
+   while the definition type includes that it's a Position label *)
+
+(* let f : t = fun ~(src_pos:[%src_pos]) () -> ()
+
+[%%expect{|
+val f : src_pos:lexing_position -> unit -> unit = <fun>
+|}] *)
+
+(*  The best we can do for now:  *)
+
+let f = fun ~(src_pos:[%src_pos]) () -> ()
+
+[%%expect{|
+val f : src_pos:lexing_position -> unit -> unit = <fun>
+|}]
+
+(*  end  *)
+
+let g ~(src_pos:[%src_pos]) () = ()
+
+[%%expect{|
+val g : src_pos:lexing_position -> unit -> unit = <fun>
 |}]
