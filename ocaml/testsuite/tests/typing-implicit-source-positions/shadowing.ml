5f44257227bcd4c18813ec6c89245d38baaf6bbf
Vivian Ding <75636819+vivianyyd@users.noreply.github.com>
2023-07-19 14:33:36 -0400

    Delete newline

--- ocaml/testsuite/tests/typing-implicit-source-positions/shadowing.ml
+++ ocaml/testsuite/tests/typing-implicit-source-positions/shadowing.ml
@@ -16,7 +16,6 @@ val f : src_pos:lexing_position/2 -> unit -> unit = <fun>
 |}]
 
 let _ = f ~src_pos:{pos_fname="hello" ; pos_lnum=1; pos_bol=2; pos_cnum=3} () ;;
-
 [%%expect{|
 - : unit = ()
 |}]
