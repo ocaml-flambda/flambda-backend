0eb85215a382f426fd4e52aa155f97a2aef3590a
TheNumbat <max@thenumb.at>
2023-07-20 13:55:45 -0400

    upstream build

--- ocaml/middle_end/printclambda.ml
+++ ocaml/middle_end/printclambda.ml
@@ -38,7 +38,7 @@ let rec value_kind0 ppf kind =
   | Pboxedintval Pint32 -> Format.pp_print_string ppf ":int32"
   | Pboxedintval Pint64 -> Format.pp_print_string ppf ":int64"
   | Pboxedvectorval (Pvec128 ty) ->
-    Format.pp_print_string ppf (":" ^ (Primitive.vec128_name ty))
+    Format.pp_print_string ppf (":" ^ (Lambda.vec128_name ty))
   | Pvariant { consts; non_consts } ->
     Format.fprintf ppf "@[<hov 1>[(consts (%a))@ (non_consts (%a))]@]"
       (Format.pp_print_list ~pp_sep:Format.pp_print_space Format.pp_print_int)
@@ -62,7 +62,8 @@ let layout (layout : Lambda.layout) =
   | Punboxed_int Pint32 -> ":unboxed_int32"
   | Punboxed_int Pint64 -> ":unboxed_int64"
   | Punboxed_int Pnativeint -> ":unboxed_nativeint"
-  | Punboxed_vector (Pvec128 ty) -> ":unboxed_" ^ (Primitive.vec128_name ty)
+  | Punboxed_vector (Pvec128 ty) ->
+    Format.sprintf ":unboxed_%s" (Lambda.vec128_name ty)
 
 let rec structured_constant ppf = function
   | Uconst_float x -> fprintf ppf "%F" x
