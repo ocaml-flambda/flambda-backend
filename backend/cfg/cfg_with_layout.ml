afb4aba4de6a4326c7803c5731d45363ff5a9af8
Nathanaëlle Courant <nathanaelle.courant@ocamlpro.com>
2023-07-20 11:38:59 +0200

    Merge into other as well

--- backend/cfg/cfg_with_layout.ml
+++ backend/cfg/cfg_with_layout.ml
@@ -85,7 +85,11 @@ let get_section t label = Hashtbl.find_opt t.sections label
 
 let merge_sections t label other =
   match get_section t other with
-  | None | Some "" -> ()
+  | None | Some "" -> (
+      match get_section t label with
+      | None -> ()
+      | Some current -> Hashtbl.replace t.sections other current
+    )
   | Some other -> (
     let current = get_section t label in
     match current with
