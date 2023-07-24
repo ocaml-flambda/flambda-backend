c05055f8013eba65a469a21fe554f05d9b17ef25
Vivian <vivianyyd@gmail.com>
2023-07-24 12:49:54 -0400

    Update tests

--- ocaml/testsuite/tests/typing-implicit-source-positions/shadowing.ml
+++ ocaml/testsuite/tests/typing-implicit-source-positions/shadowing.ml
@@ -12,16 +12,16 @@ type lexing_position = int
 (* src_pos works *)
 let f ~(src_pos:[%src_pos]) () = ();;
 [%%expect{|
-val f : src_pos:lexing_position/2 -> unit -> unit = <fun>
+val f : src_pos:[%src_pos] -> unit -> unit = <fun>
 |}]
 
-let _ = f ~src_pos:{pos_fname="hello" ; pos_lnum=1; pos_bol=2; pos_cnum=3} () ;;
+let _ = f ~src_pos:Lexing.dummy_pos () ;;
 [%%expect{|
 - : unit = ()
 |}]
 
 (* new type works *)
-let h (x:lexing_position) = x ;;
+let h (x : lexing_position) = x ;;
 [%%expect{|
 val h : lexing_position -> lexing_position = <fun>
 |}]
