3a192ca2e12c3a4a05d8ac8b9f3573c6b777a76f
Vivian <vivianyyd@gmail.com>
2023-07-24 05:17:23 -0400

    Store hard-coded string in variable

--- ocaml/parsing/pprintast.ml
+++ ocaml/parsing/pprintast.ml
@@ -297,6 +297,7 @@ let tyvar ppf s =
 
 let tyvar_loc f str = tyvar f str.txt
 let string_quot f x = pp f "`%s" x
+let src_pos_extension_node = "[%src_pos]"
 
 let maybe_local_type pty ctxt f c =
   let cattrs, is_local = check_local_attr c.ptyp_attributes in
@@ -317,7 +318,7 @@ and type_with_label ctxt f (label, c) =
   | Nolabel    -> maybe_local_type core_type1 ctxt f c (* otherwise parenthesize *)
   | Labelled s -> pp f "%s:%a" s (maybe_local_type core_type1 ctxt) c
   | Optional s -> pp f "?%s:%a" s (maybe_local_type core_type1 ctxt) c
-  | Position s -> pp f "%s:%s" s "[%src_pos]"
+  | Position s -> pp f "%s:%s" s src_pos_extension_node
 
 and core_type ctxt f x =
   match Jane_syntax.Core_type.of_ast x with
@@ -611,7 +612,7 @@ and label_exp ctxt f (l,opt,p) =
         else
           pp f "~%s" l
     | _ ->  pp f "~%s:%a" l (maybe_local_pat ctxt is_local) p)
-  | Position l -> pp f "~%s:%s" l "[%src_pos]"
+  | Position l -> pp f "~%s:%s" l src_pos_extension_node
 
 and sugar_expr ctxt f e =
   if e.pexp_attributes <> [] then false
@@ -1836,7 +1837,7 @@ and label_x_expression_param ctxt f (l,e) =
       else
         pp f "~%s:%a" lbl (simple_expr ctxt) e
   | Position lbl ->
-      pp f "~%s:%s" lbl "[%src_pos]"
+      pp f "~%s:%s" lbl src_pos_extension_node
 
 and directive_argument f x =
   match x.pdira_desc with
