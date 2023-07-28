f082b1c1aa9fefa77f3afc073f8c63bbf19edf3a
Rajeev Godse <rajgodse@gmail.com>
2023-07-28 11:01:17 -0400

    better split and document tests

--- ocaml/testsuite/tests/pattern-guards/test.ml
+++ ocaml/testsuite/tests/pattern-guards/test.ml
@@ -4,16 +4,19 @@
 (* CR-soon rgodse: We expect this output to change soon, but for now it shows
    that parsing and typechecking work (as the compiler fails at translation). *)
 
+(* Test basic usage of pattern guards. *)
 let basic_usage ~f ~default x =
-  match x with 
+  match x with
     | Some x when f x match Some y -> y
     | _ -> default
 ;;
-
-let f x = if x > 0 then Some (x - 1) else None;;
 [%%expect{|
 val basic_usage : f:('a -> 'b option) -> default:'b -> 'a option -> 'b =
   <fun>
+|}];;
+
+let f x = if x > 0 then Some (x - 1) else None;;
+[%%expect{|
 val f : int -> int option = <fun>
 |}];;
 
@@ -34,6 +37,7 @@ basic_usage ~f ~default:~-1 None;;
 - : int = -1
 |}];;
 
+(* Demonstrate parsing of sequences in boolean predicates. *)
 let seq_predicate x ~f ~g ~default =
   match x with
     | Some x when f x; g x -> x
@@ -44,21 +48,28 @@ val seq_predicate :
   'a option -> f:('a -> 'b) -> g:('a -> bool) -> default:'a -> 'a = <fun>
 |}];;
 
+(* Demonstrate semantics of sequences in pattern guard scrutinees. *)
 let seq_pattern x ~f ~g ~default =
   match x with
     | Some x when (f x; g x) match Some y -> y
     | _ -> default
 ;;
-
-let counter = ref 0;;
-let f () = incr counter;;
-let g () = if !counter > 1 then Some (!counter - 1) else None;;
 [%%expect{|
 val seq_pattern :
   'a option -> f:('a -> 'b) -> g:('a -> 'c option) -> default:'c -> 'c =
   <fun>
+|}];;
+
+let counter = ref 0;;
+[%%expect{|
 val counter : int ref = {contents = 0}
+|}];;
+let f () = incr counter;;
+[%%expect{|
 val f : unit -> unit = <fun>
+|}];;
+let g () = if !counter > 1 then Some (!counter - 1) else None;;
+[%%expect{|
 val g : unit -> int option = <fun>
 |}];;
 
@@ -73,7 +84,7 @@ seq_pattern (Some ()) ~f ~g ~default:0;;
 seq_pattern None ~f ~g ~default:0;;
 [%%expect{|
 - : int = 0
-|}]
+|}];;
 
 let complex_types (x : int list option) : bool =
   let strs_opt = match x with
@@ -87,8 +98,9 @@ let complex_types (x : int list option) : bool =
 ;;
 [%%expect {|
 val complex_types : int list option -> bool = <fun>
-|}]
+|}];;
 
+(* Check typing of pattern guard patterns. *)
 let ill_typed_pattern (x : int list option) : bool =
   match x with
     | Some [ y ] when y match None -> true
@@ -128,6 +140,34 @@ typing_no_value_clauses f (Some 5);;
 - : (int, exn) result = Ok 20
 |}];;
 
+let typing_no_value_clauses f x =
+  match x with
+    | Some x when f x match exception e -> Error e
+    | Some x -> Ok (f x)
+    | None -> Error (Failure "x is None")
+;;
+
+let f x = 100 / x;;
+[%%expect{|
+val typing_no_value_clauses : ('a -> 'b) -> 'a option -> ('b, exn) result =
+  <fun>
+val f : int -> int = <fun>
+|}];;
+
+typing_no_value_clauses f None;;
+[%%expect{|
+- : (int, exn) result = Error (Failure "x is None")
+|}];;
+typing_no_value_clauses f (Some 0);;
+[%%expect{|
+- : (int, exn) result = Error Division_by_zero
+|}];;
+typing_no_value_clauses f (Some 5);;
+[%%expect{|
+- : (int, exn) result = Ok 20
+|}];;
+
+(* Check typing of vars bound in pattern guard patterns. *)
 let ill_typed_pattern_var (x : int list option) : bool =
   match x with
     | Some xs when xs match [ y ] -> String.equal y "foo"
@@ -138,8 +178,9 @@ Line 3, characters 50-51:
                                                       ^
 Error: This expression has type int but an expression was expected of type
          String.t = string
-|}]
+|}];;
 
+(* Check that pattern guards can shadow and use outer variables correctly. *)
 let shadow_outer_variables (n : int option) (strs : string list) =
   match n with
   | Some n when List.nth_opt strs n match Some s -> s
@@ -162,7 +203,12 @@ shadow_outer_variables (Some 2) [ "foo"; "bar" ];;
 - : string = "Not found"
 |}]
 
+(* Test inward propagation of GADT type information. *)
+
 type ('a, 'b) eq = Eq : ('a, 'a) eq
+[%%expect{|
+type ('a, 'b) eq = Eq : ('a, 'a) eq
+|}];;
 
 let in_pattern_guard (type a b) (eq : (a, b) eq) (compare : a -> a -> int)
                      (x : a) (y : b) =
@@ -170,7 +216,6 @@ let in_pattern_guard (type a b) (eq : (a, b) eq) (compare : a -> a -> int)
     | Eq when compare x y match 0 -> true
     | _ -> false
 [%%expect{|
-type ('a, 'b) eq = Eq : ('a, 'a) eq
 val in_pattern_guard : ('a, 'b) eq -> ('a -> 'a -> int) -> 'a -> 'b -> bool =
   <fun>
 |}]
@@ -186,7 +231,11 @@ val from_pattern_guard :
 |}]
 
 type void = |
+[%%expect{|
+type void = |
+|}];;
 
+(* Ensure that warning 73 is appropriately issued. *)
 let exhaustive_pattern_guards (x : (unit, void option) Either.t) : int =
   match x with
     | Left u when u match () -> 0
@@ -244,14 +293,6 @@ module M : sig
   val find : int -> 'a t -> 'a
   val find_opt : int -> 'a t -> 'a option
 end = Map.Make (Int);;
-
-let say_hello (id : int option) (name_map : string M.t) =
-  match id with
-    | Some id when M.find_opt id name_map match Some name -> "Hello, " ^ name
-    | None | Some _ -> "Hello, stranger"
-;;
-
-let name_map = M.empty |> M.add 0 "Fred" |> M.add 4 "Barney";;
 [%%expect{|
 module M :
   sig
@@ -261,7 +302,20 @@ module M :
     val find : int -> 'a t -> 'a
     val find_opt : int -> 'a t -> 'a option
   end
+|}];;
+
+(* Correctness test for pattern guard matching and failure. *)
+let say_hello (id : int option) (name_map : string M.t) =
+  match id with
+    | Some id when M.find_opt id name_map match Some name -> "Hello, " ^ name
+    | None | Some _ -> "Hello, stranger"
+;;
+[%%expect{|
 val say_hello : int option -> string M.t -> string = <fun>
+|}];;
+
+let name_map = M.empty |> M.add 0 "Fred" |> M.add 4 "Barney";;
+[%%expect{|
 val name_map : string M.t = <abstr>
 |}];;
 
@@ -282,6 +336,8 @@ say_hello None name_map;;
 - : string = "Hello, stranger"
 |}]
 
+(* Correctness test for pattern guards with mixed value and exception
+   patterns. *)
 let say_hello_catching_exns id name_map =
   match id with
     | Some id when M.find id name_map match "Barney" | exception _ ->
@@ -309,13 +365,13 @@ say_hello_catching_exns None name_map;;
 - : string = "Hello, Fred"
 |}]
 
+(* Ensure that Match_failure is raised when all cases, including pattern-guarded
+   ones, fail to match. *)
 let patch_to_match_failure f default x =
   match x with
   | None -> default
   | Some x when f x match Some y -> y
 ;;
-
-let exact_half n = if n mod 2 = 0 then Some (n / 2) else None;;
 [%%expect{|
 Lines 2-4, characters 2-37:
 2 | ..match x with
@@ -327,6 +383,10 @@ Some _
 (However, some guarded clause may match this value.)
 val patch_to_match_failure : ('a -> 'b option) -> 'b -> 'a option -> 'b =
   <fun>
+|}];;
+
+let exact_half n = if n mod 2 = 0 then Some (n / 2) else None;;
+[%%expect{|
 val exact_half : int -> int option = <fun>
 |}];;
 
@@ -343,6 +403,8 @@ patch_to_match_failure exact_half ~-1 (Some 41);;
 Exception: Match_failure ("", 2, 2).
 |}];;
 
+(* Ensure that warning 8 is issued appropriately in the presence of pattern
+   guards. *)
 let warn_partial = function
   | [] -> 0
   | xs when List.hd xs match Some y -> y
@@ -359,6 +421,7 @@ _::_
 val warn_partial : int option list -> int = <fun>
 |}];;
 
+(* Ensure that warning 57 is appropriately issued for pattern guards. *)
 let warn_ambiguous = function
   | ([ x ], _) | (_, [ x ]) when (let one = 1 in Int.abs x + one) match 2 -> 1
   | _ -> 0
@@ -372,5 +435,5 @@ variable x appears in different places in different or-pattern alternatives.
 Only the first match will be used to evaluate the guard expression.
 (See manual section 11.5)
 val warn_ambiguous : int list * int list -> int = <fun>
-|}]
+|}];;
 
