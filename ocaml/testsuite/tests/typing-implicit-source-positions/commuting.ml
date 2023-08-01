88c5e6108fe21ec7247d15f99b1c8bfa778113d2
Vivian <vivianyyd@gmail.com>
2023-08-01 15:55:14 -0400

    Add commuting tests

--- ocaml/testsuite/tests/typing-implicit-source-positions/commuting.ml
+++ ocaml/testsuite/tests/typing-implicit-source-positions/commuting.ml
@@ -2,51 +2,95 @@
    * expect
 *)
 
-let f = fun ~(a:[%src_pos]) ~(b:[%src_pos]) () -> ()
+let pos_a : lexing_position = {Lexing.dummy_pos with pos_fname = "a"};;
+let pos_b : lexing_position = {Lexing.dummy_pos with pos_fname = "b"};;
 [%%expect{|
-val f : a:[%src_pos] -> b:[%src_pos] -> unit -> unit = <fun>
+val pos_a : lexing_position =
+  {pos_fname = "a"; pos_lnum = 0; pos_bol = 0; pos_cnum = -1}
+val pos_b : lexing_position =
+  {pos_fname = "b"; pos_lnum = 0; pos_bol = 0; pos_cnum = -1}
 |}]
 
-let _ = f ~b:Lexing.dummy_pos ~a:Lexing.dummy_pos () ;;
+let f = fun ~(a:[%src_pos]) ~(b:[%src_pos]) () -> a, b
 [%%expect{|
-- : unit = ()
+val f :
+  a:[%src_pos] -> b:[%src_pos] -> unit -> lexing_position * lexing_position =
+  <fun>
 |}]
 
-let g = fun ~(a:[%src_pos]) ?(c = 0) ~(b:[%src_pos]) () -> ()
+let _ = f ~b:pos_b ~a:pos_a () ;;
 [%%expect{|
-val g : a:[%src_pos] -> ?c:int -> b:[%src_pos] -> unit -> unit = <fun>
+- : lexing_position * lexing_position =
+({pos_fname = "a"; pos_lnum = 0; pos_bol = 0; pos_cnum = -1},
+ {pos_fname = "b"; pos_lnum = 0; pos_bol = 0; pos_cnum = -1})
 |}]
 
-let _ = g ~b:Lexing.dummy_pos ~a:Lexing.dummy_pos () ;;
-[%%expect{|
-- : unit = ()
+(* Partial application *)
+let x = f ~b:pos_b ;;
+let y = x ~a:pos_a ;;
+let z = y () ;;
+[%%expect {|
+val x : a:[%src_pos] -> unit -> lexing_position * lexing_position = <fun>
+val y : unit -> lexing_position * lexing_position = <fun>
+val z : lexing_position * lexing_position =
+  ({pos_fname = "a"; pos_lnum = 0; pos_bol = 0; pos_cnum = -1},
+   {pos_fname = "b"; pos_lnum = 0; pos_bol = 0; pos_cnum = -1})
 |}]
 
-let h = fun ~(a:[%src_pos]) ~(b:int) -> ()
+let g = fun ~(a:[%src_pos]) ?(c = 0) ~(b:[%src_pos]) () -> a, b, c
 [%%expect{|
-val h : a:[%src_pos] -> b:int -> unit = <fun>
+val g :
+  a:[%src_pos] ->
+  ?c:int -> b:[%src_pos] -> unit -> lexing_position * lexing_position * int =
+  <fun>
 |}]
 
-let _ = h ~a:Lexing.dummy_pos ~b:0 ;;
+let _ = g ~b:pos_b ~a:pos_a () ;;
 [%%expect{|
-- : unit = ()
+- : lexing_position * lexing_position * int =
+({pos_fname = "a"; pos_lnum = 0; pos_bol = 0; pos_cnum = -1},
+ {pos_fname = "b"; pos_lnum = 0; pos_bol = 0; pos_cnum = -1}, 0)
 |}]
 
-let k = fun ~(a:[%src_pos]) ~(a:int) -> ()
+let h = fun ~(a:[%src_pos]) ~(b:int) () -> a, b
 [%%expect{|
-val k : a:[%src_pos] -> a:int -> unit = <fun>
+val h : a:[%src_pos] -> b:int -> unit -> lexing_position * int = <fun>
 |}]
 
-let _ = k ~a:Lexing.dummy_pos ~a:0 ;;
+let _ = h ~b:0 ~a:pos_a ();;
 [%%expect{|
-- : unit = ()
+- : lexing_position * int =
+({pos_fname = "a"; pos_lnum = 0; pos_bol = 0; pos_cnum = -1}, 0)
 |}]
 
-let _ = k ~a:0 ~a:Lexing.dummy_pos ;;
+let k = fun ~(a:int) ~(a:[%src_pos])() -> a
 [%%expect{|
-Line 1, characters 13-14:
-1 | let _ = k ~a:0 ~a:Lexing.dummy_pos ;;
-                 ^
-Error: This expression has type int but an expression was expected of type
-         lexing_position
+val k : a:int -> a:[%src_pos] -> unit -> lexing_position = <fun>
+|}]
+
+let _ = k ~a:Lexing.dummy_pos ~a:0 ();;
+[%%expect{|
+Line 1, characters 13-29:
+1 | let _ = k ~a:Lexing.dummy_pos ~a:0 ();;
+                 ^^^^^^^^^^^^^^^^
+Error: This expression has type Lexing.position = lexing_position
+       but an expression was expected of type int
+|}]
+
+let _ = k ~a:0 ~a:Lexing.dummy_pos ();;
+[%%expect{|
+- : Lexing.position =
+{Lexing.pos_fname = ""; pos_lnum = 0; pos_bol = 0; pos_cnum = -1}
+|}]
+
+(* Labels on source positions can't commute in definitions *)
+let m : a:[%src_pos] -> b:[%src_pos] -> unit -> unit = fun ~(b:[%src_pos]) ~(a:[%src_pos]) () -> ()
+[%%expect{|
+Line 1, characters 55-99:
+1 | let m : a:[%src_pos] -> b:[%src_pos] -> unit -> unit = fun ~(b:[%src_pos]) ~(a:[%src_pos]) () -> ()
+                                                           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+Error: This function should have type
+         a:[%src_pos] -> b:[%src_pos] -> unit -> unit
+       but its first argument is labeled ~b:[%src_pos]
+       instead of ~a:[%src_pos]
 |}]
