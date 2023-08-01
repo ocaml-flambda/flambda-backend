7f8cf2301d961540e7a241d307a6e1a2e4a8b23b
TheNumbat <max@thenumb.at>
2023-08-01 12:01:08 -0400

    remove arg duping

--- backend/amd64/simd_selection.ml
+++ backend/amd64/simd_selection.ml
@@ -30,8 +30,6 @@ type register_behavior =
   | String_length_mask
   | String_no_length_mask
 
-let arg i args = List.nth args i
-
 (* Assumes untagged int *)
 let extract_constant args low high name =
   match args with
@@ -139,18 +137,12 @@ let select_operation_sse2 op args dbg =
   | "caml_sse2_float64x2_cmp" ->
     let i, args = extract_constant args 0 0x1f "caml_sse2_float64x2_cmp" in
     Some (Cmp_f64 (float_condition_of_int i), args)
-  | "caml_sse2_cvt_int32x4_float64x2" ->
-    Some (I32_to_f64, [arg 0 args; arg 0 args])
-  | "caml_sse2_cvt_int32x4_float32x4" ->
-    Some (I32_to_f32, [arg 0 args; arg 0 args])
-  | "caml_sse2_cvt_float64x2_int32x4" ->
-    Some (F64_to_i32, [arg 0 args; arg 0 args])
-  | "caml_sse2_cvt_float64x2_float32x4" ->
-    Some (F64_to_f32, [arg 0 args; arg 0 args])
-  | "caml_sse2_cvt_float32x4_int32x4" ->
-    Some (F32_to_i32, [arg 0 args; arg 0 args])
-  | "caml_sse2_cvt_float32x4_float64x2" ->
-    Some (F32_to_f64, [arg 0 args; arg 0 args])
+  | "caml_sse2_cvt_int32x4_float64x2" -> Some (I32_to_f64, args)
+  | "caml_sse2_cvt_int32x4_float32x4" -> Some (I32_to_f32, args)
+  | "caml_sse2_cvt_float64x2_int32x4" -> Some (F64_to_i32, args)
+  | "caml_sse2_cvt_float64x2_float32x4" -> Some (F64_to_f32, args)
+  | "caml_sse2_cvt_float32x4_int32x4" -> Some (F32_to_i32, args)
+  | "caml_sse2_cvt_float32x4_float64x2" -> Some (F32_to_f64, args)
   | "caml_sse2_cvt_int16x8_int8x16_saturating" -> Some (I16_to_i8, args)
   | "caml_sse2_cvt_int32x4_int16x8_saturating" -> Some (I32_to_i16, args)
   | "caml_sse2_cvt_int16x8_int8x16_saturating_unsigned" -> Some (I16_to_u8, args)
@@ -198,12 +190,12 @@ let select_operation_sse2 op args dbg =
     let i, args =
       extract_constant args 0 255 "caml_sse2_vec128_shuffle_high_16"
     in
-    Some (Shuffle_high_16 i, [arg 0 args; arg 0 args])
+    Some (Shuffle_high_16 i, args)
   | "caml_sse2_vec128_shuffle_low_16" ->
     let i, args =
       extract_constant args 0 255 "caml_sse2_vec128_shuffle_low_16"
     in
-    Some (Shuffle_low_16 i, [arg 0 args; arg 0 args])
+    Some (Shuffle_low_16 i, args)
   | "caml_sse2_vec128_interleave_high_8" -> Some (Interleave_high_8, args)
   | "caml_sse2_vec128_interleave_low_8" -> Some (Interleave_low_8, args)
   | "caml_sse2_vec128_interleave_high_16" -> Some (Interleave_high_16, args)
@@ -223,12 +215,9 @@ let select_operation_sse3 op args dbg =
     | "caml_sse3_float64x2_hadd" -> Some (Hadd_f64, args)
     | "caml_sse3_float32x4_hsub" -> Some (Hsub_f32, args)
     | "caml_sse3_float64x2_hsub" -> Some (Hsub_f64, args)
-    | "caml_sse3_vec128_dup_low_64" ->
-      Some (Dup_low_64, [arg 0 args; arg 0 args])
-    | "caml_sse3_vec128_dup_odd_32" ->
-      Some (Dup_odd_32, [arg 0 args; arg 0 args])
-    | "caml_sse3_vec128_dup_even_32" ->
-      Some (Dup_even_32, [arg 0 args; arg 0 args])
+    | "caml_sse3_vec128_dup_low_64" -> Some (Dup_low_64, args)
+    | "caml_sse3_vec128_dup_odd_32" -> Some (Dup_odd_32, args)
+    | "caml_sse3_vec128_dup_even_32" -> Some (Dup_even_32, args)
     | _ -> None
 
 let select_operation_ssse3 op args dbg =
@@ -236,9 +225,9 @@ let select_operation_ssse3 op args dbg =
   then None
   else
     match op with
-    | "caml_ssse3_int8x16_abs" -> Some (Abs_i8, [arg 0 args; arg 0 args])
-    | "caml_ssse3_int16x8_abs" -> Some (Abs_i16, [arg 0 args; arg 0 args])
-    | "caml_ssse3_int32x4_abs" -> Some (Abs_i32, [arg 0 args; arg 0 args])
+    | "caml_ssse3_int8x16_abs" -> Some (Abs_i8, args)
+    | "caml_ssse3_int16x8_abs" -> Some (Abs_i16, args)
+    | "caml_ssse3_int32x4_abs" -> Some (Abs_i32, args)
     | "caml_ssse3_int16x8_hadd" -> Some (Hadd_i16, args)
     | "caml_ssse3_int32x4_hadd" -> Some (Hadd_i32, args)
     | "caml_ssse3_int16x8_hadd_saturating" -> Some (Hadds_i16, args)
@@ -274,30 +263,18 @@ let select_operation_sse41 op args dbg =
     | "caml_sse41_vec128_blendv_32" -> Some (Blendv_32, args)
     | "caml_sse41_vec128_blendv_64" -> Some (Blendv_64, args)
     | "caml_sse41_int64x2_cmpeq" -> Some (Cmpeq_i64, args)
-    | "caml_sse41_cvtsx_int8x16_int16x8" ->
-      Some (I8_sx_i16, [arg 0 args; arg 0 args])
-    | "caml_sse41_cvtsx_int8x16_int32x4" ->
-      Some (I8_sx_i32, [arg 0 args; arg 0 args])
-    | "caml_sse41_cvtsx_int8x16_int64x2" ->
-      Some (I8_sx_i64, [arg 0 args; arg 0 args])
-    | "caml_sse41_cvtsx_int16x8_int32x4" ->
-      Some (I16_sx_i32, [arg 0 args; arg 0 args])
-    | "caml_sse41_cvtsx_int16x8_int64x2" ->
-      Some (I16_sx_i64, [arg 0 args; arg 0 args])
-    | "caml_sse41_cvtsx_int32x4_int64x2" ->
-      Some (I32_sx_i64, [arg 0 args; arg 0 args])
-    | "caml_sse41_cvtzx_int8x16_int16x8" ->
-      Some (U8_zx_i16, [arg 0 args; arg 0 args])
-    | "caml_sse41_cvtzx_int8x16_int32x4" ->
-      Some (U8_zx_i32, [arg 0 args; arg 0 args])
-    | "caml_sse41_cvtzx_int8x16_int64x2" ->
-      Some (U8_zx_i64, [arg 0 args; arg 0 args])
-    | "caml_sse41_cvtzx_int16x8_int32x4" ->
-      Some (U16_zx_i32, [arg 0 args; arg 0 args])
-    | "caml_sse41_cvtzx_int16x8_int64x2" ->
-      Some (U16_zx_i64, [arg 0 args; arg 0 args])
-    | "caml_sse41_cvtzx_int32x4_int64x2" ->
-      Some (U32_zx_i64, [arg 0 args; arg 0 args])
+    | "caml_sse41_cvtsx_int8x16_int16x8" -> Some (I8_sx_i16, args)
+    | "caml_sse41_cvtsx_int8x16_int32x4" -> Some (I8_sx_i32, args)
+    | "caml_sse41_cvtsx_int8x16_int64x2" -> Some (I8_sx_i64, args)
+    | "caml_sse41_cvtsx_int16x8_int32x4" -> Some (I16_sx_i32, args)
+    | "caml_sse41_cvtsx_int16x8_int64x2" -> Some (I16_sx_i64, args)
+    | "caml_sse41_cvtsx_int32x4_int64x2" -> Some (I32_sx_i64, args)
+    | "caml_sse41_cvtzx_int8x16_int16x8" -> Some (U8_zx_i16, args)
+    | "caml_sse41_cvtzx_int8x16_int32x4" -> Some (U8_zx_i32, args)
+    | "caml_sse41_cvtzx_int8x16_int64x2" -> Some (U8_zx_i64, args)
+    | "caml_sse41_cvtzx_int16x8_int32x4" -> Some (U16_zx_i32, args)
+    | "caml_sse41_cvtzx_int16x8_int64x2" -> Some (U16_zx_i64, args)
+    | "caml_sse41_cvtzx_int32x4_int64x2" -> Some (U32_zx_i64, args)
     | "caml_sse41_float32x4_dp" ->
       let i, args = extract_constant args 0 255 "caml_sse41_float32x4_dp" in
       Some (Dp_f32 i, args)
@@ -330,10 +307,10 @@ let select_operation_sse41 op args dbg =
       Some (Insert_i64 i, args)
     | "caml_sse41_float32x4_round" ->
       let i, args = extract_constant args 0 15 "caml_sse41_float32x4_round" in
-      Some (Round_f32 (float_rounding_of_int i), [arg 0 args; arg 0 args])
+      Some (Round_f32 (float_rounding_of_int i), args)
     | "caml_sse41_float64x2_round" ->
       let i, args = extract_constant args 0 15 "caml_sse41_float64x2_round" in
-      Some (Round_f64 (float_rounding_of_int i), [arg 0 args; arg 0 args])
+      Some (Round_f64 (float_rounding_of_int i), args)
     | "caml_sse41_int8x16_max" -> Some (Max_i8, args)
     | "caml_sse41_int32x4_max" -> Some (Max_i32, args)
     | "caml_sse41_int16x8_max_unsigned" -> Some (Max_u16, args)
@@ -433,39 +410,40 @@ let register_behavior_sse2 = function
   | Subs_i8 | Subs_i16 | Subs_u8 | Subs_u16 | Max_u8 | Max_i16 | Max_f64
   | Min_u8 | Min_i16 | Min_f64 | Mul_f64 | Div_f64 | And_bits | Andnot_bits
   | Or_bits | Xor_bits | Cmpeq_i8 | Cmpeq_i16 | Cmpeq_i32 | Cmpgt_i8 | Cmpgt_i16
-  | Cmpgt_i32 | Cmp_f64 _ | I32_to_f64 | I32_to_f32 | F64_to_i32 | F64_to_f32
-  | F32_to_i32 | F32_to_f64 | SLL_i16 | SLL_i32 | SLL_i64 | SRL_i16 | SRL_i32
+  | Cmpgt_i32 | Cmp_f64 _ | SLL_i16 | SLL_i32 | SLL_i64 | SRL_i16 | SRL_i32
   | SRL_i64 | SRA_i16 | SRA_i32 | Avg_u8 | Avg_u16 | SAD_u8 | Shuffle_64 _
-  | Shuffle_high_16 _ | Shuffle_low_16 _ | Interleave_high_8
-  | Interleave_high_16 | Interleave_high_64 | Interleave_low_8
-  | Interleave_low_16 | Interleave_low_64 | I16_to_i8 | I32_to_i16 | I16_to_u8
-  | I32_to_u16 ->
+  | Interleave_high_8 | Interleave_high_16 | Interleave_high_64
+  | Interleave_low_8 | Interleave_low_16 | Interleave_low_64 | I16_to_i8
+  | I32_to_i16 | I16_to_u8 | I32_to_u16 ->
     R_RM_to_fst
+  | Shuffle_high_16 _ | Shuffle_low_16 _ | I32_to_f64 | I32_to_f32 | F64_to_i32
+  | F64_to_f32 | F32_to_i32 | F32_to_f64 ->
+    RM_to_R
   | SLLi_i16 _ | SLLi_i32 _ | SLLi_i64 _ | SRLi_i16 _ | SRLi_i32 _ | SRLi_i64 _
   | SRAi_i16 _ | SRAi_i32 _ | Shift_left_bytes _ | Shift_right_bytes _ ->
     R_to_fst
   | Movemask_8 | Movemask_64 -> R_to_R
 
 let register_behavior_sse3 = function
-  | Addsub_f32 | Addsub_f64 | Hadd_f32 | Hadd_f64 | Hsub_f32 | Hsub_f64
-  | Dup_low_64 | Dup_odd_32 | Dup_even_32 ->
+  | Addsub_f32 | Addsub_f64 | Hadd_f32 | Hadd_f64 | Hsub_f32 | Hsub_f64 ->
     R_RM_to_fst
+  | Dup_low_64 | Dup_odd_32 | Dup_even_32 -> RM_to_R
 
 let register_behavior_ssse3 = function
-  | Abs_i8 | Abs_i16 | Abs_i32 | Hadd_i16 | Hadd_i32 | Hadds_i16 | Hsub_i16
-  | Hsub_i32 | Hsubs_i16 | Mulsign_i8 | Mulsign_i16 | Mulsign_i32 | Shuffle_8
-  | Alignr_i8 _ ->
+  | Hadd_i16 | Hadd_i32 | Hadds_i16 | Hsub_i16 | Hsub_i32 | Hsubs_i16
+  | Mulsign_i8 | Mulsign_i16 | Mulsign_i32 | Shuffle_8 | Alignr_i8 _ ->
     R_RM_to_fst
+  | Abs_i8 | Abs_i16 | Abs_i32 -> RM_to_R
 
 let register_behavior_sse41 = function
-  | Blend_16 _ | Blend_32 _ | Blend_64 _ | Cmpeq_i64 | I8_sx_i16 | I8_sx_i32
-  | I8_sx_i64 | I16_sx_i32 | I16_sx_i64 | I32_sx_i64 | U8_zx_i16 | U8_zx_i32
-  | U8_zx_i64 | U16_zx_i32 | U16_zx_i64 | U32_zx_i64 | Dp_f32 _ | Dp_f64 _
+  | Blend_16 _ | Blend_32 _ | Blend_64 _ | Cmpeq_i64 | Dp_f32 _ | Dp_f64 _
   | Max_i8 | Max_i32 | Max_u16 | Max_u32 | Min_i8 | Min_i32 | Min_u16 | Min_u32
-  | Round_f64 _ | Round_f32 _ | Insert_i8 _ | Insert_i16 _ | Insert_i32 _
-  | Insert_i64 _ | Multi_sad_u8 _ ->
+  | Insert_i8 _ | Insert_i16 _ | Insert_i32 _ | Insert_i64 _ | Multi_sad_u8 _ ->
     R_RM_to_fst
-  | Minpos_u16 -> RM_to_R
+  | I8_sx_i16 | I8_sx_i32 | I8_sx_i64 | I16_sx_i32 | I16_sx_i64 | I32_sx_i64
+  | U8_zx_i16 | U8_zx_i32 | U8_zx_i64 | U16_zx_i32 | U16_zx_i64 | U32_zx_i64
+  | Round_f64 _ | Round_f32 _ | Minpos_u16 ->
+    RM_to_R
   | Blendv_8 | Blendv_32 | Blendv_64 -> R_RM_XMM0_to_fst
   | Extract_i64 _ | Extract_i32 _ -> R_to_RM
   | Extract_i8 _ | Extract_i16 _ ->
