f2159a5382eb7cbe79dab9b8e03502577d7f199d
Vivian <vivianyyd@gmail.com>
2023-08-04 14:24:06 -0400

    Fix test

--- ocaml/testsuite/tests/typing-warnings/warning16.ml
+++ ocaml/testsuite/tests/typing-warnings/warning16.ml
@@ -6,7 +6,7 @@ let foo ?x = ()
 Line 1, characters 9-10:
 1 | let foo ?x = ()
              ^
-Warning 16 [unerasable-optional-argument]: this omittable argument cannot be erased.
+Warning 16 [unerasable-optional-argument]: this optional argument cannot be erased.
 val foo : ?x:'a -> unit = <fun>
 |}]
 
@@ -15,7 +15,7 @@ let foo ?x ~y = ()
 Line 1, characters 9-10:
 1 | let foo ?x ~y = ()
              ^
-Warning 16 [unerasable-optional-argument]: this omittable argument cannot be erased.
+Warning 16 [unerasable-optional-argument]: this optional argument cannot be erased.
 val foo : ?x:'a -> y:'b -> unit = <fun>
 |}]
 
@@ -34,7 +34,7 @@ class bar ?x = object end
 Line 1, characters 11-12:
 1 | class bar ?x = object end
                ^
-Warning 16 [unerasable-optional-argument]: this omittable argument cannot be erased.
+Warning 16 [unerasable-optional-argument]: this optional argument cannot be erased.
 class bar : ?x:'a -> object  end
 |}]
 
@@ -43,7 +43,7 @@ class bar ?x ~y = object end
 Line 1, characters 11-12:
 1 | class bar ?x ~y = object end
                ^
-Warning 16 [unerasable-optional-argument]: this omittable argument cannot be erased.
+Warning 16 [unerasable-optional-argument]: this optional argument cannot be erased.
 class bar : ?x:'a -> y:'b -> object  end
 |}]
 
