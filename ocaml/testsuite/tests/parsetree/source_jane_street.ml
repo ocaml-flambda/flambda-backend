00b604648112385a98c11cbd9ff56247322e7508
Nick Roberts <nroberts@janestreet.com>
2023-08-10 13:47:29 -0400

    Transplant existing pprintast tests

--- ocaml/testsuite/tests/parsetree/source_jane_street.ml
+++ ocaml/testsuite/tests/parsetree/source_jane_street.ml
@@ -45,20 +45,26 @@ let f y (type (a : immediate) (b : immediate)) = y
     when x + y > 6
 ];;
 
+
+(* User-written attributes *)
+([(x[@attr1]) for (x[@attr2]) in ([][@attr3])] [@attr4]);;
+
 (*********)
 (* Local *)
 
 (* parameters *)
-let f (local_ x) ~(local_ y) ~z:(local_ z) = x + y + z;;
+let f (local_ x) ~(local_ y) ~z:(local_ z) ?foo:(local_ w = 1) = x + y + z + w;;
 
 (* bindings *)
 let g () =
   let local_ f = () in
+  let local_ f : 'a . 'a -> 'a = fun x -> x in
   let local_ f x y = x + y in
+  let local_ f : int -> int = fun z -> z + z in
   (* nroberts: we should reenable this test when we fix
    * pprint_ast to put the (int -> int) annotation back in
    * the correct position. *)
-  (*let local_ f x y : int -> int = fun z -> x + y in*)
+  (*let local_ f x y : int -> int = fun z -> x + y + z in*)
   ();;
 
 (* expressions *)
@@ -78,16 +84,31 @@ type record =
   ; normal_field : int
   };;
 
+type 'a parameterized_record = {
+  mutable a: 'a ;
+  global_ c: 'a };;
+
 type fn = local_ int -> local_ int;;
 type nested_fn = (local_ int -> local_ int) -> local_ int;;
+type ('a, 'b) labeled_fn =
+  a:local_ 'a -> ?b:local_ b -> local_ 'a -> (int -> local_ 'b);;
 
 (*******************)
 (* Include functor *)
 
 module T = struct
   include functor F
-end
+end;;
 
 module type S = sig
   include functor F
-end
+end;;
+
+(********************)
+(* Immutable arrays *)
+
+let f x =
+  match x with
+  | [::] -> [::]
+  | (([:x:])[@attr1 ]) -> (([:x:])[@attr1 ])
+  | (([:x;y:])[@attr2 ][@attr3 ]) -> (([:x;y:])[@attr2 ][@attr3 ]);;
