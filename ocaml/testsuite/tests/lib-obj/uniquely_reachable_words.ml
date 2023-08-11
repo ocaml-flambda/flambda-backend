2309f3b70a9a36bba4ce2765b5c7391a862cd310
apilat <apilat@janestreet.com>
2023-08-11 12:05:27 +0100

    Make [tests/lib-obj/uniquely_reachable_words.ml] more robust
    Instead of relying on an exact number of words used which can differ
    slightly with the backend due to different optimizations, we deduce what
    objects are reachable bases on the sum of their sizes.

--- ocaml/testsuite/tests/lib-obj/uniquely_reachable_words.ml
+++ ocaml/testsuite/tests/lib-obj/uniquely_reachable_words.ml
@@ -10,16 +10,28 @@ let native =
 let sizes xs = Obj.uniquely_reachable_words (List.map Obj.repr xs |> Array.of_list)
   |> Array.to_list
 
-let expect_sizes xs exp_bytecode exp_native =
-  let actual = sizes xs in
-  let expected = if native then exp_native else exp_bytecode in
-  List.combine actual expected
-  |> List.iteri (fun i (a, e) ->
-    if a <> e then
-      Printf.printf "index = %i; actual = %i; expected = %i\n" i a e)
+let deduce_reachable size =
+  let reachable = ref []
+  and cur = ref 0
+  and binary = ref ((size + 50) / 100) in
+  while !binary > 0 do
+    if !binary land 1 = 1 then
+      reachable := !cur :: !reachable;
+    cur := !cur + 1;
+    binary := !binary / 2
+  done;
+  List.rev !reachable
 
-type node = { id: int; used_memory: int list; mutable children: node list }
-let make id ch = { id; used_memory = List.init (Int.shift_left 1 id) (fun _ -> id); children = ch }
+let expect_reachable roots expected :unit=
+  let actual = sizes roots in
+  List.combine (List.map deduce_reachable actual) expected
+  |> List.iter (fun (a, e) ->
+    if List.(a <> e) then
+      let string_of_arr x = List.map string_of_int x |> String.concat "," in
+      Printf.printf "actual = %s; expected = %s\n" (string_of_arr a) (string_of_arr e))
+
+type node = { id: int; used_memory: int array; mutable children: node list }
+let make id ch = { id; used_memory = Array.make (Int.shift_left 100 id) 0; children = ch }
 
 (* Note that this all needs to be in a function to ensure our nodes actually get
    allocated on the heap and are not static values in the binary (whose size we
@@ -51,33 +63,33 @@ let f () =
    *   /-> 12 >--\
    * 11 -> 13 >- 14
    *)
-  expect_sizes [n0; n1; n2] [13; 3092; 1445] [13; 3092; 1442]; (* Proper roots *)
-  expect_sizes [n0; n2; n1] [13; 1445; 3092] [13; 1442; 3092]; (* check permutation doesn't matter *)
-  expect_sizes [n1; n0; n2] [3092; 13; 1445] [3092; 13; 1442];
-  expect_sizes [n1; n2; n0] [3092; 1445; 13] [3092; 1442; 13];
-  expect_sizes [n2; n0; n1] [1445; 13; 3092] [1442; 13; 3092];
-  expect_sizes [n2; n1; n0] [1445; 3092; 13] [1442; 3092; 13];
-  expect_sizes [n1; n2] [4735; 1473] [4735; 1470];
-  expect_sizes [n0; n2] [1656; 1445] [1656; 1442];
-  expect_sizes [n0; n1] [41; 3092] [41; 3092];
+  expect_reachable [n0; n1; n2] [[0]; [1; 10]; [2; 4; 6; 7; 8]]; (* Proper roots *)
+  expect_reachable [n0; n2; n1] [[0]; [2; 4; 6; 7; 8]; [1; 10]]; (* check permutation doesn't matter *)
+  expect_reachable [n1; n0; n2] [[1; 10]; [0]; [2; 4; 6; 7; 8]];
+  expect_reachable [n1; n2; n0] [[1; 10]; [2; 4; 6; 7; 8]; [0]];
+  expect_reachable [n2; n0; n1] [[2; 4; 6; 7; 8]; [0]; [1; 10]];
+  expect_reachable [n2; n1; n0] [[2; 4; 6; 7; 8]; [1; 10]; [0]];
+  expect_reachable [n1; n2] [[1; 5; 9; 10]; [2; 3; 4; 6; 7; 8]];
+  expect_reachable [n0; n2] [[0; 5; 9]; [2; 4; 6; 7; 8]];
+  expect_reachable [n0; n1] [[0; 3]; [1; 10]];
 
-  expect_sizes [n6; n7] [202; 394] [199; 391]; (* Cycles between roots *)
-  expect_sizes [n6; n7; n2] [202; 394; 105] [199; 391; 105];
-  expect_sizes [n6; n7; n8] [202; 394; 772] [199; 391; 772];
+  expect_reachable [n6; n7] [[6]; [7]]; (* Cycles between roots *)
+  expect_reachable [n6; n7; n2] [[6]; [7]; [2; 3; 4]];
+  expect_reachable [n6; n7; n8] [[6]; [7]; [8]];
 
-  expect_sizes [n5; n9] [103; 1540] [103; 1540]; (* Root is parent of another root *)
-  expect_sizes [n5; n9; n3] [103; 1540; 28] [103; 1540; 28];
-  expect_sizes [n5; n9; n3; n0] [103; 1540; 28; 13] [103; 1540; 28; 13];
-  expect_sizes [n1; n10; n5] [16; 3076; 1643] [16; 3076; 1643];
+  expect_reachable [n5; n9] [[5]; [9]]; (* Root is parent of another root *)
+  expect_reachable [n5; n9; n3] [[5]; [9]; [3]];
+  expect_reachable [n5; n9; n3; n0] [[5]; [9]; [3]; [0]];
+  expect_reachable [n1; n10; n5] [[1]; [10]; [5; 9]];
 
-  expect_sizes [n12; n13] [12295; 24583] [12292; 24580]; (* Multiple owners *)
-  expect_sizes [n12; n13; n14] [12295; 24583; 49156] [12292; 24580; 49156];
-  expect_sizes [n11; n12] [30737; 12295] [30734; 12292];
-  expect_sizes [n12; n11] [12295; 30737] [12292; 30734];
-  expect_sizes [n11; n12; n13] [6154; 12295; 24583] [6154; 12292; 24580];
-  expect_sizes [n11] [92188] [92185];
+  expect_reachable [n12; n13] [[12]; [13]]; (* Multiple owners *)
+  expect_reachable [n12; n13; n14] [[12]; [13]; [14]];
+  expect_reachable [n11; n12] [[11; 13]; [12]];
+  expect_reachable [n12; n11] [[12]; [11; 13]];
+  expect_reachable [n11; n12; n13] [[11]; [12]; [13]];
+  expect_reachable [n11] [[11; 12; 13; 14]];
 
-  expect_sizes [n8; n9; n10] [772; 1540; 3076] [772; 1540; 3076]; (* Leaves *)
+  expect_reachable [n8; n9; n10] [[8]; [9]; [10]]; (* Leaves *)
 
   ()
 
