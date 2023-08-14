f9d8b007d7558117bc9ad4e8b0fb3c949fb1913c
Max Slater <max@thenumb.at>
2023-08-14 15:04:24 -0400

    fix dbg info order?

--- backend/cmm_helpers.ml
+++ backend/cmm_helpers.ml
@@ -2003,7 +2003,7 @@ let check_bound safety access_size dbg base length a2 k =
     match check_align with
     | 0 -> check_bound
     | align ->
-      Csequence (check_bound, make_checkalign dbg align [add_int base a2 dbg]))
+      Csequence (make_checkalign dbg align [add_int base a2 dbg], check_bound))
 
 let opaque e dbg = Cop (Copaque, [e], dbg)
 
