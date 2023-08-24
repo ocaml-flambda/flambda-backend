f51ade1239782f1c9e20f63fd0e4be9519930709
Greta Yorsh <gyorsh@janestreet.com>
2023-08-24 11:19:07 +0100

    Run zero_alloc check before comballoc and cse
    These passes do not change allocations but manipulate
    debuginfo. Checking before it does not need to
    adjust these passes for Debuginfo.assume_zero_alloc.
    
    This should be reconsidered if add other properties to check, but so
    far the properties we have considered can be checked before
    comballoc (no indirect calls, no raise, no updates for dynamic
    memory).

--- backend/asmgen.ml
+++ backend/asmgen.ml
@@ -277,14 +277,14 @@ let compile_fundecl ~ppf_dump ~funcnames fd_cmm =
   ++ Profile.record ~accumulate:true "polling"
        (Polling.instrument_fundecl ~future_funcnames:funcnames)
   ++ Compiler_hooks.execute_and_pipe Compiler_hooks.Mach_polling
+  ++ Profile.record ~accumulate:true "checkmach"
+       (Checkmach.fundecl ~future_funcnames:funcnames ppf_dump)
   ++ Profile.record ~accumulate:true "comballoc" Comballoc.fundecl
   ++ Compiler_hooks.execute_and_pipe Compiler_hooks.Mach_combine
   ++ pass_dump_if ppf_dump dump_combine "After allocation combining"
   ++ Profile.record ~accumulate:true "cse" CSE.fundecl
   ++ Compiler_hooks.execute_and_pipe Compiler_hooks.Mach_cse
   ++ pass_dump_if ppf_dump dump_cse "After CSE"
-  ++ Profile.record ~accumulate:true "checkmach"
-       (Checkmach.fundecl ~future_funcnames:funcnames ppf_dump)
   ++ Profile.record ~accumulate:true "regalloc" (fun (fd : Mach.fundecl) ->
     match register_allocator fd with
     | ((IRC | LS) as regalloc) ->
