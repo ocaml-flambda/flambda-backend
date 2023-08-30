963bfbccb5826af9ae182abb0185ca7eb8f00310
apilatjs <141742870+apilatjs@users.noreply.github.com>
2023-08-30 07:44:45 -0400

    flambda-backend: Add [Obj.uniquely_reachable_words] (#1705)
    * Add [Obj.uniqely_reachable_words]
    
    This function takes in a list of objects and for each one computes the number
    of words of memory that can be reachabed from that object, but no others in
    the list.
    
    * Fix implementation of [Obj.reachable_words]
    
    The previous changes to [caml_obj_reachable_words_once] were not
    compatible with the current usage in [Obj.reachable_words] but were
    overlooked.
    
    * Fix incorrect use of hashtable when adding roots
    
    Previously, we simply hashed the value instead of using
    [extern_lookup_position]. However, this doesn't appropriately guard
    against collisions and can cause our state to be inconsistent.
    
    Test case which gave incorrect, non-determinstic input before the fix
    ```
    let data = List.init 10 (fun _ -> List.init 1 (fun i -> i))
    let direct = List.map Obj.repr data @ [ Obj.repr data ]
    let print_s xs = List.map string_of_int xs |> String.concat "," |> print_endline
    let () = print_s (Obj.uniquely_reachable_words direct)
    let () = print_s (List.map Obj.reachable_words direct)
    ```
    
    * Address code review
    
    - Move array iteration into C
    - Give meaningful names to constants in traversal code
    - Other minor changes
    
    * Make [tests/lib-obj/uniquely_reachable_words.ml] more robust
    
    Instead of relying on an exact number of words used which can differ
    slightly with the backend due to different optimizations, we deduce what
    objects are reachable bases on the sum of their sizes.
    
    * Optimize algorithm to only require a single iteration through the roots
    
    * Clean up code following review suggestions
    
    * Code style improvements
    
    * Additionally return the total size of shared memory
    
    This enables easier evaluation of the retainer profiling functionality
    as we can quantify how much memory we were able to assign uniquely to a
    root.

--- stdlib/obj.mli
+++ stdlib/obj.mli
@@ -32,7 +32,7 @@ val is_block : t -> bool
 external is_int : t -> bool = "%obj_is_int"
 external tag : t -> int = "caml_obj_tag" [@@noalloc]
 val size : t -> int
-external reachable_words : t -> int = "caml_obj_reachable_words"
+val reachable_words : t -> int
   (**
      Computes the total size (in words, including the headers) of all
      heap blocks accessible from the argument.  Statically
@@ -40,6 +40,16 @@ external reachable_words : t -> int = "caml_obj_reachable_words"
      was configured with [--disable-naked-pointers].
 
      @since 4.04
+  *)
+
+val uniquely_reachable_words : t array -> int array * int
+(** For each element of the array, computes the total size (as defined
+    above by [reachable_words]) of all heap blocks accessible from the
+    argument but excluding all blocks accessible from any other arguments.
+
+    Also returns a single number denoting the total memory reachable from
+    at least two of the roots. We make no attempt to classify which two
+    (or more) roots are responsible for this memory.
   *)
 
 val field : t -> int -> t
