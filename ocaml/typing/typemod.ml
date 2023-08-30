546e73a334dadb5eb8bfb8d13adbdeae3cc8258a
Luke Maurer <lmaurer@janestreet.com>
2023-08-30 18:29:52 +0100

    Raise error on combination of `-as-parameter` and `-for-pack`

--- ocaml/typing/typemod.ml
+++ ocaml/typing/typemod.ml
@@ -87,6 +87,7 @@ type error =
   | With_cannot_remove_packed_modtype of Path.t * module_type
   | Toplevel_nonvalue of string * sort
   | Cannot_implement_parameter of filepath
+  | Cannot_pack_parameter
   | Cannot_compile_implementation_as_parameter
 
 exception Error of Location.t * Env.t * error
@@ -3352,7 +3353,10 @@ let save_signature modname tsg outputprefix source_file initial_env cmi =
   Cms_format.save_cms  (outputprefix ^ ".cmsi") modname
     (Some source_file) None
 
-let type_interface env ast =
+let type_interface modulename env ast =
+  if !Clflags.as_parameter && Compilation_unit.is_packed modulename then begin
+    raise(Error(Location.none, Env.empty, Cannot_pack_parameter))
+  end;
   transl_signature env ast
 
 (* "Packaging" of several compilation units into one unit
@@ -3650,6 +3654,9 @@ let report_error ~loc _env = function
         "@[Interface %s@ found for this unit is flagged as a parameter.@ \
          It cannot be implemented directly. Use -as-argument-for instead.@]"
         path
+  | Cannot_pack_parameter ->
+      Location.errorf ~loc
+        "Cannot compile a parameter with -for-pack."
   | Cannot_compile_implementation_as_parameter ->
       Location.errorf ~loc
         "Cannot compile an implementation with -as-parameter."
