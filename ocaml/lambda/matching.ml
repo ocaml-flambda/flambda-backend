e476c380651a2a851f59d67c8a013bc7757dae96
Nick Roberts <nroberts@janestreet.com>
2023-09-12 14:31:49 -0400

    Fix AFL test in flambda2

--- ocaml/lambda/matching.ml
+++ ocaml/lambda/matching.ml
@@ -2024,7 +2024,7 @@ let inline_lazy_force arg pos loc =
         ap_result_layout = Lambda.layout_lazy_contents;
         ap_region_close = pos;
         ap_mode = alloc_heap;
-        ap_inlined = Default_inlined;
+        ap_inlined = Never_inlined;
         ap_specialised = Default_specialise;
         ap_probe=None;
       }
