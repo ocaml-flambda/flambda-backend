717098c4a5283ec6f87f8023d03ac1442cf852d7
Chris Casinghino <ccasinghino@janestreet.com>
2023-09-20 17:06:28 -0400

    Backport of upstream -H implementation

--- ocaml/driver/main_args.ml
+++ ocaml/driver/main_args.ml
@@ -153,6 +153,11 @@ let mk_i f =
 let mk_I f =
   "-I", Arg.String f, "<dir>  Add <dir> to the list of include directories"
 
+let mk_H f =
+  "-H", Arg.String f,
+  "<dir>  Add <dir> to the list of \"hidden\" include directories\n\
+ \     (Like -I, but the program can not directly reference these dependencies)"
+
 let mk_impl f =
   "-impl", Arg.String f, "<file>  Compile <file> as a .ml file"
 
@@ -842,6 +847,7 @@ module type Common_options = sig
   val _no_absname : unit -> unit
   val _alert : string -> unit
   val _I : string -> unit
+  val _H : string -> unit
   val _labels : unit -> unit
   val _alias_deps : unit -> unit
   val _no_alias_deps : unit -> unit
@@ -1128,6 +1134,7 @@ struct
     mk_stop_after ~native:false F._stop_after;
     mk_i F._i;
     mk_I F._I;
+    mk_H F._H;
     mk_impl F._impl;
     mk_intf F._intf;
     mk_intf_suffix F._intf_suffix;
@@ -1229,6 +1236,7 @@ struct
     mk_no_absname F._no_absname;
     mk_alert F._alert;
     mk_I F._I;
+    mk_H F._H;
     mk_init F._init;
     mk_labels F._labels;
     mk_alias_deps F._alias_deps;
@@ -1336,6 +1344,7 @@ struct
     mk_no_probes F._no_probes;
     mk_i F._i;
     mk_I F._I;
+    mk_H F._H;
     mk_impl F._impl;
     mk_inline F._inline;
     mk_inline_toplevel F._inline_toplevel;
@@ -1478,6 +1487,7 @@ module Make_opttop_options (F : Opttop_options) = struct
     mk_alert F._alert;
     mk_compact F._compact;
     mk_I F._I;
+    mk_H F._H;
     mk_init F._init;
     mk_inline F._inline;
     mk_inline_toplevel F._inline_toplevel;
@@ -1590,6 +1600,7 @@ struct
     mk_no_absname F._no_absname;
     mk_alert F._alert;
     mk_I F._I;
+    mk_H F._H;
     mk_impl F._impl;
     mk_intf F._intf;
     mk_intf_suffix F._intf_suffix;
@@ -1731,7 +1742,8 @@ module Default = struct
 
   module Core = struct
     include Common
-    let _I dir = include_dirs := (dir :: (!include_dirs))
+    let _I dir = include_dirs := dir :: (!include_dirs)
+    let _H dir = hidden_include_dirs := dir :: (!hidden_include_dirs)
     let _color = Misc.set_or_ignore color_reader.parse color
     let _dlambda = set dump_lambda
     let _dparsetree = set dump_parsetree
@@ -1979,6 +1991,11 @@ module Default = struct
     let _I(_:string) =
       (* placeholder:
          Odoc_global.include_dirs := (s :: (!Odoc_global.include_dirs))
+      *) ()
+    let _H(_:string) =
+      (* placeholder:
+         Odoc_global.hidden_include_dirs :=
+           (s :: (!Odoc_global.hidden_include_dirs))
       *) ()
     let _impl (_:string) =
       (* placeholder:
