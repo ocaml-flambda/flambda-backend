9885c239da1996ac8e89426e9f176864d4af5a81
David Allsopp <david.allsopp@metastack.com>
2023-09-24 11:45:12 +0100

    [BACKPORT] Adapt to 4.14 runtime
    (cherry picked from commit 38985a2d0d0a06adb1552f1611cd47347df62b7c)

--- stdlib/filename.ml
+++ stdlib/filename.ml
@@ -346,6 +346,18 @@ let temp_file_name temp_dir prefix suffix =
   let rnd = (Random.State.bits random_state) land 0xFFFFFF in
   concat temp_dir (Printf.sprintf "%s%06x%s" prefix rnd suffix)
 
+(* BACKPORT BEGIN *)
+module Domain = struct
+  module DLS = struct
+    let new_key ~split_from_parent:_ f =
+      ref (f ())
+
+    let get = (!)
+    let set = (:=)
+  end
+end
+(* BACKPORT END *)
+
 let current_temp_dir_name =
   Domain.DLS.new_key ~split_from_parent:Fun.id (fun () -> temp_dir_name)
 
