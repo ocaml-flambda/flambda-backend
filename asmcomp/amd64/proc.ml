77e7806a3f2e3ae8c9c159cedfabe763dd0871e8
David Allsopp <david.allsopp@metastack.com>
2023-09-28 11:15:28 +0100

    [BACKPORT] Adapt to 4.14 runtime
    (cherry picked from commit 45c63cddd9edffb85bc85fe510f74281ce44ca29)

--- asmcomp/amd64/proc.ml
+++ asmcomp/amd64/proc.ml
@@ -238,7 +238,11 @@ let win64_float_external_arguments =
 let win64_loc_external_arguments arg =
   let loc = Array.make (Array.length arg) Reg.dummy in
   let reg = ref 0
+(* BACKPORT BEGIN
   and ofs = ref 0 in
+*)
+  and ofs = ref 32 in
+(* BACKPORT END *)
   for i = 0 to Array.length arg - 1 do
     match arg.(i) with
     | Val | Int | Addr as ty ->
