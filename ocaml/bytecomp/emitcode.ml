243264c73fb2af5eea09a16f790283252e5f64f4
Luke Maurer <lmaurer@janestreet.com>
2023-10-04 16:13:32 +0100

    Also change the new `cu_implements_param` field

--- ocaml/bytecomp/emitcode.ml
+++ ocaml/bytecomp/emitcode.ml
@@ -418,7 +418,10 @@ let to_file outchan unit_name objfile ~required_globals code =
       cu_codesize = !out_position;
       cu_reloc = List.rev !reloc_info;
       cu_implements_param =
-        !Clflags.as_argument_for |> Option.map Compilation_unit.Name.of_string;
+        (* Currently, parameters don't have parameters, so we assume the argument
+           list is empty *)
+        !Clflags.as_argument_for
+        |> Option.map (fun head -> Global.Name.create head []);
       cu_imports = Env.imports() |> Array.of_list;
       cu_runtime_params = runtime_params;
       cu_primitives = List.map Primitive.byte_name
--- ocaml/file_formats/cmo_format.mli
+++ ocaml/file_formats/cmo_format.mli
@@ -30,7 +30,7 @@ type compilation_unit_descr =
     mutable cu_pos: int;                (* Absolute position in file *)
     cu_codesize: int;                   (* Size of code block *)
     cu_reloc: (reloc_info * int) list;  (* Relocation information *)
-    cu_implements_param: Compilation_unit.Name.t option;
+    cu_implements_param: Global.Name.t option;
                                         (* If this is an argument unit, the
                                            parameter it implements *)
     cu_imports: Import_info.t array;    (* Names and CRC of intfs imported *)
