f0270948c7beef007685f9b50be8432166ff8825
Mark Shinwell <mshinwell@pm.me>
2023-10-06 16:10:19 +0100

    Headers

--- backend/debug/dwarf/dwarf_ocaml/dwarf_type.ml
+++ backend/debug/dwarf/dwarf_ocaml/dwarf_type.ml
@@ -1,3 +1,17 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                 OCaml                                  *)
+(*                                                                        *)
+(*           Tomasz Nowak and Mark Shinwell, Jane Street Europe           *)
+(*                                                                        *)
+(*   Copyright 2023 Jane Street Group LLC                                 *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
+(*   special exception on linking described in the file LICENSE.          *)
+(*                                                                        *)
+(**************************************************************************)
+
 open! Dwarf_low
 open! Dwarf_high
 module Uid = Flambda2_identifiers.Flambda_uid
