0989dff6d8df5a5d54388115277dcc2d6f90c1ed
Mark Shinwell <mshinwell@pm.me>
2023-10-06 17:39:14 +0100

    Dwarf_low and Dwarf_high changes for DWARF type emission (#1911)

--- backend/debug/dwarf/dwarf_low/encoding_attribute.ml
+++ backend/debug/dwarf/dwarf_low/encoding_attribute.ml
@@ -15,19 +15,31 @@
 [@@@ocaml.warning "+a-4-30-40-41-42"]
 
 type t =
+  | DW_ATE_boolean
   | DW_ATE_signed
   | DW_ATE_float
+  | DW_ATE_unsigned
+
+let boolean = DW_ATE_boolean
 
 let signed = DW_ATE_signed
 
 let float = DW_ATE_float
 
+let unsigned = DW_ATE_unsigned
+
 let name t =
   match t with
+  | DW_ATE_boolean -> "DW_ATE_boolean"
   | DW_ATE_signed -> "DW_ATE_signed"
   | DW_ATE_float -> "DW_ATE_float"
+  | DW_ATE_unsigned -> "DW_ATE_unsigned"
 
-let encode = function DW_ATE_float -> 0x04 | DW_ATE_signed -> 0x05
+let encode = function
+  | DW_ATE_boolean -> 0x02
+  | DW_ATE_float -> 0x04
+  | DW_ATE_signed -> 0x05
+  | DW_ATE_unsigned -> 0x07
 
 let size _t = 1
 
