454de0d51b79b78a240917b2e63f411ced2af1bd
Luke Maurer <lmaurer@janestreet.com>
2023-10-10 18:08:46 +0100

    Test native compilation
    Currently failing because compilation is happening in the wrong order.

--- ocaml/testsuite/tests/templates/basic/test.ml
+++ ocaml/testsuite/tests/templates/basic/test.ml
@@ -271,6 +271,239 @@ all_modules = "\
 output = "test.output"
 ********************************** check-program-output
 reference = "test.reference"
+* setup-ocamlopt.byte-build-env
+** ocamlopt.byte
+flags = "-as-parameter"
+module = "monoid.mli"
+*** ocamlopt.byte
+flags = ""
+module = "bad_ref_direct.ml"
+compiler_output = "bad_ref_direct.output"
+ocamlopt_byte_exit_status = "2"
+**** check-ocamlopt.byte-output
+compiler_reference = "bad_ref_direct.reference"
+*** ocamlopt.byte
+flags = "-as-argument-for Monoid"
+module = "bad_arg_impl.ml"
+compiler_output = "bad_arg_impl.output"
+ocamlopt_byte_exit_status = "2"
+**** check-ocamlopt.byte-output
+compiler_reference = "bad_arg_impl.reference"
+*** ocamlopt.byte
+flags = "-as-argument-for Monoid"
+module = "bad_arg_intf.mli"
+compiler_output = "bad_arg_intf.output"
+ocamlopt_byte_exit_status = "2"
+**** check-ocamlopt.byte-output
+compiler_reference = "bad_arg_intf.reference"
+*** copy
+src = "string_monoid.ml"
+dst = "string_monoid_no_mli.ml"
+**** ocamlopt.byte
+flags = "-as-argument-for Monoid"
+module = "string_monoid_no_mli.ml string_monoid.mli string_monoid.ml"
+***** ocamlopt.byte
+flags = ""
+module = "test_direct_access.ml"
+****** ocamlopt.byte
+flags = ""
+program = "${test_build_directory}/test_direct_access.exe"
+module = ""
+all_modules = "\
+   string_monoid.cmx \
+   string_monoid_no_mli.cmx \
+   test_direct_access.cmx \
+"
+******* run
+output = "test_direct_access.output"
+******** check-program-output
+reference = "test_direct_access.reference"
+*** ocamlopt.byte
+module = "semigroup.mli"
+**** ocamlopt.byte
+module = "category.mli"
+***** ocamlopt.byte
+flags = "-parameter Semigroup -as-argument-for Monoid"
+module = "monoid_of_semigroup.mli"
+****** ocamlopt.byte
+module = "monoid_of_semigroup.ml"
+******* ocamlopt.byte
+flags = "-as-parameter"
+module = "list_element.mli"
+******** ocamlopt.byte
+flags = "-parameter List_element -as-argument-for Monoid"
+module = "list_monoid.mli list_monoid.ml"
+********* ocamlopt.byte
+flags = "-parameter Monoid"
+module = "monoid_utils.mli monoid_utils.ml"
+********** ocamlopt.byte
+flags = ""
+module = "bad_ref_indirect.ml"
+compiler_output = "bad_ref_indirect.output"
+ocamlopt_byte_exit_status = "2"
+*********** check-ocamlopt.byte-output
+compiler_reference = "bad_ref_indirect.reference"
+********** ocamlopt.byte
+flags = "-parameter List_element"
+module = "bad_instance_arg_name_not_found.ml"
+compiler_output = "bad_instance_arg_name_not_found.output"
+ocamlopt_byte_exit_status = "2"
+*********** check-ocamlopt.byte-output
+compiler_reference = "bad_instance_arg_name_not_found.reference"
+********** ocamlopt.byte
+flags = "-parameter List_element"
+module = "bad_instance_arg_value_not_arg.ml"
+compiler_output = "bad_instance_arg_value_not_arg.output"
+ocamlopt_byte_exit_status = "2"
+*********** check-ocamlopt.byte-output
+compiler_reference = "bad_instance_arg_value_not_arg.reference"
+********** ocamlopt.byte
+flags = "-parameter List_element"
+module = "bad_instance_arg_value_not_found.ml"
+compiler_output = "bad_instance_arg_value_not_found.output"
+ocamlopt_byte_exit_status = "2"
+*********** check-ocamlopt.byte-output
+compiler_reference = "bad_instance_arg_value_not_found.reference"
+********** ocamlopt.byte
+flags = "-parameter Semigroup"
+module = "bad_ref_direct_imported.ml"
+compiler_output = "bad_ref_direct_imported.output"
+ocamlopt_byte_exit_status = "2"
+*********** check-ocamlopt.byte-output
+compiler_reference = "bad_ref_direct_imported.reference"
+********** ocamlopt.byte
+flags = "-parameter Category"
+module = "chain.mli chain.ml"
+*********** ocamlopt.byte
+flags = "-parameter Category"
+module = "category_utils.mli category_utils.ml"
+************ ocamlopt.byte
+flags = "-parameter Monoid -as-argument-for Category"
+module = "category_of_monoid.mli category_of_monoid.ml"
+************* ocamlopt.byte
+flags = "-parameter List_element"
+module = "bad_instance_arg_value_wrong_type.ml"
+compiler_output = "bad_instance_arg_value_wrong_type.output"
+ocamlopt_byte_exit_status = "2"
+************** check-ocamlopt.byte-output
+compiler_reference = "bad_instance_arg_value_wrong_type.reference"
+************* ocamlopt.byte
+flags = "-parameter Semigroup -parameter List_element -w -misplaced-attribute"
+module = "import.ml"
+************** ocamlopt.byte
+flags = "-as-argument-for Semigroup"
+module = "string_semigroup.mli"
+*************** ocamlopt.byte
+module = "string_semigroup.ml"
+**************** ocamlopt.byte
+module = ""
+flags = "-instantiate"
+program = "monoid_of_semigroup-String_semigroup.cmx"
+all_modules = "monoid_of_semigroup.cmx string_semigroup.cmx"
+***************** ocamlopt.byte
+flags = "-parameter Semigroup -parameter List_element -w -misplaced-attribute"
+module = "main.mli"
+****************** ocamlopt.byte
+flags = "-parameter Semigroup -parameter List_element -w -misplaced-attribute -i"
+module = "main.ml"
+compiler_output = "main.output"
+******************* check-ocamlopt.byte-output
+compiler_reference = "main.reference"
+****************** ocamlopt.byte
+module = "main.ml"
+******************* ocamlopt.byte
+flags = "-as-argument-for List_element"
+module = "int_list_element.mli int_list_element.ml"
+******************** ocamlopt.byte
+module = ""
+flags = "-instantiate -as-argument-for Monoid"
+program = "list_monoid-Int_list_element.cmx"
+all_modules = "list_monoid.cmx int_list_element.cmx"
+********************* ocamlopt.byte
+module = ""
+flags = "-instantiate -as-argument-for Monoid"
+program = "monoid_of_semigroup-String_semigroup.cmx"
+all_modules = "monoid_of_semigroup.cmx string_semigroup.cmx"
+********************** ocamlopt.byte
+module = ""
+flags = "-instantiate -as-argument-for Monoid"
+program = "monoid_utils-Monoid_of_semigroup--String_semigroup.cmx"
+all_modules = "monoid_utils.cmx monoid_of_semigroup-String_semigroup.cmx"
+*********************** ocamlopt.byte
+module = ""
+flags = "-instantiate -as-argument-for Category"
+program = "category_of_monoid-List_monoid--Int_list_element.cmx"
+all_modules = "category_of_monoid.cmx list_monoid-Int_list_element.cmx"
+************************ ocamlopt.byte
+module = ""
+flags = "-instantiate -as-argument-for Category"
+program = "category_of_monoid-Monoid_of_semigroup--String_semigroup.cmx"
+all_modules = "category_of_monoid.cmx monoid_of_semigroup-String_semigroup.cmx"
+************************* ocamlopt.byte
+module = ""
+flags = "-instantiate"
+program = "chain-Category_of_monoid--List_monoid---Int_list_element.cmx"
+all_modules = "chain.cmx category_of_monoid-List_monoid--Int_list_element.cmx"
+************************** ocamlopt.byte
+module = ""
+flags = "-instantiate"
+program = "chain-Category_of_monoid--Monoid_of_semigroup---String_semigroup.cmx"
+all_modules = "chain.cmx category_of_monoid-Monoid_of_semigroup--String_semigroup.cmx"
+*************************** ocamlopt.byte
+module = ""
+flags = "-instantiate"
+program = "import-Int_list_element-String_semigroup.cmx"
+all_modules = "import.cmx int_list_element.cmx string_semigroup.cmx"
+**************************** ocamlopt.byte
+module = ""
+flags = "-instantiate"
+program = "category_utils-Category_of_monoid--List_monoid---Int_list_element.cmx"
+all_modules = "category_utils.cmx category_of_monoid-List_monoid--Int_list_element.cmx"
+***************************** ocamlopt.byte
+module = ""
+flags = "-instantiate"
+program = "category_utils-Category_of_monoid--Monoid_of_semigroup---String_semigroup.cmx"
+all_modules = "category_utils.cmx category_of_monoid-Monoid_of_semigroup--String_semigroup.cmx"
+****************************** ocamlopt.byte
+module = ""
+flags = "-instantiate"
+program = "main-Int_list_element-String_semigroup.cmx"
+all_modules = "main.cmx int_list_element.cmx string_semigroup.cmx"
+******************************* ocamlopt.byte
+flags = "-w -misplaced-attribute"
+module = "test.ml"
+******************************** ocamlopt.byte
+flags = ""
+program = "${test_build_directory}/test.exe"
+module = ""
+all_modules = "\
+   string_semigroup.cmx \
+   monoid_of_semigroup.cmx \
+   monoid_of_semigroup-String_semigroup.cmx \
+   monoid_utils.cmx \
+   monoid_utils-Monoid_of_semigroup--String_semigroup.cmx \
+   int_list_element.cmx \
+   list_monoid.cmx \
+   list_monoid-Int_list_element.cmx \
+   category_of_monoid.cmx \
+   category_of_monoid-List_monoid--Int_list_element.cmx \
+   category_of_monoid-Monoid_of_semigroup--String_semigroup.cmx \
+   chain.cmx \
+   chain-Category_of_monoid--List_monoid---Int_list_element.cmx \
+   chain-Category_of_monoid--Monoid_of_semigroup---String_semigroup.cmx \
+   category_utils.cmx \
+   category_utils-Category_of_monoid--List_monoid---Int_list_element.cmx \
+   category_utils-Category_of_monoid--Monoid_of_semigroup---String_semigroup.cmx \
+   import.cmx \
+   import-Int_list_element-String_semigroup.cmx \
+   main.cmx \
+   main-Int_list_element-String_semigroup.cmx \
+   test.cmx \
+"
+********************************* run
+output = "test.output"
+********************************** check-program-output
+reference = "test.reference"
 *)
 
 module M =
