0bd315ee2036a6209715ad7f5a9a69971bf75294
alanechang <alanechang@janestreet.com>
2023-10-11 04:49:35 -0400

    fix probe test

--- ocaml/testsuite/tests/typing-layouts-err-msg/probe.ml
+++ ocaml/testsuite/tests/typing-layouts-err-msg/probe.ml
@@ -1,8 +1,12 @@
 (* TEST
-   flags = "-extension layouts_alpha"
-   compiler_reference = "${test_source_directory}/probe.reference"
-* native
 
+* setup-ocamlopt.opt-build-env
+** ocamlopt.opt
+flags = "-extension layouts_alpha"
+compiler_reference2 = "${test_source_directory}/probe.reference"
+ocamlopt_opt_exit_status = "2"
+compile_only = "true"
+*** check-ocamlopt.opt-output
 *)
 let f (x: float#) = [%probe "a" (
   let f () = x in
