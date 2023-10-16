a4395f232e017329dbed1e499d00e3d330f58f48
Greta Yorsh <gyorsh@janestreet.com>
2023-10-16 18:00:43 +0100

    Change test slightly to avoid dependency on stdlib.ml line numbers

--- tests/backend/checkmach/fail23.ml
+++ tests/backend/checkmach/fail23.ml
@@ -1,7 +1,8 @@
+exception Exn of string * int
 (* expect to fail with -zero-alloc-check-opt *)
 let[@zero_alloc opt strict] test1 x =
   if x > 0
   then x+1
   else
-    failwith ("expected positive, got "^(string_of_int x))
+    raise (Exn ("expected positive", x))
 
