52ea8fd716c5efb95e50c7dea2ac75b333769cf6
Luke Maurer <lmaurer@janestreet.com>
2023-10-19 17:46:34 +0100

    Revert "Disable test of error message"
    This reverts commit 6f86fb94ac2f048660da8adbf6f8072f86712e1a.

--- ocaml/testsuite/tests/templates/basic/bad_param_impl.ml
+++ ocaml/testsuite/tests/templates/basic/bad_param_impl.ml
@@ -11,8 +11,6 @@ flags = ""
 module = "bad_param_impl.ml"
 ocamlc_byte_exit_status = "2"
 compiler_output = "bad_param_impl.output"
-**** skip
-reason = "error broken, will be fixed by #1764"
-***** check-ocamlc.byte-output
+**** check-ocamlc.byte-output
 compiler_reference = "bad_param_impl.reference"
 *)
