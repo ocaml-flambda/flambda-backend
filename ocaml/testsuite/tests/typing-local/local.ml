de16beaeda1f4a68503871692e469fcf4dacb495
Richard Eisenberg <reisenberg@janestreet.com>
2023-10-19 21:00:24 +0100

    Make `assert false` behave as local_ or not, depending on what's better (+ 2 bugfixes) (#1899)
    * Add test cases
    
    * Refactor is_local_returning_function; improve error
    
    * Actually allow [assert false] to imply [local_]
    
    * Add test case around local newtype annotations
    
    * Look through newtypes with layout annotations

--- ocaml/testsuite/tests/typing-local/local.ml
+++ ocaml/testsuite/tests/typing-local/local.ml
@@ -2769,3 +2769,28 @@ Line 2, characters 33-58:
 Error: This expression has type int -> local_ (int -> int)
        but an expression was expected of type int -> (int -> int)
 |}];;
+
+(* test that [function] checks all its branches either for local_ or the
+   absence thereof *)
+let foo = function
+  | false -> local_ 5
+  | true -> 6
+
+[%%expect{|
+Line 3, characters 12-13:
+3 |   | true -> 6
+                ^
+Error: This function return is not annotated with "local_"
+       whilst other returns were.
+|}]
+
+(* test that [assert false] can mix with other returns being [local_] *)
+let foo b =
+  if b
+  then assert false
+  else local_ Some 6
+
+[%%expect{|
+val foo : bool -> local_ int option = <fun>
+|}]
+
