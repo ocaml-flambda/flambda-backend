eed5850614d5d18b94ddb71117bdfec504081e70
alanechang <alanechang@janestreet.com>
2023-10-25 14:21:52 -0400

    fix result_layout on native

--- ocaml/lambda/matching.ml
+++ ocaml/lambda/matching.ml
@@ -2197,18 +2197,22 @@ let get_expr_args_array ~scopes kind head (arg, _mut, _sort, _layout) rem =
     if pos >= len then
       rem
     else
+      let prim = Parrayrefu Lambda.(array_ref_kind alloc_heap kind) in
+      let result_layout =
+        if !Clflags.native_code then primitive_result_layout prim else layout_field
+      in
       (* CR ncourant: could do better than layout_field using kind *)
       ( Lprim
           (* TODO: The resulting float should be allocated to at the mode of the
              array pattern, once that's available *)
-          (Parrayrefu Lambda.(array_ref_kind alloc_heap kind),
+          (prim,
            [ arg; Lconst (Const_base (Const_int pos)) ],
            loc),
         (match am with
         | Mutable   -> StrictOpt
         | Immutable -> Alias),
         Typeopt.array_element_sort kind,
-        layout_field)
+        result_layout)
       :: make_args (pos + 1)
   in
   make_args 0
