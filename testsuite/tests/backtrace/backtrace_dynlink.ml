1846e787aaf9fa8c869f3bb505db12be32e9de45
Mark Shinwell <mshinwell@pm.me>
2023-11-02 17:55:21 +0000

    flambda-backend: Fix backtrace_dynlink

--- testsuite/tests/backtrace/backtrace_dynlink.ml
+++ testsuite/tests/backtrace/backtrace_dynlink.ml
@@ -33,6 +33,43 @@ reference = "${test_source_directory}/backtrace_dynlink.flambda.reference"
 (* https://github.com/ocaml-multicore/ocaml-multicore/issues/440 *)
 (* https://github.com/ocaml-multicore/ocaml-multicore/pull/499 *)
 
+(* Postprocess backtrace to ignore differences between dune and make
+   builds (in the former, Dynlink.Native is Dynlink_internal_native.Native) *)
+let begins_with ?(from = 0) str ~prefix =
+  (* From utils/misc.ml *)
+  let rec helper idx =
+    if idx < 0 then true
+    else
+      String.get str (from + idx) = String.get prefix idx && helper (idx-1)
+  in
+  let n = String.length str in
+  let m = String.length prefix in
+  if n >= from + m then helper (m-1) else false
+
+let process_backtrace bt =
+  let bt = String.split_on_char '\n' bt in
+  let bt =
+    List.map (fun line ->
+        let prefix = "Called from Dynlink.Native" in
+        if begins_with line ~prefix
+        then
+          "Called from Dynlink_internal_native.Native" ^
+            (String.sub line (String.length prefix)
+              (String.length line - String.length prefix))
+        else
+          let prefix = "Re-raised at Dynlink.Native" in
+          if begins_with line ~prefix
+          then
+            "Re-raised at Dynlink_internal_native.Native" ^
+              (String.sub line (String.length prefix)
+                (String.length line - String.length prefix))
+          else
+            line
+      )
+      bt
+  in
+  String.concat "\n" bt
+
 let ()  =
   Dynlink.allow_unsafe_modules true;
   try
@@ -40,7 +77,7 @@ let ()  =
   with
   | Dynlink.Error err ->
      print_endline @@ Dynlink.error_message err;
-     Printexc.print_backtrace stdout;
+     print_string (process_backtrace (Printexc.get_backtrace ()))
   | exn ->
      Printexc.to_string exn |> print_endline;
      print_endline "ERROR"
