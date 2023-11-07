5a620b176d359846d92db0f4f1186931a0a6ee1e
Chris Casinghino <ccasinghino@janestreet.com>
2023-11-07 12:11:33 -0500

    Delete some CRs in ocamldoc - we don't care about this tool

--- ocaml/ocamldoc/odoc_ast.ml
+++ ocaml/ocamldoc/odoc_ast.ml
@@ -261,8 +261,6 @@ module Analyser =
             iter_pattern pat
 
         | Typedtree.Tpat_tuple patlist ->
-            (* CR labeled tuples: Check odoc handling of labeled tuple
-               patterns. *)
             Tuple
               (List.map (fun (_, p) -> iter_pattern p) patlist,
                Odoc_env.subst_type env pat.pat_type)
