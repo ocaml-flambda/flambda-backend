cacc11b6029dcde0eb37d70be69fad591fdf7a72
Greta Yorsh <gyorsh@janestreet.com>
2023-11-08 15:27:31 +0000

    Update comments

--- backend/cfg/cfg.mli
+++ backend/cfg/cfg.mli
@@ -154,11 +154,7 @@ val print_instruction :
    exception handling. It has a lot of redundancy and the result of the
    computation is not used.
 
-   Redundancy: linear_to_cfg reconstructs intraprocedural exception handling
-   stacks from linear IR and annotates each block with this information.
-   However, CFG instructions still include the original push/poptraps from
-   Linear.
-
+   CFG instructions still include push/poptraps.
    To remove these push/poptraps from CFG IR, we need to split blocks at every
    push/poptrap. Then, we can annotate the blocks with the top of the trap
    stack, instead of carrying the copy of the stack. *)
