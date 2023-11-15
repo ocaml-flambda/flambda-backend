cebaf445c521d52a7e3b85a54c8d55be5f1e527c
Max Slater <max@thenumb.at>
2023-11-15 13:36:06 -0500

    Ignore backport gc change

--- ocaml/stdlib/gc.mli
+++ ocaml/stdlib/gc.mli
@@ -463,11 +463,8 @@ external eventlog_resume : unit -> unit = "caml_eventlog_resume"
    notice. *)
 module Memprof :
   sig
-(* BACKPORT BEGIN
-    type t
-*)
     type t = unit
-(* BACKPORT END *)
+
     (** the type of a profile *)
 
     type allocation_source = Normal | Marshal | Custom
