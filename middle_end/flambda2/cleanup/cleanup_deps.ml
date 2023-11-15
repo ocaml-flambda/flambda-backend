04ae9c8ee79f62dfcef7a05bf5cc69138b52fbc9
Pierre Chambart <pierre.chambart@ocamlpro.com>
2023-11-15 17:52:59 +0100

    Format

--- middle_end/flambda2/cleanup/cleanup_deps.ml
+++ middle_end/flambda2/cleanup/cleanup_deps.ml
@@ -25,29 +25,31 @@ module DepSet = Set.Make (Dep)
 
 type fun_graph =
   { name_to_dep : (Code_id_or_name.t, DepSet.t) Hashtbl.t;
-    used : (Code_id_or_name.t, unit) Hashtbl.t;
+    used : (Code_id_or_name.t, unit) Hashtbl.t
   }
 
 type graph =
-  {
-    toplevel_graph : fun_graph ;
-    function_graphs : (Code_id.t, fun_graph) Hashtbl.t ;
+  { toplevel_graph : fun_graph;
+    function_graphs : (Code_id.t, fun_graph) Hashtbl.t
   }
-
 
 let pp_used_fun_graph ppf (graph : fun_graph) =
   let elts = List.of_seq @@ Hashtbl.to_seq graph.used in
   let pp ppf l =
     let pp_sep ppf () = Format.pp_print_string ppf ",@ " in
-    let pp ppf (name, ()) = Format.fprintf ppf "%a" Code_id_or_name.print name in
+    let pp ppf (name, ()) =
+      Format.fprintf ppf "%a" Code_id_or_name.print name
+    in
     Format.pp_print_list ~pp_sep pp ppf l
   in
   Format.fprintf ppf "{ %a }" pp elts
 
 let pp_used ppf (graph : graph) =
   Format.fprintf ppf "toplevel: %a@ " pp_used_fun_graph graph.toplevel_graph;
-  Hashtbl.iter (fun code_id graph ->
-      Format.fprintf ppf "%a: %a@ " Code_id.print code_id pp_used_fun_graph graph)
+  Hashtbl.iter
+    (fun code_id graph ->
+      Format.fprintf ppf "%a: %a@ " Code_id.print code_id pp_used_fun_graph
+        graph)
     graph.function_graphs
 
 let create () = { name_to_dep = Hashtbl.create 100; used = Hashtbl.create 100 }
