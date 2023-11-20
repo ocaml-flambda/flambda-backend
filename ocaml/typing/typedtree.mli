9f0c5c2ad3223b3e740dca90f00aa08efb5c5354
Nathanaëlle Courant <nathanaelle.courant@ocamlpro.com>
2023-11-20 18:13:58 +0100

    Add comment

--- ocaml/typing/typedtree.mli
+++ ocaml/typing/typedtree.mli
@@ -231,8 +231,11 @@ and expression_desc =
       region : bool; curry : fun_curry_state;
       warnings : Warnings.state;
       arg_mode : Mode.Alloc.t;
+      (* Mode at which the closure is allocated *)
       arg_sort : Jkind.sort;
       ret_mode : Mode.Alloc.t;
+      (* Mode where the function allocates, ie local for a function of
+         type 'a -> local_ 'b, and heap for a function of type 'a -> 'b *)
       ret_sort : Jkind.sort;
       alloc_mode : Mode.Alloc.t}
         (** [Pexp_fun] and [Pexp_function] both translate to [Texp_function].
