21e43b2c061d3599ba498abca1fe75556c7674d8
Xavier Clerc <xclerc@janestreet.com>
2023-11-22 15:23:45 +0000

    Update test suite.

--- ocaml/testsuite/tests/backtrace/sanitize-backtrace.sh
+++ ocaml/testsuite/tests/backtrace/sanitize-backtrace.sh
@@ -8,6 +8,5 @@
 #   this "by primitive operation" for e.g. array accesses
 # In addition to the above it now irons out some runtime4/5 differences.
 sed -e 's/ (inlined)//' -e 's/ by primitive operation//' \
-  -e 's/CamlinternalLazy\.Lazy[45]\./CamlinternalLazy.Lazy./' \
-  -e 's/CamlinternalLazy.Lazy.force_lazy_block.*/CamlinternalLazy.Lazy.<force>/' \
-  -e 's/CamlinternalLazy.Lazy.do_force_block.*/CamlinternalLazy.Lazy.<force>/'
+  -e 's/CamlinternalLazy.force_lazy_block.*/CamlinternalLazy.<force>/' \
+  -e 's/CamlinternalLazy.do_force_block.*/CamlinternalLazy.<force>/'
