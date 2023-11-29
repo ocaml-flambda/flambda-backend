0f9dde98716f04ccd4d66172df769942bb943583
Nathanaëlle Courant <nathanaelle.courant@ocamlpro.com>
2023-11-29 11:34:54 +0100

    Fix shadowed variable

--- middle_end/flambda2/simplify/simplify_apply_expr.ml
+++ middle_end/flambda2/simplify/simplify_apply_expr.ml
@@ -638,7 +638,7 @@ let simplify_direct_partial_application ~simplify_expr dacc apply
         applied_values
       |> Value_slot.Map.of_list
     in
-    ( Set_of_closures.create ~value_slots apply_alloc_mode function_decls,
+    ( Set_of_closures.create ~value_slots new_closure_alloc_mode function_decls,
       dacc,
       code_id,
       code )
