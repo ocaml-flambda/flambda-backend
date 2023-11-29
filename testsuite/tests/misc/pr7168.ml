45457d9f98aeb2a6bd90d24b62a9125006701d5f
Xavier Clerc <xclerc@users.noreply.github.com>
2023-11-29 11:55:36 +0000

    flambda-backend: Remove CRs from the test suite (#2097)
    * Reclassify 'CR ocaml 5 runtime' comments
    
    * CR
    
    * Reimport "lib-hashtbl" test from upstream.
    
    * Simplify "pr7168" test.
    
    ---------
    
    Co-authored-by: Mark Shinwell <mshinwell@pm.me>

--- testsuite/tests/misc/pr7168.ml
+++ testsuite/tests/misc/pr7168.ml
@@ -72,16 +72,5 @@ let rec f x =
   ()
 
 let _ =
-  (* CR ocaml 5 runtime: we should be able
-   * to simplify this test along the same
-   * lines as upstream, as stack overflow
-   * detection is always supported in
-  * ocaml 5. *)
-  if (Gc.get ()).Gc.stack_limit = 0 then begin
-    (* We are in native code. Skip the test because some platforms cannot
-       reliably detect stack overflow. *)
-    Printf.printf "OK\n"
-  end else begin
-    try Sys.with_async_exns (fun () -> f 1)
-    with Stack_overflow -> Printf.printf "OK\n"
-  end
+  try Sys.with_async_exns (fun () -> f 1)
+  with Stack_overflow -> Printf.printf "OK\n"
