690c4aae158fc6d3a4bd1bf2318ac04ad4c5b4d8
Max Slater <max@thenumb.at>
2023-12-11 12:13:41 -0500

    tests

--- tests/simd/scalar_ops.ml
+++ tests/simd/scalar_ops.ml
@@ -9,6 +9,10 @@ let eq lv hv l h =
   if h <> hv then Printf.printf "%016Lx <> %016Lx\n" hv h
 ;;
 
+let eq' x y =
+  if x <> y then Printf.printf "%016Lx <> %016Lx\n" x y
+;;
+
 module Int64x2 = struct
 
   type t = int64x2
@@ -40,10 +44,9 @@ module Int64 = struct
       [@@noalloc] [@@unboxed] [@@builtin]
 
   let () =
-    eq (bit_deposit 3L 4L) (bit_deposit 235L 522L) 0x4L 0xAL
-  ;;
-
-  let () =
-    eq (bit_extract 3L 4L) (bit_extract 235L 522L) 0x0L 0x3L
+    eq' (bit_deposit 3L 4L) 0x4L;
+    eq' (bit_deposit 235L 522L) 0xAL;
+    eq' (bit_extract 3L 4L) 0x0L;
+    eq' (bit_extract 235L 522L) 0x3L
   ;;
 end
