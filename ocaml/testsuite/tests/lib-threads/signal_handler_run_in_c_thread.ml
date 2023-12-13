494ebf519401d525df45cc3204c88543989d24bb
Mark Shinwell <mshinwell@pm.me>
2023-12-13 16:14:04 +0000

    Add a test, which would segfault before this PR

---
+++ ocaml/testsuite/tests/lib-threads/signal_handler_run_in_c_thread.ml
@@ -0,0 +1,19 @@
+(* TEST
+   modules = "signal_handler_run_in_c_thread_stubs.c"
+   * runtime5
+   ** native
+   *** threads
+*)
+
+(* This doesn't actually need systhreads, but the requirement should
+   ensure the C pthreads-using code will build. *)
+
+external c_stub : unit -> unit = "test_signal_handler_run_in_c_thread"
+
+let () =
+  Sys.set_signal Sys.sigusr1 (Signal_handle (fun _ -> exit 0));
+  c_stub ();
+  while true do
+    (* Ensure pending actions are run, by forcing allocation *)
+    ignore (Sys.opaque_identity (Random.int 42, Random.int 42))
+  done
---
+++ ocaml/testsuite/tests/lib-threads/signal_handler_run_in_c_thread_stubs.c
@@ -0,0 +1,22 @@
+#include <pthread.h>
+#include <signal.h>
+#include <caml/mlvalues.h>
+
+static pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+static void* in_thread(void* unused)
+{
+  (void) pthread_cond_signal(&cond);
+  /* Signal to be received in this thread by the OCaml signal handler */
+  while (1);
+}
+
+value test_signal_handler_run_in_c_thread(value unit)
+{
+  pthread_t thread;
+  pthread_create(&thread, NULL, &in_thread, thread);
+  pthread_cond_wait(&cond, &mutex);
+  pthread_kill(thread, SIGUSR1);
+  return Val_unit;
+}
