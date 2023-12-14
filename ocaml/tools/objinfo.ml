a4b0c8250d61bebb7ed1a0aa602a786d0a2f8788
Luke Maurer <lmaurer@janestreet.com>
2023-12-14 11:01:43 +0000

    Copy new functionality from flambda_backend_objinfo to objinfo
    Since they need to pass the same tests, they need to support the same things.

--- ocaml/tools/objinfo.ml
+++ ocaml/tools/objinfo.ml
@@ -56,8 +56,14 @@ let print_impl_import import =
   let crco = Import_info.crc import in
   print_name_crc (Compilation_unit.name unit) crco
 
+let print_global_name_binding global =
+  printf "\t%a\n" Global.output global
+
 let print_line name =
   printf "\t%s\n" name
+
+let print_global_line glob =
+  printf "\t%a\n" Global.Name.output glob
 
 let print_global_as_name_line global =
   printf "\t%a\n" Global.Name.output (Global.to_name global)
@@ -69,6 +75,13 @@ let print_cmo_infos cu =
   printf "Unit name: %a\n" Compilation_unit.output cu.cu_name;
   print_string "Interfaces imported:\n";
   Array.iter print_intf_import cu.cu_imports;
+  let () =
+   match cu.cu_runtime_params with
+     | [||] -> ()
+     | params ->
+     print_string "Runtime parameters:\n";
+     Array.iter print_global_line params
+  in
   print_string "Required globals:\n";
   List.iter print_required_global cu.cu_required_globals;
   printf "Uses unsafe features: ";
@@ -96,7 +109,7 @@ let print_cma_infos (lib : Cmo_format.library) =
   printf "\n";
   List.iter print_cmo_infos lib.lib_units
 
-let print_cmi_infos name crcs kind params =
+let print_cmi_infos name crcs kind params global_name_bindings =
   let open Cmi_format in
   printf "Unit name: %a\n" Compilation_unit.Name.output name;
   let is_param =
@@ -108,7 +121,9 @@ let print_cmi_infos name crcs kind params =
   print_string "Parameters:\n";
   List.iter print_global_as_name_line params;
   printf "Interfaces imported:\n";
-  Array.iter print_intf_import crcs
+  Array.iter print_intf_import crcs;
+  printf "Globals in scope:\n";
+  Array.iter print_global_name_binding global_name_bindings
 
 let print_cmt_infos cmt =
   let open Cmt_format in
@@ -353,6 +368,7 @@ let dump_obj_by_kind filename ic obj_kind =
          | Some cmi ->
             print_cmi_infos cmi.Cmi_format.cmi_name cmi.Cmi_format.cmi_crcs
               cmi.Cmi_format.cmi_kind cmi.Cmi_format.cmi_params
+              cmi.Cmi_format.cmi_globals
        end;
        begin match cmt with
          | None -> ()
