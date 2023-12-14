0a56ff7030fced93124e8999bd1d87823b954724
Guillaume Bury <guillaume.bury@gmail.com>
2023-12-14 14:55:50 +0100

    Add CR

--- middle_end/flambda2/tests/tools/flexpect.ml
+++ middle_end/flambda2/tests/tools/flexpect.ml
@@ -39,6 +39,8 @@ let run_expect_test ~get_module_info ~extension ~filename
   let before_fl = Fexpr_to_flambda.conv comp_unit before in
   check_invariants before_fl;
   let cmx_loader = Flambda_cmx.create_loader ~get_module_info in
+  (* CR gbury/lmaurer: add a proper traversal to compute the actual
+        code_slot_offsets here (as well as free_names) *)
   let { Simplify.unit = actual_fl; _ } =
     Simplify.run ~cmx_loader ~round:0 before_fl
       ~code_slot_offsets:Code_id.Map.empty
