9c2d879d400ecd1bb1a23124c40cf8c9db1af30b
Greta Yorsh <gyorsh@janestreet.com>
2023-12-14 10:49:54 +0000

    Add a comment to the test

--- tests/backend/checkmach/test_assume_on_call.ml
+++ tests/backend/checkmach/test_assume_on_call.ml
@@ -1,5 +1,9 @@
 let[@inline always] bar x = (x,x)
 
+(* The following functions currently report misplaced attribute warnings.
+   They are expected to compile successfully and pass the check
+   when zero_alloc annotations on function calls are supported. *)
+
 let[@zero_alloc] test1 x =
   (bar[@zero_alloc]) x
 
