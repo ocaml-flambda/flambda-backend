79acdaa7fc79536c08e644376b421464e9069f84
Guillaume Bury <guillaume.bury@gmail.com>
2023-12-14 15:04:17 +0100

    make fmt

--- middle_end/flambda2/tests/tools/flexpect.ml
+++ middle_end/flambda2/tests/tools/flexpect.ml
@@ -40,7 +40,7 @@ let run_expect_test ~get_module_info ~extension ~filename
   check_invariants before_fl;
   let cmx_loader = Flambda_cmx.create_loader ~get_module_info in
   (* CR gbury/lmaurer: add a proper traversal to compute the actual
-        code_slot_offsets here (as well as free_names) *)
+     code_slot_offsets here (as well as free_names) *)
   let { Simplify.unit = actual_fl; _ } =
     Simplify.run ~cmx_loader ~round:0 before_fl
       ~code_slot_offsets:Code_id.Map.empty
