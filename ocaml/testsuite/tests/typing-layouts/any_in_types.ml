b57bbb971782da9bc0e8aa613b4b35d32d581f21
alanechang <alanechang@janestreet.com>
2023-12-21 17:34:57 -0500

    small test changes

--- ocaml/testsuite/tests/typing-layouts/any_in_types.ml
+++ ocaml/testsuite/tests/typing-layouts/any_in_types.ml
@@ -1,5 +1,5 @@
 (* TEST
-   flags = "-extension layouts_alpha"
+   flags = "-extension layouts"
    * native
    * bytecode
 *)
@@ -17,6 +17,7 @@ module type S = sig
   val print : (unit -> t) -> unit
 end
 
+(* type substitution *)
 module M1 : S with type t = float# = struct
   type t = float#
 
@@ -30,12 +31,29 @@ module M2 : S with type t = int = struct
 
   let add x y = x + y
   let one () = 1
-  let print f = Printf.printf "Printing a int:  %d\n" (f ())
+  let print f = Printf.printf "Printing a int: %d\n" (f ())
 end
 
 let () = Printf.printf "%f\n" (Stdlib__Float_u.to_float (M1.add (unbox 10.) (unbox 10.)))
 let () = Printf.printf "%d\n" (M2.add 10 10)
 
+(* destructive substitution *)
+module M3 : S with type t := float# = struct
+  let add x y = Stdlib__Float_u.add x y
+  let one () = unbox 1.
+  let print f = Printf.printf "Printing a float#: %f\n" (Stdlib__Float_u.to_float (f ()))
+end
+
+module M4 : S with type t := int = struct
+  let add x y = x + y
+  let one () = 1
+  let print f = Printf.printf "Printing a int: %d\n" (f ())
+end
+
+let () = Printf.printf "%f\n" (Stdlib__Float_u.to_float (M3.add (unbox 10.) (unbox 10.)))
+let () = Printf.printf "%d\n" (M4.add 10 10)
+
+(* functor *)
 module type Q = sig
   include S
 
@@ -53,6 +71,8 @@ module M2' = Make (M2)
 
 let () = M1'.print_one ()
 let () = M2'.print_one ()
+
+(* edge cases and order of evaluation *)
 let g : type (a : any). unit -> a -> a = fun () -> assert false
 
 let () =
