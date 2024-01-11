102334a7755f1c4a0abc14775e00a2abdf8c825b
Xavier Clerc <xclerc@janestreet.com>
2024-01-11 09:55:21 +0000

    Greedy POC.

---
+++ backend/regalloc/regalloc_gi_state.ml
@@ -0,0 +1,44 @@
+[@@@ocaml.warning "+a-4-30-40-41-42"]
+
+open! Regalloc_utils
+open! Regalloc_gi_utils
+
+type t =
+  { mutable assignments : Hardware_register.location Reg.Map.t;
+    mutable introduced_temporaries : Reg.Set.t;
+    stack_slots : Regalloc_stack_slots.t;
+    mutable next_instruction_id : Instruction.id
+  }
+
+let[@inline] make ~stack_slots ~next_instruction_id =
+  let assignments = Reg.Map.empty in
+  let introduced_temporaries = Reg.Set.empty in
+  { assignments; introduced_temporaries; stack_slots; next_instruction_id }
+
+let[@inline] add_assignment state reg ~to_ =
+  state.assignments <- Reg.Map.add reg to_ state.assignments
+
+let[@inline] remove_assignment state reg =
+  state.assignments <- Reg.Map.remove reg state.assignments
+
+let[@inline] find_assignment state reg = Reg.Map.find_opt reg state.assignments
+
+let[@inline] clear_assignments state = state.assignments <- Reg.Map.empty
+
+let[@inline] add_introduced_temporaries_list state l =
+  state.introduced_temporaries
+    <- List.fold_left l ~init:state.introduced_temporaries ~f:(fun set reg ->
+           Reg.Set.add reg set)
+
+let[@inline] mem_introduced_temporaries state reg =
+  Reg.Set.mem reg state.introduced_temporaries
+
+let[@inline] iter_introduced_temporaries state ~f =
+  Reg.Set.iter f state.introduced_temporaries
+
+let[@inline] stack_slots state = state.stack_slots
+
+let[@inline] get_and_incr_instruction_id state =
+  let res = state.next_instruction_id in
+  state.next_instruction_id <- succ res;
+  res
