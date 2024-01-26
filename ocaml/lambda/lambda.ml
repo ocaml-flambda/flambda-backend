6f39b8cbf4f91f10174e40e91714900f38a6012f
Mark Shinwell <mshinwell@pm.me>
2024-01-26 11:31:16 +0000

    Update primitive_result_layout

--- ocaml/lambda/lambda.ml
+++ ocaml/lambda/lambda.ml
@@ -746,7 +746,17 @@ let layout_unboxed_nativeint = Punboxed_int Pnativeint
 let layout_unboxed_int32 = Punboxed_int Pint32
 let layout_unboxed_int64 = Punboxed_int Pint64
 let layout_string = Pvalue Pgenval
+let layout_unboxed_int ubi = Punboxed_int ubi
 let layout_boxedint bi = Pvalue (Pboxedintval bi)
+
+let layout_unboxed_vector (v : Primitive.boxed_vector) =
+  match v with
+  | Pvec128 Int8x16 -> Punboxed_vector (Pvec128 Int8x16)
+  | Pvec128 Int16x8 -> Punboxed_vector (Pvec128 Int16x8)
+  | Pvec128 Int32x4 -> Punboxed_vector (Pvec128 Int32x4)
+  | Pvec128 Int64x2 -> Punboxed_vector (Pvec128 Int64x2)
+  | Pvec128 Float32x4 -> Punboxed_vector (Pvec128 Float32x4)
+  | Pvec128 Float64x2 -> Punboxed_vector (Pvec128 Float64x2)
 
 let layout_boxed_vector : Primitive.boxed_vector -> layout = function
   | Pvec128 Int8x16 -> Pvalue (Pboxedvectorval (Pvec128 Int8x16))
@@ -1674,12 +1684,19 @@ let primitive_result_layout (p : primitive) =
   | Pbbswap (bi, _) | Pbox_int (bi, _) ->
       layout_boxedint bi
   | Punbox_int bi -> Punboxed_int bi
-  | Pstring_load_32 _ | Pbytes_load_32 _ | Pbigstring_load_32 _ ->
+  | Pstring_load_32 _ | Pbytes_load_32 _
+  | Pbigstring_load_32 { boxed = true; _ } ->
       layout_boxedint Pint32
-  | Pstring_load_64 _ | Pbytes_load_64 _ | Pbigstring_load_64 _ ->
+  | Pstring_load_64 _ | Pbytes_load_64 _
+  | Pbigstring_load_64 { boxed = true; _ } ->
       layout_boxedint Pint64
-  | Pstring_load_128 _ | Pbytes_load_128 _ | Pbigstring_load_128 _ ->
+  | Pstring_load_128 _ | Pbytes_load_128 _
+  | Pbigstring_load_128 { boxed = true; _ } ->
       layout_boxed_vector (Pvec128 Int8x16)
+  | Pbigstring_load_32 { boxed = false; _ } -> layout_unboxed_int Pint32
+  | Pbigstring_load_64 { boxed = false; _ } -> layout_unboxed_int Pint64
+  | Pbigstring_load_128 { boxed = false; _ } ->
+      layout_unboxed_vector (Pvec128 Int8x16)
   | Pbigarrayref (_, _, kind, _) ->
       begin match kind with
       | Pbigarray_unknown -> layout_any_value
