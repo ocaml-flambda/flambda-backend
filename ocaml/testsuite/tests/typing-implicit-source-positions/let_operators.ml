6dbf7c47a288b212802b8923d7a431de7814e375
enoumy <enoumy@gmail.com>
2024-02-06 20:09:29 -0500

    Implicit Source Positions Conflict resolution
    This feature solely fixes merge conflicts that the implicit
    source positions project had after not having been rebased since the
    summer.
    
    Sadly, I mistakenly committed a new test! I meant to do it on a
    different branch, but I accidentally committed some more changes
    after that making it trickier to split... Please let me know if I should
    only keep this feature do "merge conflict resolution". Thanks!
    
    Testing
    -------
    - I ran `make test`, and it passes.
    - `make install` works, I migrated bonsai to start using this and it works!
    - Added test on let operators.
      - Sadly it doesn't quite work, but I think it would be cool if it did
        work as it would allow codebases that use let+ and let* to get
        source code locations! I am unsure about its sound-ness though and
        left CR src_pos'es/question CR's.
    
    Please let me know if there is other additional testing/ci I should perform. Thanks!
    
    I also performed additional cleanup in this feature from self-questions
    I had during the merge. Please let me know if there are any changes I
    should perform. Thanks!

--- ocaml/testsuite/tests/typing-implicit-source-positions/let_operators.ml
+++ ocaml/testsuite/tests/typing-implicit-source-positions/let_operators.ml
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 (* TEST
    * expect
 *)
@@ -50,3 +51,58 @@ val ( >>| ) : src_pos:[%src_pos] -> 'a -> (lexing_position * 'a -> 'b) -> 'b =
 - : lexing_position =
 {pos_fname = ""; pos_lnum = 3; pos_bol = 1108; pos_cnum = 1110}
 |}]
+||||||| parent of 6b207b13 (Implicit Source Positions Conflict resolution)
+=======
+(* TEST
+   * expect
+*)
+
+let ( let+ ) ~(src_pos : [%src_pos]) a f = f (src_pos, a);;
+[%%expect{|
+val ( let+ ) : src_pos:[%src_pos] -> 'a -> (lexing_position * 'a -> 'b) -> 'b =
+  <fun>
+|}]
+
+(* Would be nice to add support for implicit position parameters and (also maybe optional
+   arguments) for let operators. *)
+let _ = 
+  let+ (src_pos, a) = 1 in
+  src_pos
+
+[%%expect{|
+Line 2, characters 2-6:
+2 |   let+ (src_pos, a) = 1 in
+      ^^^^
+Error: The operator let+ has type
+         src_pos:[%src_pos] -> 'a -> (lexing_position * 'a -> 'b) -> 'b
+       but it was expected to have type 'c -> ('d -> 'e) -> 'f
+|}]
+
+let ( let* ) ?(src_pos = 1) a g = g (src_pos, a);; 
+
+let _ =
+  let* (src_pos, a) = 1 in
+  src_pos
+
+[%%expect{|
+val ( let* ) : ?src_pos:int -> 'a -> (int * 'a -> 'b) -> 'b = <fun>
+Line 4, characters 2-6:
+4 |   let* (src_pos, a) = 1 in
+      ^^^^
+Error: The operator let* has type
+         ?src_pos:int -> 'a -> (int * 'a -> 'b) -> 'b
+       but it was expected to have type 'c -> ('d -> 'e) -> 'f
+|}]
+
+(* Infix operators work! *)
+let ( >>| ) ~(src_pos : [%src_pos]) x f = f (src_pos, x)
+let _ =
+  1 >>| fun (src_pos, a) -> src_pos
+
+[%%expect{|
+val ( >>| ) : src_pos:[%src_pos] -> 'a -> (lexing_position * 'a -> 'b) -> 'b =
+  <fun>
+- : lexing_position =
+{pos_fname = ""; pos_lnum = 3; pos_bol = 1108; pos_cnum = 1110}
+|}]
+>>>>>>> 6b207b13 (Implicit Source Positions Conflict resolution)
