0c0560cd995b899b5f90cceab5e812b83d2c5d6d
Zesen Qian <github@riaqn.org>
2024-02-20 13:54:19 +0000

    improve tests

--- ocaml/testsuite/tests/typing-unique/unique.ml
+++ ocaml/testsuite/tests/typing-unique/unique.ml
@@ -542,38 +542,38 @@ type box = { x : int }
 type box = { x : int; }
 |}]
 
-let curry (unique_ b1 : box) (unique_ b2 : box) = b1
+let curry (unique_ b1 : box) (unique_ b2 : box) = ()
 [%%expect{|
-val curry : unique_ box -> unique_ box -> box = <fun>
+val curry : unique_ box -> unique_ box -> unit = <fun>
 |}]
 
-let curry : unique_ box -> unique_ box -> unique_ box = fun b1 b2 -> b1
+let curry : unique_ box -> unique_ box -> unit = fun b1 b2 -> ()
 [%%expect{|
-val curry : unique_ box -> unique_ box -> unique_ box = <fun>
+val curry : unique_ box -> unique_ box -> unit = <fun>
 |}]
 
-let curry : unique_ box -> (unique_ box -> unique_ box) = fun b1 b2 -> b1
+let curry : unique_ box -> (unique_ box -> unit) = fun b1 b2 -> ()
 [%%expect{|
-Line 1, characters 58-73:
-1 | let curry : unique_ box -> (unique_ box -> unique_ box) = fun b1 b2 -> b1
-                                                              ^^^^^^^^^^^^^^^
+Line 1, characters 51-66:
+1 | let curry : unique_ box -> (unique_ box -> unit) = fun b1 b2 -> ()
+                                                       ^^^^^^^^^^^^^^^
 Error: This function when partially applied returns a once value,
        but expected to be many.
 |}]
 
-let curry : unique_ box -> (unique_ box -> unique_ box) = fun b1 -> function | b2 -> b2
+let curry : unique_ box -> (unique_ box -> unit) = fun b1 -> function | b2 -> ()
 [%%expect{|
-Line 1, characters 58-87:
-1 | let curry : unique_ box -> (unique_ box -> unique_ box) = fun b1 -> function | b2 -> b2
-                                                              ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+Line 1, characters 51-80:
+1 | let curry : unique_ box -> (unique_ box -> unit) = fun b1 -> function | b2 -> ()
+                                                       ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 Error: This function when partially applied returns a once value,
        but expected to be many.
 |}]
 
 (* For nested functions, inner functions are not constrained *)
-let no_curry : unique_ box -> (unique_ box -> unique_ box) = fun b1 -> fun b2 -> b2
+let no_curry : unique_ box -> (unique_ box -> unit) = fun b1 -> fun b2 -> ()
 [%%expect{|
-val no_curry : unique_ box -> (unique_ box -> unique_ box) = <fun>
+val no_curry : unique_ box -> (unique_ box -> unit) = <fun>
 |}]
 
 (* If both type and mode are wrong, complain about type *)
