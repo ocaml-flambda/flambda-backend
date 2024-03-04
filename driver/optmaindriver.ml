f20575d76e64e68b5a26a976459753f704968b06
Xavier Clerc <xclerc@janestreet.com>
2024-03-04 13:34:17 +0000

    Add a constructor to stop after middle-end.

--- driver/optmaindriver.ml
+++ driver/optmaindriver.ml
@@ -90,7 +90,7 @@ let main unix argv ppf ~flambda2 =
       | None ->
           Compenv.fatal "Please specify at most one of -pack, -a, -shared, -c, \
                          -output-obj";
-      | Some ((P.Parsing | P.Typing | P.Lambda | P.Scheduling
+      | Some ((P.Parsing | P.Typing | P.Lambda | P.Middle_end | P.Scheduling
               | P.Simplify_cfg | P.Emit | P.Selection) as p) ->
         assert (P.is_compilation_pass p);
         Printf.ksprintf Compenv.fatal
