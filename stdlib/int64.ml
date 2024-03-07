536d7febf0758b5116fcb1c1b17f1c3f0b17b43e
alanechang <alanechang@janestreet.com>
2024-03-07 12:31:03 -0500

    flambda-backend: Add `caml_*_of_string_unboxed` runtime primitives for boxed ints (#2315)

--- stdlib/int64.ml
+++ stdlib/int64.ml
@@ -66,7 +66,8 @@ let unsigned_to_int =
 external format : string -> int64 -> string = "caml_int64_format"
 let[@inline available] to_string n = format "%d" n
 
-external of_string : string -> int64 = "caml_int64_of_string"
+external of_string : string -> (int64[@unboxed])
+  = "caml_int64_of_string" "caml_int64_of_string_unboxed"
 
 let[@inline available] of_string_opt s =
   (* TODO: expose a non-raising primitive directly. *)
--- stdlib/nativeint.ml
+++ stdlib/nativeint.ml
@@ -65,7 +65,8 @@ let unsigned_to_int =
 external format : string -> nativeint -> string = "caml_nativeint_format"
 let[@inline available] to_string n = format "%d" n
 
-external of_string: string -> nativeint = "caml_nativeint_of_string"
+external of_string: string -> (nativeint[@unboxed])
+  = "caml_nativeint_of_string" "caml_nativeint_of_string_unboxed"
 
 let[@inline available] of_string_opt s =
   (* TODO: expose a non-raising primitive directly. *)
