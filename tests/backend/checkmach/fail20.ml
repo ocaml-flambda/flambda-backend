bd88137dce540f3060a086fd8625e8a4f16bff09
Greta Yorsh <gyorsh@janestreet.com>
2024-03-07 17:38:56 +0000

    Add a test for unresolved dependencies

--- tests/backend/checkmach/fail20.ml
+++ tests/backend/checkmach/fail20.ml
@@ -9,3 +9,11 @@ let[@zero_alloc strict] foo x =
   else if x < 0 then
     List.nth (div (x+2)) x
   else nor x
+
+let[@zero_alloc][@inline never][@local never]  foo x =
+  let[@inline never][@local never] bar x y =
+    (Sys.opaque_identity (x + y), x)
+  in
+  fst (bar x (x*x))
+
+let[@zero_alloc] zee x f = foo (Sys.opaque_identity x+1)
