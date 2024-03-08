35056a911c6300f29e2f19c798e45784b4203a80
alanechang <alanechang@janestreet.com>
2024-03-08 13:51:29 -0500

    lock&unlock mutex

--- ocaml/testsuite/tests/lib-threads/signal_handler_run_in_c_thread_stubs.c
+++ ocaml/testsuite/tests/lib-threads/signal_handler_run_in_c_thread_stubs.c
@@ -1,6 +1,7 @@
 #include <pthread.h>
 #include <signal.h>
 #include <caml/mlvalues.h>
+#include <assert.h>
 
 static pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
@@ -16,7 +17,9 @@ value test_signal_handler_run_in_c_thread(value unit)
 {
   pthread_t thread;
   pthread_create(&thread, NULL, &in_thread, NULL);
+  assert(!pthread_mutex_lock(&mutex));
   pthread_cond_wait(&cond, &mutex);
+  assert(!pthread_mutex_unlock(&mutex));
   pthread_kill(thread, SIGUSR1);
   return Val_unit;
 }
