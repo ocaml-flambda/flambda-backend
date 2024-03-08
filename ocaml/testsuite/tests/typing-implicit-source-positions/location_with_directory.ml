44f1801251e34ac30990e50b7c6e535de41904c5
enoumy <enoumy@gmail.com>
2024-03-08 16:58:36 -0500

    Changed test to properly test that the right basename is supplied

--- ocaml/testsuite/tests/typing-implicit-source-positions/location_with_directory.ml
+++ ocaml/testsuite/tests/typing-implicit-source-positions/location_with_directory.ml
@@ -1,17 +1,12 @@
 (* TEST
-   * expect
+   * native
 
    flags = "-dir app/foo"
 *)
 
 let f = fun ~(src_pos:[%src_pos]) () -> src_pos
-[%%expect{|
-val f : src_pos:[%src_pos] -> unit -> lexing_position = <fun>
-|}]
+let _ =
+  print_string (f ()).pos_fname;;
 
-let _ = f ();;
-[%%expect{|
-- : lexing_position =
-{pos_fname = "app/foo/"; pos_lnum = 1; pos_bol = 178; pos_cnum = 186}
-|}]
+
 
