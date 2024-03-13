6c19a9882af7846a2ef955c10f0888169cf4401b
Greta Yorsh <gyorsh@janestreet.com>
2024-03-13 14:50:01 +0000

    Add new tests for -disable-precise-checkmach and -disable-checkmach
    and -function-layout

--- tests/backend/checkmach/gen/gen_dune.ml
+++ tests/backend/checkmach/gen/gen_dune.ml
@@ -123,5 +123,11 @@ let () =
   print_test_expected_output ~cutoff:default_cutoff ~extra_dep:None ~exit_code:2 "test_attr_check_opt";
   print_test_expected_output ~cutoff:default_cutoff ~extra_dep:None ~exit_code:0 "test_attr_check_none";
   print_test_expected_output ~cutoff:default_cutoff ~extra_dep:None ~exit_code:2 "fail24";
-  print_test "test_raise_message.ml";
+  print_test ~extra_flags:"-zero-alloc-check default -function-layout topological"  "test_raise_message.ml";
+  print_test_expected_output ~cutoff:default_cutoff
+    ~extra_flags:"-zero-alloc-check default -disable-precise-checkmach -function-layout source"
+    ~extra_dep:None ~exit_code:2 "fail25";
+  print_test_expected_output ~cutoff:default_cutoff
+    ~extra_flags:"-zero-alloc-check default -disable-checkmach -function-layout source"
+    ~extra_dep:None ~exit_code:2 "fail26";
   ()
