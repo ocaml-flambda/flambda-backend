284b012c5222da69513a4e760d95d0ce821a97fa
Zesen Qian <github@riaqn.org>
2024-03-19 09:20:31 +0000

    add missing reduction

--- ocaml/typing/mode.ml
+++ ocaml/typing/mode.ml
@@ -883,13 +883,24 @@ module Lattices_mono = struct
       type a b c d.
       c obj -> (b, c, d) morph -> (a, b, d) morph -> (a, c, d) morph option =
    fun dst m0 m1 ->
+    let mid = src dst m0 in
     match m0, m1 with
     | Id, m -> Some m
     | m, Id -> Some m
     | Meet_with c0, Meet_with c1 -> Some (Meet_with (meet dst c0 c1))
     | Join_with c0, Join_with c1 -> Some (Join_with (join dst c0 c1))
+    | Imply c0, Imply c1 -> Some (Imply (meet dst c0 c1))
+    | Subtract c0, Subtract c1 -> Some (Subtract (join dst c0 c1))
+    | Imply c0, Join_with c1 when le dst c0 c1 -> Some (Join_with (max dst))
+    | Subtract c0, Meet_with c1 when le dst c1 c0 -> Some (Meet_with (min dst))
     | Meet_with c0, m1 when le dst (max dst) c0 -> Some m1
     | Join_with c0, m1 when le dst c0 (min dst) -> Some m1
+    | Imply c0, m1 when le dst (max dst) c0 -> Some m1
+    | Subtract c0, m1 when le dst c0 (min dst) -> Some m1
+    | m1, Meet_with c0 when le mid (max mid) c0 -> Some m1
+    | m1, Join_with c0 when le mid c0 (min mid) -> Some m1
+    | m1, Imply c0 when le mid (max mid) c0 -> Some m1
+    | m1, Subtract c0 when le mid c0 (min mid) -> Some m1
     | Compose (f0, f1), g -> (
       let mid = src dst f0 in
       match maybe_compose mid f1 g with
@@ -901,9 +912,9 @@ module Lattices_mono = struct
       | Some m -> Some (compose dst m g1)
       | None -> None)
     | Proj (mid, ax), Meet_with c ->
-      Some (Compose (Meet_with (proj ax c), Proj (mid, ax)))
+      Some (compose dst (Meet_with (proj ax c)) (Proj (mid, ax)))
     | Proj (mid, ax), Join_with c ->
-      Some (Compose (Join_with (proj ax c), Proj (mid, ax)))
+      Some (compose dst (Join_with (proj ax c)) (Proj (mid, ax)))
     | Proj (_, ax0), Max_with ax1 -> (
       match eq_axis ax0 ax1 with None -> None | Some Refl -> Some Id)
     | Proj (_, ax0), Min_with ax1 -> (
@@ -962,6 +973,20 @@ module Lattices_mono = struct
         (compose dst
            (Meet_with (max_with dst Linearity linearity))
            (Map_comonadic (compose dst0 f (Meet_with areality))))
+    | Map_comonadic f, Imply c ->
+      let dst0 = proj_obj Areality dst in
+      let areality, linearity = c in
+      Some
+        (compose dst
+           (Imply (max_with dst Linearity linearity))
+           (Map_comonadic (compose dst0 f (Imply areality))))
+    | Map_comonadic f, Subtract c ->
+      let dst0 = proj_obj Areality dst in
+      let areality, linearity = c in
+      Some
+        (compose dst
+           (Subtract (min_with dst Linearity linearity))
+           (Map_comonadic (compose dst0 f (Subtract areality))))
     | Regional_to_global, Locality_as_regionality -> Some Id
     | Regional_to_global, Local_to_regional -> Some (Meet_with Locality.Global)
     | Local_to_regional, Regional_to_local -> None
