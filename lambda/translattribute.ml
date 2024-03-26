73200c0a534d77e60f2f5cbc95aaf1b82d5a17e3
Greta Yorsh <45005955+gretay-js@users.noreply.github.com>
2024-03-26 11:12:11 +0000

    flambda-backend: zero alloc: assume on calls (#2165)

--- lambda/translattribute.ml
+++ lambda/translattribute.ml
@@ -463,14 +463,26 @@ let add_local_attribute expr loc attributes =
     end
   | _ -> expr
 
-let assume_zero_alloc ?mark_used attributes =
+let assume_zero_alloc ?mark_used attributes : Assume_info.t =
   let p = Zero_alloc in
   let attr = find_attribute ?mark_used (is_property_attribute p) attributes in
-  match parse_property_attribute attr p with
-  | Default_check -> false
-  | Ignore_assert_all _ -> false
-  | Assume { property = Zero_alloc; _ } -> true
-  | Check { property = Zero_alloc; _ } -> false
+  let res = parse_property_attribute attr p in
+  match res with
+  | Default_check -> Assume_info.none
+  | Ignore_assert_all _ -> Assume_info.none
+  | Assume { strict; never_returns_normally; } ->
+    Assume_info.create ~strict ~never_returns_normally
+  | Check { loc; _ } ->
+    let attr = Option.get attr in
+    let name = attr.attr_name.txt in
+    let msg = "Only the following combinations are supported in this context: \
+               'zero_alloc assume', \
+               `zero_alloc assume strict`, \
+               `zero_alloc assume never_returns_normally`,\
+               `zero_alloc assume never_returns_normally strict`."
+    in
+    Location.prerr_warning loc (Warnings.Attribute_payload (name, msg));
+    Assume_info.none
 
 let get_assume_zero_alloc ~with_warnings attributes =
   if with_warnings then
---
+++ utils/zero_alloc_utils.ml
@@ -0,0 +1,120 @@
+module type WS = sig
+  type t
+
+  val join : t -> t -> t
+
+  val meet : t -> t -> t
+
+  val print : Format.formatter -> t -> unit
+
+  val compare : t -> t -> int
+end
+
+module Make (Witnesses : WS) = struct
+  (** Abstract value for each component of the domain. *)
+  module V = struct
+    type t =
+      | Top of Witnesses.t
+      | Safe
+      | Bot
+
+    let join c1 c2 =
+      match c1, c2 with
+      | Bot, Bot -> Bot
+      | Safe, Safe -> Safe
+      | Top w1, Top w2 -> Top (Witnesses.join w1 w2)
+      | Safe, Bot | Bot, Safe -> Safe
+      | Top w1, Bot | Top w1, Safe | Bot, Top w1 | Safe, Top w1 -> Top w1
+
+    let meet c1 c2 =
+      match c1, c2 with
+      | Bot, Bot -> Bot
+      | Safe, Safe -> Safe
+      | Top w1, Top w2 -> Top (Witnesses.meet w1 w2)
+      | Safe, Bot | Bot, Safe -> Bot
+      | Top _, Bot | Bot, Top _ -> Bot
+      | Top _, Safe | Safe, Top _ -> Safe
+
+    let lessequal v1 v2 =
+      match v1, v2 with
+      | Bot, Bot -> true
+      | Safe, Safe -> true
+      | Top _, Top _ -> true
+      | Bot, Safe -> true
+      | Bot, Top _ -> true
+      | Safe, Top _ -> true
+      | Top _, (Bot | Safe) -> false
+      | Safe, Bot -> false
+
+    let compare t1 t2 =
+      match t1, t2 with
+      | Bot, Bot -> 0
+      | Safe, Safe -> 0
+      | Top w1, Top w2 -> Witnesses.compare w1 w2
+      | Bot, (Safe | Top _) -> -1
+      | (Safe | Top _), Bot -> 1
+      | Safe, Top _ -> -1
+      | Top _, Safe -> 1
+
+    let is_not_safe = function Top _ -> true | Safe | Bot -> false
+
+    let print ~witnesses ppf = function
+      | Bot -> Format.fprintf ppf "bot"
+      | Top w ->
+        Format.fprintf ppf "top";
+        if witnesses then Format.fprintf ppf " (%a)" Witnesses.print w
+      | Safe -> Format.fprintf ppf "safe"
+  end
+
+  module Value = struct
+    (** Lifts V to triples  *)
+    type t =
+      { nor : V.t;
+        exn : V.t;
+        div : V.t
+      }
+
+    let bot = { nor = V.Bot; exn = V.Bot; div = V.Bot }
+
+    let lessequal v1 v2 =
+      V.lessequal v1.nor v2.nor && V.lessequal v1.exn v2.exn
+      && V.lessequal v1.div v2.div
+
+    let join v1 v2 =
+      { nor = V.join v1.nor v2.nor;
+        exn = V.join v1.exn v2.exn;
+        div = V.join v1.div v2.div
+      }
+
+    let meet v1 v2 =
+      { nor = V.meet v1.nor v2.nor;
+        exn = V.meet v1.exn v2.exn;
+        div = V.meet v1.div v2.div
+      }
+
+    let normal_return = { bot with nor = V.Safe }
+
+    let exn_escape = { bot with exn = V.Safe }
+
+    let diverges = { bot with div = V.Safe }
+
+    let safe = { nor = V.Safe; exn = V.Safe; div = V.Safe }
+
+    let top w = { nor = V.Top w; exn = V.Top w; div = V.Top w }
+
+    let relaxed w = { nor = V.Safe; exn = V.Top w; div = V.Top w }
+
+    let print ~witnesses ppf { nor; exn; div } =
+      let pp = V.print ~witnesses in
+      Format.fprintf ppf "{ nor=%a; exn=%a; div=%a }" pp nor pp exn pp div
+
+    let compare { nor = n1; exn = e1; div = d1 }
+        { nor = n2; exn = e2; div = d2 } =
+      let c = V.compare n1 n2 in
+      if c <> 0
+      then c
+      else
+        let c = V.compare e1 e2 in
+        if c <> 0 then c else V.compare d1 d2
+  end
+end
