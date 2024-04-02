b339ddddbb2642a70ac836b4829022e95e49d509
Richard Eisenberg <reisenberg@janestreet.com>
2024-04-02 10:28:34 -0400

    ocamlformat in JS-only files in lambda (#1882)
    ocamlformat in JS-only files in lambda/
    
    Co-authored-by: Zesen Qian <github@riaqn.org>

--- ocaml/lambda/transl_array_comprehension.mli
+++ ocaml/lambda/transl_array_comprehension.mli
@@ -21,10 +21,10 @@ open Debuginfo.Scoped_location
     This function needs to translate expressions from Typedtree into Lambda, and
     so is parameterized by [Translcore.transl_exp], its [scopes] argument, and
     the [loc]ation. *)
-val comprehension
-  :  transl_exp:(scopes:scopes -> Jkind.sort -> expression -> lambda)
-  -> scopes:scopes
-  -> loc:scoped_location
-  -> array_kind:array_kind
-  -> comprehension
-  -> lambda
+val comprehension :
+  transl_exp:(scopes:scopes -> Jkind.sort -> expression -> lambda) ->
+  scopes:scopes ->
+  loc:scoped_location ->
+  array_kind:array_kind ->
+  comprehension ->
+  lambda
