134c753913325bce601e1c6bddece291ef4261df
TheNumbat <max@thenumb.at>
2024-04-02 11:25:10 -0400

    restore copyright headers

--- ocaml/runtime/float32.c
+++ ocaml/runtime/float32.c
@@ -2,9 +2,11 @@
 /*                                                                        */
 /*                                 OCaml                                  */
 /*                                                                        */
+/*             Xavier Leroy, projet Cristal, INRIA Rocquencourt           */
 /*                      Max Slater, Jane Street                           */
 /*                                                                        */
-/*   Copyright 2024 Jane Street Group LLC                                 */
+/*   Copyright 1996 Institut National de Recherche en Informatique et     */
+/*     en Automatique.                                                    */
 /*                                                                        */
 /*   All rights reserved.  This file is distributed under the terms of    */
 /*   the GNU Lesser General Public License version 2.1, with the          */
