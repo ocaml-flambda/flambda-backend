d2029c6be4507b6cd18a3f7453770d08e06d2cda
Mark Shinwell <mshinwell@pm.me>
2024-04-09 13:32:41 +0100

    Add backend/cfg/cfg_stack_checks.ml{,i}

---
+++ backend/cfg/cfg_stack_checks.mli
@@ -0,0 +1,15 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                 OCaml                                  *)
+(*                                                                        *)
+(*                       Xavier Clerc, Jane Street                        *)
+(*                                                                        *)
+(*   Copyright 2024 Jane Street Group LLC                                 *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
+(*   special exception on linking described in the file LICENSE.          *)
+(*                                                                        *)
+(**************************************************************************)
+
+val cfg : Cfg_with_layout.t -> Cfg_with_layout.t
