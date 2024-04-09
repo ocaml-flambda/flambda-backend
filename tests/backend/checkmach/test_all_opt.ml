9877b4630a0251fcd332d5005aad97ae5014a61f
Greta Yorsh <gyorsh@janestreet.com>
2024-04-09 11:52:12 +0100

    Update test output and remove CR-soon.

--- tests/backend/checkmach/test_all_opt.ml
+++ tests/backend/checkmach/test_all_opt.ml
@@ -7,13 +7,3 @@ let[@zero_alloc opt] foo x = (x,x)
 let[@zero_alloc] bar x = [x;x]
 
 let baz x = [|x;x+1|]
-(* CR-soon gyorsh:
-   The check fails on [baz] with -check-zero-alloc default/all/opt.
-   This shows that the current interpretation of [@@@zero_alloc all] on functions
-   that do not have an explicit "zero_alloc" attribute
-   is to require "zero all" with both "opt" and "default" flags.
-   This behavior behavior exposes an implementation detail that does not matter in
-   practice because "opt" flag is only intended for testing, and the behavior
-   with "default" and "all" flags is indistinguishable.
-   Fix it in the next commit.
-*)
