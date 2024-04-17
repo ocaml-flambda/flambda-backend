683d7dda82d3b6347912caf6745a1d5ad69b4b9d
Xavier Clerc <xclerc@janestreet.com>
2024-04-17 11:32:10 +0100

    Update tests to new fomat.

--- ocaml/testsuite/tests/shape-index/index_aliases.ml
+++ ocaml/testsuite/tests/shape-index/index_aliases.ml
@@ -1,17 +1,17 @@
 (* TEST
-
-flags = "-bin-annot -bin-annot-occurrences"
-compile_only = "true"
-* setup-ocamlc.byte-build-env
-all_modules = "index_aliases.ml"
-* ocamlc.byte
-** check-ocamlc.byte-output
-
-program = "-quiet -index -decls index_aliases.cmt"
-output = "out_objinfo"
-*** ocamlobjinfo
-
-**** check-program-output
+ flags = "-bin-annot -bin-annot-occurrences";
+ compile_only = "true";
+ {
+   all_modules = "index_aliases.ml";
+   setup-ocamlc.byte-build-env;
+ }{
+   ocamlc.byte;
+   program = "-quiet -index -decls index_aliases.cmt";
+   output = "out_objinfo";
+   check-ocamlc.byte-output;
+   ocamlobjinfo;
+   check-program-output;
+ }
 *)
 
 
