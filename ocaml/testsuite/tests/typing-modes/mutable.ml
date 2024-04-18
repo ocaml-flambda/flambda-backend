3a3580a1f757ce7eaf55238350b970807928addb
Zesen Qian <github@riaqn.org>
2024-04-18 16:57:25 +0100

    clean up mutable() logic as commented

--- ocaml/testsuite/tests/typing-modes/mutable.ml
+++ ocaml/testsuite/tests/typing-modes/mutable.ml
@@ -8,3 +8,8 @@
    yet. *)
 
 (* CR zqian: add test for mutable when mutable is decoupled from modalities. *)
+
+(* CR mode-crossing: Uncomment this example once portability is added. It would
+fail because we don't support mutable(m0) crosses modes yet. *)
+(* type r = {mutable a : int}
+let r @ portable = {a = 42} *)
