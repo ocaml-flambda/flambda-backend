d15c6240540b913f941179b00fcb74ce9401c838
Zesen Qian <riaqn@users.noreply.github.com>
2024-04-21 10:18:02 +0100

    clean up mutable() logic as commented (#2467)
    * clean up mutable() logic as commented
    
    * address comment

--- ocaml/testsuite/tests/typing-modes/mutable.ml
+++ ocaml/testsuite/tests/typing-modes/mutable.ml
@@ -8,3 +8,24 @@
    yet. *)
 
 (* CR zqian: add test for mutable when mutable is decoupled from modalities. *)
+
+(* CR mode-crossing: Uncomment the following examples once portability is added. *)
+
+(* The [m0] in mutable should cross modes upon construction. *)
+(*
+type r =
+  { f : string -> string;
+    mutable a : int }
+let r @ portable =
+  { f = fun x -> x;
+    a = 42 } *)
+
+(* The [m0] in mutable corresponds to the field type wrapped in modality; as a
+   result, it enjoys mode crossing enabled by the modality. *)
+(*
+type r =
+  { f : string -> string;
+    mutable g : string -> string @@ portable }
+let r @ portable =
+  { f = fun x -> x;
+    g = fun x -> x } *)
