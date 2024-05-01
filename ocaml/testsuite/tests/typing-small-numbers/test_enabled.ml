97b165ad7afd4cb48f8482e7fd9e5530ce4a4499
Max Slater <max@thenumb.at>
2024-05-01 12:54:24 -0400

    test

--- ocaml/testsuite/tests/typing-small-numbers/test_enabled.ml
+++ ocaml/testsuite/tests/typing-small-numbers/test_enabled.ml
@@ -12,52 +12,52 @@ type t = float32
 
 let _ : float32 = 1.0s;;
 [%%expect{|
-- : float32 = 1.
+- : float32 = 1.s
 |}];;
 
 let _ : float32 = 1.s;;
 [%%expect{|
-- : float32 = 1.
+- : float32 = 1.s
 |}];;
 
 let _ : float32 = 1e10s;;
 [%%expect{|
-- : float32 = 1e+10
+- : float32 = 1e+10s
 |}];;
 
 let _ : float32 = 1e+1s;;
 [%%expect{|
-- : float32 = 10.
+- : float32 = 10.s
 |}];;
 
 let _ : float32 = 1e-1s;;
 [%%expect{|
-- : float32 = 0.100000001
+- : float32 = 0.100000001s
 |}];;
 
 let _ : float32 = 0x111.000s;;
 [%%expect{|
-- : float32 = 273.
+- : float32 = 273.s
 |}];;
 
 let _ : float32 = 0x1.4p+0s;;
 [%%expect{|
-- : float32 = 1.25
+- : float32 = 1.25s
 |}];;
 
 let _ : float32 = 0xf.ffffffffffff8p+1020s;;
 [%%expect{|
-- : float32 = inf
+- : float32 = infs
 |}];;
 
 let _ : float32 = 0x8p-972s;;
 [%%expect{|
-- : float32 = 0.
+- : float32 = 0.s
 |}];;
 
 let _ : float32 = 0xc.d5e6fp+1_24s;;
 [%%expect{|
-- : float32 = 2.72982066e+38
+- : float32 = 2.72982066e+38s
 |}];;
 
 (* A (trivial) match with no float32 cases is allowed. *)
