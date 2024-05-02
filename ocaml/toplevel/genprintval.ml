e353ced56272e463f7553c887179639ef76605de
Nick Roberts <nroberts@janestreet.com>
2024-05-02 14:26:06 +0800

    Add test for recursive mixed blocks

--- ocaml/toplevel/genprintval.ml
+++ ocaml/toplevel/genprintval.ml
@@ -255,7 +255,7 @@ module Make(O : OBJ)(EVP : EVALPATH with type valu = O.t) = struct
       | Immediate64 | Immediate | Non_null_value | Value -> Print_as_value
       | Void -> Print_as "<void>"
       | Any -> Print_as "<any>"
-      | Float64 | Bits32 | Bits64 | Word -> Print_as "<unboxed>"
+      | Float64 | Bits32 | Bits64 | Word -> Print_as "<abstr>"
 
     let outval_of_value max_steps max_depth check_depth env obj ty =
 
