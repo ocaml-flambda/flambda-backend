99e5ff098899e97174a6b09e413fbc08b0d3cf7d
Max Slater <max@thenumb.at>
2024-05-08 15:41:00 -0400

    update generated tests

--- ocaml/testsuite/tests/mixed-blocks/constructor_args.ml
+++ ocaml/testsuite/tests/mixed-blocks/constructor_args.ml
@@ -1,5 +1,6 @@
 (* TEST
-   flags = "-extension layouts_beta";
+   flags = "-extension layouts_beta -extension small_numbers";
+   include beta;
    flambda2;
    {
    native;
@@ -11,6 +12,7 @@
 (*****************************************)
 (* Prelude: Functions on unboxed numbers *)
 
+module Float32_u = Beta.Float32_u
 module Float_u = Stdlib__Float_u
 module Int32_u = Stdlib__Int32_u
 module Int64_u = Stdlib__Int64_u
@@ -34,6 +36,9 @@ type t =
   | Mixed6 of float * int32# * float#
   | Mixed7 of float * int64# * float# * nativeint#
   | Mixed8 of float * int32# * float# * int64# * float#
+  | Mixed9 of float * float# * float32#
+  | Mixed10 of float * float32# * float# * int64# * float#
+  | Mixed11 of float * int32# * float32# * float# * int64# * float#
   | Uniform2 of float * float
 
 type t_ext = ..
@@ -47,6 +52,9 @@ type t_ext +=
   | Ext_mixed6 of float * int32# * float#
   | Ext_mixed7 of float * int64# * float# * nativeint#
   | Ext_mixed8 of float * int32# * float# * int64# * float#
+  | Ext_mixed9 of float * float# * float32#
+  | Ext_mixed10 of float * float32# * float# * int64# * float#
+  | Ext_mixed11 of float * int32# * float32# * float# * int64# * float#
 
 let sprintf = Printf.sprintf
 
@@ -75,6 +83,17 @@ let to_string = function
       sprintf "Mixed8 (%f, %i, %f, %i, %f)"
         x1 (Int32_u.to_int x2) (Float_u.to_float x3) (Int64_u.to_int x4)
         (Float_u.to_float x5)
+  | Mixed9 (x1, x2, x3) ->
+      sprintf "Mixed9 (%f, %f, %f)" x1 (Float_u.to_float x2)
+      (Float_u.to_float (Float32_u.to_float x3))
+  | Mixed10 (x1, x2, x3, x4, x5) ->
+      sprintf "Mixed10 (%f, %f, %f, %i, %f)"
+        x1 (Float_u.to_float (Float32_u.to_float x2)) (Float_u.to_float x3)
+        (Int64_u.to_int x4) (Float_u.to_float x5)
+  | Mixed11 (x1, x2, x3, x4, x5, x6) ->
+      sprintf "Mixed11 (%f, %i, %f, %f, %i, %f)"
+        x1 (Int32_u.to_int x2) (Float_u.to_float (Float32_u.to_float x3))
+        (Float_u.to_float x4) (Int64_u.to_int x5) (Float_u.to_float x6)
   | Uniform2 (x1, x2) -> sprintf "Uniform2 (%f, %f)" x1 x2
 
 let ext_to_string = function
@@ -100,6 +119,17 @@ let ext_to_string = function
     sprintf "Ext_mixed8 (%f, %i, %f, %i, %f)"
       x1 (Int32_u.to_int x2) (Float_u.to_float x3) (Int64_u.to_int x4)
       (Float_u.to_float x5)
+  | Ext_mixed9 (x1, x2, x3) ->
+      sprintf "Ext_mixed9 (%f, %f, %f)" x1 (Float_u.to_float x2)
+      (Float_u.to_float (Float32_u.to_float x3))
+  | Ext_mixed10 (x1, x2, x3, x4, x5) ->
+      sprintf "Ext_mixed10 (%f, %f, %f, %i, %f)"
+        x1 (Float_u.to_float (Float32_u.to_float x2)) (Float_u.to_float x3)
+        (Int64_u.to_int x4) (Float_u.to_float x5)
+  | Ext_mixed11 (x1, x2, x3, x4, x5, x6) ->
+      sprintf "Ext_mixed11 (%f, %i, %f, %f, %i, %f)"
+        x1 (Int32_u.to_int x2) (Float_u.to_float (Float32_u.to_float x3))
+        (Float_u.to_float x4) (Int64_u.to_int x5) (Float_u.to_float x6)
   | _ -> "<ext>"
 
 let print t = print_endline ("  " ^ to_string t)
@@ -128,12 +158,12 @@ let () = run #17.0
    exercise an optimization code path.
 *)
 
-let sum uf uf' f f' i i32 i64 i_n =
+let sum uf uf' f f' i i32 i64 i_n f32 =
   Float_u.to_float uf +. Float_u.to_float uf' +. f +. f' +.
   Int32_u.to_float i32 +. Int64_u.to_float i64 +. Nativeint_u.to_float i_n
-  +. float_of_int i
+  +. float_of_int i +. (Float_u.to_float (Float32_u.to_float f32))
 
-let construct_and_destruct uf uf' f f' i i32 i64 i_n =
+let construct_and_destruct uf uf' f f' i i32 i64 i_n f32 =
   let Constant = Constant in
   let Uniform1 f = Uniform1 f in
   let Mixed1 uf = Mixed1 uf in
@@ -144,6 +174,9 @@ let construct_and_destruct uf uf' f f' i i32 i64 i_n =
   let Mixed6 (f, i32, uf) = Mixed6 (f, i32, uf) in
   let Mixed7 (f, i64, uf, i_n) = Mixed7 (f, i64, uf, i_n) in
   let Mixed8 (f, i32, uf, i64, uf') = Mixed8 (f, i32, uf, i64, uf') in
+  let Mixed9 (f, uf, f32) = Mixed9 (f, uf, f32) in
+  let Mixed10 (f, f32, uf, i64, uf') = Mixed10 (f, f32, uf, i64, uf') in
+  let Mixed11 (f, i32, f32, uf, i64, uf') = Mixed11 (f, i32, f32, uf, i64, uf') in
   let Ext_mixed1 uf = Ext_mixed1 uf in
   let Ext_mixed2 (f, uf) = Ext_mixed2 (f, uf) in
   let Ext_mixed3 (f, uf, uf') = Ext_mixed3 (f, uf, uf') in
@@ -152,8 +185,11 @@ let construct_and_destruct uf uf' f f' i i32 i64 i_n =
   let Ext_mixed6 (f, i32, uf) = Ext_mixed6 (f, i32, uf) in
   let Ext_mixed7 (f, i64, uf, i_n) = Ext_mixed7 (f, i64, uf, i_n) in
   let Ext_mixed8 (f, i32, uf, i64, uf') = Ext_mixed8 (f, i32, uf, i64, uf') in
+  let Ext_mixed9 (f, uf, f32) = Ext_mixed9 (f, uf, f32) in
+  let Ext_mixed10 (f, f32, uf, i64, uf') = Ext_mixed10 (f, f32, uf, i64, uf') in
+  let Ext_mixed11 (f, i32, f32, uf, i64, uf') = Ext_mixed11 (f, i32, f32, uf, i64, uf') in
   let Uniform2 (f, f') = Uniform2 (f, f') in
-  sum uf uf' f f' i i32 i64 i_n
+  sum uf uf' f f' i i32 i64 i_n f32
 [@@ocaml.warning "-partial-match"]
 
 let () =
@@ -165,10 +201,11 @@ let () =
   and i32 = #12l
   and i64 = #42L
   and i_n = #56n
+  and f32 = #1.2s
   in
   let () =
-    let sum1 = sum uf uf' f f' i i32 i64 i_n in
-    let sum2 = construct_and_destruct uf uf' f f' i i32 i64 i_n in
+    let sum1 = sum uf uf' f f' i i32 i64 i_n f32 in
+    let sum2 = construct_and_destruct uf uf' f f' i i32 i64 i_n f32 in
     Printf.printf
       "Test (construct and destruct): %f = %f (%s)\n"
       sum1
