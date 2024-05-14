7895759d9f294ad1716041060392a0297372fab7
Diana Kalinichenko <dkalinichenko@janestreet.com>
2024-05-14 12:01:02 -0400

    Move `Iarray` to `Stdlib_stable`

--- ocaml/otherlibs/stdlib_stable/stdlib_stable.mli
+++ ocaml/otherlibs/stdlib_stable/stdlib_stable.mli
@@ -2,5 +2,5 @@ module Float_u = Float_u
 module Int32_u = Int32_u
 module Int64_u = Int64_u
 module Nativeint_u = Nativeint_u
-module Iarray = Stdlib__Iarray
-module IarrayLabels = Stdlib__IarrayLabels
+module Iarray = Iarray
+module IarrayLabels = IarrayLabels
