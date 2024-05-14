fc8e778495bff7f31d96a0c4b64c4f518fdccdac
dkalinichenko-js <118547217+dkalinichenko-js@users.noreply.github.com>
2024-05-14 11:28:19 -0400

    flambda-backend: Move unboxed definitions from `Stdlib` to `Stable` (#2555)
    * Fix `include` in expect tests
    
    * Add unboxed types to `Stable`
    
    * Add `Nativeint_u` to `Stable`
    
    * Remove `Float_u` from `Stdlib`
    
    * Remove unboxed ints from `Stdlib`
    
    * Clean up build rules
    
    * Fix mixed blocks tests
    
    * Fix parsetree tests
    
    * Fix typing-layouts tests
    
    * Fix typing-layouts-arrays tests
    
    * Fix typing-layouts-bits32 tests
    
    * Fix typing-layouts-bits64 tests
    
    * Fix typing-layouts-float64 tests
    
    * Fix typing-layouts-word tests
    
    * Fix build
    
    * Use `include` in expect tests
    
    * Define separate modules instead of opening Stable
    
    * Rename test files
    
    * Fix mixed blocks tests
    
    * Fix layouts tests
    
    ---------
    
    Co-authored-by: Diana Kalinichenko <dkalinichenko@janestreet.com>

--- testsuite/tests/typing-layouts-bits32/unboxed_bits32s.ml
+++ testsuite/tests/typing-layouts-bits32/unboxed_bits32s.ml
@@ -1,4 +1,5 @@
 (* TEST
+ include stable;
  flambda2;
  {
    native;
@@ -26,7 +27,7 @@
 (* Prelude: Functions on unboxed int32s. *)
 
 module Int32_u = struct
-  include Stdlib__Int32_u
+  include Stable.Int32_u
 
   let ( + ) = add
   let ( - ) = sub
