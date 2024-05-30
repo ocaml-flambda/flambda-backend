93c25a117603f47610ed15fa5e120eb2c48cc1f2
Pierre Chambart <chambart@users.noreply.github.com>
2024-05-30 16:55:02 +0200

    Remove duplication of defining function in denv (#2638)
    Also clean useless optional arguments and go systematically through
    define_var to define variables

--- middle_end/flambda2/simplify/env/downwards_env.mli
+++ middle_end/flambda2/simplify/env/downwards_env.mli
@@ -100,7 +100,6 @@ val define_parameters : t -> params:Bound_parameters.t -> t
 
 val add_parameters :
   ?name_mode:Name_mode.t ->
-  ?at_unit_toplevel:bool ->
   t ->
   Bound_parameters.t ->
   param_types:Flambda2_types.t list ->
@@ -109,7 +108,6 @@ val add_parameters :
 val add_parameters_with_unknown_types :
   ?alloc_modes:Alloc_mode.For_types.t list ->
   ?name_mode:Name_mode.t ->
-  ?at_unit_toplevel:bool ->
   t ->
   Bound_parameters.t ->
   t
--- middle_end/flambda2/simplify/simplify_let_cont_expr.ml
+++ middle_end/flambda2/simplify/simplify_let_cont_expr.ml
@@ -1064,8 +1064,8 @@ let simplify_single_recursive_handler ~simplify_expr cont_uses_env_so_far
      the unboxing extra_params_and_args to later, when we will have seen all
      uses (needed for the recursive continuation handlers). *)
   let handler_env =
-    DE.add_parameters_with_unknown_types ~at_unit_toplevel:false denv_to_reset
-      params
+    assert (not (DE.at_unit_toplevel denv_to_reset));
+    DE.add_parameters_with_unknown_types denv_to_reset params
   in
   let handler_env = LCS.add_to_denv handler_env consts_lifted_during_body in
   let code_age_relation = TE.code_age_relation (DA.typing_env dacc) in
