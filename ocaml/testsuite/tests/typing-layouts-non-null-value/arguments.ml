24220f7b68ae934632a431c67fb2e54f07c80e0a
Diana Kalinichenko <dkalinichenko@janestreet.com>
2024-06-03 15:43:34 -0400

    Tests

--- ocaml/testsuite/tests/typing-layouts-non-null-value/arguments.ml
+++ ocaml/testsuite/tests/typing-layouts-non-null-value/arguments.ml
@@ -127,3 +127,70 @@ let _ = my_id4 (Fake_or_null.some 4)
 - : int Fake_or_null.t = <abstr>
 - : int Fake_or_null.t = <abstr>
 |}]
+
+(* Check behavior of type arguments and unboxed annotations. *)
+
+module M2 : sig
+  type 'a t = { v : 'a } [@@unboxed]
+
+  val box : 'a -> 'a t
+  val unbox : 'a t -> 'a
+end = struct
+  type 'a t = { v : 'a } [@@unboxed]
+
+  let box v = { v }
+  let unbox { v } = v
+end
+
+[%%expect{|
+Lines 6-11, characters 6-3:
+ 6 | ......struct
+ 7 |   type 'a t = { v : 'a } [@@unboxed]
+ 8 |
+ 9 |   let box v = { v }
+10 |   let unbox { v } = v
+11 | end
+Error: Signature mismatch:
+       Modules do not match:
+         sig
+           type 'a t = { v : 'a; } [@@unboxed]
+           val box : 'a -> 'a t
+           val unbox : ('a : non_null_value). 'a t -> 'a
+         end
+       is not included in
+         sig
+           type 'a t = { v : 'a; } [@@unboxed]
+           val box : 'a -> 'a t
+           val unbox : 'a t -> 'a
+         end
+       Values do not match:
+         val unbox : ('a : non_null_value). 'a t -> 'a
+       is not included in
+         val unbox : 'a t -> 'a
+       The type 'a t -> 'a is not compatible with the type 'b t -> 'b
+       The layout of 'a is value, because
+         of the definition of unbox at line 5, characters 2-24.
+       But the layout of 'a must be a sublayout of non_null_value, because
+         of the definition of unbox at line 10, characters 12-21.
+|}]
+
+module M3 : sig
+  type 'a t = V of 'a [@@unboxed]
+
+  val box : 'a -> 'a t
+  val unbox : 'a t -> 'a
+end = struct
+  type 'a t = V of 'a [@@unboxed]
+
+  let box v = V v
+  let unbox (V v) = v
+end
+
+[%%expect{|
+module M3 :
+  sig
+    type 'a t = V of 'a [@@unboxed]
+    val box : 'a -> 'a t
+    val unbox : 'a t -> 'a
+  end
+|}]
