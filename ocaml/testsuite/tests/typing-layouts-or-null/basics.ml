9288bedda1048f9962fd7c36400cbafbcd4519a7
Diana Kalinichenko <dkalinichenko@janestreet.com>
2024-06-06 10:00:39 -0400

    some basic tests

---
+++ ocaml/testsuite/tests/typing-layouts-or-null/basics.ml
@@ -0,0 +1,235 @@
+(* TEST
+ flags = "-extension layouts_alpha";
+ expect;
+*)
+
+type t_any : any
+type t_any_non_null : any_non_null
+type t_value_or_null : value_or_null
+type t_value : value
+
+[%%expect{|
+type t_any : any
+type t_any_non_null : any_non_null
+type t_value_or_null : value_or_null
+type t_value : value
+|}]
+
+(* [any_non_null] is not representable *)
+let f (x : t_any_non_null) = x
+
+[%%expect{|
+Line 1, characters 6-26:
+1 | let f (x : t_any_non_null) = x
+          ^^^^^^^^^^^^^^^^^^^^
+Error: This pattern matches values of type t_any_non_null
+       but a pattern was expected which matches values of type
+         ('a : '_representable_layout_1)
+       The layout of t_any_non_null is any_non_null, because
+         of the definition of t_any_non_null at line 2, characters 0-34.
+       But the layout of t_any_non_null must be representable, because
+         we must know concretely how to pass a function argument.
+|}]
+
+type t = { x : t_any_non_null }
+
+[%%expect{|
+Line 1, characters 11-29:
+1 | type t = { x : t_any_non_null }
+               ^^^^^^^^^^^^^^^^^^
+Error: Record element types must have a representable layout.
+       The layout of t_any_non_null is any_non_null, because
+         of the definition of t_any_non_null at line 2, characters 0-34.
+       But the layout of t_any_non_null must be representable, because
+         it is the type of record field x.
+|}]
+
+module type S1 = sig
+  val x : t_any_non_null
+end
+
+[%%expect{|
+Line 2, characters 10-24:
+2 |   val x : t_any_non_null
+              ^^^^^^^^^^^^^^
+Error: This type signature for x is not a value type.
+       The layout of type t_any_non_null is any_non_null, because
+         of the definition of t_any_non_null at line 2, characters 0-34.
+       But the layout of type t_any_non_null must be a sublayout of value, because
+         it's the type of something stored in a module structure.
+|}]
+
+(* This is ok for some reason *)
+
+module type S2 = sig
+  val f : unit -> t_any
+  val g : unit -> t_any_non_null
+end
+
+[%%expect{|
+module type S2 = sig val f : unit -> t_any val g : unit -> t_any_non_null end
+|}]
+
+module M2 (X : S2) = struct
+  let g () = X.g ()
+end
+
+[%%expect{|
+Line 2, characters 13-19:
+2 |   let g () = X.g ()
+                 ^^^^^^
+Error: This expression has type t_any_non_null
+       but an expression was expected of type ('a : '_representable_layout_2)
+       The layout of t_any_non_null is any_non_null, because
+         of the definition of t_any_non_null at line 2, characters 0-34.
+       But the layout of t_any_non_null must be representable, because
+         we must know concretely how to return a function result.
+|}]
+
+(* CR layouts v3.0: [value_or_null] should be representable *)
+
+let f (x : t_value_or_null) = x
+
+[%%expect{|
+Line 1, characters 6-27:
+1 | let f (x : t_value_or_null) = x
+          ^^^^^^^^^^^^^^^^^^^^^
+Error: This pattern matches values of type t_value_or_null
+       but a pattern was expected which matches values of type ('a : value)
+       The layout of t_value_or_null is value_or_null, because
+         of the definition of t_value_or_null at line 3, characters 0-36.
+       But the layout of t_value_or_null must be a sublayout of value, because
+         we must know concretely how to pass a function argument, defaulted to layout value.
+|}]
+
+type t = { x : t_value_or_null }
+
+[%%expect {|
+Line 1, characters 11-30:
+1 | type t = { x : t_value_or_null }
+               ^^^^^^^^^^^^^^^^^^^
+Error: Record element types must have a representable layout.
+       The layout of t_value_or_null is value_or_null, because
+         of the definition of t_value_or_null at line 3, characters 0-36.
+       But the layout of t_value_or_null must be a sublayout of value, because
+         it is the type of record field x, defaulted to layout value.
+|}]
+
+module type S1 = sig
+  val x : t_value_or_null
+end
+
+[%%expect {|
+Line 2, characters 10-25:
+2 |   val x : t_value_or_null
+              ^^^^^^^^^^^^^^^
+Error: This type signature for x is not a value type.
+       The layout of type t_value_or_null is value_or_null, because
+         of the definition of t_value_or_null at line 3, characters 0-36.
+       But the layout of type t_value_or_null must be a sublayout of value, because
+         it's the type of something stored in a module structure.
+|}]
+
+module type S2 = sig
+  val f : unit -> t_value_or_null
+end
+
+[%%expect {|
+module type S2 = sig val f : unit -> t_value_or_null end
+|}]
+
+module M2 (X : S2) = struct
+  let f () = X.f ()
+end
+
+[%%expect{|
+Line 2, characters 13-19:
+2 |   let f () = X.f ()
+                 ^^^^^^
+Error: This expression has type t_value_or_null
+       but an expression was expected of type ('a : value)
+       The layout of t_value_or_null is value_or_null, because
+         of the definition of t_value_or_null at line 3, characters 0-36.
+       But the layout of t_value_or_null must be a sublayout of value, because
+         we must know concretely how to return a function result, defaulted to layout value.
+|}]
+
+type ('a : any) id_any = 'a
+type ('a : any_non_null) id_any_non_null = 'a
+type ('a : value_or_null) id_value_or_null = 'a
+type ('a : value) id_value = 'a
+type ('a : bits64) id_bits64 = 'a
+
+[%%expect{|
+type ('a : any) id_any = 'a
+type ('a : any_non_null) id_any_non_null = 'a
+type ('a : value_or_null) id_value_or_null = 'a
+type 'a id_value = 'a
+type ('a : bits64) id_bits64 = 'a
+|}]
+
+(* [any_non_null] is a subtype of [any] *)
+
+type t = t_any_non_null id_any
+
+[%%expect{|
+type t = t_any_non_null id_any
+|}]
+
+module M (X : sig type t : any_non_null end) : sig type t : any end = X
+
+[%%expect{|
+module M :
+  functor (X : sig type t : any_non_null end) -> sig type t : any end
+|}]
+
+(* CR layouts v3.0: [any] should not be a subtype of [any_non_null] *)
+
+type t = t_any id_any_non_null
+
+[%%expect{|
+type t = t_any id_any_non_null
+|}]
+
+module M (X : sig type t : any end) : sig type t : any_non_null end = X
+
+[%%expect{|
+module M :
+  functor (X : sig type t : any end) -> sig type t : any_non_null end
+|}]
+
+(* [value_or_null] is not a subtype of [value] *)
+
+type t = t_value_or_null id_value
+
+[%%expect{|
+Line 1, characters 9-24:
+1 | type t = t_value_or_null id_value
+             ^^^^^^^^^^^^^^^
+Error: This type t_value_or_null should be an instance of type ('a : value)
+       The layout of t_value_or_null is value_or_null, because
+         of the definition of t_value_or_null at line 3, characters 0-36.
+       But the layout of t_value_or_null must be a sublayout of value, because
+         of the definition of id_value at line 4, characters 0-31.
+|}]
+
+module M (X : sig type t : value_or_null end) : sig type t : value end = X
+
+[%%expect{|
+Line 1, characters 73-74:
+1 | module M (X : sig type t : value_or_null end) : sig type t : value end = X
+                                                                             ^
+Error: Signature mismatch:
+       Modules do not match:
+         sig type t = X.t end
+       is not included in
+         sig type t : value end
+       Type declarations do not match:
+         type t = X.t
+       is not included in
+         type t : value
+       The layout of the first is value_or_null, because
+         of the definition of t at line 1, characters 18-40.
+       But the layout of the first must be a sublayout of value, because
+         of the definition of t at line 1, characters 52-66.
+|}]
