d4368cb5080d7474134b16786891eb47a7c380db
Mark Shinwell <mshinwell@pm.me>
2024-06-07 17:22:38 +0100

    config.common.ml

--- ocaml/utils/config.common.ml
+++ ocaml/utils/config.common.ml
@@ -129,6 +129,9 @@ let configuration_variables () =
   p_bool "supports_shared_libraries" supports_shared_libraries;
   p_bool "native_dynlink" native_dynlink;
   p_bool "naked_pointers" naked_pointers;
+  p_bool "no_stack_checks" no_stack_checks;
+  p_bool "poll_insertion" poll_insertion;
+  p_bool "new_safepoints_strategy" new_safepoints_strategy;
 (*
   Disabled in flambda-backend (for now)
   p_bool "compression_supported" (Marshal.compression_supported());
