1bb0bd6d1d94dc0e11649f8158f23c24e865b9ba
Liam Stevenson <lstevenson@janestreet.com>
2024-06-25 10:32:53 -0400

    Fix Jkind.format

--- ocaml/typing/jkind.mli
+++ ocaml/typing/jkind.mli
@@ -158,6 +158,9 @@ module Const : sig
 
   val to_out_jkind_const : t -> Outcometree.out_jkind_const
 
+  val print_out_jkind_const :
+    Format.formatter -> Outcometree.out_jkind_const -> unit
+
   val format : Format.formatter -> t -> unit
 
   val equal : t -> t -> bool
@@ -391,6 +394,8 @@ val set_externality_upper_bound : t -> Externality.t -> t
 
 (*********************************)
 (* pretty printing *)
+
+val print_out_jkind : Format.formatter -> Outcometree.out_jkind -> unit
 
 val format : Format.formatter -> t -> unit
 
