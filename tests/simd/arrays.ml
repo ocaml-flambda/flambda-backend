3f7a8924f25f2f5d895a66a99931b40126419f4b
Max Slater <max@thenumb.at>
2024-06-25 07:08:36 -0400

    Expose float32 mask symbols (#2696)

--- tests/simd/arrays.ml
+++ tests/simd/arrays.ml
@@ -1,6 +1,7 @@
 open Stdlib
 
 [@@@ocaml.warning "-unused-value-declaration"]
+[@@@ocaml.warning "-unused-module"]
 
 external int8x16_of_int64s : int64 -> int64 -> int8x16 = "" "vec128_of_int64s" [@@noalloc] [@@unboxed]
 external int8x16_low_int64 : int8x16 -> int64 = "" "vec128_low_int64" [@@noalloc] [@@unboxed]
