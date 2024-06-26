68b5487d104d9e3d98e5b0c4d01d756f4d2d461c
Luke Maurer <lmaurer@janestreet.com>
2024-06-26 20:07:22 +0100

    Also suppress zero-alloc info
    It's not as noisy as the full export info but it does make tests more sensitive.

--- tools/flambda_backend_objinfo.ml
+++ tools/flambda_backend_objinfo.ml
@@ -278,7 +278,9 @@ let print_cmx_infos (uir, sections, crc) =
   end;
   print_generic_fns uir.uir_generic_fns;
   printf "Force link: %s\n" (if uir.uir_force_link then "YES" else "no");
-  Zero_alloc_info.Raw.print uir.uir_zero_alloc_info
+  if not (!no_code || !no_approx) then begin
+    Zero_alloc_info.Raw.print uir.uir_zero_alloc_info
+  end
 
 let print_cmxa_infos (lib : Cmx_format.library_infos) =
   printf "Extra C object files:";
