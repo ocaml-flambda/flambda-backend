7d5340233a9583786aeb91efa2b59636cf1c2d74
Nathanaëlle Courant <nathanaelle.courant@ocamlpro.com>
2024-07-02 16:12:03 +0200

    Check local allocations

--- ocaml/testsuite/tests/typing-local/close_at_apply.ml
+++ ocaml/testsuite/tests/typing-local/close_at_apply.ml
@@ -1,4 +1,6 @@
 (* TEST
+   modules = "cstubs.c";
+   stack-allocation;
    flambda2;
    {
      ocamlopt_flags="-Oclassic";
@@ -6,6 +8,8 @@
      ocamlopt_flags="-O3";
    }
 *)
+
+external local_stack_offset : unit -> int = "caml_local_stack_offset"
 
 external ignore : ('a[@local_opt]) -> unit = "%ignore"
 external opaque_identity : ('a[@local_opt]) -> ('a[@local_opt]) = "%opaque"
@@ -21,6 +25,8 @@ let locally_allocate =
   fun [@inline never] () -> ignore (opaque_identity (f +. 1.0) : float)
 ;;
 
+let saved_stack_offset = ref 0
+
 let foo list =
   (* The Simplif local function optimization will transform [run] and
      [wrapper] into continuations. *)
@@ -28,6 +34,8 @@ let foo list =
   let[@local always][@inline never] wrapper f =
     match Sys.opaque_identity true with
     | true ->
+      (* Make a local allocation *)
+      ignore (opaque_identity (opaque_identity 1.0 +. 1.0));
       (* This tail call gets moved into non-tail position after the above
          inlining transformation.  As such it's important that its
          [Rc_close_at_apply] semantics is respected carefully: only the
@@ -36,9 +44,13 @@ let foo list =
       f ()
     | false -> run (fun () -> f ()) [@nontail]
   in
-  wrapper (fun _ ->
+  let f = fun _ ->
+    assert (local_stack_offset () = !saved_stack_offset);
     locally_allocate ();
-    Stdlib.List.hd list) [@nontail]
+    Stdlib.List.hd list
+  in
+  saved_stack_offset := local_stack_offset ();
+  wrapper f [@nontail]
 ;;
 
 let _ : int = foo [ 1 ]
