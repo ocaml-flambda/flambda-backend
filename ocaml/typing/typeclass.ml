e9996c56ccfb93483ff19be04c5a7a8f43b45f00
Chris Casinghino <ccasinghino@janestreet.com>
2024-07-08 01:53:18 -0400

    Infer needed `zero_alloc` checks from signatures (#2742)

--- ocaml/typing/typeclass.ml
+++ ocaml/typing/typeclass.ml
@@ -490,7 +490,7 @@ let enter_ancestor_met ~loc name ~sign ~meths ~cl_num ~ty ~attrs met_env =
   let desc =
     { val_type = ty; val_modalities = Modality.Value.id; val_kind = kind;
       val_attributes = attrs;
-      val_zero_alloc = Builtin_attributes.Default_zero_alloc;
+      val_zero_alloc = Zero_alloc.default;
       Types.val_loc = loc;
       val_uid = Uid.mk ~current_unit:(Env.get_unit_name ()) }
   in
@@ -506,7 +506,7 @@ let add_self_met loc id sign self_var_kind vars cl_num
   let desc =
     { val_type = ty; val_modalities = Modality.Value.id; val_kind = kind;
       val_attributes = attrs;
-      val_zero_alloc = Builtin_attributes.Default_zero_alloc;
+      val_zero_alloc = Zero_alloc.default;
       Types.val_loc = loc;
       val_uid = Uid.mk ~current_unit:(Env.get_unit_name ()) }
   in
@@ -523,7 +523,7 @@ let add_instance_var_met loc label id sign cl_num attrs met_env =
     { val_type = ty; val_modalities = Modality.Value.id; val_kind = kind;
       val_attributes = attrs;
       Types.val_loc = loc;
-      val_zero_alloc = Builtin_attributes.Default_zero_alloc;
+      val_zero_alloc = Zero_alloc.default;
       val_uid = Uid.mk ~current_unit:(Env.get_unit_name ()) }
   in
   Env.add_value ~mode:Mode.Value.legacy id desc met_env
@@ -1467,7 +1467,7 @@ and class_expr_aux cl_num val_env met_env virt self_scope scl =
                 val_modalities = Modality.Value.id;
                 val_kind = Val_ivar (Immutable, cl_num);
                 val_attributes = [];
-                val_zero_alloc = Builtin_attributes.Default_zero_alloc;
+                val_zero_alloc = Zero_alloc.default;
                 Types.val_loc = vd.val_loc;
                 val_uid = vd.val_uid;
                }
