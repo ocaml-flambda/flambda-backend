022439c05b150e1faecf41de13162cb8012b07bc
Mark Shinwell <mshinwell@pm.me>
2024-07-09 16:56:50 +0100

    flambda-backend: Fix padding bug for float32 static values (#2763)
    * Fix padding bug for float32 static values
    
    * Add test case
    
    * semicolon
    
    * Add comment

---
+++ testsuite/tests/mixed-blocks/closure_padding_float32.ml
@@ -0,0 +1,35 @@
+(* TEST
+   runtime5;
+   include stdlib_beta;
+   ocamlopt_flags="-extension layouts_alpha -extension small_numbers";
+   native;
+*)
+
+(* From https://github.com/ocaml-flambda/flambda-backend/pull/2698
+   although submitted as part of
+   https://github.com/ocaml-flambda/flambda-backend/pull/2763
+*)
+
+module Float32_u = Stdlib_beta.Float32_u
+
+type t =
+  | Mutable_str of { mutable x : string }
+  | Float32 of float32#
+
+let[@inline always] go x y =
+  let f =
+    match x with
+    | Float32 f32 ->
+      (fun[@inline never] () ->
+         match y with
+         | Mutable_str _ -> f32
+         | Float32 _ -> assert false)
+    | Mutable_str _ -> assert false
+  in
+  f ()
+
+let () =
+  let x = Float32 #4.s in
+  let y = Mutable_str { x = "s" } in
+  let _ = go x y in
+  ()
