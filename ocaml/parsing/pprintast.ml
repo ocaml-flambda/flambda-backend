62920884ae9fa545023c25f4a064ec1886062e15
Jakub Bachurski <kbachurski@gmail.com>
2024-07-12 17:57:14 +0100

    Parsing for arrow kinds in Jane_syntax

--- ocaml/parsing/pprintast.ml
+++ ocaml/parsing/pprintast.ml
@@ -388,6 +388,9 @@ and jkind ctxt f k = match (k : Jane_syntax.Jkind.t) with
   | With (t, ty) ->
     pp f "%a with %a" (jkind ctxt) t (core_type ctxt) ty
   | Kind_of ty -> pp f "kind_of_ %a" (core_type ctxt) ty
+  | Arrow (args, result) ->
+      pp f "(%a) => %a" (list (jkind ctxt) ~sep:", ") args (jkind ctxt) result
+
 
 and jkind_annotation ctxt f annot = jkind ctxt f annot.txt
 
