4246b35eda7bbb692f49c4e8089d3c4f022705c0
Xavier Clerc <xclerc@janestreet.com>
2024-07-12 09:46:55 +0100

    Port pull requests about atomic loads.

--- backend/CSEgen.ml
+++ backend/CSEgen.ml
@@ -62,8 +62,14 @@ method class_of_operation op =
   | Iextcall _ | Iprobe _ | Iopaque -> assert false  (* treated specially *)
   | Istackoffset _ -> Op_other
   | Iload { mutability; is_atomic } ->
-    (* #12173: disable CSE for atomic loads. *)
-    if is_atomic then Op_other
+    (* #12173: disable CSE for atomic loads.
+          #12825: atomic loads cannot be treated as Op_other
+            because they update our view / the frontier of the
+            non-atomic locations, so past non-atomic (mutable) loads
+            may be not be valid anymore.
+          We conservatively tread them as non-initializing stores.
+    *)
+    if is_atomic then Op_store true
     else Op_load (match mutability with
       | Mutable -> Mutable
       | Immutable -> Immutable)
