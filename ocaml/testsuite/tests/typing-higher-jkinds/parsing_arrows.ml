49cd690b716c34daa4d8bc3c846ee27890c14ae5
Jakub Bachurski <kbachurski@gmail.com>
2024-07-15 14:10:41 +0100

    The parsing test passes properly now

--- ocaml/testsuite/tests/typing-higher-jkinds/parsing_arrows.ml
+++ ocaml/testsuite/tests/typing-higher-jkinds/parsing_arrows.ml
@@ -1,9 +1,6 @@
 (* TEST
+  flags = "-extension layouts_alpha";
   expect;
-  ocamlc_byte_exit_status = "2";
-  setup-ocamlc.byte-build-env;
-  ocamlc.byte;
-  check-ocamlc.byte-output;
 *)
 
 type p : value => value
@@ -13,6 +10,9 @@ type s : (value, value mod local) => value
 type t : (value) => (value) => value
 
 [%%expect{|
-Uncaught exception: Failure("Arrow jkind (=>) syntax parsed, but annotations are not implemented")
-
+type p : (value) => value
+type q : (value, value) => value
+type r : ((value) => value, value) => value
+type s : (value, value mod local) => value
+type t : (value) => (value) => value
 |}]
