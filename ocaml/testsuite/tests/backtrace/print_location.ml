39d0c8f6e661a25aad41acee9c44efc7fec44192
Xavier Clerc <xclerc@janestreet.com>
2024-07-16 15:19:52 +0100

    Missing test files.

---
+++ ocaml/testsuite/tests/backtrace/print_location.ml
@@ -0,0 +1,13 @@
+(* TEST
+ runtime5;
+ flags = "-g";
+ ocamlrunparam += ",b=1";
+ ocamlopt_flags = "-inline 0";
+ exit_status = "2";
+*)
+
+let f () = raise
+  Exit [@@inline never]
+
+let () =
+  f ()
