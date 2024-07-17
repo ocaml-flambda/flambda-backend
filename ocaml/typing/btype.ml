b6c0391a37bf5b08e93438eab7016f174247c282
Nick Roberts <nroberts@janestreet.com>
2024-07-17 16:39:25 -0400

    Simple updates: `patterns.ml`, `btype.ml`

--- ocaml/typing/btype.ml
+++ ocaml/typing/btype.ml
@@ -128,19 +128,13 @@ let newmarkedgenvar () =
 let is_Tvar ty = match get_desc ty with Tvar _ -> true | _ -> false
 let is_Tunivar ty = match get_desc ty with Tunivar _ -> true | _ -> false
 let is_Tconstr ty = match get_desc ty with Tconstr _ -> true | _ -> false
-<<<<<<< HEAD
 let is_Tpoly ty = match get_desc ty with Tpoly _ -> true | _ -> false
-let type_kind_is_abstract decl =
-  match decl.type_kind with Type_abstract _ -> true | _ -> false
-||||||| 121bedcfd2
-=======
 let type_kind_is_abstract decl =
   match decl.type_kind with Type_abstract _ -> true | _ -> false
 let type_origin decl =
   match decl.type_kind with
   | Type_abstract origin -> origin
   | Type_variant _ | Type_record _ | Type_open -> Definition
->>>>>>> 5.2.0
 
 let dummy_method = "*dummy method*"
 
