fff9e1c5bd70b57ad6fc7917c11b92a6323f3439
Nick Roberts <nroberts@janestreet.com>
2024-07-18 14:35:30 -0400

    Fix easy typing conflicts

--- ocaml/typing/includecore.ml
+++ ocaml/typing/includecore.ml
@@ -1100,7 +1100,6 @@ let type_declarations ?(equality = false) ~loc env ~mark name
   in
   if err <> None then err else
   let err = match (decl1.type_kind, decl2.type_kind) with
-<<<<<<< HEAD
       (_, Type_abstract _) ->
        (* Note that [decl2.type_jkind] is an upper bound.
           If it isn't tight, [decl2] must
@@ -1110,11 +1109,6 @@ let type_declarations ?(equality = false) ~loc env ~mark name
         (match Ctype.check_decl_jkind env decl1 decl2.type_jkind with
          | Ok _ -> None
          | Error v -> Some (Jkind v))
-||||||| 121bedcfd2
-      (_, Type_abstract) -> None
-=======
-      (_, Type_abstract _) -> None
->>>>>>> 5.2.0
     | (Type_variant (cstrs1, rep1), Type_variant (cstrs2, rep2)) ->
         if mark then begin
           let mark usage cstrs =
@@ -1154,39 +1148,7 @@ let type_declarations ?(equality = false) ~loc env ~mark name
     | (_, _) -> Some (Kind (of_kind decl1.type_kind, of_kind decl2.type_kind))
   in
   if err <> None then err else
-<<<<<<< HEAD
   let abstr = Btype.type_kind_is_abstract decl2 && decl2.type_manifest = None in
-||||||| 121bedcfd2
-  let abstr = decl2.type_kind = Type_abstract && decl2.type_manifest = None in
-  (* If attempt to assign a non-immediate type (e.g. string) to a type that
-   * must be immediate, then we error *)
-  let err =
-    if not abstr then
-      None
-    else
-      match
-        Type_immediacy.coerce decl1.type_immediate ~as_:decl2.type_immediate
-      with
-      | Ok () -> None
-      | Error violation -> Some (Immediate violation)
-  in
-  if err <> None then err else
-=======
-  let abstr = Btype.type_kind_is_abstract decl2 && decl2.type_manifest = None in
-  (* If attempt to assign a non-immediate type (e.g. string) to a type that
-   * must be immediate, then we error *)
-  let err =
-    if not abstr then
-      None
-    else
-      match
-        Type_immediacy.coerce decl1.type_immediate ~as_:decl2.type_immediate
-      with
-      | Ok () -> None
-      | Error violation -> Some (Immediate violation)
-  in
-  if err <> None then err else
->>>>>>> 5.2.0
   let need_variance =
     abstr || decl1.type_private = Private || decl1.type_kind = Type_open in
   if not need_variance then None else
