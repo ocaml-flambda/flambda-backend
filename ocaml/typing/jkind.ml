5b83c9e9eeec1b081ccfdadfafdfc6c5ecbd0f68
Liam Stevenson <lstevenson@janestreet.com>
2024-07-18 16:29:48 -0400

    Remove stale cr

--- ocaml/typing/jkind.ml
+++ ocaml/typing/jkind.ml
@@ -1063,7 +1063,6 @@ let of_new_legacy_sort_var ~why =
 
 let of_new_legacy_sort ~why = fst (of_new_legacy_sort_var ~why)
 
-(* CR layouts v2.8: remove this function *)
 let of_const ~why
     ({ layout;
        modes_upper_bounds;
