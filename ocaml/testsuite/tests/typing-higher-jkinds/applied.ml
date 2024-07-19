a47ae2a773c0746d568ad605fe5ee49376d70d02
Jakub Bachurski <kbachurski@gmail.com>
2024-07-19 10:52:55 +0100

    Update test

--- ocaml/testsuite/tests/typing-higher-jkinds/applied.ml
+++ ocaml/testsuite/tests/typing-higher-jkinds/applied.ml
@@ -13,7 +13,7 @@ type t : (value) => value
 type p = int t
 
 [%%expect {|
-
+type p = int t
 |}]
 
 
@@ -24,7 +24,7 @@ end = struct
 end
 
 [%%expect {|
-
+module M : sig type p = int t end
 |}]
 
 
@@ -35,7 +35,7 @@ end = struct
 end
 
 [%%expect {|
-
+module M : sig type 'a p = 'a t end
 |}]
 
 
@@ -53,21 +53,33 @@ module M : sig type p end
 type p = t t
 
 [%%expect {|
-
+Line 1, characters 9-12:
+1 | type p = t t
+             ^^^
+Error: Layout mismatch in final type declaration consistency check.
+       This is most often caused by the fact that type inference is not
+       clever enough to propagate layouts through variables in different
+       declarations. It is also not clever enough to produce a good error
+       message, so we'll say this instead:
+         The layout of t is ((value) => value) (...??)
+         But the layout of t must be a sublayout of value, because
+           of the definition of t at line 1, characters 0-23.
+       A good next step is to add a layout annotation on a parameter to
+       the declaration where this error is reported.
 |}]
 
 
 type r : (value => value) => value
 
 [%%expect {|
-type r' : ((value) => value) => value
+type r : ((value) => value) => value
 |}]
 
 
 type ('a : value => value) r
 
 [%%expect {|
-type ('a : ((higher))) r'
+type ('a : (value) => value) r
 |}]
 
 
@@ -76,7 +88,7 @@ module type M = sig
 end
 
 [%%expect{|
-
+module type M = sig val g : ('a : (value) => value). 'a r -> 'a r end
 |}]
 
 
@@ -87,7 +99,12 @@ module type M = sig
 end
 
 [%%expect{|
-
+module type M =
+  sig
+    type r : ((value) => value) => value
+    type s : (value) => value
+    val g : s r -> s r
+  end
 |}]
 
 
@@ -98,5 +115,10 @@ module type M = sig
 end
 
 [%%expect{|
-
+module type M =
+  sig
+    type r : ((value) => value) => value
+    type s : (value) => value
+    val g : int s r -> int s r
+  end
 |}]
