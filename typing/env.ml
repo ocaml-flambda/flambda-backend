cb65fd3be61583c8edc8599899389db81d4b5da3
dkalinichenko-js <118547217+dkalinichenko-js@users.noreply.github.com>
2024-07-19 17:39:05 -0400

    flambda-backend: Fix issues in the tree related to `Or_null` (#2808)
    * Probes are `value_or_null`
    
    * `Captured_in_object` variables can be null
    
    * fix build on macos?
    
    * fix mac os
    
    * comment
    
    * fix?
    
    * Update ocaml/typing/env.ml
    
    Co-authored-by: Richard Eisenberg <reisenberg@janestreet.com>
    
    * `not-macos`
    
    * wrong `not-macos`
    
    ---------
    
    Co-authored-by: Diana Kalinichenko <dkalinichenko@janestreet.com>
    Co-authored-by: Richard Eisenberg <reisenberg@janestreet.com>

--- typing/env.ml
+++ typing/env.ml
@@ -3095,7 +3095,12 @@ let unboxed_type ~errors ~env ~loc ~lid ty =
   match ty with
   | None -> ()
   | Some ty ->
-    match !constrain_type_jkind env ty Jkind.Primitive.(value ~why:Captured_in_object) with
+    (* The type is the type of a variable in the environment. It thus is likely generic. Despite
+       the fact that instantiated variables work better in [constrain_type_jkind] (because they
+       can be assigned more specific jkinds), we actually want to work on these generic types
+       here. After all, it's the value in the environment that is getting captured by the object,
+       not a specific instance of that variable. *)
+    match !constrain_type_jkind env ty Jkind.Primitive.(value_or_null ~why:Captured_in_object) with
     | Ok () -> ()
     | Result.Error err ->
       may_lookup_error errors loc env (Non_value_used_in_object (lid, ty, err))
--- typing/jkind.ml
+++ typing/jkind.ml
@@ -1477,12 +1477,14 @@ module Format_history = struct
       fprintf ppf "it's the type of something stored in a module structure"
     | V1_safety_check ->
       fprintf ppf "it has to be value for the V1 safety check"
+    | Probe -> format_with_notify_js ppf "it's a probe"
+    | Captured_in_object ->
+      fprintf ppf "it's the type of a variable captured in an object"
 
   let format_value_creation_reason ppf ~layout_or_kind :
       History.value_creation_reason -> _ = function
     | Class_let_binding ->
       fprintf ppf "it's the type of a let-bound variable in a class expression"
-    | Probe -> format_with_notify_js ppf "it's a probe"
     | Object -> fprintf ppf "it's the type of an object"
     | Instance_variable -> fprintf ppf "it's the type of an instance variable"
     | Object_field -> fprintf ppf "it's the type of an object field"
@@ -1524,8 +1526,6 @@ module Format_history = struct
     | Debug_printer_argument ->
       format_with_notify_js ppf
         "it's the type of an argument to a debugger printer function"
-    | Captured_in_object ->
-      fprintf ppf "it's the type of a variable captured in an object"
     | Recmod_fun_arg ->
       fprintf ppf
         "it's the type of the first argument to a function in a recursive \
@@ -1945,10 +1945,11 @@ module Debug_printers = struct
     | Polymorphic_variant_field -> fprintf ppf "Polymorphic_variant_field"
     | Structure_element -> fprintf ppf "Structure_element"
     | V1_safety_check -> fprintf ppf "V1_safety_check"
+    | Probe -> fprintf ppf "Probe"
+    | Captured_in_object -> fprintf ppf "Captured_in_object"
 
   let value_creation_reason ppf : History.value_creation_reason -> _ = function
     | Class_let_binding -> fprintf ppf "Class_let_binding"
-    | Probe -> fprintf ppf "Probe"
     | Object -> fprintf ppf "Object"
     | Instance_variable -> fprintf ppf "Instance_variable"
     | Object_field -> fprintf ppf "Object_field"
@@ -1975,7 +1976,6 @@ module Debug_printers = struct
     | Class_type_argument -> fprintf ppf "Class_type_argument"
     | Class_term_argument -> fprintf ppf "Class_term_argument"
     | Debug_printer_argument -> fprintf ppf "Debug_printer_argument"
-    | Captured_in_object -> fprintf ppf "Captured_in_object"
     | Recmod_fun_arg -> fprintf ppf "Recmod_fun_arg"
     | Unknown s -> fprintf ppf "Unknown %s" s
 
