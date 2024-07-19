ff6556c102d37dffe9b54dc163a5bc0e9c7143b8
Greta Yorsh <45005955+gretay-js@users.noreply.github.com>
2024-07-19 11:26:11 +0300

    Zero alloc fix flambda2 invalid no return take2 (#2810)

--- tests/backend/zero_alloc_checker/t.ml
+++ tests/backend/zero_alloc_checker/t.ml
@@ -326,3 +326,18 @@ module Test1 = struct
       |> h |> List.hd  (* ignore this allocation *)
     | answer -> (answer * 2)
 end
+
+module Test_caml_flambda2_invalid = struct
+  external opaque : 'a -> 'a = "%opaque"
+  external box : float# -> (float[@local_opt]) = "%box_float"
+  external unbox : (float[@local_opt]) -> float# = "%unbox_float"
+
+  let f _ = assert false
+
+  let _test () =
+    let a = opaque 0.0 |> unbox in
+    List.iter
+      (fun [@zero_alloc] _ ->
+         let _ : float = box a +. 0.0 |> opaque in
+         f #0.0) []
+end
