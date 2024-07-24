343dd318358c791dc7bc322f44743f07dac74ece
Liam Stevenson <liam.m.stevenson@gmail.com>
2024-07-24 10:59:09 -0400

    flambda-backend: Update arrow types to cross contention and uniqueness (#2835)
    * Update arrow types to cross contention and uniqueness
    
    * Remove arg_type and ret_type from for_arrow
    
    * Fix typo in test portable-contend.ml
    
    * Fix failing tests
    
    * Update cr
    
    * remove bad tests
    
    ---------
    
    Co-authored-by: Zesen Qian <github@riaqn.org>

--- testsuite/tests/typing-modal-kinds/basics.ml
+++ testsuite/tests/typing-modal-kinds/basics.ml
@@ -678,12 +678,8 @@ Line 2, characters 71-72:
 
 |}]
 
-(* CR layouts: this should succeed. *)
 let foo : (string -> string) -> (string -> string) @ unique
   = fun f -> f
 [%%expect{|
-Line 2, characters 13-14:
-2 |   = fun f -> f
-                 ^
-Error: This value is shared but expected to be unique.
+val foo : (string -> string) -> unique_ (string -> string) = <fun>
 |}]
