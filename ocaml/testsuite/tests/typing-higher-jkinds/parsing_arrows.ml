d0abc7ee45de17f353ff30a6cc1dfb687dbc1613
Jakub Bachurski <kbachurski@gmail.com>
2024-07-24 15:01:28 +0100

    Update test outputs

--- ocaml/testsuite/tests/typing-higher-jkinds/parsing_arrows.ml
+++ ocaml/testsuite/tests/typing-higher-jkinds/parsing_arrows.ml
@@ -14,8 +14,11 @@ type w : value => value => value
 
 [%%expect{|
 type p : (value) => value
-type q : (value, value) => value
-type r : ((value) => value, value) => value
-type s : (value, value mod local) => value
-type t : (value) => (value) => value
+type q : (value) => value
+type r : ((value) => value) => value
+type s : (value, value) => value
+type t : ((value) => value, value) => value
+type u : (value, value mod local) => value
+type v : (value) => (value) => value
+type w : (value) => (value) => value
 |}]
