4b88f99fde4bde771b6eed17b2064c66193e5cd6
Milan Tom <milant076@gmail.com>
2024-07-24 09:41:34 +0100

    Use "pass" rather than "stage"

--- ocaml/utils/profile.ml
+++ ocaml/utils/profile.ml
@@ -393,7 +393,7 @@ let output_to_csv ppf columns =
   let sanitise = String.map (fun c -> if c = ',' then '_' else c) in
   let to_csv cell_strings = cell_strings |> List.map sanitise |> String.concat "," in
   let string_columns = List.map (fun col -> List.assoc col column_mapping) columns in
-  Format.fprintf ppf "%s\n" (to_csv ("stage name" :: string_columns));
+  Format.fprintf ppf "%s\n" (to_csv ("pass name" :: string_columns));
   let output_row_f = output_rows
     ~output_row:(fun ~prefix ~cell_strings ~name ->
       Format.fprintf ppf "%s%s\n" prefix (to_csv (name :: cell_strings)))
