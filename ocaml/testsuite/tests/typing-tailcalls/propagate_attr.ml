105c41a687349bede96b09eb4686e37e3ddbf29f
Tyler Hou <tyhou@janestreet.com>
2024-07-25 12:24:17 +0100

    Skip propagate_attr on more configs

--- ocaml/testsuite/tests/typing-tailcalls/propagate_attr.ml
+++ ocaml/testsuite/tests/typing-tailcalls/propagate_attr.ml
@@ -1,15 +1,19 @@
 (* TEST
  setup-ocamlopt.opt-build-env;
- flags = "-dlambda -dflambda -dcmm -dsel -dcfg -c";
+ flags = "-dlambda -dflambda -dcmm -dsel -dcfg -c -O3";
  ocamlopt.opt;
  {
-   stack-allocation; flambda2; arch_amd64;
+   arch_amd64; runtime4; flambda2; stack-allocation; frame_pointers;
    check-ocamlopt.opt-output;
  }
 *)
 
-(* CR less-tco: We should remove this test. It is fragile because it
-   dumps cmm/mach output. *)
+(* CR less-tco: We should remove this test.
+
+   This test is fragile because it dumps cmm/mach output. This test does
+   not need runtime4, frame_pointers, etc. but only enabling the test when
+   those features are present makes the test slighly less fragile.
+*)
 external ext : unit -> unit = "ext"
 
 let foo () = ext ()
