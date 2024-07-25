21b4916fdfcb82373d89000097ae55b33dd01fd4
Milan Tom <milant076@gmail.com>
2024-07-25 11:15:48 +0100

    Move units to second header of summary CSV

--- scripts/combine-profile-information.py
+++ scripts/combine-profile-information.py
@@ -6,7 +6,7 @@ a single summary profile information CSV file.
 from argparse import ArgumentParser
 import csv
 from pathlib import Path
-from typing import Dict, List, Union
+from typing import Dict, Iterable, Iterator, List, Tuple, Union
 
 parser = ArgumentParser(description="Combine multiple profile information CSVs into one")
 parser.add_argument("dump_dir", help="The folder the profile information has been dumped in")
@@ -22,6 +22,10 @@ if args.summary_path is None:
     args.summary_path = DUMP_DIR.parent / "summary.csv"
 SUMMARY_PATH = Path(args.summary_path)
 
+CsvRow = Dict[str, str]
+CsvRows = List[CsvRow]
+SummaryRow = Dict[str, str]
+
 
 def parse_counters(counters: str) -> Dict[str, str]:
     if not counters:
@@ -30,18 +34,30 @@ def parse_counters(counters: str) -> Dict[str, str]:
     return dict(map(lambda x: tuple(x.split(" = ")), split_counters))
 
 
-def get_csv_rows(csv_path: Path) -> List[Dict]:
+def get_csv_rows(csv_path: Path) -> CsvRows:
     with open(csv_path) as csv_file:
         csv_reader = csv.DictReader(csv_file)
         return list(csv_reader)
+
+
+def get_input_csv_paths() -> Iterator[Tuple[Path, CsvRows]]:
+    return (csv_path for csv_path in DUMP_DIR.glob("*.csv"))
+
+
+def split_into_number_and_unit(number_string: str) -> (Union[int, float], str):
+    numeric = "".join(map(str, range(10))) + "."
+    unit_start_position = next((i for i, c in enumerate(number_string) if c not in numeric), len(number_string))
+    number, unit = float(number_string[:unit_start_position]), number_string[unit_start_position:]
+    if number.is_integer():
+        number = int(number)
+    return number, unit
 
 
 PRIMARY_KEY = "pass name"
 summary_non_counter_fields = set()
 summary_counter_fields = set()
 
-for csv_path in DUMP_DIR.glob("*.csv"):
-    rows = get_csv_rows(csv_path)
+for rows in map(get_csv_rows, get_input_csv_paths()):
     curr_fields = set(rows[0])
     curr_fields.remove(PRIMARY_KEY)
     if "counters" in curr_fields:
@@ -51,7 +67,7 @@ for csv_path in DUMP_DIR.glob("*.csv"):
     summary_non_counter_fields.update(curr_fields)
 
 # CR mitom: Not supporting memory for now
-SUMMARY_UNSUPPORTED_FIELDS = {
+summary_non_counter_fields -= {
     "alloc",
     "top-heap",
     "absolute-top-heap",
@@ -60,17 +76,26 @@ SUMMARY_UNSUPPORTED_FIELDS = {
 # Ensure consistent ordering of summary fields
 field_collections = {PRIMARY_KEY}, summary_non_counter_fields, summary_counter_fields
 SUMMARY_FIELD_NAMES = sum(
-    map(lambda fields: sorted(fields - SUMMARY_UNSUPPORTED_FIELDS), field_collections),
+    map(lambda fields: sorted(fields), field_collections),
     []
 )
 
+units = {}
+for field in summary_non_counter_fields:
+    first_row = get_csv_rows(next(get_input_csv_paths()))[0]
+    _, units[field] = split_into_number_and_unit(first_row[field])
 
-def row_summary(row: Dict[str, str]) -> Dict[str, str]:
-    full_summary = {**row, **parse_counters(row["counters"])}
+
+def strip_units(row: CsvRow) -> CsvRow:
+    return {k: (v[:-len(units[k])] if k in units else v) for k, v in row.items()}
+
+
+def row_summary(row: CsvRow) -> SummaryRow:
+    full_summary = {**strip_units(row), **parse_counters(row["counters"])}
     return {k: v for k, v in full_summary.items() if k in SUMMARY_FIELD_NAMES}
 
 
-def csv_to_summaries(rows: List[Dict]) -> List[Dict[str, str]]:
+def csv_to_summaries(rows: CsvRows) -> Iterable[SummaryRow]:
     counter_rows = [row for row in rows if row["counters"]]
     return map(row_summary, counter_rows)
 
@@ -95,7 +120,8 @@ def process_csv(csv_path: Path, summary_csv_writer: csv.DictWriter) -> None:
 with open(SUMMARY_PATH, "w", newline="") as summary_csv_file:
     summary_csv_writer = csv.DictWriter(summary_csv_file, fieldnames=SUMMARY_FIELD_NAMES)
     summary_csv_writer.writeheader()
+    summary_csv_writer.writerow(units)
 
-    for curr_csv_path in DUMP_DIR.glob("*.csv"):
+    for curr_csv_path in get_input_csv_paths():
         process_csv(curr_csv_path, summary_csv_writer)
         curr_csv_path.unlink()
