69189a34fea120bb8f833f7a91e66ca433cae069
Tyler Hou <tyhou@janestreet.com>
2024-07-25 13:31:08 +0100

    Disable propagate_attr in CI instead of ocamltest

--- ocaml/testsuite/tests/typing-tailcalls/propagate_attr.ml
+++ ocaml/testsuite/tests/typing-tailcalls/propagate_attr.ml
@@ -1,19 +1,12 @@
 (* TEST
  setup-ocamlopt.opt-build-env;
- flags = "-dlambda -dflambda -dcmm -dsel -dcfg -c -O3";
+ flags = "-dlambda -dflambda -dcmm -dsel -dcfg -c";
  ocamlopt.opt;
- {
-   arch_amd64; runtime4; flambda2; stack-allocation; frame_pointers;
-   check-ocamlopt.opt-output;
- }
+ check-ocamlopt.opt-output;
 *)
 
-(* CR less-tco: We should remove this test.
-
-   This test is fragile because it dumps cmm/mach output. This test does
-   not need runtime4, frame_pointers, etc. but only enabling the test when
-   those features are present makes the test slighly less fragile.
-*)
+(* CR less-tco: We should remove this test. It is fragile because it dumps
+   cmm/mach output. In CI it is disabled for all configs except for flambda2_dev. *)
 external ext : unit -> unit = "ext"
 
 let foo () = ext ()
