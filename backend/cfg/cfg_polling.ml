f7ba32857e20cc37d852e6c922ff0c81d23a8697
Xavier Clerc <xclerc@janestreet.com>
2024-07-25 10:50:12 +0100

    ocamlformat

--- backend/cfg/cfg_polling.ml
+++ backend/cfg/cfg_polling.ml
@@ -52,7 +52,8 @@ type polling_point = Polling.polling_point =
   | Function_call
   | External_call
 
-type error = Polling.error = Poll_error of Debuginfo.t * (polling_point * Debuginfo.t) list
+type error = Polling.error =
+  | Poll_error of Debuginfo.t * (polling_point * Debuginfo.t) list
 
 exception Error = Polling.Error
 
