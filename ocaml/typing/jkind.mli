6cbb09a1da7482b864d877db657792ac71e8cfc0
Jakub Bachurski <kbachurski@gmail.com>
2024-07-25 17:54:04 +0100

    Merge history types for type jkinds and general jkinds

--- ocaml/typing/jkind.mli
+++ ocaml/typing/jkind.mli
@@ -403,9 +403,8 @@ val to_type_jkind : t -> Type.t
 (** Convert a type jkind into a general jkind *)
 val of_type_jkind : Type.t -> t
 
-(** Construct the jkind from an arrow with a given history *)
-val of_arrow :
-  history:Types.type_expr Jkind_types.history -> t Jkind_types.Arrow.t -> t
+(** Construct the jkind from an arrow with a given reason *)
+val of_arrow : why:Types.type_expr Jkind_types.history -> t Jkind_types.Arrow.t -> t
 
 (******************************)
 (* elimination and defaulting *)
