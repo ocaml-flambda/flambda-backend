5b59d6d7c7928748df86a58c319e85daf17c00f6
Jakub Bachurski <kbachurski@gmail.com>
2024-07-26 12:15:31 +0100

    Create tops with the constructor

--- ocaml/typing/jkind_intf.ml
+++ ocaml/typing/jkind_intf.ml
@@ -275,6 +275,7 @@ module History = struct
     | Bits32_creation of bits32_creation_reason
     | Bits64_creation of bits64_creation_reason
     | Concrete_creation of concrete_jkind_reason
+    | Top_creation of top_creation_reason
     | Imported
     | Imported_type_argument of
         { parent_path : Path.t;
