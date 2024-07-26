45b5c9b7a92b5fe26fb4293956c2e0867678b9cc
Jakub Bachurski <kbachurski@gmail.com>
2024-07-26 13:06:24 +0100

    Constrain jkinds to any as appropriate

--- ocaml/typing/typetexp.ml
+++ ocaml/typing/typetexp.ml
@@ -640,6 +640,14 @@ let transl_bound_vars : (_, _) Either.t -> _ =
   | Right vars_jkinds -> TyVarEnv.make_poly_univars_jkinds
                            ~context:(fun v -> Univar ("'" ^ v)) vars_jkinds
 
+let constrain_type_expr_jkind_to_any ~loc ~vloc env typ =
+  let any = Jkind.Type.Primitive.any ~why:Dummy_jkind |> Jkind.of_type_jkind in
+  match constrain_type_jkind env typ any with
+  | Ok _ -> ()
+  | Error err ->
+    raise (Error(loc, env,
+                Non_sort {vloc; err; typ}))
+
 let rec transl_type env ~policy ?(aliased=false) ~row_context mode styp =
   Builtin_attributes.warning_scope styp.ptyp_attributes
     (fun () -> transl_type_aux env ~policy ~aliased ~row_context mode styp)
@@ -698,6 +706,8 @@ and transl_type_aux env ~row_context ~aliased ~policy mode styp =
                 (newconstr Predef.path_option [Btype.tpoly_get_mono arg_ty])
             end
           in
+          constrain_type_expr_jkind_to_any ~loc:arg_cty.ctyp_loc ~vloc:Fun_arg env arg_cty.ctyp_type;
+          constrain_type_expr_jkind_to_any ~loc:ret_cty.ctyp_loc ~vloc:Fun_ret env ret_cty.ctyp_type;
           let arg_mode = Alloc.of_const arg_mode in
           let ret_mode = Alloc.of_const ret_mode in
           let arrow_desc = (l, arg_mode, ret_mode) in
