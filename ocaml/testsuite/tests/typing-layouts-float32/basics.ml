7ce49af73b995a6f1f30e70cbcf54a5e8cad275e
Jakub Bachurski <kbachurski@gmail.com>
2024-07-30 11:58:38 +0100

    Promote test outputs

--- ocaml/testsuite/tests/typing-layouts-float32/basics.ml
+++ ocaml/testsuite/tests/typing-layouts-float32/basics.ml
@@ -174,7 +174,7 @@ type ('a : float32) t4_6 = 'a * 'a
 Line 1, characters 27-29:
 1 | type ('a : float32) t4_6 = 'a * 'a
                                ^^
-Error: This type ('a : value) should be an instance of type ('a0 : float32)
+Error: Tuple element types must have layout value.
        The layout of 'a is float32, because
          of the annotation on 'a in the declaration of the type t4_6.
        But the layout of 'a must overlap with value, because
@@ -184,10 +184,10 @@ Error: This type ('a : value) should be an instance of type ('a0 : float32)
 (* check for layout propagation *)
 type ('a : float32, 'b) t4_7 = ('a as 'b) -> ('b * 'b);;
 [%%expect{|
-Line 1, characters 32-34:
+Line 1, characters 46-48:
 1 | type ('a : float32, 'b) t4_7 = ('a as 'b) -> ('b * 'b);;
-                                    ^^
-Error: This type ('b : value) should be an instance of type ('a : float32)
+                                                  ^^
+Error: Tuple element types must have layout value.
        The layout of 'a is float32, because
          of the annotation on 'a in the declaration of the type t4_7.
        But the layout of 'a must overlap with value, because
@@ -382,7 +382,7 @@ type ('a : float32) f7_5 = [ `A of 'a ];;
 Line 1, characters 35-37:
 1 | type ('a : float32) f7_5 = [ `A of 'a ];;
                                        ^^
-Error: This type ('a : value) should be an instance of type ('a0 : float32)
+Error: Polymorphic variant constructor argument types must have layout value.
        The layout of 'a is float32, because
          of the annotation on 'a in the declaration of the type f7_5.
        But the layout of 'a must overlap with value, because
@@ -613,10 +613,10 @@ Error: Object field types must have layout value.
 
 type ('a : float32) t12_2 = < x : 'a >;;
 [%%expect{|
-Line 1, characters 34-36:
+Line 1, characters 30-36:
 1 | type ('a : float32) t12_2 = < x : 'a >;;
-                                      ^^
-Error: This type ('a : value) should be an instance of type ('a0 : float32)
+                                  ^^^^^^
+Error: Object field types must have layout value.
        The layout of 'a is float32, because
          of the annotation on 'a in the declaration of the type t12_2.
        But the layout of 'a must overlap with value, because
@@ -643,7 +643,7 @@ end;;
 Line 2, characters 13-15:
 2 |   method x : 'a t_float32_id -> 'a t_float32_id = assert false
                  ^^
-Error: This type ('a : float32) should be an instance of type ('a0 : value)
+Error: This type ('a : value) should be an instance of type ('b : float32)
        The layout of 'a is value, because
          it's a type argument to a class constructor.
        But the layout of 'a must overlap with float32, because
@@ -694,7 +694,7 @@ end
 Line 2, characters 10-12:
 2 |   val x : 'a t_float32_id -> 'a t_float32_id
               ^^
-Error: This type ('a : float32) should be an instance of type ('a0 : value)
+Error: This type ('a : value) should be an instance of type ('b : float32)
        The layout of 'a is value, because
          it's a type argument to a class constructor.
        But the layout of 'a must overlap with float32, because
