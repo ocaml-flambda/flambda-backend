7686999f9ac6994c5d16dc937b8e174049ff0a96
Jakub Bachurski <kbachurski@gmail.com>
2024-07-30 14:32:59 +0100

    Constrain jkinds in type applications

---
+++ ocaml/testsuite/tests/typing-higher-jkinds/jkind_inference.ml
@@ -0,0 +1,52 @@
+(* TEST
+ flags = "-extension layouts_alpha";
+ expect;
+*)
+
+type ('a : value => value) t = unit 'a
+[%%expect {|
+type ('a : value => value) t = unit 'a
+|}]
+
+type 'a t = unit 'a
+[%%expect {|
+Line 1, characters 12-19:
+1 | type 'a t = unit 'a
+                ^^^^^^^
+Error: Types of jkind (immediate) cannot be applied to '_representable_layout_1.
+|}]
+
+type ('a, 'm) t = 'a 'm
+[%%expect {|
+Line 1, characters 18-23:
+1 | type ('a, 'm) t = 'a 'm
+                      ^^^^^
+Error: Types of jkind ('_representable_layout_2) cannot be applied to '_representable_layout_3.
+|}]
+
+type ('a, 'm : value => value) t = 'a 'm
+[%%expect {|
+type ('a, 'm : value => value) t = 'a 'm
+|}]
+
+type ('a, 'm : immediate => immediate) t = 'a 'm
+[%%expect {|
+type ('a : immediate, 'm : immediate => immediate) t = 'a 'm
+|}]
+
+module type M = sig
+  val f : ('a : value => value). unit -> unit 'a
+end
+[%%expect {|
+module type M = sig val f : ('a : value => value). unit -> unit 'a end
+|}]
+
+module type M = sig
+  val f : unit -> unit 'a
+end
+[%%expect {|
+Line 2, characters 18-25:
+2 |   val f : unit -> unit 'a
+                      ^^^^^^^
+Error: Types of jkind (immediate) cannot be applied to '_representable_layout_4.
+|}]
