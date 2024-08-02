af0e032a79d9dc47878251e4c80e6b71fdbb0294
Liam Stevenson <lstevenson@janestreet.com>
2024-08-02 10:42:34 -0400

    Modify cms format

--- ocaml/file_formats/cms_format.ml
+++ ocaml/file_formats/cms_format.ml
@@ -26,8 +26,10 @@ type cms_infos = {
   cms_comments : (string * Location.t) list;
   cms_sourcefile : string option;
   cms_builddir : string;
+  cms_loadpath : Load_path.paths;
   cms_source_digest : Digest.t option;
-  cms_uid_to_loc : Location.t Shape.Uid.Tbl.t;
+  cms_initial_env : Env.t;
+  cms_uid_to_loc : string Location.loc Shape.Uid.Tbl.t;
   cms_uid_to_attributes : Parsetree.attributes Shape.Uid.Tbl.t;
   cms_impl_shape : Shape.t option; (* None for mli *)
   cms_ident_occurrences :
@@ -60,6 +62,7 @@ let read filename =
 let toplevel_attributes = ref []
 
 let register_toplevel_attributes uid ~attributes ~loc =
+  let loc : _ Location.loc = { txt = ""; loc } in
   toplevel_attributes := (uid, loc, attributes) :: !toplevel_attributes
 
 let uid_tables_of_binary_annots binary_annots =
@@ -71,27 +74,28 @@ let uid_tables_of_binary_annots binary_annots =
     !toplevel_attributes;
   Cmt_format.iter_declarations binary_annots
     ~f:(fun uid decl ->
-      let loc, attrs =
+      let loc = Typedtree.loc_of_decl ~uid decl in
+      let attrs =
         match decl with
-        | Value v -> v.val_loc, v.val_attributes
-        | Value_binding v -> v.vb_loc, v.vb_attributes
-        | Type v -> v.typ_loc, v.typ_attributes
-        | Constructor v -> v.cd_loc, v.cd_attributes
-        | Extension_constructor v -> v.ext_loc, v.ext_attributes
-        | Label v -> v.ld_loc, v.ld_attributes
-        | Module v -> v.md_loc, v.md_attributes
-        | Module_substitution v -> v.ms_loc, v.ms_attributes
-        | Module_binding v -> v.mb_loc, v.mb_attributes
-        | Module_type v -> v.mtd_loc, v.mtd_attributes
-        | Class v -> v.ci_loc, v.ci_attributes
-        | Class_type v -> v.ci_loc, v.ci_attributes
+        | Value v -> v.val_attributes
+        | Value_binding v -> v.vb_attributes
+        | Type v -> v.typ_attributes
+        | Constructor v -> v.cd_attributes
+        | Extension_constructor v -> v.ext_attributes
+        | Label v -> v.ld_attributes
+        | Module v -> v.md_attributes
+        | Module_substitution v -> v.ms_attributes
+        | Module_binding v -> v.mb_attributes
+        | Module_type v -> v.mtd_attributes
+        | Class v -> v.ci_attributes
+        | Class_type v -> v.ci_attributes
       in
       Types.Uid.Tbl.add cms_uid_to_loc uid loc;
       Types.Uid.Tbl.add cms_uid_to_attributes uid attrs
     );
   cms_uid_to_loc, cms_uid_to_attributes
 
-let save_cms filename modname binary_annots sourcefile shape =
+let save_cms filename modname binary_annots sourcefile initial_env shape =
   if (!Clflags.binary_annotations_cms && not !Clflags.print_types) then begin
     Misc.output_to_file_via_temporary
        ~mode:[Open_binary] filename
@@ -112,7 +116,10 @@ let save_cms filename modname binary_annots sourcefile shape =
             cms_comments = Lexer.comments ();
             cms_sourcefile = sourcefile;
             cms_builddir = Location.rewrite_absolute_path (Sys.getcwd ());
+            cms_loadpath = Load_path.get_paths ();
             cms_source_digest = source_digest;
+            cms_initial_env = if Cmt_format.need_to_clear_env
+              then Env.keep_only_summary initial_env else initial_env;
             cms_uid_to_loc;
             cms_uid_to_attributes;
             cms_impl_shape = shape;
