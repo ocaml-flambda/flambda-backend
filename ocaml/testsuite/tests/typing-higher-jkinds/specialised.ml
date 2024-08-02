23597ca02695bd3a56ef177fe7199ff9621a2d4d
Jakub Bachurski <kbachurski@gmail.com>
2024-08-02 13:54:12 +0100

    Mess with some tests

--- ocaml/testsuite/tests/typing-higher-jkinds/specialised.ml
+++ ocaml/testsuite/tests/typing-higher-jkinds/specialised.ml
@@ -49,49 +49,89 @@ let foo : type (a : value => value). a t -> int a = function
 val foo : ('a : value => value). 'a t -> int 'a = <fun>
 |}]
 
-type ('m : value => value) functor_instance = {
+type l : value => value = list
+let x: int l = [1]
+[%%expect{|
+type l = list
+val x : int l = <abstr>
+|}]
+
+(* Basic GADTs *)
+
+type ('a : value => value) t =
+  | List : list t
+  | Option : option t
+[%%expect{|
+type ('a : value => value) t = List : list t | Option : option t
+|}]
+
+let foo : type (a : value => value). a t -> int a = function
+  | List -> ([1] : int a)
+  | Option -> Some 2
+[%%expect{|
+val foo : ('a : value => value). 'a t -> int 'a = <fun>
+|}]
+
+type l : value => value = list
+let x: int l = [1]
+[%%expect{|
+type l = list
+val x : int l = <abstr>
+|}]
+
+(* Basic GADTs *)
+
+type ('a : value => value) t =
+  | List : list t
+  | Option : option t
+[%%expect{|
+type ('a : value => value) t = List : list t | Option : option t
+|}]
+
+let foo : type (a : value => value). a t -> int a = function
+  | List -> [1]
+  | Option -> Some 2
+[%%expect{|
+val foo : ('a : value => value). 'a t -> int 'a = <fun>
+|}]
+
+(* Annotations on both [functor_instance] and [funct] are required *)
+
+type ('m : value => value) functor_impl = {
   return : 'a. 'a -> 'a 'm;
   map : 'a 'b. ('a -> 'b) -> ('a 'm -> 'b 'm)
 }
-
 [%%expect{|
-type ('m : value => value) functor_instance = {
+type ('m : value => value) functor_impl = {
   return : 'a. 'a -> 'a 'm;
   map : 'a 'b. ('a -> 'b) -> 'a 'm -> 'b 'm;
 }
 |}]
 
 type 'a id = { id : 'a }
-
-type ('m : value => value) funct =
+type (_ : value => value) funct =
   | Id : id funct
   | List : list funct
-  | Instance : 'm functor_instance -> 'm funct
-
+  | Instance : 'm functor_impl -> 'm funct
 [%%expect{|
 type 'a id = { id : 'a; }
-Line 6, characters 15-17:
-6 |   | Instance : 'm functor_instance -> 'm funct
-                   ^^
-Error: This type ('m : '_representable_layout_1)
-       should be an instance of type ('a : value => value)
-       The kind of 'm is '_representable_layout_1
-         because it's a fresh unification variable.
-       But the kind of 'm must overlap with ((value) => value)
-         because of the definition of functor_instance at lines 1-4, characters 0-1.
+type (_ : value => value) funct =
+    Id : id funct
+  | List : list funct
+  | Instance : ('m : value => value). 'm functor_impl -> 'm funct
 |}]
 
 let return : type a (m : value => value). m funct -> a -> a m = fun f x -> match f with
-  | Id -> { id = x }
+  | Id -> ({ id = x })
   | List -> [x]
   | Instance inst -> inst.return x
 
 [%%expect{|
-Line 1, characters 44-49:
-1 | let return : type a (m : value => value). m funct -> a -> a m = fun f x -> match f with
-                                                ^^^^^
-Error: Unbound type constructor funct
-Hint: Did you mean unit?
+Line 2, characters 10-22:
+2 |   | Id -> ({ id = x })
+              ^^^^^^^^^^^^
+Error: This expression has type a id but an expression was expected of type
+         a m
 |}]
 
 let map : type a b (m : value => value). m funct -> (a -> b) -> a m -> b m = fun f t x -> match f with
