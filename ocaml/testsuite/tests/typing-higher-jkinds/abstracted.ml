3fac2fdec5998ea7bf16eeed5d6d16bd99d4d1f8
Jakub Bachurski <kbachurski@gmail.com>
2024-08-02 14:38:04 +0100

    Extend test for datatype attribute

--- ocaml/testsuite/tests/typing-higher-jkinds/abstracted.ml
+++ ocaml/testsuite/tests/typing-higher-jkinds/abstracted.ml
@@ -66,9 +66,49 @@ Error: Signature mismatch:
 |}]
 
 module M : sig
+  type t : (value, value) => value
+end = struct
+  type ('a, 'b) t [@@datatype]
+end
+[%%expect {|
+Lines 3-5, characters 6-3:
+3 | ......struct
+4 |   type ('a, 'b) t
+5 | end
+Error: Signature mismatch:
+       Modules do not match:
+         sig type ('a, 'b) t end
+       is not included in
+         sig type t : (value, value) => value end
+       Type declarations do not match:
+         type ('a, 'b) t
+       is not included in
+         type t : (value, value) => value
+       They have different arities.
+|}]
+
+module M : sig
   type t : value => value
 end = struct
   type t : value => value
+end
+[%%expect {|
+module M : sig type t : value => value end
+|}]
+
+module M : sig
+  type t : value => value
+end = struct
+  type 'a t [@@datatype]
+end
+[%%expect {|
+module M : sig type t : value => value end
+|}]
+
+module M : sig
+  type 'a t
+end = struct
+  type 'a t [@@datatype]
 end
 [%%expect {|
 module M : sig type t : value => value end
