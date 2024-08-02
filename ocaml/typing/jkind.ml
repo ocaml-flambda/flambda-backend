9f0c343a2d546819a379689c1b6ad83aaaa8a87f
Jakub Bachurski <kbachurski@gmail.com>
2024-08-02 16:20:51 +0100

    bring back sort unions.. for now

--- ocaml/typing/jkind.ml
+++ ocaml/typing/jkind.ml
@@ -2039,7 +2039,7 @@ let rec intersection_or_error ~reason (t1 : t) (t2 : t) =
     | Some jkind ->
       Ok
         { jkind = Type jkind;
-          history = combine_histories reason t1 t2;
+          history;
           has_warned = t1.has_warned || t2.has_warned
         })
   | ( Arrow { args = args1; result = result1 },
@@ -2057,7 +2057,7 @@ and union_or_error ~reason (t1 : t) (t2 : t) =
     (* Union is infallible at type jkinds *)
     Ok
       { jkind = Type (Type.Jkind_desc.union ty1.jkind ty2.jkind);
-        history = combine_histories reason t1 t2;
+        history;
         has_warned = ty1.has_warned || ty2.has_warned
       }
   | ( Arrow { args = args1; result = result1 },
