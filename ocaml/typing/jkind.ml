3cd16fd7041a95653483fb29fc74602b58ef58f3
Jakub Bachurski <kbachurski@gmail.com>
2024-08-02 19:12:41 +0100

    Unions at top

--- ocaml/typing/jkind.ml
+++ ocaml/typing/jkind.ml
@@ -2478,7 +2478,7 @@ and union_or_error ~reason (t1 : t) (t2 : t) =
           in
           let* result = union_or_error ~reason result1 result2 in
           Ok (of_arrow ~history:(combine_histories reason t1 t2) ~args ~result)))
-  | _, (Arrow _ | Type _ | Top) -> Error violation
+  | _, (Arrow _ | Type _ | Top) -> Ok (Primitive.top ~why:Dummy_jkind)
 
 and intersection_list_or_error ~reason ~violation =
   combine_list_or_error ~violation (intersection_or_error ~reason)
