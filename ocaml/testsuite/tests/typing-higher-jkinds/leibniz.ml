bb8326e9fd507e346c25d9247fa7ac9a0c800b1d
Jakub Bachurski <kbachurski@gmail.com>
2024-08-05 10:41:05 +0100

    add incomplete leibniz test

---
+++ ocaml/testsuite/tests/typing-higher-jkinds/leibniz.ml
@@ -0,0 +1,38 @@
+(* TEST
+  reason = "Leibniz equality is better with partial type application";
+  skip;
+  flags = "-extension layouts_alpha";
+  expect;
+*)
+
+module Eq : sig
+  type ('a : top, 'b : top) eq
+
+  val refl : unit -> ('a, 'a) eq
+  val subst : ('a : top) ('b : top) ('f : top => value). ('a, 'b) eq -> ('a 'f -> 'b 'f)
+end = struct
+  type ('a : top, 'b : top) eq = { subst : ('f : top => value). 'a 'f -> 'b 'f }
+  let refl () = { subst = (fun x -> x) }
+  let subst eq x = eq.subst x
+end
+
+[%%expect{|
+module Eq :
+  sig
+    type ('a : top, 'b : top) eq
+    val refl : unit -> ('a, 'a) eq
+    val subst :
+      ('a : top) ('b : top) ('f : top => value).
+        ('a, 'b) eq -> 'a 'f -> 'b 'f
+  end
+|}]
+
+open Eq
+(* FIXME: this needs both inference and abstract datatypes *)
+let trans : ('a, 'b) eq -> ('b, 'c) eq -> ('a, 'c) eq = fun ab bc -> subst bc ab
+
+(* how does this error?? some intermediate type gets placed in a Tapp, I guess? *)
+[%%expect{|
+Uncaught exception: Failure("no jkind for tconstr???")
+
+|}]
