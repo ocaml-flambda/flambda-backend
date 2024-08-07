2827334ad4682cd2d6290e1e2aa999b87a5955c6
Mark Shinwell <mshinwell@pm.me>
2024-08-07 12:44:30 +0100

    Add pr2848.ml

---
+++ ocaml/testsuite/tests/typing-local/pr2848.ml
@@ -0,0 +1,10 @@
+(* TEST *)
+
+let f (x : int) y =
+  let h (y : int) =
+    let _ = local_ (x, y) in
+    if y < 42 then failwith "foo"
+  in
+  let _m = ref 42 in
+  let l = local_ (x, x) in
+  h (fst l)
