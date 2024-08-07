b1d211e2800b102d2b49b41dcdb7ca3104157910
Max Slater <max@thenumb.at>
2024-08-07 03:29:57 -0400

    Add float32 unboxer to global list (#2897)

--- tests/small_numbers/float32_u_lib.ml
+++ tests/small_numbers/float32_u_lib.ml
@@ -841,3 +841,30 @@ module Bigarray = struct
   end
 end
 
+module Noalloc = struct
+
+  let don't_allocate f : 'a =
+    (* NB: right-to-left evaluation order gets this right *)
+    let baseline_allocation = Gc.allocated_bytes() -. Gc.allocated_bytes() in
+    let before = Gc.allocated_bytes () in
+    let result = (f[@inlined never]) () in
+    let after = Gc.allocated_bytes () in
+    (match Sys.backend_type with
+    | Native ->  assert ((after -. before) = baseline_allocation)
+    | _ -> ());
+    result
+
+  let[@inline] of_int32_preserve_order x =
+    let open Stdlib_beta.Float32 in
+    if Stdlib.( >= ) x 0l then of_bits x else neg (of_bits (Stdlib.Int32.neg x))
+  ;;
+
+  let[@inline] of_int32_preserve_order i : float32# =
+    F32.of_float32 ((of_int32_preserve_order [@inlined hint]) i)
+  ;;
+
+  let _ =
+    don't_allocate (fun () -> of_int32_preserve_order (Sys.opaque_identity 0l))
+  ;;
+
+end
