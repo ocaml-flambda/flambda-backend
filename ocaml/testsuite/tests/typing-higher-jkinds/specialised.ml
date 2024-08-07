f2bd21237edf380ebf7fe19e2829b131b0315df5
Jakub Bachurski <kbachurski@gmail.com>
2024-08-07 12:04:56 +0100

    Cleanup rebase and try to fix GADTs once more

--- ocaml/testsuite/tests/typing-higher-jkinds/specialised.ml
+++ ocaml/testsuite/tests/typing-higher-jkinds/specialised.ml
@@ -33,70 +33,7 @@ type l = list
 val x : int l = <abstr>
 |}]
 
-(* Basic GADTs *)
-
-type ('a : value => value) t =
-  | List : list t
-  | Option : option t
-[%%expect{|
-type ('a : value => value) t = List : list t | Option : option t
-|}]
-
-let foo : type (a : value => value). a t -> int a = function
-  | List -> [1]
-  | Option -> Some 2
-[%%expect{|
-val foo : ('a : value => value). 'a t -> int 'a = <fun>
-|}]
-
-type l : value => value = list
-let x: int l = [1]
-[%%expect{|
-type l = list
-val x : int l = <abstr>
-|}]
-
-(* Basic GADTs *)
-
-type ('a : value => value) t =
-  | List : list t
-  | Option : option t
-[%%expect{|
-type ('a : value => value) t = List : list t | Option : option t
-|}]
-
-let foo : type (a : value => value). a t -> int a = function
-  | List -> ([1] : int a)
-  | Option -> Some 2
-[%%expect{|
-val foo : ('a : value => value). 'a t -> int 'a = <fun>
-|}]
-
-type l : value => value = list
-let x: int l = [1]
-[%%expect{|
-type l = list
-val x : int l = <abstr>
-|}]
-
-(* Basic GADTs *)
-
-type ('a : value => value) t =
-  | List : list t
-  | Option : option t
-[%%expect{|
-type ('a : value => value) t = List : list t | Option : option t
-|}]
-
-let foo : type (a : value => value). a t -> int a = function
-  | List -> [1]
-  | Option -> Some 2
-[%%expect{|
-val foo : ('a : value => value). 'a t -> int 'a = <fun>
-|}]
-
 (* Annotations on both [functor_instance] and [funct] are required *)
-
 type ('m : value => value) functor_impl = {
   return : 'a. 'a -> 'a 'm;
   map : 'a 'b. ('a -> 'b) -> ('a 'm -> 'b 'm)
@@ -127,11 +64,7 @@ let return : type a (m : value => value). m funct -> a -> a m = fun f x -> match
   | Instance inst -> inst.return x
 
 [%%expect{|
-Line 2, characters 10-22:
-2 |   | Id -> ({ id = x })
-              ^^^^^^^^^^^^
-Error: This expression has type a id but an expression was expected of type
-         a m
+val return : ('m : value => value) 'a. 'm funct -> 'a -> 'a 'm = <fun>
 |}]
 
 let map : type a b (m : value => value). m funct -> (a -> b) -> a m -> b m = fun f t x -> match f with
@@ -140,9 +73,7 @@ let map : type a b (m : value => value). m funct -> (a -> b) -> a m -> b m = fun
   | Instance inst -> inst.map t x
 
 [%%expect{|
-Line 1, characters 43-48:
-1 | let map : type a b (m : value => value). m funct -> (a -> b) -> a m -> b m = fun f t x -> match f with
-                                               ^^^^^
-Error: Unbound type constructor funct
-Hint: Did you mean unit?
+val map :
+  ('m : value => value) 'a 'b. 'm funct -> ('a -> 'b) -> 'a 'm -> 'b 'm =
+  <fun>
 |}]
