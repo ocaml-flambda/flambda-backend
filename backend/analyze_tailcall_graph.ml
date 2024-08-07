7b834afdec152c1fe09a83b3de1c45428f1635e8
Tyler Hou <tyhou@janestreet.com>
2024-08-07 17:49:22 +0100

    Decompose the tail call graph into strongly connected components (#2868)
    For the purpose of the current analysis, we ignore non-inferred nontail edges (these are edges explicitly marked as `[@nontail]`, and function applications that were not in tail position and also not optimized to be tail calls). The dot output has been changed to show SCCs in clusters, and also highlight clusters that have at least one inferred nontail edge (red edges).
    
    Example graph: https://github.com/user-attachments/assets/c33581a0-db42-4edd-b67b-2b4642274c14

--- backend/analyze_tailcall_graph.ml
+++ backend/analyze_tailcall_graph.ml
@@ -38,7 +38,15 @@ module Hashset = struct
 
     val add : t -> elt -> unit
 
+    val remove : t -> elt -> unit
+
+    val mem : t -> elt -> bool
+
     val iter : t -> f:(elt -> unit) -> unit
+
+    val length : t -> int
+
+    val to_seq : t -> elt Seq.t
   end
 
   module Make (T : Hashtbl.S) : S with type elt = T.key = struct
@@ -50,7 +58,15 @@ module Hashset = struct
 
     let add t k = T.replace t k ()
 
+    let remove t k = T.remove t k
+
+    let mem t k = T.mem t k
+
     let iter t ~f = T.iter (fun e () -> f e) t
+
+    let length t = T.length t
+
+    let to_seq t = T.to_seq_keys t
   end
 end
 
@@ -59,6 +75,8 @@ module Graph : sig
     type t
 
     val unknown : t
+
+    module Hashset : Hashset.S with type elt = t
   end
 
   type t
@@ -81,7 +99,13 @@ module Graph : sig
     original_position:Typedtree.position_and_tail_attribute ->
     unit
 
-  val print_dot : t -> Format.formatter -> unit
+  (* Partitions vertices into hashsets where two vertices are in the same
+     hashset iff they are in the same strongly-connected component.
+
+     Explicit_nontail_edges are ignored in this SCC decomposition. *)
+  val decompose_tailcall_sccs : t -> Vertex.Hashset.t list
+
+  val print_dot : Format.formatter -> t -> sccs:Vertex.Hashset.t list -> unit
 end = struct
   module Vertex = struct
     module T = struct
@@ -114,6 +138,7 @@ end = struct
       match t with Unknown_fn -> "<unknown>" | Known_fn { name; _ } -> name
 
     module Tbl = Hashtbl.Make (T)
+    module Hashset = Hashset.Make (Tbl)
   end
 
   module Edge = struct
@@ -282,15 +307,109 @@ end = struct
       let edges = Vertex.Tbl.find t.adjacencies from in
       Edge.Hashset.add edges edge
 
-  let print_vertex_line ppf kv =
+  type vertex_state =
+    { preorder : int;
+      mutable lowlink : int
+    }
+
+  type vertex_visited =
+    | Not_visited
+    | Visited of vertex_state
+
+  let decompose_tailcall_sccs t =
+    (* CR less-tco: Refactor this to be more functional. *)
+    let states =
+      t.adjacencies |> Vertex.Tbl.to_seq_keys
+      |> Seq.map (fun v -> v, Not_visited)
+      |> Vertex.Tbl.of_seq
+    in
+    (* Invariant: A vertex is in stack iff it is in stack_set. *)
+    let stack = Stack.create () and stack_set = Vertex.Hashset.create 10 in
+    let push_vertex v =
+      Vertex.Hashset.add stack_set v;
+      Stack.push v stack
+    and pop_vertex () =
+      let popped = Stack.pop stack in
+      Vertex.Hashset.remove stack_set popped;
+      popped
+    in
+    let next_num =
+      let num_visited = ref 0 in
+      fun () ->
+        let num = !num_visited in
+        incr num_visited;
+        num
+    in
+    let sccs = Stack.create () in
+    (* Invariant: v must not be visited. *)
+    let rec visit (vertex : Vertex.t) : vertex_state =
+      let num = next_num () in
+      let state = { preorder = num; lowlink = num } in
+      Vertex.Tbl.replace states vertex (Visited state);
+      push_vertex vertex;
+      (* Recursively traverse successors and update this vertex's state
+         accordingly. *)
+      Edge.Hashset.iter (successors t vertex) ~f:(fun { label; to_; _ } ->
+          match label with
+          | Explicit_nontail_edge ->
+            (* Ignore these; before less-tco they already allocate stack space,
+               so are unlikely to be part of some cycle that blows the stack. *)
+            ()
+          | Explicit_tail_edge | Inferred_tail_edge | Inferred_nontail_edge -> (
+            let to_state = Vertex.Tbl.find states to_ in
+            match to_state with
+            | Not_visited ->
+              let to_state = visit to_ in
+              state.lowlink <- min state.lowlink to_state.lowlink
+            | Visited to_state ->
+              if Vertex.Hashset.mem stack_set to_
+              then
+                (* Back-edge *)
+                state.lowlink <- min state.lowlink to_state.preorder));
+      (* After recursively visiting all successors, if the original vertex is
+         the root of the SCC in the DFS tree, pop from the stack to get all the
+         vertices in the SCC. *)
+      if state.preorder = state.lowlink
+      then (
+        let scc = Vertex.Hashset.create 10 in
+        let cur = ref (pop_vertex ()) in
+        while not (Vertex.equal !cur vertex) do
+          Vertex.Hashset.add scc !cur;
+          cur := pop_vertex ()
+        done;
+        (* Stopping condition: !cur = vertex *)
+        Vertex.Hashset.add scc vertex;
+        Stack.push scc sccs);
+      state
+    in
+    Vertex.Tbl.iter
+      (fun v _edges ->
+        match Vertex.Tbl.find states v with
+        | Not_visited -> ignore (visit v)
+        | Visited _ -> ())
+      t.adjacencies;
+    sccs |> Stack.to_seq |> List.of_seq
+
+  let possibly_overflowing_edges t ~(scc : Vertex.Hashset.t) =
+    Vertex.Hashset.to_seq scc
+    |> Seq.concat_map (fun v ->
+           successors t v |> Edge.Hashset.to_seq
+           |> Seq.filter (fun (e : Edge.t) ->
+                  match e.label with
+                  | Explicit_tail_edge | Explicit_nontail_edge
+                  | Inferred_tail_edge ->
+                    false
+                  | Inferred_nontail_edge -> Vertex.Hashset.mem scc e.to_))
+    |> List.of_seq
+
+  let print_vertex_line ~indent ppf kv =
     let color = if Vertex.is_unknown kv then "red" else "black" in
-    Format.fprintf ppf
-      "  %s [label=\"%s\" shape=box color=\"%s\" fontcolor=\"%s\"]\n"
-      (Vertex.to_dot_id kv) (Vertex.to_dot_label kv) color color
+    Format.fprintf ppf "%s%s [label=\"%s\" color=\"%s\" fontcolor=\"%s\"]\n"
+      indent (Vertex.to_dot_id kv) (Vertex.to_dot_label kv) color color
 
   let hide_unknown_edges = true
 
-  let print_edge_line ppf ({ from; to_; label } : Edge.t) =
+  let print_edge_line ~indent ppf ({ from; to_; label } : Edge.t) =
     if Vertex.is_unknown from && hide_unknown_edges
     then ()
     else
@@ -311,18 +430,45 @@ end = struct
         | Inferred_tail_edge -> "solid"
         | Inferred_nontail_edge -> "solid"
       in
-      Format.fprintf ppf "  %s -> %s [color=\"%s\" style=\"%s\"]\n"
+      Format.fprintf ppf "%s%s -> %s [color=\"%s\" style=\"%s\"]\n" indent
         (Vertex.to_dot_id from) (Vertex.to_dot_id to_) color style
 
-  let print_dot t ppf =
+  let print_dot ppf t ~sccs =
     Format.fprintf ppf "digraph {\n";
-    Format.fprintf ppf "  rankdir=LR\n";
-    Vertex.Tbl.iter
-      (fun vtx edges ->
-        print_vertex_line ppf vtx;
-        Edge.Hashset.iter edges ~f:(fun e -> print_edge_line ppf e);
-        ())
-      t.adjacencies;
+    Format.fprintf ppf "  rankdir=LR\n\n";
+    List.iteri
+      (fun idx scc ->
+        let indent = "    " in
+        let has_possibly_overflowing_edges =
+          List.length (possibly_overflowing_edges t ~scc) > 0
+        in
+        Format.fprintf ppf "  subgraph cluster_%d {\n" idx;
+        Format.fprintf ppf "    label=\"%d\"\n" idx;
+        if has_possibly_overflowing_edges
+        then (
+          Format.fprintf ppf "    color=mistyrose\n";
+          Format.fprintf ppf "    style=filled\n")
+        else if Vertex.Hashset.length scc = 1
+        then Format.fprintf ppf "    style=invis\n";
+        Vertex.Hashset.iter scc ~f:(fun vtx ->
+            let edges = successors t vtx in
+            print_vertex_line ~indent ppf vtx;
+            Edge.Hashset.iter edges ~f:(fun e ->
+                (* If an edge is in a cluster, dot seems to layout the to_ node
+                   within the same cluster. So only print interior SCC edges
+                   here. *)
+                if Vertex.Hashset.mem scc e.to_
+                then print_edge_line ~indent ppf e);
+            ());
+        Format.fprintf ppf "  }\n";
+        (* Print cross-SCC edges here. *)
+        Vertex.Hashset.iter scc ~f:(fun vtx ->
+            let edges = successors t vtx in
+            Edge.Hashset.iter edges ~f:(fun e ->
+                if not (Vertex.Hashset.mem scc e.to_)
+                then print_edge_line ~indent:"  " ppf e));
+        Format.fprintf ppf "\n")
+      sccs;
     Format.fprintf ppf "}\n\n"
 end
 
@@ -358,5 +504,7 @@ module Global_state = struct
           ());
     cfg_with_layout
 
-  let print_dot ppf = Graph.print_dot graph ppf
+  let print_dot ppf =
+    let sccs = Graph.decompose_tailcall_sccs graph in
+    Graph.print_dot ppf graph ~sccs
 end
