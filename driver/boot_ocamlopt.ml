c27a8cf2b933206e04244f7ccee943b4ea7c8e75
Mark Shinwell <mshinwell@pm.me>
2024-08-12 11:22:50 +0100

    Revert: git checkout 785447feae03ae89c8c198b8ee7c5f556fbc095b -- driver/boot_ocamlopt.ml dune

--- driver/boot_ocamlopt.ml
+++ driver/boot_ocamlopt.ml
@@ -1,4 +1,4 @@
 let () =
-  exit (Optmaindriver.main (Obj.magic 0) (*(module Unix : Compiler_owee.Unix_intf.S) XXX *) Sys.argv
+  exit (Optmaindriver.main (module Unix : Compiler_owee.Unix_intf.S) Sys.argv
     Format.err_formatter
     ~flambda2:Flambda2.lambda_to_cmm)
