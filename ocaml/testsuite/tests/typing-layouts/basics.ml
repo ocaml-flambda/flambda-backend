52b5ab106d8e1b3fd53f6c2c6844009b24ffcfd0
Jakub Bachurski <kbachurski@gmail.com>
2024-08-13 10:06:57 +0100

    Update test outputs post minus-20 rebase

--- ocaml/testsuite/tests/typing-layouts/basics.ml
+++ ocaml/testsuite/tests/typing-layouts/basics.ml
@@ -1068,8 +1068,7 @@ Error: The type constraints are not consistent.
        The layout of t_float64 is float64
          because of the definition of t_float64 at line 4, characters 0-24.
        But the layout of t_float64 must be a sublayout of value
-         because it instantiates an unannotated type parameter of t,
-         defaulted to layout value.
+         because it's the type of an object field.
 |}];;
 
 (*******************************************************************)
--- ocaml/testsuite/tests/typing-layouts/basics_alpha.ml
+++ ocaml/testsuite/tests/typing-layouts/basics_alpha.ml
@@ -884,8 +884,7 @@ Error: The type constraints are not consistent.
        The layout of t_void is void
          because of the definition of t_void at line 6, characters 0-19.
        But the layout of t_void must be a sublayout of value
-         because it instantiates an unannotated type parameter of t,
-         defaulted to layout value.
+         because it's the type of an object field.
 |}];;
 
 (*******************************************************************)
