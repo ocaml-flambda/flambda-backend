8dab4abbcf56da0566902451261b96caffdc4eba
ITO444 <tsz.oi.ip@gmail.com>
2024-08-16 12:20:32 +0100

    added dependencies for individual arguments for each instruction

--- backend/cfg/vectorize.ml
+++ backend/cfg/vectorize.ml
@@ -72,15 +72,29 @@ module Dependency_graph : sig
   val from_cfg: Cfg.t -> t
   val dump:Format.formatter -> t -> Cfg_with_layout.t -> unit
 end = struct
-  module Node = struct
+
+  module Node  = struct
+    module Reg_node = struct
     type t =
-      { id : Instruction.Id.t;
+      { reg : Reg.t;
+        depends_on : Instruction.Id.t option;
+      }
+
+    let init reg : t =
+      { reg;
+        depends_on = None;
+      }
+  end
+    type t =
+      { instruction : Instruction.t;
+      reg_nodes : Reg_node.t array;
         depends_on : Instruction.Id.Set.t;
         is_dependency_of : Instruction.Id.Set.t
       }
 
-    let init id : t =
-      { id;
+    let init instruction : t =
+      let arguments = Instruction.arguments instruction in
+      { instruction; reg_nodes=Array.init (Array.length arguments) (fun i -> arguments.( i) |> Reg_node.init);
         depends_on = Instruction.Id.Set.empty;
         is_dependency_of = Instruction.Id.Set.empty
       }
@@ -88,6 +102,9 @@ end = struct
 
   type t = Node.t Instruction.Id.Tbl.t
 
+  let add = Instruction.Id.Tbl.add
+  let find = Instruction.Id.Tbl.find
+  let replace = Instruction.Id.Tbl.replace
   let init () : t = Instruction.Id.Tbl.create 100
 
   let from_basic_block (block : Cfg.basic_block) ~(dependency_graph : t) =
@@ -107,44 +124,54 @@ end = struct
           else latest)
         ~init:None
     in
-    let add_dependency_for_one_arg instruction arg =
+    let add_arg_dependency instruction arg_i arg =
       let id = Instruction.id instruction in
       let dependency = latest_change ~current:id arg in
-      Option.fold ~none:()
-        ~some:(fun instruction ->
-          let old_node = Instruction.Id.Tbl.find dependency_graph id in
-          Instruction.Id.Tbl.replace dependency_graph id
-            { old_node with
-              depends_on =
-                Instruction.Id.Set.add
-                  (Instruction.id instruction)
-                  old_node.depends_on
-            })
-        dependency
+      let node = find dependency_graph id in
+      let reg_node =  node.reg_nodes. (arg_i) in
+      node.reg_nodes. (arg_i) <-{reg_node with depends_on =
+      Option.fold ~none:(None) ~some:(Option.some << Instruction.id) dependency}
+
     in
-    let find_dependencies (instruction : Instruction.t) =
-      let id = Instruction.id instruction in
-      Instruction.Id.Tbl.add dependency_graph id (Node.init id);
-      Array.iter
-        (add_dependency_for_one_arg instruction)
-        (Instruction.arguments instruction)
+      let add_arg_dependencies (instruction : Instruction.t) =
+        Array.iteri
+          (add_arg_dependency instruction)
+          (Instruction.arguments instruction)
+      in
+
+    let add_dependencies (instruction : Instruction.t) =
+          let id = Instruction.id instruction in
+          add dependency_graph id (Node.init instruction);
+    add_arg_dependencies instruction;
+    let arg_indices = (Instruction.arguments instruction|>Array.mapi (fun arg_i _ -> arg_i) ) in
+    let instruction_dependencies = Array.fold_left (fun dependencies arg_i ->
+
+      Option.fold ~none:(dependencies) ~some:(fun dependency -> Instruction.Id.Set.add dependency dependencies) ((find dependency_graph id).reg_nodes.(arg_i).depends_on)) (Instruction.Id.Set.empty) arg_indices  in
+      let node = find dependency_graph id in
+      replace dependency_graph id  {node with depends_on = instruction_dependencies}
     in
-    let body = block.body in
-    DLL.iter body ~f:(fun instruction -> find_dependencies (Basic instruction));
-    find_dependencies (Terminator block.terminator);
-    let set_is_dependency_of (instruction : Instruction.t) =
-      let id = Instruction.id instruction in
-      let node = Instruction.Id.Tbl.find dependency_graph id in
-      let set_is_dependency_of_one id dependency =
-        let old_node = Instruction.Id.Tbl.find dependency_graph dependency in
-        Instruction.Id.Tbl.replace dependency_graph dependency
-          { old_node with
-            is_dependency_of = Instruction.Id.Set.add id old_node.is_dependency_of
+
+
+    let add_all_dependencies () =
+    DLL.iter block.body ~f:(fun instruction -> add_dependencies (Basic instruction));
+    add_dependencies (Terminator block.terminator) in
+    let set_is_dependency_of instruction_id dependency_id =
+        let dependency = find dependency_graph dependency_id in
+        replace dependency_graph dependency_id
+          { dependency with
+            is_dependency_of = Instruction.Id.Set.add instruction_id dependency.is_dependency_of
           }
       in
-      Instruction.Id.Set.iter (set_is_dependency_of_one id) node.depends_on
+
+    let set_is_dependency_of_plural (instruction : Instruction.t) =
+      let id = Instruction.id instruction in
+      let node = find dependency_graph id in
+
+      Instruction.Id.Set.iter (set_is_dependency_of id) node.depends_on
     in
-    DLL.iter body ~f:(fun instruction -> set_is_dependency_of (Basic instruction))
+    let set_all_is_dependency_of () =
+      DLL.iter block.body ~f:(fun instruction -> set_is_dependency_of_plural (Basic instruction)) in
+      add_all_dependencies (); set_all_is_dependency_of()
 
   let from_cfg (cfg : Cfg.t) : t =
     let dependency_graph = init () in
@@ -153,18 +180,24 @@ end = struct
 
   let dump ppf (t : t) cfg_with_layout =
     let open Format in
-    fprintf ppf "\nDependency graph:\n";
+    let print_reg_node arg_i (reg_node : Node.Reg_node.t) =
+      let dependency = Option.fold ~none:"none" ~some:(sprintf "instruction %d" << Instruction.Id.to_int) reg_node.depends_on in
+      fprintf ppf "\nargument %d (reg %d) depends on %s\n" arg_i reg_node.reg.stamp dependency;
+    in
     let print_node (instruction : Instruction.t) =
       let id = Instruction.id instruction in
-      let node = Instruction.Id.Tbl.find t id in
-      fprintf ppf "\n%d:\n" (Instruction.Id.to_int node.id);
+      let node = find t id in
+      fprintf ppf "\n%d:\n" (Instruction.id node.instruction |> Instruction.Id.to_int);
       Instruction.print ppf instruction;
+      Array.iteri (print_reg_node) node.reg_nodes;
       fprintf ppf "\ndepends on:\n";
       Instruction.Id.Set.iter (  (fprintf ppf "%d ")<<Instruction.Id.to_int) node.depends_on;
       fprintf ppf "\nis a dependency of:\n";
       Instruction.Id.Set.iter (fprintf ppf "%d "<<Instruction.Id.to_int) node.is_dependency_of;
+      fprintf ppf "\narg dependencies:\n";
       fprintf ppf "\n"
     in
+    fprintf ppf "\ndependency graph:\n";
     Cfg_with_layout.iter_instructions cfg_with_layout
       ~instruction:(fun instruction -> print_node (Basic instruction))
       ~terminator:(fun instruction -> print_node (Terminator instruction))
