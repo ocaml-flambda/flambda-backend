e7b0815d5dd8c3916ce2ed22b7be918d4316560f
Chris Casinghino <ccasinghino@janestreet.com>
2024-08-16 14:00:46 -0400

    Revert "Remove "scheduling" leftovers" (#2935)
    Revert "Remove "scheduling" leftovers (#2857)"
    
    This reverts commit ef78547773c230e5216e8f2181403a8e4ce6e65c.

--- ocaml/testsuite/tests/tool-ocamlopt-save-ir/save_ir_after_scheduling.ml
+++ ocaml/testsuite/tests/tool-ocamlopt-save-ir/save_ir_after_scheduling.ml
@@ -1,10 +1,10 @@
 (* TEST
  native-compiler;
  setup-ocamlopt.byte-build-env;
- flags = "-save-ir-after linearization -S";
+ flags = "-save-ir-after scheduling -S";
  ocamlopt.byte;
  check-ocamlopt.byte-output;
- script = "sh ${test_source_directory}/save_ir_after_linearization.sh";
+ script = "sh ${test_source_directory}/save_ir_after_scheduling.sh";
  script;
 *)
 
