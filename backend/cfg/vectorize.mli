456d2ad6ba495cd8841fe65a16e8dd9fe5bc0d0f
ITO444 <tsz.oi.ip@gmail.com>
2024-08-19 12:15:39 +0100

    change description of vector.mli

--- backend/cfg/vectorize.mli
+++ backend/cfg/vectorize.mli
@@ -1,4 +1,4 @@
-(* Finds adjacent memory chunks and tries to use vector operations if
-   possible *)
+(* Finds independent scalar operations within the same basic block and tries to
+   use vector operations if possible *)
 
 val cfg : Format.formatter -> Cfg_with_layout.t -> Cfg_with_layout.t
