a524c8cea57b0e7ead3bac0d8bee5618fc65e3bf
Nick Roberts <nroberts@janestreet.com>
2024-08-20 16:58:29 -0400

    Workaround for tmc issue where local returns can't be compiled

--- ocaml/testsuite/tests/tmc/shortcut.ml
+++ ocaml/testsuite/tests/tmc/shortcut.ml
@@ -1,11 +1,15 @@
 (* TEST flags = "-w -71"; *)
 
-let[@tail_mod_cons] rec f () = Some (g ())
-and[@tail_mod_cons] g () = false && true
+module Test1 = struct
+  let[@tail_mod_cons] rec f () = Some (g ())
+  and[@tail_mod_cons] g () = false && true
 
-let () = assert (f () = Some false)
+  let () = assert (f () = Some false)
+end
 
-let[@tail_mod_cons] rec f () = Some (g ())
-and[@tail_mod_cons] g () = true || false
+module Test2 = struct
+  let[@tail_mod_cons] rec f () = Some (g ())
+  and[@tail_mod_cons] g () = true || false
 
-let () = assert (f () = Some true)
+  let () = assert (f () = Some true)
+end
