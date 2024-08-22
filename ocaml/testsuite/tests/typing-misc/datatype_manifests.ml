b2806d73184675b99456e4d35a7299b595a91357
Jakub Bachurski <kbachurski@gmail.com>
2024-08-22 11:49:18 +0800

    Raise the error in non-Tconstr cases as before (revert line from #2749)

--- ocaml/testsuite/tests/typing-misc/datatype_manifests.ml
+++ ocaml/testsuite/tests/typing-misc/datatype_manifests.ml
@@ -4,10 +4,17 @@
 
 type t = int * int = { foo : string }
 [%%expect{|
-type t = int * int = { foo : string; }
+Line 1, characters 0-37:
+1 | type t = int * int = { foo : string }
+    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+Error: This variant or record definition does not match that of type
+         int * int
 |}];;
 
 let f (x : int * int) = (x : t)
 [%%expect{|
-val f : int * int -> t = <fun>
+Line 1, characters 29-30:
+1 | let f (x : int * int) = (x : t)
+                                 ^
+Error: Unbound type constructor t
 |}];;
