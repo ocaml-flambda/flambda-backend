25ec3c50ab22e9df8a752620f1f14f7684371ac0
Milan Tom <milant076@gmail.com>
2024-08-28 16:30:23 +0100

    Add pattern occurrence test for immutable arrays

--- ocaml/testsuite/tests/profile/counters/language_extensions/immutable_arrays/ml_counters.ml
+++ ocaml/testsuite/tests/profile/counters/language_extensions/immutable_arrays/ml_counters.ml
@@ -1,8 +1,16 @@
 (* Immutable arrays *)
 
+(* 2 type, 1 expression *)
 let a_iarray : int iarray = [: 1; 2; 3; 4; 5 :]
 
+(* 2 type, 1 expression *)
 let b_iarray : float iarray = [: 1.; 2.; 3.; 4.; 5. :]
+
+(* 1 expression, 1 pattern *)
+let and_iarray =
+  match [: true; false :] with
+  | [: iarray_val_1; iarray_val_2 :] -> iarray_val_1 && iarray_val_2
+  | _ -> false
 
 (* CR-someday mitom: Add tests for [Iarray] module from stdlib *)
 
@@ -13,3 +21,8 @@ let c_array : int array = [| 1; 2; 3; 4; 5 |]
 let d_array : float array = [| 1.; 2.; 3.; 4.; 5. |]
 
 let e_array : bool array = [| true; false; true |]
+
+let and_array =
+  match [| true; false |] with
+  | [| array_val_1; array_val_2 |] -> array_val_1 && array_val_2
+  | _ -> false
--- ocaml/testsuite/tests/profile/counters/language_extensions/immutable_arrays/mli_counters.mli
+++ ocaml/testsuite/tests/profile/counters/language_extensions/immutable_arrays/mli_counters.mli
@@ -1,9 +1,12 @@
 (* Immutable arrays *)
 
+(* 1 type *)
 val a_iarray : int iarray
 
+(* 1 type *)
 val b_iarray : float iarray
 
+(* 1 type *)
 val c_iarray : 'a iarray
 
 (* Mutable arrays (should not be counted) *)
