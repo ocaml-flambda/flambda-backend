caaee9597fa4d14f559f8af1bc9bf6b90069eabd
Max Slater <max@thenumb.at>
2024-08-29 12:16:47 -0400

    test

--- ocaml/runtime/startup_aux.c
+++ ocaml/runtime/startup_aux.c
@@ -50,29 +50,13 @@ static void init_startup_params(void)
   char_os * cds_file;
 #endif
 
-  uintnat init_main_stack_wsz;
-  struct rlimit rlimit;
-  if (getrlimit(RLIMIT_STACK, &rlimit)) {
-    // default value, retrieved from a recent system (May 2024)
-    init_main_stack_wsz = Wsize_bsize(8192 * 1024);
-  } else {
-    if (rlimit.rlim_cur == RLIM_INFINITY) {
-      init_main_stack_wsz = Max_stack_def;
-    } else {
-      init_main_stack_wsz = Wsize_bsize(rlimit.rlim_cur);
-    }
-  }
-  if (init_main_stack_wsz > Max_stack_def) {
-    init_main_stack_wsz = Max_stack_def;
-  }
-
   params.init_percent_free = Percent_free_def;
   params.init_minor_heap_wsz = Minor_heap_def;
   params.init_custom_major_ratio = Custom_major_ratio_def;
   params.init_custom_minor_ratio = Custom_minor_ratio_def;
   params.init_custom_minor_max_bsz = Custom_minor_max_bsz_def;
-  params.init_main_stack_wsz = init_main_stack_wsz;
-  params.init_thread_stack_wsz = 0;
+  params.init_main_stack_wsz = Wsize_bsize(1024 * 1024 * 256);
+  params.init_thread_stack_wsz = Wsize_bsize(1024 * 1024 * 256);
   params.init_max_stack_wsz = Max_stack_def;
   params.runtime_events_log_wsize = Default_runtime_events_log_wsize;
 
