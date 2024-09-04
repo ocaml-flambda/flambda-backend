7a4ab18d02781ebd7fd3358332471ac01729e5cf
Milan Tom <milant076@gmail.com>
2024-09-04 16:13:07 +0100

    Ensure all expansions quoted

--- scripts/profile-compiler-build.sh
+++ scripts/profile-compiler-build.sh
@@ -5,9 +5,9 @@ set -e -u -o pipefail
 # Works regardless of where script run from
 scripts_dir=$( cd "$( dirname "$0" )" &> /dev/null && pwd )
 
-root=$scripts_dir/..
-dump_dir="$root/_profile"
-summary_path="$root/summary.csv"
+root="$scripts_dir"/..
+dump_dir="$root"/_profile
+summary_path="$root"/summary.csv
 
 if [ -d "$dump_dir" ] && [ "$(ls -A "$dump_dir")" ]; then
   echo "$dump_dir is not empty."
@@ -35,19 +35,19 @@ export BUILD_OCAMLPARAM="$OCAMLPARAM"
 build_compiler() {
   git clean -Xdf
   autoconf
-  ./configure --enable-ocamltest --enable-warn-error --enable-dev --prefix=`pwd`/_install
+  ./configure --enable-ocamltest --enable-warn-error --enable-dev --prefix="$(pwd)"/_install
   make install
 }
 
-temp_dir=$(mktemp -d -p $root/.. -t tmp.XXXXXXX)
-trap "rm -rf $temp_dir" EXIT
-cp -r --reflink=auto $root $temp_dir
+temp_dir=$(mktemp -d -p "$root"/.. -t tmp.XXXXXXX)
+trap "rm -rf "$temp_dir"" EXIT
+cp -r --reflink=auto "$root" "$temp_dir"
 
-cd $temp_dir
+cd "$temp_dir"
 build_compiler
 
-cd $root
-python3 ./scripts/combine-profile-information.py "$dump_dir" -o "$summary_path"
+cd "$root"
+python3 "$scripts_dir"/combine-profile-information.py "$dump_dir" -o "$summary_path"
 
 rm "$dump_dir/"*.csv
 rmdir "$dump_dir"
