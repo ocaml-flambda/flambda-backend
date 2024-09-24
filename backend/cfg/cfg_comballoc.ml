78a195d884e486ea47d9e55a83d4417821798af4
Anton Lorenzen <anfelor@posteo.de>
2024-09-24 16:41:17 -0400

    Removal of Lambda.alloc_mode: Fix formatting

--- backend/cfg/cfg_comballoc.ml
+++ backend/cfg/cfg_comballoc.ml
@@ -89,8 +89,7 @@ let find_compatible_allocations :
       | Op (Begin_region | End_region) -> (
         match curr_mode with
         | Local -> return ()
-        | Heap ->
-          loop allocations (DLL.next cell) ~curr_mode ~curr_size)
+        | Heap -> loop allocations (DLL.next cell) ~curr_mode ~curr_size)
       | Op Poll -> return ()
       | Reloadretaddr | Poptrap | Prologue | Pushtrap _ | Stack_check _ ->
         (* CR-soon xclerc for xclerc: is it too conservative? (note: only the
