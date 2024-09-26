c690f45b83aba82d6550035893db3a4a24039823
Richard Eisenberg <reisenberg@janestreet.com>
2024-09-26 16:06:24 -0400

    Check labeled tuples extension

--- ocaml/testsuite/tests/typing-labeled-tuples/no_extension.ml
+++ ocaml/testsuite/tests/typing-labeled-tuples/no_extension.ml
@@ -6,53 +6,80 @@
 type t = x:int * y:string
 
 [%%expect{|
-type t = x:int * y:string
+Line 1, characters 9-25:
+1 | type t = x:int * y:string
+             ^^^^^^^^^^^^^^^^
+Error: The extension "labeled_tuples" is disabled and cannot be used
 |}]
 
 let f x y = ~x, ~y
 
 [%%expect{|
-val f : 'a -> 'b -> x:'a * y:'b = <fun>
+Line 1, characters 12-18:
+1 | let f x y = ~x, ~y
+                ^^^^^^
+Error: The extension "labeled_tuples" is disabled and cannot be used
 |}]
 
 let x = ~x:5, ~y:"hi"
 
 [%%expect{|
-val x : x:int * y:string = (~x:5, ~y:"hi")
+Line 1, characters 8-21:
+1 | let x = ~x:5, ~y:"hi"
+            ^^^^^^^^^^^^^
+Error: The extension "labeled_tuples" is disabled and cannot be used
 |}]
 
 let f x y = ~(x : int), ~(y : string)
 
 [%%expect{|
-val f : int -> string -> x:int * y:string = <fun>
+Line 1, characters 12-37:
+1 | let f x y = ~(x : int), ~(y : string)
+                ^^^^^^^^^^^^^^^^^^^^^^^^^
+Error: The extension "labeled_tuples" is disabled and cannot be used
 |}]
 
 let f (~x, ~y) = x, y
 
 [%%expect{|
-val f : (x:'a * y:'b) -> 'a * 'b = <fun>
+Line 1, characters 6-14:
+1 | let f (~x, ~y) = x, y
+          ^^^^^^^^
+Error: The extension "labeled_tuples" is disabled and cannot be used
 |}]
 
 let f (~x:_, ~y:_) = ()
 
 [%%expect{|
-val f : (x:'a * y:'b) -> unit = <fun>
+Line 1, characters 6-18:
+1 | let f (~x:_, ~y:_) = ()
+          ^^^^^^^^^^^^
+Error: The extension "labeled_tuples" is disabled and cannot be used
 |}]
 
 let f (~(x:int), ~(y:string)) = x, y
 
 [%%expect{|
-val f : (x:int * y:string) -> int * string = <fun>
+Line 1, characters 6-29:
+1 | let f (~(x:int), ~(y:string)) = x, y
+          ^^^^^^^^^^^^^^^^^^^^^^^
+Error: The extension "labeled_tuples" is disabled and cannot be used
 |}]
 
 let f ((x, ..) : (int * string * float)) = x
 
 [%%expect{|
-val f : int * string * float -> int = <fun>
+Line 1, characters 7-14:
+1 | let f ((x, ..) : (int * string * float)) = x
+           ^^^^^^^
+Error: The extension "labeled_tuples" is disabled and cannot be used
 |}]
 
 let f ((x, y, ..) : (int * string * float)) = x, y
 
 [%%expect{|
-val f : int * string * float -> int * string = <fun>
+Line 1, characters 7-17:
+1 | let f ((x, y, ..) : (int * string * float)) = x, y
+           ^^^^^^^^^^
+Error: The extension "labeled_tuples" is disabled and cannot be used
 |}]
