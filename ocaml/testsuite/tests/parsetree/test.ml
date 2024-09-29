a39797366fd0a2e52347f28f0ce6d0f55d27be41
Nick Roberts <nroberts@janestreet.com>
2024-09-29 16:28:04 -0400

    to return to

--- ocaml/testsuite/tests/parsetree/test.ml
+++ ocaml/testsuite/tests/parsetree/test.ml
@@ -52,17 +52,23 @@ let to_tmp_file print_fun ast =
   close_out oc;
   fn
 
-let test parse_fun pprint print map filename =
+let format_lexing_position ppf lexing_pos =
+  let (filename, line, col) = Location.get_pos_info lexing_pos in
+  Printf.fprintf ppf "%s %d:%d" filename line col
+
+let test ~here parse_fun pprint print map filename =
   match from_file parse_fun filename with
   | exception exn ->
-      Printf.printf "%s: FAIL, CANNOT PARSE\n" filename;
+      Printf.printf "%s: FAIL, CANNOT PARSE (%a)\n" filename
+        format_lexing_position here;
       report_err exn;
       print_endline "====================================================="
   | ast ->
       let str = to_string pprint ast in
       match from_string parse_fun str with
       | exception exn ->
-          Printf.printf "%s: FAIL, CANNOT REPARSE\n" filename;
+          Printf.printf "%s: FAIL, CANNOT REPARSE (%a)\n" filename
+            format_lexing_position here;
           report_err exn;
           print_endline str;
           print_endline "====================================================="
@@ -70,7 +76,9 @@ let test parse_fun pprint print map filename =
           let ast = map remove_locs remove_locs ast in
           let ast2 = map remove_locs remove_locs ast2 in
           if ast <> ast2 then begin
-            Printf.printf "%s:  FAIL, REPARSED AST IS DIFFERENT\n%!" filename;
+            Printf.printf "%s:  FAIL, REPARSED AST IS DIFFERENT (%a)\n%!"
+              filename
+              format_lexing_position here;
             let f1 = to_tmp_file print ast in
             let f2 = to_tmp_file print ast2 in
             let cmd = Printf.sprintf "%s %s %s" diff
@@ -79,16 +87,17 @@ let test parse_fun pprint print map filename =
             print_endline"====================================================="
           end
 
-let test parse_fun pprint print map filename =
-  try test parse_fun pprint print map filename
+let test ~here parse_fun pprint print map filename =
+  try test ~here parse_fun pprint print map filename
   with exn -> report_err exn
 
-let rec process path =
+let rec process ~(here : [%call_pos]) path =
   if Sys.is_directory path then
     let files = Sys.readdir path in
-    Array.iter (fun s -> process (Filename.concat path s)) files
+    Array.iter (fun s -> process ~here (Filename.concat path s)) files
   else if Filename.check_suffix path ".ml" then
     test
+      ~here
       Parse.implementation
       Pprintast.structure
       Printast.implementation
@@ -96,6 +105,7 @@ let rec process path =
       path
   else if Filename.check_suffix path ".mli" then
     test
+      ~here
       Parse.interface
       Pprintast.signature
       Printast.interface
@@ -107,20 +117,28 @@ let () =
   Language_extension.set_universe_and_enable_all
     Language_extension.Universe.maximal;
   process "source_jane_street.ml";
-  (* Additionally check that merely parsing doesn't attempt
+  (* Additionally check that merely parsing and printing doesn't attempt
      to check extensions enabledness. This is actually an important property.
-     That's because ppxes run the parser code in a separate process from the
-     compiler, and ppxes always enable extensions to the max. So checks
-     in the parser are ineffective in a common mode of running the
-     compiler.
+
+     For parsing:
+       ppxes run the parser code in a separate process from the
+       compiler, and ppxes always enable extensions to the max. So checks
+       in the parser are ineffective in a common mode of running the
+       compiler.
+
+     For printing:
+       It's a shame if disabling an extension makes it the toolchain
+       can't print the source code that includes the extension. I can't
+       remember a detailed reason why, but we thought this check was
+       important at some point.
   *)
   Language_extension.disable_all ();
-  match from_file Parse.implementation "source_jane_street.ml" with
-  | (_ : _ list) -> ()
+  match process "source_jane_street.ml" with
+  | () -> ()
   | exception _ ->
       print_endline
-        "Failed to parse with all extensions disabled after successfully\
-        \ parsing with all extensions enabled. Does the parser check for\
+        "Failed to parse or print with all extensions disabled after successfully\
+        \ parsing/print with all extensions enabled. Does the parser (or Pprintast) check for\
         \ extension enabledness, which is almost certainly a bug? (See the\
         \ comment by this test.)"
 ;;
