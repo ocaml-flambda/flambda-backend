ce7b5f5517a135558454298153dd8654d344ac33
Richard Eisenberg <reisenberg@janestreet.com>
2024-10-07 16:44:10 -0400

    I can't live without my monads

--- utils/misc.mli
+++ utils/misc.mli
@@ -299,16 +299,58 @@ module Stdlib : sig
   external compare : 'a -> 'a -> int = "%compare"
 
   module Monad : sig
+    module type Basic = sig
+      type 'a t
+
+      val bind : 'a t -> ('a -> 'b t) -> 'b t
+      val return : 'a -> 'a t
+
+      (** The following identities ought to hold (for some value of =):
+
+          - [return x >>= f = f x]
+          - [t >>= fun x -> return x = t]
+          - [(t >>= f) >>= g = t >>= fun x -> (f x >>= g)]
+
+          Note: [>>=] is the infix notation for [bind]) *)
+    end
+
     module type Basic2 = sig
-      (** Multi parameter monad. The second parameter gets unified across all the computation.
-          This is used to encode monads working on a multi parameter data structure like
-          ([('a,'b) result]). *)
+      (** Multi parameter monad. The second parameter gets unified across all
+          the computation.  This is used to encode monads working on a multi
+          parameter data structure like ([('a,'b) result]). *)
 
       type ('a, 'e) t
 
       val bind : ('a, 'e) t -> ('a -> ('b, 'e) t) -> ('b, 'e) t
 
       val return : 'a -> ('a, _) t
+    end
+
+    module type S = sig
+      type 'a t
+
+      val bind : 'a t -> ('a -> 'b t) -> 'b t
+
+      (** [>>=] is a synonym for [bind] *)
+      val (>>=) : 'a t -> ('a -> 'b t) -> 'b t
+
+      (** [return v] returns the (trivial) computation that returns v. *)
+      val return : 'a -> 'a t
+
+      val map : ('a -> 'b) -> 'a t -> 'b t
+
+      (** [join t] is [t >>= (fun t' -> t')]. *)
+      val join : 'a t t -> 'a t
+
+      (** [ignore_m t] is [map (fun _ -> ()) t]. *)
+      val ignore_m : 'a t -> unit t
+
+      val all : 'a t list -> 'a list t
+
+      (** Like [all], but ensures that every monadic value in the list produces
+          a unit value, all of which are discarded rather than being collected
+          into a list. *)
+      val all_unit : unit t list -> unit t
     end
 
     module type S2 = sig
@@ -323,8 +365,10 @@ module Stdlib : sig
       val all_unit : (unit, 'e) t list -> (unit, 'e) t
     end
 
+    module Make (X : Basic) : S with type 'a t = 'a X.t
     module Make2 (X : Basic2) : S2 with type ('a, 'e) t = ('a, 'e) X.t
 
+    module Option : S with type 'a t = 'a option
     module Result : S2 with type ('a, 'e) t = ('a, 'e) result
   end
 end
