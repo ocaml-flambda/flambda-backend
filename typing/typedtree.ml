d0fbd68c7f9c3c62a6dee6b4356e1b0084d389ac
Liam Stevenson <liam.m.stevenson@gmail.com>
2024-10-08 13:03:26 +0100

    flambda-backend: Add fields to newtype (#3091)
    * Add fields to newtype
    
    * Add comments explaining new field
    
    * Fix build error in chamelon
    
    * Format files

--- typing/typedtree.ml
+++ typing/typedtree.ml
@@ -133,7 +133,7 @@ and exp_extra =
   | Texp_constraint of core_type option * Mode.Alloc.Const.Option.t
   | Texp_coerce of core_type option * core_type
   | Texp_poly of core_type option
-  | Texp_newtype of string * Jkind.annotation option
+  | Texp_newtype of Ident.t * string loc * Jkind.annotation option * Uid.t
   | Texp_stack
 
 and arg_label = Types.arg_label =
@@ -281,7 +281,7 @@ and function_param =
     fp_sort: Jkind.sort;
     fp_mode: Mode.Alloc.l;
     fp_curry: function_curry;
-    fp_newtypes: (string loc * Jkind.annotation option) list;
+    fp_newtypes: (Ident.t * string loc * Jkind.annotation option * Uid.t) list;
     fp_loc: Location.t;
   }
 
