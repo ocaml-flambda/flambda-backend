486dc41e5deac5c62b5f1b47a6364d1d048b428e
Greta Yorsh <gyorsh@janestreet.com>
2024-10-10 10:05:33 +0100

    Fix test (disable deprecated alert)

--- ocaml/testsuite/tests/runtime-objects/Tests.ml
+++ ocaml/testsuite/tests/runtime-objects/Tests.ml
@@ -23,14 +23,14 @@ let o = object end in
   assert ((id o, id o', id o'') = (3, 4, 5));
 
 let o = object val x = 33 method m = x end in
-  let s = Marshal.to_string o [Marshal.Closures] in
+  let s = Marshal.to_string o [Marshal.Closures[@alert "-deprecated"]] in
   let o' : <m:int> = Marshal.from_string s 0 in
   let o'' : <m:int> = Marshal.from_string s 0 in
   assert ((id o, id o', id o'', o#m, o'#m)
           = (6, 7, 8, 33, 33));;
 
 let o = object val x = 33 val y = 44 method m = x end in
-  let s = Marshal.to_string (o,o) [Marshal.Closures] in
+  let s = Marshal.to_string (o,o) [Marshal.Closures[@alert "-deprecated"]] in
   let (o1, o2) : (<m:int> * <m:int>) = Marshal.from_string s 0 in
   let (o3, o4) : (<m:int> * <m:int>) = Marshal.from_string s 0 in
   assert ((id o, id o1, id o2, id o3, id o4, o#m, o1#m)
