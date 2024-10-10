9a1e10b5f53a43ce3da098c9cff4c4ae550ac738
Xavier Clerc <xclerc@janestreet.com>
2024-10-10 09:58:41 +0100

    Fixes

--- backend/cfg/cfgize.mli
+++ backend/cfg/cfgize.mli
@@ -1,6 +1,10 @@
 val terminator_of_test :
   Mach.test -> label_false:Label.t -> label_true:Label.t -> Cfg.terminator
 
+module Stack_offset_and_exn : sig
+  val update_cfg : Cfg.t -> unit
+end
+
 val fundecl :
   Mach.fundecl ->
   before_register_allocation:bool ->
