024c118fe70d0f847f82c47379802a747d93eeea
Mark Shinwell <mshinwell@pm.me>
2024-10-17 18:31:10 +0100

    Pmakearray_dynamic

--- middle_end/flambda2/from_lambda/lambda_to_lambda_transforms.mli
+++ middle_end/flambda2/from_lambda/lambda_to_lambda_transforms.mli
@@ -44,6 +44,7 @@ val switch_for_if_then_else :
 val transform_primitive :
   Lambda_to_flambda_env.t ->
   Lambda.primitive ->
+  (* CR mshinwell: consider [Ident.t list] instead for the arguments. *)
   Lambda.lambda list ->
   Lambda.scoped_location ->
-  primitive_transform_result
+  Lambda_to_flambda_env.t * primitive_transform_result
