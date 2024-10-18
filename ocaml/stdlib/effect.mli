3754fe65c7e1bdb6a3fa99a0431b7543af802ae8
TheNumbat <max@thenumb.at>
2024-10-18 15:38:53 -0400

    effect handler portable

--- ocaml/stdlib/effect.mli
+++ ocaml/stdlib/effect.mli
@@ -84,7 +84,7 @@ module Deep : sig
       exnc: exn -> 'b;
       effc: 'c.'c t @ contended -> (('c,'b) continuation @ portable -> 'b) option }
 
-  val match_with_portable: ('c @ contended -> 'a) @ portable -> 'c @ contended -> ('a,'b) handler_portable -> 'b @@ portable
+  val match_with_portable: ('c @ contended -> 'a) @ portable -> 'c @ contended -> ('a,'b) handler_portable @ portable -> 'b @@ portable
 
   type 'a effect_handler =
     { effc: 'b. 'b t -> (('b, 'a) continuation -> 'a) option }
@@ -98,7 +98,7 @@ module Deep : sig
   type 'a effect_handler_portable =
     { effc: 'b. 'b t @ contended -> (('b, 'a) continuation @ portable -> 'a) option }
 
-  val try_with_portable: ('c @ contended -> 'a) @ portable -> 'c @ contended -> 'a effect_handler_portable -> 'a @@ portable
+  val try_with_portable: ('c @ contended -> 'a) @ portable -> 'c @ contended -> 'a effect_handler_portable @ portable -> 'a @@ portable
 
   external get_callstack :
     ('a,'b) continuation -> int -> Printexc.raw_backtrace @@ portable =
