2c83120ffeaaf195813bc6297e2365b46964453f
Xavier Clerc <xclerc@janestreet.com>
2024-10-22 14:58:00 +0100

    DLL for ranges and intervals (linscan).

--- backend/regalloc/regalloc_ls_utils.mli
+++ backend/regalloc/regalloc_ls_utils.mli
@@ -1,6 +1,7 @@
 [@@@ocaml.warning "+a-4-30-40-41-42"]
 
 open Regalloc_utils
+module DLL = Flambda_backend_utils.Doubly_linked_list
 
 val ls_debug : bool
 
@@ -42,11 +43,11 @@ module Range : sig
 
   val print : Format.formatter -> t -> unit
 
-  val overlap : t list -> t list -> bool
+  val overlap : t DLL.t -> t DLL.t -> bool
 
-  val is_live : t list -> pos:int -> bool
+  val is_live : t DLL.t -> pos:int -> bool
 
-  val remove_expired : t list -> pos:int -> t list
+  val remove_expired : t DLL.t -> pos:int -> unit
 end
 
 module Interval : sig
@@ -55,7 +56,7 @@ module Interval : sig
     { reg : Reg.t;
       mutable begin_ : int;
       mutable end_ : int;
-      mutable ranges : Range.t list
+      ranges : Range.t DLL.t
     }
 
   val copy : t -> t
