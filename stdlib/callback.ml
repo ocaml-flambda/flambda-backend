c7b46d1643e631496c241928a5b0bcb5fea713ec
Thomas Del Vecchio <tdelvecchio@janestreet.com>
2024-10-31 15:20:16 -0400

    Callback
    Signed-off-by: Thomas Del Vecchio <tdelvecchio@janestreet.com>

--- stdlib/callback.ml
+++ stdlib/callback.ml
@@ -19,13 +19,17 @@ open! Stdlib
 
 (* Registering OCaml values with the C runtime for later callbacks *)
 
-external register_named_value : string -> Obj.t -> unit
-                              = "caml_register_named_value"
+external register_named_value : string -> Obj.t -> unit @@ portable
+  = "caml_register_named_value"
 
 let register name v =
   register_named_value name (Obj.repr v)
+
+let register_safe name v = register name v
 
 let register_exception name (exn : exn) =
   let exn = Obj.repr exn in
   let slot = if Obj.tag exn = Obj.object_tag then exn else Obj.field exn 0 in
   register_named_value name slot
+
+let register_exception_safe name exn = register_exception name exn
