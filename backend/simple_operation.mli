22dfa977bc21c47de279d10d2a87135724cc9db1
Xavier Clerc <xclerc@users.noreply.github.com>
2024-11-07 11:23:21 +0000

    Format the `Simple_operation` module (#3235)
    Format the `Simple_operation` module.

--- backend/simple_operation.mli
+++ backend/simple_operation.mli
@@ -16,15 +16,14 @@
 (* Operations common to Mach and CFG. *)
 
 type trap_stack =
-  | Uncaught
-  (** Exceptions escape the current function *)
+  | Uncaught  (** Exceptions escape the current function *)
   | Specific_trap of Cmm.trywith_shared_label * trap_stack
-  (** Current handler is a delayed/shared Trywith *)
+      (** Current handler is a delayed/shared Trywith *)
 
 val equal_trap_stack : trap_stack -> trap_stack -> bool
 
 type integer_comparison =
-    Isigned of Cmm.integer_comparison
+  | Isigned of Cmm.integer_comparison
   | Iunsigned of Cmm.integer_comparison
 
 val equal_integer_comparison : integer_comparison -> integer_comparison -> bool
@@ -32,10 +31,20 @@ val equal_integer_comparison : integer_comparison -> integer_comparison -> bool
 val invert_integer_comparison : integer_comparison -> integer_comparison
 
 type integer_operation =
-    Iadd | Isub | Imul | Imulh of { signed: bool } | Idiv | Imod
-  | Iand | Ior | Ixor | Ilsl | Ilsr | Iasr
-  | Iclz of { arg_is_non_zero: bool; }
-  | Ictz of { arg_is_non_zero: bool; }
+  | Iadd
+  | Isub
+  | Imul
+  | Imulh of { signed : bool }
+  | Idiv
+  | Imod
+  | Iand
+  | Ior
+  | Ixor
+  | Ilsl
+  | Ilsr
+  | Iasr
+  | Iclz of { arg_is_non_zero : bool }
+  | Ictz of { arg_is_non_zero : bool }
   | Ipopcnt
   | Icomp of integer_comparison
 
@@ -50,18 +59,26 @@ type float_width = Cmm.float_width
 val equal_float_width : float_width -> float_width -> bool
 
 type float_operation =
-  | Inegf | Iabsf | Iaddf | Isubf | Imulf | Idivf
+  | Inegf
+  | Iabsf
+  | Iaddf
+  | Isubf
+  | Imulf
+  | Idivf
   | Icompf of float_comparison
 
 val equal_float_operation : float_operation -> float_operation -> bool
 
-type mutable_flag = Immutable | Mutable
+type mutable_flag =
+  | Immutable
+  | Mutable
 
 val of_ast_mutable_flag : Asttypes.mutable_flag -> mutable_flag
+
 val to_ast_mutable_flag : mutable_flag -> Asttypes.mutable_flag
 
 type test =
-    Itruetest
+  | Itruetest
   | Ifalsetest
   | Iinttest of integer_comparison
   | Iinttest_imm of integer_comparison * int
@@ -69,4 +86,4 @@ type test =
   | Ioddtest
   | Ieventest
 
-val invert_test : test->test
+val invert_test : test -> test
