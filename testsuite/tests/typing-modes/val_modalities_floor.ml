0248c1373bfe50a203ae2b31121adbf0bea75727
Zesen Qian <github@riaqn.org>
2024-11-15 15:41:45 -0500

    modalities zap to id for mli-less files

--- testsuite/tests/typing-modes/val_modalities_floor.ml
+++ testsuite/tests/typing-modes/val_modalities_floor.ml
@@ -1,12 +1,8 @@
-(* We test that cmi generated without mli has the modalities pushed to the
-strongest instead of legacy *)
-
 (* TEST
  readonly_files = "\
    def_portable.ml \
-   def_nonportable.ml \
-   use_portable.ml use_portable.bad.reference \
- ";
+   use_portable.ml use_portable.reference \
+  ";
 {
    setup-ocamlopt.byte-build-env;
    flags = "-extension mode";
@@ -21,27 +17,15 @@ strongest instead of legacy *)
     ocamlopt.byte;
 
     module = "use_portable.ml";
-
-    ocamlopt_byte_exit_status = "0";
-    ocamlopt.byte;
-   }
-
-   {
-    src = "def_nonportable.ml";
-    dst = "maybe_portable.ml";
-    copy;
-
-    module = "maybe_portable.ml";
-    ocamlopt_byte_exit_status = "0";
-    ocamlopt.byte;
-
-    module = "use_portable.ml";
-    compiler_output = "use_portable.bad.output";
+    compiler_output = "use_portable.output";
     ocamlopt_byte_exit_status = "2";
     ocamlopt.byte;
 
-    compiler_reference = "use_portable.bad.reference";
+    compiler_reference = "use_portable.reference";
     check-ocamlopt.byte-output;
     }
 }
 *)
+
+(* We test that cmi generated without mli has the modalities pushed to the
+legacy instead of strongest *)
