11e57fa6fd0be62d51b0a5c4f94b88cf5cf39144
Jacob Van Buren <jvanburen@janestreet.com>
2024-12-13 16:38:52 -0500

    inlined library functions

--- otherlibs/stdlib_beta/int16_u.ml
+++ otherlibs/stdlib_beta/int16_u.ml
@@ -26,62 +26,62 @@ external of_int16 : int16 -> int16# = "%untag_int16" [@@warning "-187"]
 
 let int_size = Int16.int_size
 
-let zero () = of_int16 Int16.zero
+let[@inline always] zero () = of_int16 Int16.zero
 
-let one () = of_int16 Int16.one
+let[@inline always] one () = of_int16 Int16.one
 
-let minus_one () = of_int16 Int16.minus_one
+let[@inline always] minus_one () = of_int16 Int16.minus_one
 
-let max_int () = of_int16 Int16.max_int
+let[@inline always] max_int () = of_int16 Int16.max_int
 
-let min_int () = of_int16 Int16.min_int
+let[@inline always] min_int () = of_int16 Int16.min_int
 
-let neg x = of_int16 (Int16.neg (to_int16 x))
+let[@inline always] neg x = of_int16 (Int16.neg (to_int16 x))
 
-let add x y = of_int16 (Int16.add (to_int16 x) (to_int16 y))
+let[@inline always] add x y = of_int16 (Int16.add (to_int16 x) (to_int16 y))
 
-let sub x y = of_int16 (Int16.sub (to_int16 x) (to_int16 y))
+let[@inline always] sub x y = of_int16 (Int16.sub (to_int16 x) (to_int16 y))
 
-let mul x y = of_int16 (Int16.mul (to_int16 x) (to_int16 y))
+let[@inline always] mul x y = of_int16 (Int16.mul (to_int16 x) (to_int16 y))
 
-let div x y = of_int16 (Int16.div (to_int16 x) (to_int16 y))
+let[@inline always] div x y = of_int16 (Int16.div (to_int16 x) (to_int16 y))
 
-let rem x y = of_int16 (Int16.rem (to_int16 x) (to_int16 y))
+let[@inline always] rem x y = of_int16 (Int16.rem (to_int16 x) (to_int16 y))
 
-let succ x = of_int16 (Int16.succ (to_int16 x))
+let[@inline always] succ x = of_int16 (Int16.succ (to_int16 x))
 
-let pred x = of_int16 (Int16.pred (to_int16 x))
+let[@inline always] pred x = of_int16 (Int16.pred (to_int16 x))
 
-let abs x = of_int16 (Int16.abs (to_int16 x))
+let[@inline always] abs x = of_int16 (Int16.abs (to_int16 x))
 
-let logand x y = of_int16 (Int16.logand (to_int16 x) (to_int16 y))
+let[@inline always] logand x y = of_int16 (Int16.logand (to_int16 x) (to_int16 y))
 
-let logor x y = of_int16 (Int16.logor (to_int16 x) (to_int16 y))
+let[@inline always] logor x y = of_int16 (Int16.logor (to_int16 x) (to_int16 y))
 
-let logxor x y = of_int16 (Int16.logxor (to_int16 x) (to_int16 y))
+let[@inline always] logxor x y = of_int16 (Int16.logxor (to_int16 x) (to_int16 y))
 
-let lognot x = of_int16 (Int16.lognot (to_int16 x))
+let[@inline always] lognot x = of_int16 (Int16.lognot (to_int16 x))
 
-let shift_left x y = of_int16 (Int16.shift_left (to_int16 x) y)
+let[@inline always] shift_left x y = of_int16 (Int16.shift_left (to_int16 x) y)
 
-let shift_right x y = of_int16 (Int16.shift_right (to_int16 x) y)
+let[@inline always] shift_right x y = of_int16 (Int16.shift_right (to_int16 x) y)
 
-let shift_right_logical x y = of_int16 (Int16.shift_right_logical (to_int16 x) y)
+let[@inline always] shift_right_logical x y = of_int16 (Int16.shift_right_logical (to_int16 x) y)
 
-let equal x y = Int16.equal (to_int16 x) (to_int16 y)
+let[@inline always] equal x y = Int16.equal (to_int16 x) (to_int16 y)
 
-let compare x y = Int16.compare (to_int16 x) (to_int16 y)
+let[@inline always] compare x y = Int16.compare (to_int16 x) (to_int16 y)
 
-let min x y = of_int16 (Int16.min (to_int16 x) (to_int16 y))
+let[@inline always] min x y = of_int16 (Int16.min (to_int16 x) (to_int16 y))
 
-let max x y = of_int16 (Int16.max (to_int16 x) (to_int16 y))
+let[@inline always] max x y = of_int16 (Int16.max (to_int16 x) (to_int16 y))
 
-let of_float f = of_int16 (Int16.of_float f)
+let[@inline always] of_float f = of_int16 (Int16.of_float f)
 
-let to_float t = Int16.to_float (to_int16 t)
+let[@inline always] to_float t = Int16.to_float (to_int16 t)
 
-let to_string t = Int16.to_string (to_int16 t)
+let[@inline always] to_string t = Int16.to_string (to_int16 t)
 
-let to_int t = Int16.to_int (to_int16 t)
+let[@inline always] to_int t = Int16.to_int (to_int16 t)
 
-let of_int i = of_int16 (Int16.of_int i)
+let[@inline always] of_int i = of_int16 (Int16.of_int i)
--- otherlibs/stdlib_beta/int8_u.ml
+++ otherlibs/stdlib_beta/int8_u.ml
@@ -26,62 +26,62 @@ external of_int8 : int8 -> int8# = "%untag_int8" [@@warning "-187"]
 
 let int_size = Int8.int_size
 
-let zero () = of_int8 Int8.zero
+let[@inline always] zero () = of_int8 Int8.zero
 
-let one () = of_int8 Int8.one
+let[@inline always] one () = of_int8 Int8.one
 
-let minus_one () = of_int8 Int8.minus_one
+let[@inline always] minus_one () = of_int8 Int8.minus_one
 
-let max_int () = of_int8 Int8.max_int
+let[@inline always] max_int () = of_int8 Int8.max_int
 
-let min_int () = of_int8 Int8.min_int
+let[@inline always] min_int () = of_int8 Int8.min_int
 
-let neg x = of_int8 (Int8.neg (to_int8 x))
+let[@inline always] neg x = of_int8 (Int8.neg (to_int8 x))
 
-let add x y = of_int8 (Int8.add (to_int8 x) (to_int8 y))
+let[@inline always] add x y = of_int8 (Int8.add (to_int8 x) (to_int8 y))
 
-let sub x y = of_int8 (Int8.sub (to_int8 x) (to_int8 y))
+let[@inline always] sub x y = of_int8 (Int8.sub (to_int8 x) (to_int8 y))
 
-let mul x y = of_int8 (Int8.mul (to_int8 x) (to_int8 y))
+let[@inline always] mul x y = of_int8 (Int8.mul (to_int8 x) (to_int8 y))
 
-let div x y = of_int8 (Int8.div (to_int8 x) (to_int8 y))
+let[@inline always] div x y = of_int8 (Int8.div (to_int8 x) (to_int8 y))
 
-let rem x y = of_int8 (Int8.rem (to_int8 x) (to_int8 y))
+let[@inline always] rem x y = of_int8 (Int8.rem (to_int8 x) (to_int8 y))
 
-let succ x = of_int8 (Int8.succ (to_int8 x))
+let[@inline always] succ x = of_int8 (Int8.succ (to_int8 x))
 
-let pred x = of_int8 (Int8.pred (to_int8 x))
+let[@inline always] pred x = of_int8 (Int8.pred (to_int8 x))
 
-let abs x = of_int8 (Int8.abs (to_int8 x))
+let[@inline always] abs x = of_int8 (Int8.abs (to_int8 x))
 
-let logand x y = of_int8 (Int8.logand (to_int8 x) (to_int8 y))
+let[@inline always] logand x y = of_int8 (Int8.logand (to_int8 x) (to_int8 y))
 
-let logor x y = of_int8 (Int8.logor (to_int8 x) (to_int8 y))
+let[@inline always] logor x y = of_int8 (Int8.logor (to_int8 x) (to_int8 y))
 
-let logxor x y = of_int8 (Int8.logxor (to_int8 x) (to_int8 y))
+let[@inline always] logxor x y = of_int8 (Int8.logxor (to_int8 x) (to_int8 y))
 
-let lognot x = of_int8 (Int8.lognot (to_int8 x))
+let[@inline always] lognot x = of_int8 (Int8.lognot (to_int8 x))
 
-let shift_left x y = of_int8 (Int8.shift_left (to_int8 x) y)
+let[@inline always] shift_left x y = of_int8 (Int8.shift_left (to_int8 x) y)
 
-let shift_right x y = of_int8 (Int8.shift_right (to_int8 x) y)
+let[@inline always] shift_right x y = of_int8 (Int8.shift_right (to_int8 x) y)
 
-let shift_right_logical x y = of_int8 (Int8.shift_right_logical (to_int8 x) y)
+let[@inline always] shift_right_logical x y = of_int8 (Int8.shift_right_logical (to_int8 x) y)
 
-let equal x y = Int8.equal (to_int8 x) (to_int8 y)
+let[@inline always] equal x y = Int8.equal (to_int8 x) (to_int8 y)
 
-let compare x y = Int8.compare (to_int8 x) (to_int8 y)
+let[@inline always] compare x y = Int8.compare (to_int8 x) (to_int8 y)
 
-let min x y = of_int8 (Int8.min (to_int8 x) (to_int8 y))
+let[@inline always] min x y = of_int8 (Int8.min (to_int8 x) (to_int8 y))
 
-let max x y = of_int8 (Int8.max (to_int8 x) (to_int8 y))
+let[@inline always] max x y = of_int8 (Int8.max (to_int8 x) (to_int8 y))
 
-let of_float f = of_int8 (Int8.of_float f)
+let[@inline always] of_float f = of_int8 (Int8.of_float f)
 
-let to_float t = Int8.to_float (to_int8 t)
+let[@inline always] to_float t = Int8.to_float (to_int8 t)
 
-let to_string t = Int8.to_string (to_int8 t)
+let[@inline always] to_string t = Int8.to_string (to_int8 t)
 
-let to_int t = Int8.to_int (to_int8 t)
+let[@inline always] to_int t = Int8.to_int (to_int8 t)
 
-let of_int i = of_int8 (Int8.of_int i)
+let[@inline always] of_int i = of_int8 (Int8.of_int i)
