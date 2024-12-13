da76c43cebf5d8077cac7ba11f3e4c19595cba53
Thomas Del Vecchio <tdelvecchio@janestreet.com>
2024-12-13 12:13:49 -0500

    Make Printexc.Safe
    Signed-off-by: Thomas Del Vecchio <tdelvecchio@janestreet.com>

--- stdlib/effect.ml
+++ stdlib/effect.ml
@@ -27,7 +27,7 @@ let () =
         Some msg
     | _ -> None
   in
-  Printexc.register_printer printer
+  Printexc.Safe.register_printer printer
 
 (* Register the exceptions so that the runtime can access it *)
 type _ t += Should_not_see_this__ : unit t
--- stdlib/fun.ml
+++ stdlib/fun.ml
@@ -26,7 +26,7 @@ let negate p v = not (p v)
 
 exception Finally_raised of exn
 
-let () = Printexc.register_printer @@ function
+let () = Printexc.Safe.register_printer @@ function
 | Finally_raised exn -> Some ("Fun.Finally_raised: " ^ Printexc.to_string exn)
 | _ -> None
 
