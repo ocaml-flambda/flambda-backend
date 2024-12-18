f729ec4624a09733c7c70b0219b59a524f4f2edb
Zesen Qian <github@riaqn.org>
2024-12-18 16:23:45 +0000

    promote val_modalities to stable

--- testsuite/tests/typing-modes/modes.ml
+++ testsuite/tests/typing-modes/modes.ml
@@ -455,18 +455,6 @@ let foo () =
 val foo : unit -> unit = <fun>
 |}]
 
-(* modalities on normal values requires [-extension mode_alpha] *)
-module type S = sig
-  val x : string -> string @ local @@ foo bar
-end
-[%%expect{|
-Line 2, characters 38-41:
-2 |   val x : string -> string @ local @@ foo bar
-                                          ^^^
-Error: The extension "mode" is disabled and cannot be used
-|}]
-
-
 (*
  * Modification of return modes in argument position
  *)
