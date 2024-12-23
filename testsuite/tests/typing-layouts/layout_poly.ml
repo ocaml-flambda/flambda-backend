ccasin/oxcaml

Expanded product array tests - passes in bytecode

ocaml/testsuite/tests/typing-layouts/layout_poly.ml
@@ -721,19 +721,15 @@ Error: "[@layout_poly]" on this external declaration has no
 external[@layout_poly] makearray_dynamic : ('a : any). int -> 'a -> 'a array =
   "%makearray_dynamic"
 [%%expect{|
-Lines 1-2, characters 0-22:
-1 | external[@layout_poly] makearray_dynamic : ('a : any). int -> 'a -> 'a array =
-2 |   "%makearray_dynamic"
-Error: This construct requires the alpha version of the extension "layouts", which is disabled and cannot be used
+external makearray_dynamic : ('a : any). int -> 'a -> 'a array
+  = "%makearray_dynamic" [@@layout_poly]
 |}]
 
 external[@layout_poly] arrayblit :
   ('a : any). 'a array -> int -> 'a array -> int -> int -> unit =
   "%arrayblit"
 [%%expect{|
-Lines 1-3, characters 0-14:
-1 | external[@layout_poly] arrayblit :
-2 |   ('a : any). 'a array -> int -> 'a array -> int -> int -> unit =
-3 |   "%arrayblit"
-Error: This construct requires the alpha version of the extension "layouts", which is disabled and cannot be used
+external arrayblit :
+  ('a : any). 'a array -> int -> 'a array -> int -> int -> unit
+  = "%arrayblit" [@@layout_poly]
 |}]

ocaml-flambda/flambda-backend

(cherry picked from commit a55f456a2d1688848f9565621e35dca696ceeebb)

testsuite/tests/typing-layouts/layout_poly.ml
@@ -721,19 +721,15 @@ Error: "[@layout_poly]" on this external declaration has no
 external[@layout_poly] makearray_dynamic : ('a : any_non_null). int -> 'a -> 'a array =
   "%makearray_dynamic"
 [%%expect{|
-Lines 1-2, characters 0-22:
-1 | external[@layout_poly] makearray_dynamic : ('a : any_non_null). int -> 'a -> 'a array =
-2 |   "%makearray_dynamic"
-Error: This construct requires the alpha version of the extension "layouts", which is disabled and cannot be used
+external makearray_dynamic : ('a : any_non_null). int -> 'a -> 'a array
+  = "%makearray_dynamic" [@@layout_poly]
 |}]
 
 external[@layout_poly] arrayblit :
   ('a : any_non_null). 'a array -> int -> 'a array -> int -> int -> unit =
   "%arrayblit"
 [%%expect{|
-Lines 1-3, characters 0-14:
-1 | external[@layout_poly] arrayblit :
-2 |   ('a : any_non_null). 'a array -> int -> 'a array -> int -> int -> unit =
-3 |   "%arrayblit"
-Error: This construct requires the alpha version of the extension "layouts", which is disabled and cannot be used
+external arrayblit :
+  ('a : any_non_null). 'a array -> int -> 'a array -> int -> int -> unit
+  = "%arrayblit" [@@layout_poly]
 |}]