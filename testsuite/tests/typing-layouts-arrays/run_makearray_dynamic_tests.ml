a62469eece1526c152974ca20218170cda080b76
Ryan Tjoa <rtjoa@janestreet.com>
2024-12-23 12:29:58 -0500

    Test oclassic and o3, more opaque_identity

--- testsuite/tests/typing-layouts-arrays/run_makearray_dynamic_tests.ml
+++ testsuite/tests/typing-layouts-arrays/run_makearray_dynamic_tests.ml
@@ -14,11 +14,41 @@
    include stdlib_stable;
    include stdlib_upstream_compatible;
    ocamlopt.opt;
-   arguments = "native";
-   output = "${test_source_directory}/generated_test.ml.corrected";
+   arguments = "byte";
+   output = "${test_source_directory}/generated_test_byte.ml.corrected";
    run;
-   output = "${test_source_directory}/generated_test.ml.corrected";
-   reference = "${test_source_directory}/generated_test.ml";
+   output = "${test_source_directory}/generated_test_byte.ml.corrected";
+   reference = "${test_source_directory}/generated_test_byte.ml";
+   check-program-output;
+ }
+ {
+   setup-ocamlopt.opt-build-env;
+   stack-allocation;
+   program = "${test_source_directory}/generate.out";
+   all_modules = "generate_makearray_dynamic_tests.ml";
+   include stdlib_stable;
+   include stdlib_upstream_compatible;
+   ocamlopt.opt;
+   arguments = "native-oclassic";
+   output = "${test_source_directory}/generated_test_native_oclassic.ml.corrected";
+   run;
+   output = "${test_source_directory}/generated_test_native_oclassic.ml.corrected";
+   reference = "${test_source_directory}/generated_test_native_oclassic.ml";
+   check-program-output;
+ }
+ {
+   setup-ocamlopt.opt-build-env;
+   stack-allocation;
+   program = "${test_source_directory}/generate.out";
+   all_modules = "generate_makearray_dynamic_tests.ml";
+   include stdlib_stable;
+   include stdlib_upstream_compatible;
+   ocamlopt.opt;
+   arguments = "native-o3";
+   output = "${test_source_directory}/generated_test_native_o3.ml.corrected";
+   run;
+   output = "${test_source_directory}/generated_test_native_o3.ml.corrected";
+   reference = "${test_source_directory}/generated_test_native_o3.ml";
    check-program-output;
  }
 *)
