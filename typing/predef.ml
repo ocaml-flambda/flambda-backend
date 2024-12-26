862ced26837f2b8d9c76af8edab7509708ec39d5
dkalinichenko-js <118547217+dkalinichenko-js@users.noreply.github.com>
2024-12-26 15:20:06 -0500

    Add `Variant_with_null` and `Null` variant constructors (#2870)
    * `Variant_with_null`
    
    * `Null` tagged constructors
    
    * precise value kind
    
    * No private re-export
    
    ---------
    
    Co-authored-by: Diana Kalinichenko <dkalinichenko@janestreet.com>

--- typing/predef.ml
+++ typing/predef.ml
@@ -477,6 +477,8 @@ let add_small_number_beta_extension_types add_type env =
   |> add_type ident_int16 ~jkind:Jkind.Const.Builtin.immediate
 
 let or_null_kind tvar =
+  (* CR layouts v3: use [Variant_with_null] when it's supported
+     in the backend. *)
   variant [cstr ident_null [];
            cstr ident_this [unrestricted tvar or_null_argument_sort]]
 
