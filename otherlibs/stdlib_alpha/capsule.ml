55f1b2aafab1cece6d31112f2ab89dcba511bfb7
TheNumbat <max@thenumb.at>
2025-01-02 17:57:50 -0500

    raise wrapped exceptions with existing backtrace

--- otherlibs/stdlib_alpha/capsule.ml
+++ otherlibs/stdlib_alpha/capsule.ml
@@ -124,6 +124,8 @@ end
 (* Like [Stdlib.raise], but [portable], and the value
    it never returns is also [portable] *)
 external reraise : exn -> 'a @ portable @@ portable = "%reraise"
+external raise_with_backtrace: exn -> Printexc.raw_backtrace -> 'a @ portable @@ portable = "%raise_with_backtrace"
+external get_raw_backtrace: unit -> Printexc.raw_backtrace @@ portable = "caml_get_exception_raw_backtrace"
 
 module Data = struct
   type ('a, 'k) t : value mod portable uncontended
@@ -143,10 +145,10 @@ module Data = struct
   let create f = unsafe_mk (f ())
 
   let reraise_encapsulated password exn =
-    reraise (Encapsulated (Password.name password, unsafe_mk exn))
+    raise_with_backtrace (Encapsulated (Password.name password, unsafe_mk exn)) (get_raw_backtrace ())
 
   let reraise_encapsulated_shared password exn =
-    reraise (Encapsulated (Password.Shared.name password, unsafe_mk exn))
+    raise_with_backtrace (Encapsulated (Password.Shared.name password, unsafe_mk exn)) (get_raw_backtrace ())
 
   let map pw f t =
     let v = unsafe_get t in
@@ -381,7 +383,11 @@ exception Protected : 'k Mutex.t * (exn, 'k) Data.t -> exn
 let protect_local f = exclave_
   let (P name) = Name.make () in
   let password = Password.unsafe_mk name in
-  let reraise data = reraise (Protected ({ name; mutex = M.create (); poisoned = false }, data)) in
+  let reraise data =
+    let backtrace = get_raw_backtrace () in
+    let exn = (Protected ({ name; mutex = M.create (); poisoned = false }, data)) in
+    raise_with_backtrace exn backtrace
+  in
   try f (Password.P password) with
   | Encapsulated (inner, data) as exn ->
     (match Name.equality_witness name inner with
