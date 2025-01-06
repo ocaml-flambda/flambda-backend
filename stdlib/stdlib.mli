9a6034afc746b8b6df3f03f885a0b89ec3f987b5
Thomas Del Vecchio <tdelvecchio@janestreet.com>
2025-01-06 15:18:26 -0500

    fixup
    Signed-off-by: Thomas Del Vecchio <tdelvecchio@janestreet.com>

--- stdlib/stdlib.mli
+++ stdlib/stdlib.mli
@@ -717,7 +717,7 @@ val char_of_int : int -> char
 
 (** {1 Unit operations} *)
 
-external ignore : 'a -> unit = "%ignore"
+external ignore : 'a @ contended -> unit = "%ignore"
 (** Discard the value of its argument and return [()].
    For instance, [ignore(f x)] discards the result of
    the side-effecting function [f].  It is equivalent to
