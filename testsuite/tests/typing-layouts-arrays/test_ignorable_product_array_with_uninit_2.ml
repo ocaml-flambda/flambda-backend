63c3de7db79ed1ad314a335d4fddf6b267b9d149
Mark Shinwell <mshinwell@pm.me>
2025-01-14 17:45:36 +0000

    Enable all makearray_dynamic tests on runtime4

--- testsuite/tests/typing-layouts-arrays/test_ignorable_product_array_with_uninit_2.ml
+++ testsuite/tests/typing-layouts-arrays/test_ignorable_product_array_with_uninit_2.ml
@@ -6,7 +6,6 @@
  modules = "${readonly_files}";
  flambda2;
  arch_amd64;
- runtime5;
  {
    flags = "-extension layouts_beta";
    bytecode;
@@ -16,11 +15,6 @@
    native;
  }
 *)
-
-(* This test exhibited intermittent failures with very low probability on
-   runtime4, and extensive investigation has not found the cause.  It is
-   suspected it might be a problem in the runtime4 GC.  So we have restricted
-   the test to runtime5 at least for now. *)
 
 (* CR mshinwell: enable for arm64 once float32 is available *)
 
