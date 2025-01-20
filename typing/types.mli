ef701a237ce8b0894451292d9d81e63539b0b7bb
Liam Stevenson <lstevenson@janestreet.com>
2025-01-20 16:22:19 -0500

    Box with bound type info

--- typing/types.mli
+++ typing/types.mli
@@ -74,7 +74,7 @@ module Jkind_bounds :
 (** Information tracked about an individual type within the with-bounds for a jkind *)
 module With_bounds_type_info : sig
   (** The axes that the with-bound applies to *)
-  type t = Jkind_axis.Axis_set.t
+  type t = { relevant_axes : Jkind_axis.Axis_set.t } [@@unboxed]
 end
 
 type type_expr
