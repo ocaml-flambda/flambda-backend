19c60c52c0bca86a1cc76f2baed632c2e2c030ce
Ryan Tjoa <rtjoa@janestreet.com>
2025-01-21 17:58:29 -0500

    [Derivative test] typing_misc_implicit_unboxed_records

--- testsuite/tests/typing-layouts-products/typing_misc_implicit_unboxed_records.ml
+++ testsuite/tests/typing-layouts-products/typing_misc_implicit_unboxed_records.ml
@@ -5,18 +5,18 @@
 *)
 
 (* NOTE: When adding tests to this file, consider updating
-   [typing-layouts-products/typing_misc_implicit_unboxed_records.ml] *)
+   [typing-layouts-products/typing_misc_unboxed_records.ml] *)
 
-type 'a ref_u = #{ contents : 'a }
+type 'a ref_u = { contents : 'a }
 
 (* This test is based on testsuite/tests/typing-misc/records.ml *)
 
 (* undefined labels *)
-type t = #{x:int;y:int};;
+type t = {x:int;y:int};;
 #{x=3;z=2};;
 [%%expect{|
-type 'a ref_u = #{ contents : 'a; }
-type t = #{ x : int; y : int; }
+type 'a ref_u = { contents : 'a; }
+type t = { x : int; y : int; }
 Line 7, characters 6-7:
 7 | #{x=3;z=2};;
           ^
@@ -41,10 +41,10 @@ Error: The unboxed record field "contents" belongs to the type "'a ref_u"
 |}];;
 
 (* private types *)
-type u = private #{u:int};;
+type u = private {u:int};;
 #{u=3};;
 [%%expect{|
-type u = private #{ u : int; }
+type u = private { u : int; }
 Line 2, characters 0-6:
 2 | #{u=3};;
     ^^^^^^
@@ -53,7 +53,7 @@ Error: Cannot create values of the private type "u"
 
 (* Punning and abbreviations *)
 module M = struct
-  type t = #{x: int; y: int}
+  type t = {x: int; y: int}
 end;;
 [%%expect{|
 module M : sig type t = #{ x : int; y : int; } end
@@ -84,12 +84,12 @@ Error: This pattern matches values of type "int ref_u"
 
 
 (* bugs *)
-type foo = #{ y: int; z: int };;
-type bar = #{ x: int };;
-let f (r: bar) = (#{ r with z = 3 } : foo)
+type foo = { y: int; z: int };;
+type bar = { x: int };;
+let f (r: bar#) = (#{ r with z = 3 } : foo)
 [%%expect{|
-type foo = #{ y : int; z : int; }
-type bar = #{ x : int; }
+type foo = { y : int; z : int; }
+type bar = { x : int; }
 Line 3, characters 21-22:
 3 | let f (r: bar) = (#{ r with z = 3 } : foo)
                          ^
@@ -97,10 +97,10 @@ Error: This expression has type "bar" but an expression was expected of type
          "foo"
 |}];;
 
-type foo = #{ x: int };;
-let r : foo = #{ ZZZ.x = 2 };;
+type foo = { x: int };;
+let r : foo# = #{ ZZZ.x = 2 };;
 [%%expect{|
-type foo = #{ x : int; }
+type foo = { x : int; }
 Line 2, characters 17-22:
 2 | let r : foo = #{ ZZZ.x = 2 };;
                      ^^^^^
@@ -133,22 +133,22 @@ Line 1, characters 0-27:
 Error: This expression has type "bool" which is not a unboxed record type.
 |}];;
 
-type ('a, 'b) t = #{ fst : 'a; snd : 'b };;
+type ('a, 'b) t = { fst : 'a; snd : 'b };;
 let with_fst r fst = #{ r with fst };;
 let #{ fst;  snd} = with_fst #{ fst=""; snd="" } 2;;
 [%%expect{|
-type ('a, 'b) t = #{ fst : 'a; snd : 'b; }
+type ('a, 'b) t = { fst : 'a; snd : 'b; }
 val with_fst : ('a, 'b) t -> 'c -> ('c, 'b) t = <fun>
 val fst : int = 2
 val snd : string = ""
 |}];;
 
 (* PR#7695 *)
-type 'a t = #{ f : 'a; g : 'a };;
+type 'a t = { f : 'a; g : 'a };;
 let x () = #{ f = 12; g = 43 };;
 let foo () = let x = x () in #{x with f = "hola"};;
 [%%expect{|
-type 'a t = #{ f : 'a; g : 'a; }
+type 'a t = { f : 'a; g : 'a; }
 val x : unit -> int t = <fun>
 Line 3, characters 29-49:
 3 | let foo () = let x = x () in #{x with f = "hola"};;
@@ -172,11 +172,11 @@ Exception: Assert_failure ("", 1, 11).
 
 (* reexport *)
 
-type ('a,'b) def = #{ x:int } constraint 'b = [> `A]
+type ('a,'b) def = { x:int } constraint 'b = [> `A]
 
-type arity = (int, [`A]) def = #{x:int};;
+type arity = (int, [`A]) def = {x:int};;
 [%%expect{|
-type ('a, 'b) def = #{ x : int; } constraint 'b = [> `A ]
+type ('a, 'b) def = { x : int; } constraint 'b = [> `A ]
 Line 3, characters 0-39:
 3 | type arity = (int, [`A]) def = #{x:int};;
     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
@@ -185,7 +185,7 @@ Error: This variant or record definition does not match that of type
        They have different arities.
 |}]
 
-type ('a,'b) ct = (int,'b) def = #{x:int};;
+type ('a,'b) ct = (int,'b) def = {x:int};;
 [%%expect{|
 Line 1, characters 0-41:
 1 | type ('a,'b) ct = (int,'b) def = #{x:int};;
@@ -206,12 +206,12 @@ Error: This variant or record definition does not match that of type
        The original is an unboxed record, but this is a variant.
 |}]
 
-type d = #{ x:int; y : int }
+type d = { x:int; y : int }
 [%%expect{|
-type d = #{ x : int; y : int; }
+type d = { x : int; y : int; }
 |}]
 
-type missing = d = #{ x:int }
+type missing = d = { x:int }
 [%%expect{|
 Line 1, characters 0-29:
 1 | type missing = d = #{ x:int }
@@ -220,7 +220,7 @@ Error: This variant or record definition does not match that of type "d"
        An extra field, "y", is provided in the original definition.
 |}]
 
-type wrong_type = d = #{x:float}
+type wrong_type = d = {x:float}
 [%%expect{|
 Line 1, characters 0-32:
 1 | type wrong_type = d = #{x:float}
@@ -234,18 +234,7 @@ Error: This variant or record definition does not match that of type "d"
        2. An extra field, "y", is provided in the original definition.
 |}]
 
-type mono = #{foo:int}
-type unboxed = mono = #{foo:int} [@@unboxed]
-[%%expect{|
-type mono = #{ foo : int; }
-Line 2, characters 0-44:
-2 | type unboxed = mono = #{foo:int} [@@unboxed]
-    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-Error: This type cannot be unboxed because
-       [@@unboxed] may not be used on unboxed records.
-|}]
-
-type perm = d = #{y:int; x:int}
+type perm = d = {y:int; x:int}
 [%%expect{|
 Line 1, characters 0-31:
 1 | type perm = d = #{y:int; x:int}
@@ -254,13 +243,13 @@ Error: This variant or record definition does not match that of type "d"
        Fields "x" and "y" have been swapped.
 |}]
 
-type t = #{ f1 : int ; f2 : int }
+type t = { f1 : int ; f2 : int }
 
 let f () = #{ f1 = 0
         ; Coq__10.f2 = 0 }
 
 [%%expect{|
-type t = #{ f1 : int; f2 : int; }
+type t = { f1 : int; f2 : int; }
 Line 4, characters 10-20:
 4 |         ; Coq__10.f2 = 0 }
               ^^^^^^^^^^
@@ -268,7 +257,7 @@ Error: Unbound module "Coq__10"
 |}]
 
 module Coq__11 = struct
-  type t = #{ f1 : int ; f2 : int; f3 : int }
+  type t = { f1 : int ; f2 : int; f3 : int }
 end
 
 let f () = #{ f1 = 0
@@ -285,7 +274,7 @@ Hint: Did you mean "Coq__11"?
 |}]
 
 type a = unit
-type b = a = #{ a : int }
+type b = a = { a : int }
 [%%expect{|
 type a = unit
 Line 2, characters 0-25:
@@ -296,7 +285,7 @@ Error: This variant or record definition does not match that of type "a"
 |}]
 
 type a = unit
-type b = a = #{ a : int }
+type b = a = { a : int }
 [%%expect{|
 type a = unit
 Line 2, characters 0-25:
