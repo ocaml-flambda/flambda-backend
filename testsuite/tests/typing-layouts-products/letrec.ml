97a8eb730e53d8e5ac3b71345358a270cf86f062
Ryan Tjoa <rtjoa@janestreet.com>
2025-01-21 12:25:43 -0500

    Test exhaustiveness, letrec, unused warnings

--- testsuite/tests/typing-layouts-products/letrec.ml
+++ testsuite/tests/typing-layouts-products/letrec.ml
@@ -19,6 +19,17 @@ Error: The definition of "t" is recursive without boxing:
          "t" contains "t"
 |}]
 
+
+type t : value = { u : t }
+let rec t = #{ u = t }
+[%%expect{|
+type t = { u : t; }
+Line 2, characters 12-22:
+2 | let rec t = #{ u = t }
+                ^^^^^^^^^^
+Error: This expression has type "t#" but an expression was expected of type "t"
+|}]
+
 type bx = { bx : ubx }
 and ubx = #{ ubx : bx }
 [%%expect{|
@@ -36,12 +47,39 @@ let rec t = { bx = #{ ubx = t } }
 val t : bx = {bx = <cycle>}
 |}]
 
+type bx = { bx : bx2# }
+and bx2 = { bx2 : bx }
+[%%expect{|
+type bx = { bx : bx2#; }
+and bx2 = { bx2 : bx; }
+|}]
+
+let rec t = #{ bx2 = { bx = t } }
+[%%expect{|
+val t : bx2# = #{bx2 = {bx = <cycle>}}
+|}]
+
+let rec t = { bx = #{ bx2 = t } }
+[%%expect{|
+val t : bx = {bx = <cycle>}
+|}]
+
 (* The below is adapted from [testsuite/tests/letrec-check/unboxed.ml]. *)
 
 type t = #{x: int64}
 let rec x = #{x = y} and y = 3L;;
 [%%expect{|
 type t = #{ x : int64; }
+Line 2, characters 12-20:
+2 | let rec x = #{x = y} and y = 3L;;
+                ^^^^^^^^
+Error: This kind of expression is not allowed as right-hand side of "let rec"
+|}];;
+
+type t = {x: int64}
+let rec x = #{x = y} and y = 3L;;
+[%%expect{|
+type t = { x : int64; }
 Line 2, characters 12-20:
 2 | let rec x = #{x = y} and y = 3L;;
                 ^^^^^^^^
@@ -77,6 +115,27 @@ let rec a =
 [%%expect{|
 type a = #{ a : b; }
 and b = X of a | Y
+Lines 5-9, characters 2-10:
+5 | ..#{a=
+6 |     (if Sys.opaque_identity true then
+7 |        X a
+8 |      else
+9 |        Y)}..
+Error: This kind of expression is not allowed as right-hand side of "let rec"
+|}];;
+
+type a = { a: b }
+and b = X of a# | Y
+
+let rec a =
+  #{a=
+    (if Sys.opaque_identity true then
+       X a
+     else
+       Y)};;
+[%%expect{|
+type a = { a : b; }
+and b = X of a# | Y
 Lines 5-9, characters 2-10:
 5 | ..#{a=
 6 |     (if Sys.opaque_identity true then
