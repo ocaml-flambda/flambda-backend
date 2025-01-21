bc1e30c41d04605567fed5fb2a20f6262deadd40
Ryan Tjoa <rtjoa@janestreet.com>
2025-01-21 12:19:14 -0500

    New tests: hash_types, derived_unboxed, derived_unboxed_signatures

---
+++ testsuite/tests/typing-layouts/derived_unboxed_signatures.ml
@@ -0,0 +1,374 @@
+(* TEST
+ flags = "-extension layouts_beta";
+ expect;
+*)
+
+(* CR rtjoa: Do all these tests with type parameters *)
+
+(*************)
+(* Shadowing *)
+
+module type S = sig
+  type t = { i : int }
+end
+[%%expect{|
+module type S = sig type t = { i : int; } end
+|}]
+
+(* Shadowing a boxed record type also shadows the ghost unboxed record type... *)
+module type Shadowed = sig
+  include S
+  type t
+end
+[%%expect {|
+module type Shadowed = sig type t end
+|}]
+
+(* ...so we can't reference the derived type... *)
+module M(Shadowed : Shadowed) = struct
+  type t = Shadowed.t#
+end
+[%%expect{|
+Line 2, characters 11-22:
+2 |   type t = Shadowed.t#
+               ^^^^^^^^^^^
+Error: "Shadowed.t" has no unboxed version.
+|}]
+
+(* ...or its labels. *)
+module M(Shadowed : Shadowed) = struct
+  let x = #{ i = 1 }
+end
+[%%expect{|
+Line 2, characters 13-14:
+2 |   let x = #{ i = 1 }
+                 ^
+Error: Unbound unboxed record field "i"
+|}]
+
+(*************************)
+(* With type constraints *)
+
+module type S = sig
+  type t = { i : int }
+  val dummy : int
+  val f : t# -> t#
+end
+
+type t' = { i : int }
+[%%expect{|
+module type S =
+  sig type t = { i : int; } val dummy : int val f : t# -> t# end
+type t' = { i : int; }
+|}]
+
+module type S' = S with type t = t'
+[%%expect{|
+module type S' =
+  sig type t = t' = { i : int; } val dummy : int val f : t# -> t# end
+|}]
+
+(* Check that the constraint was added (we can't check this by printing because
+   t# is hidden) *)
+type t = { i : int ; j : int }
+
+module type S = sig
+  type t = { i : int ; j : int }
+end
+with type t = t
+
+(* This compiles if the constraints were added *)
+module Check_constraints(M:S) = struct
+  let f : t -> M.t = fun x -> x
+  let f : t# -> M.t# = fun x -> x
+end
+[%%expect{|
+type t = { i : int; j : int; }
+module type S = sig type t = t = { i : int; j : int; } end
+module Check_constraints : functor (M : S) -> sig val f : t# -> M.t# end
+|}]
+
+(*******************************)
+(* With type subst constraints *)
+
+(* Unboxed number #-type *)
+
+type t = float
+module type S = sig
+  type t_to_replace = float
+  type th = t_to_replace#
+end with type t_to_replace := t
+[%%expect{|
+type t = float
+module type S = sig type th = t# end
+|}]
+
+module F (M : S) = struct
+  let f : M.th -> float# = fun x -> x
+  let g : M.th -> t# = fun x -> x
+end
+[%%expect{|
+module F :
+  functor (M : S) -> sig val f : M.th -> float# val g : M.th -> t# end
+|}]
+
+module Bad (M : S) = struct
+  type bad = M.t_to_replace#
+end
+[%%expect{|
+Line 2, characters 13-28:
+2 |   type bad = M.t_to_replace#
+                 ^^^^^^^^^^^^^^^
+Error: Unbound type constructor "M.t_to_replace"
+|}]
+
+module type Bad = sig
+  type t = float
+end with type t := float#
+[%%expect{|
+Lines 1-3, characters 18-25:
+1 | ..................sig
+2 |   type t = float
+3 | end with type t := float#
+Error: In this "with" constraint, the new definition of "t"
+       does not match its original definition in the constrained signature:
+       Type declarations do not match:
+         type t = float#
+       is not included in
+         type t = float
+       The type "float#" is not equal to the type "float"
+|}]
+
+module type S = sig
+  type t : float64
+  type fu = t
+end with type t := float#
+[%%expect{|
+module type S = sig type fu = float# end
+|}]
+
+module type Bad = sig
+  type t
+end with type t := float#
+[%%expect{|
+Line 3, characters 9-25:
+3 | end with type t := float#
+             ^^^^^^^^^^^^^^^^
+Error: The layout of type "float#" is float64
+         because it is unboxed version of the primitive type float.
+       But the layout of type "float#" must be a sublayout of value
+         because of the definition of t at line 2, characters 2-8.
+|}]
+
+module type Bad = sig
+  type t = int32#
+end with type t := float#
+[%%expect{|
+Lines 1-3, characters 18-25:
+1 | ..................sig
+2 |   type t = int32#
+3 | end with type t := float#
+Error: In this "with" constraint, the new definition of "t"
+       does not match its original definition in the constrained signature:
+       Type declarations do not match:
+         type t = float#
+       is not included in
+         type t = int32#
+       The type "float#" is not equal to the type "int32#"
+|}]
+
+module F (M : S) = struct
+  let f : M.th -> float# = fun x -> x
+  let g : M.th -> t# = fun x -> x
+end
+[%%expect{|
+Line 2, characters 10-14:
+2 |   let f : M.th -> float# = fun x -> x
+              ^^^^
+Error: Unbound type constructor "M.th"
+|}]
+
+module Bad (M : S) = struct
+  type bad = M.t_to_replace#
+end
+[%%expect{|
+Line 2, characters 13-28:
+2 |   type bad = M.t_to_replace#
+                 ^^^^^^^^^^^^^^^
+Error: Unbound type constructor "M.t_to_replace"
+|}]
+
+(* Implicit unboxed record #-type *)
+
+type t = { i : int ; j : string }
+
+module type S = sig
+  type t_to_replace = { i : int ; j : string }
+
+  type r = t_to_replace
+  type ru = t_to_replace#
+end with type t_to_replace := t
+[%%expect{|
+type t = { i : int; j : string; }
+module type S = sig type r = t type ru = t# end
+|}]
+
+module CheckSubsted(M : S) = struct
+  let f : t -> M.r = fun x -> x
+  let f : t# -> M.ru = fun x -> x
+end
+[%%expect{|
+module CheckSubsted : functor (M : S) -> sig val f : t# -> M.ru end
+|}]
+
+module Bad(M : S) = struct
+  type bad = t_to_replace#
+end
+[%%expect{|
+Line 2, characters 13-26:
+2 |   type bad = t_to_replace#
+                 ^^^^^^^^^^^^^
+Error: Unbound type constructor "t_to_replace"
+|}]
+
+(* Check that order stays the same after replacing *)
+
+type s' = { s : string }
+type i' = { i : int }
+
+module type s = sig
+  type top
+  and s = { s : string }
+  and mid
+  and i = { i : int }
+  and bot
+end with type s = s'
+    with type i = i'
+[%%expect {|
+type s' = { s : string; }
+type i' = { i : int; }
+module type s =
+  sig
+    type top
+    and s = s' = { s : string; }
+    and mid
+    and i = i' = { i : int; }
+    and bot
+  end
+|}]
+
+
+(**********************************************)
+(* Shadowing behavior with predefined #-types *)
+
+(* float# starts out as an unboxed float *)
+type float64 : float64 = float#
+[%%expect{|
+type float64 = float#
+|}]
+
+(* it doesn't normally get shadowed by "float" *)
+type float = int
+type float64 : float64 = float#
+[%%expect{|
+type float = int
+Line 2, characters 25-31:
+2 | type float64 : float64 = float#
+                             ^^^^^^
+Error: "float" has no unboxed version.
+|}]
+
+(* but it gets shadowed by a "float" boxed record! *)
+type float = { i : int ; j : int }
+type ij : value & value = float#
+
+(* and when we shadow "float", the predefined "float#" becomes visible again *)
+type float = int
+type float64 : float64 = float#
+[%%expect{|
+type float = { i : int; j : int; }
+type ij = float#
+type float = int
+Line 6, characters 25-31:
+6 | type float64 : float64 = float#
+                             ^^^^^^
+Error: "float" has no unboxed version.
+|}]
+
+(********************************************)
+(* Illegal shadowing with predefined #-types*)
+
+(* CR layouts v7.2: "float/2#" makes sense with the view of [#] as an operator,
+   but we may aesthetically prefer "float#/2". *)
+include struct type float = { a : int } end
+type float = string
+let x = #{ a = 1 }
+[%%expect{|
+type float = { a : int; }
+type float = string
+val x : float/2# = #{a = 1}
+|}]
+
+include struct type floot = { a : int } end
+type floot = string
+let x = #{ a = 1 }
+[%%expect{|
+type floot = { a : int; }
+type floot = string
+val x : floot/2# = #{a = 1}
+|}]
+
+
+(*****************************************************)
+(* Propagating constraints from recursive type decls *)
+
+type ('a : value & float64) t
+[%%expect{|
+type ('a : value & float64) t
+|}]
+
+type s = r# t
+and r = {x:int; y:float#}
+[%%expect{|
+Line 2, characters 18-24:
+2 | and r = {x:int; y:float#}
+                      ^^^^^^
+Error: "float" has no unboxed version.
+|}]
+
+type s = q t
+and r = {x:int; y:float#}
+and q = r#
+[%%expect{|
+Line 2, characters 18-24:
+2 | and r = {x:int; y:float#}
+                      ^^^^^^
+Error: "float" has no unboxed version.
+|}]
+
+type s_bad = r# t
+and r = {x:int; y:bool}
+[%%expect{|
+Line 2, characters 0-23:
+2 | and r = {x:int; y:bool}
+    ^^^^^^^^^^^^^^^^^^^^^^^
+Error:
+       The layout of r# is any & any
+         because it is an unboxed record.
+       But the layout of r# must be a sublayout of value & float64
+         because of the definition of t at line 1, characters 0-29.
+|}]
+
+type s_bad = q t
+and r = {x:int; y:bool}
+and q = r#
+[%%expect{|
+Line 3, characters 0-10:
+3 | and q = r#
+    ^^^^^^^^^^
+Error:
+       The layout of q is any & any
+         because it is an unboxed record.
+       But the layout of q must be a sublayout of value & float64
+         because of the definition of t at line 1, characters 0-29.
+|}]
---
+++ testsuite/tests/typing-layouts/hash_types.ml
@@ -0,0 +1,292 @@
+(* TEST
+ flags = "-extension layouts_beta";
+ expect;
+*)
+
+(* Non-existent #-type *)
+type bad = a#
+[%%expect{|
+Line 1, characters 11-13:
+1 | type bad = a#
+               ^^
+Error: Unbound type constructor "a"
+|}]
+
+type t = float#
+[%%expect{|
+type t = float#
+|}]
+
+type t = float
+[%%expect{|
+type t = float
+|}]
+
+(* Alias gets its own #-type *)
+type u = t#
+[%%expect{|
+type u = t#
+|}]
+
+(* Shadowing hides #-type *)
+type float
+[%%expect{|
+type float
+|}]
+
+type bad = float#
+
+(* But it's still accessible via the alias *)
+type u2 = t#
+[%%expect{|
+Line 1, characters 11-17:
+1 | type bad = float#
+               ^^^^^^
+Error: "float" has no unboxed version.
+|}]
+
+module M = struct
+  type t = int32
+end
+[%%expect{|
+module M : sig type t = int32 end
+|}]
+
+type int32_u = M.t#
+[%%expect{|
+type int32_u = M.t#
+|}]
+
+(* Restore float *)
+type float = t
+[%%expect{|
+type float = t
+|}]
+
+module type S = sig
+  type t = float
+  type u = t#
+end with type t := float
+[%%expect{|
+module type S = sig type u = float# end
+|}]
+
+module S : sig
+  type t = float
+  type u = t#
+  val f : u -> t#
+end with type u := float# = struct
+  type t = float
+  let f x = x
+end
+[%%expect{|
+module S : sig type t = float val f : float# -> t# end
+|}]
+
+type 'a t = float
+type s = int t#
+type v = string t#
+
+let (_ : s -> v) = fun x -> x
+[%%expect{|
+type 'a t = float
+type s = int t#
+type v = string t#
+- : s -> v = <fun>
+|}]
+
+type t = float
+and s = t#
+[%%expect{|
+type t = float
+and s = t#
+|}]
+
+type t = int
+and s = t#
+[%%expect{|
+Line 2, characters 0-10:
+2 | and s = t#
+    ^^^^^^^^^^
+Error: "t" has no unboxed version. (typedecl.ml)
+|}]
+
+type t = s#
+and s = t#
+[%%expect{|
+Line 1, characters 0-11:
+1 | type t = s#
+    ^^^^^^^^^^^
+Error: "s" has no unboxed version. (typedecl.ml)
+|}]
+
+type t = s#
+and s = float
+[%%expect{|
+type t = s#
+and s = float
+|}]
+
+type t = float
+and s = t#
+[%%expect{|
+type t = float
+and s = t#
+|}]
+
+module rec M : sig
+  type t = M2.t#
+end = struct
+  type t = M2.t#
+end
+and M2 : sig
+  type t = float
+end = struct
+  type t = float
+end
+[%%expect{|
+module rec M : sig type t = M2.t# end
+and M2 : sig type t = float end
+|}]
+
+module rec M : sig
+  type t = M2.t#
+end = struct
+  type t = M2.t#
+end
+and M2 : sig
+  type t = int
+end = struct
+  type t = int
+end
+[%%expect{|
+Line 2, characters 11-16:
+2 |   type t = M2.t#
+               ^^^^^
+Error: "M2.t" has no unboxed version.
+|}]
+
+module rec Bad1 : sig
+  type t = Bad2.t#
+end = struct
+  type t = Bad2.t#
+end
+and Bad2 : sig
+  type t = Bad1.t#
+end = struct
+  type t = Bad1.t#
+end
+[%%expect{|
+Line 2, characters 11-18:
+2 |   type t = Bad2.t#
+               ^^^^^^^
+Error: "Bad2.t" has no unboxed version.
+|}]
+
+type t = { i : int ; j : int }
+type s = t#
+[%%expect{|
+type t = { i : int; j : int; }
+type s = t#
+|}]
+
+type t = { i : int ; j : int }
+and s = t#
+[%%expect{|
+type t = { i : int; j : int; }
+and s = t#
+|}]
+
+let x () = #{ i = 1 ; j = 2 }
+[%%expect{|
+val x : unit -> t# = <fun>
+|}]
+
+type t = { s : s# }
+and s = { t : t# }
+[%%expect{|
+Line 1, characters 0-19:
+1 | type t = { s : s# }
+    ^^^^^^^^^^^^^^^^^^^
+Error: The definition of "t#" is recursive without boxing:
+         "t#" contains "s#",
+         "s#" contains "t#"
+|}]
+
+(* Many cases from the [recursive.ml] test to try... *)
+
+(* Do we give a type a hash type in enter type based on manifest, kind or both?
+   I think we give it if *either* has a hash type, in order for both of the
+   following to give good errors:
+
+     type a = { i : int }
+     type b = a = { i : int } [@@unboxed]
+     and s = b#
+
+     type a = { i : int } [@@unboxed]
+     type b = a = { i : int }
+     and s = b#
+
+   (Also consider if there are other variations than boxed/unboxed versions of
+   records, consider abstravct types with unboxed versions (only possible with
+   [boxes]). A further challenge: float records; we need to type these to
+   determine whether or not to give them dummy unboxed versions. Maybe we give
+   all records dummy unboxed versions, then have a check at the end that it's
+   [Record_boxed]?)
+
+   Also... these dummy declarations need to have good-enough jkinds for
+   typechecking. E.g. products need the right arity (remember the
+   product-of-[any]s hack.)
+
+   Another tricky case:
+   (Maybe not necessary for exactly this case, but it's worth thinking about
+   what dummy jkind [s] has)
+
+   type t = { i : int ; j : int }
+
+   type s = t
+   and m = s# = #{ i : int ; j : int }
+
+   (It's fine if we disallow this, I think.)
+
+   Also need to check that unused type error are eliminated by using the hash
+   version.
+*)
+
+module M = struct
+  type t = {x:int}
+  module N = struct
+    type b = t
+    type u = t#
+  end
+end
+[%%expect{|
+module M :
+  sig type t = { x : int; } module N : sig type b = t type u = t# end end
+|}]
+
+type a = b
+and b = int
+type bad = a#
+[%%expect{|
+type a = b
+and b = int
+Line 3, characters 11-13:
+3 | type bad = a#
+               ^^
+Error: "a" has no unboxed version.
+|}]
+
+type t = a = { i : int }
+and a = { i : int }
+[%%expect{|
+type t = a = { i : int; }
+and a = { i : int; }
+|}]
+
+type a = { i : int }
+and t = a = { i : int }
+[%%expect{|
+type a = { i : int; }
+and t = a = { i : int; }
+|}]
