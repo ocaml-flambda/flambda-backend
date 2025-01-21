bc0f9be10c9b9de6622cdc054775f65360cb3296
Ryan Tjoa <rtjoa@janestreet.com>
2025-01-21 17:52:00 -0500

    Derived separability test

--- testsuite/tests/typing-layouts-products/separability_implicit_unboxed_records.ml
+++ testsuite/tests/typing-layouts-products/separability_implicit_unboxed_records.ml
@@ -8,26 +8,26 @@
 *)
 
 (* NOTE: When adding tests to this file, consider updating
-   [typing-layouts-products/separability_implicit_unboxed_records.ml] *)
+   [typing-layouts-products/separability.ml] *)
 
-type 'a r = #{ a : 'a }
-and 'a ok = F : 'a r -> 'a ok [@@unboxed]
+type 'a r = { a : 'a }
+and 'a ok = F : 'a r# -> 'a ok [@@unboxed]
 [%%expect{|
-type 'a r = #{ a : 'a; }
+type 'a r = { a : 'a; }
 and 'a ok = F : 'a r -> 'a ok [@@unboxed]
 |}]
 
-type 'a r = #{ a : 'a }
-and 'a ok = F : { x : 'a r } -> 'a ok [@@unboxed]
+type 'a r = { a : 'a }
+and 'a ok = F : { x : 'a r# } -> 'a ok [@@unboxed]
 [%%expect{|
-type 'a r = #{ a : 'a; }
+type 'a r = { a : 'a; }
 and 'a ok = F : { x : 'a r; } -> 'a ok [@@unboxed]
 |}]
 
-type 'a r = #{ a : 'a }
-type bad = F : 'a r -> bad [@@unboxed]
+type 'a r = { a : 'a }
+type bad = F : 'a r# -> bad [@@unboxed]
 [%%expect{|
-type 'a r = #{ a : 'a; }
+type 'a r = { a : 'a; }
 Line 2, characters 0-38:
 2 | type bad = F : 'a r -> bad [@@unboxed]
     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
@@ -37,10 +37,10 @@ Error: This type cannot be unboxed because
        You should annotate it with "[@@ocaml.boxed]".
 |}]
 
-type 'a r = #{ a : 'a }
-type bad = F : { x : 'a r } -> bad [@@unboxed]
+type 'a r = { a : 'a }
+type bad = F : { x : 'a r# } -> bad [@@unboxed]
 [%%expect{|
-type 'a r = #{ a : 'a; }
+type 'a r = { a : 'a; }
 Line 2, characters 0-46:
 2 | type bad = F : { x : 'a r } -> bad [@@unboxed]
     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
@@ -50,9 +50,9 @@ Error: This type cannot be unboxed because
        You should annotate it with "[@@ocaml.boxed]".
 |}]
 
-type 'a r = #{ a : 'a }
-and 'a r2 = #{ a : 'a r }
-and bad = F : 'a r2 -> bad [@@unboxed]
+type 'a r = { a : 'a }
+and 'a r2 = { a : 'a r# }
+and bad = F : 'a r2# -> bad [@@unboxed]
 [%%expect{|
 Line 3, characters 0-38:
 3 | and bad = F : 'a r2 -> bad [@@unboxed]
@@ -63,8 +63,8 @@ Error: This type cannot be unboxed because
        You should annotate it with "[@@ocaml.boxed]".
 |}]
 
-type 'a r = #{ a : 'a }
-and bad = F : { x : 'a r } -> bad [@@unboxed]
+type 'a r = { a : 'a }
+and bad = F : { x : 'a r# } -> bad [@@unboxed]
 [%%expect{|
 Line 2, characters 0-45:
 2 | and bad = F : { x : 'a r } -> bad [@@unboxed]
@@ -76,10 +76,11 @@ Error: This type cannot be unboxed because
 |}]
 
 (* CR layouts v12: Once we allow products containing void in unboxed GADTs,
-   we'll have to make sure the below fails separability checking: *)
+   and boxed records containing void, we'll have to make sure the below fails
+   separability checking: *)
 type t_void : void
-and 'a r = #{ a : 'a ; v : t_void }
-and bad = F : 'a r -> bad [@@unboxed]
+and 'a r = { a : 'a ; v : t_void }
+and bad = F : 'a r# -> bad [@@unboxed]
 [%%expect{|
 Line 2, characters 0-35:
 2 | and 'a r = #{ a : 'a ; v : t_void }
@@ -92,8 +93,8 @@ Error:
 |}]
 
 type t_void : void
-and 'a r = #{ a : 'a ; v : t_void }
-and bad = F : { x : 'a r } -> bad [@@unboxed]
+and 'a r = { a : 'a ; v : t_void }
+and bad = F : { x : 'a r# } -> bad [@@unboxed]
 [%%expect{|
 Line 2, characters 0-35:
 2 | and 'a r = #{ a : 'a ; v : t_void }
