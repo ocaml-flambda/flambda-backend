ccfbe20e29f1929041df4e477e11918d8ba74c4f
Basile Clément <129742207+bclement-ocp@users.noreply.github.com>
2025-02-03 15:59:04 +0100

    flambda2-types: Recover aliases after meet (#3534)
    The function `TG.recover_some_aliases` is called by the typing
    environment in order to detect aliases prior to adding equations.
    
    With the advanced meet (and also sometimes with the basic meet), this
    alias recovery is done before computing the meet with the existing type
    of the variable, which means that we can lose aliases if the meet
    discovers a new alias.
    
    This patch changes the logic to perfom alias recovery after the meet has
    been performed, and also gives more descriptive names to the different
    steps of `add_equation` to make the logic more clear.
    
    If we have:
    
    ```
    x: (Variant (blocks {tag_0}) (tagged_imms ((= #0))))
    ```
    
    and we add:
    
    ```
    x: (Variant (blocks ⊥) (tagged_imms ⊤))
    ```
    
    without this patch we get:
    
    ```
    x: (Variant (blocks ⊥) (tagged_imms (= #0)))
    ```
    
    with this patch we get:
    
    ```
    x: (= 0)
    ```

--- middle_end/flambda2/types/grammar/type_grammar.mli
+++ middle_end/flambda2/types/grammar/type_grammar.mli
@@ -809,4 +809,4 @@ module Head_of_kind_naked_vec128 :
     with type n = Vector_types.Vec128.Bit_pattern.t
     with type n_set = Vector_types.Vec128.Bit_pattern.Set.t
 
-val recover_some_aliases : t -> t
+val recover_const_alias : t -> Reg_width_const.t option
