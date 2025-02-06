3c4ce6109dfe107d250221e927aeede8afc6da0c
Greta Yorsh <45005955+gretay-js@users.noreply.github.com>
2025-02-06 14:43:21 +0000

    Add tests for custom message in sig

--- flambda-backend/tests/backend/zero_alloc_checker/test_custom_error_msg.ml
+++ flambda-backend/tests/backend/zero_alloc_checker/test_custom_error_msg.ml
@@ -31,3 +31,63 @@ let syntax_error_missing_msg x = [|x;x*2|]
 
 let syntax_error_missing_msg_arity x = [|x;x*2|]
 [@@zero_alloc arity]
+
+
+(* [@zero_alloc custom_error_message "string"] in signatures: module inclusion
+   results in concatenated messages. *)
+module T1 = struct
+  module type S1 = sig
+    val f : 'a -> ('a * 'a) [@@zero_alloc custom_error_message "foo"]
+  end
+
+  module type S2 = sig
+    val f : 'a -> ('a * 'a) [@@zero_alloc custom_error_message "bar"]
+  end
+
+  module M1 = struct
+    let f x = (x,x)
+  end
+
+  module M' : S1 = M1
+
+  module M'' : S2 = M1
+end
+
+(* If there is a zero_alloc annotation on the structure (with or without a custom
+   error message), throw away the custom message string from the signature. *)
+module T2 = struct
+  module type S1 = sig
+    val f : 'a -> ('a * 'a) [@@zero_alloc custom_error_message "foo"]
+  end
+
+  module type S2 = sig
+    val f : 'a -> ('a * 'a) [@@zero_alloc custom_error_message "bar"]
+  end
+
+  module M2 = struct
+    let[@zero_alloc] f x = (x,x)
+  end
+
+  module M' : S1 = M2
+
+  module M'' : S2 = M2
+end
+
+module T3 = struct
+  module type S1 = sig
+    val f : 'a -> ('a * 'a) [@@zero_alloc custom_error_message "foo"]
+  end
+
+  module type S2 = sig
+    val f : 'a -> ('a * 'a) [@@zero_alloc custom_error_message "bar"]
+  end
+
+  module M3 = struct
+    let[@zero_alloc custom_error_message "use this, throw the others away"] f x =
+      (x,x)
+  end
+
+  module M' : S1 = M3
+
+  module M'' : S2 = M3
+end
