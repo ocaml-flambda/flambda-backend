11e560621bf7ce9849c82f79698ebfcb010f8220
Luke Maurer <lmaurer@janestreet.com>
2025-02-07 15:39:09 +0000

    Remove leftover debugging code from tests

--- testsuite/tests/templates/dunelike/p_int.ml
+++ testsuite/tests/templates/dunelike/p_int.ml
@@ -5,19 +5,3 @@ type t = int
 let create () = 1
 let frob = succ
 let to_string = string_of_int
-
-module Flunf = struct
-  module Biz = struct
-    type t = unit
-    let thing = 42
-  end
-
-  module Boz = struct
-    type t = unit
-    let stuff = 99
-  end
-end
-
-module Fleez = Flunf
-
-let frob_biz t = t
--- testsuite/tests/templates/dunelike/p_int.mli
+++ testsuite/tests/templates/dunelike/p_int.mli
@@ -5,21 +5,3 @@ type t = int
 val create : unit -> t
 val frob : t -> t
 val to_string : t -> string
-
-module Flunf : sig
-  module Biz : sig
-    type t
-    val thing : int
-  end
-
-  module Boz : sig
-    type t
-    val stuff : int
-  end
-end
-
-include module type of struct
-  module Fleez = (Flunf : sig module Biz : sig type t end end)
-end
-
-val frob_biz : Fleez.Biz.t -> Fleez.Biz.t
