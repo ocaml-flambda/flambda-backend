606fbae787f52b3f7a1ec3993f6cc2c2256afe68
Luke Maurer <lmaurer@janestreet.com>
2025-02-10 15:19:13 +0000

    Add comments to submodule files in tests
    Most of the files in the `dunelike/` tests are single-module libraries and thus
    have obvious `-open` flags, but `flourish.ml{,i}` and `ornament.ml{,i}` are in
    the `fancy` library so they're less obvious. (Ideally, we would just have the
    files be in subdirectories to begin with so they'd all be obvious, but ocamltest
    doesn't currently support putting `readonly_files` in subdirectories.)

--- testsuite/tests/templates/dunelike/flourish.ml
+++ testsuite/tests/templates/dunelike/flourish.ml
@@ -1,5 +1,8 @@
 (* Parameters: P, Q *)
 
+(* This is in the [fancy] library, so it's compiled with
+[-open Fancy__ -open No_direct_access_to_fancy] *)
+
 type t = P.t * P.t
 
 let create p = p, P.frob (P.create ())
--- testsuite/tests/templates/dunelike/ornament.mli
+++ testsuite/tests/templates/dunelike/ornament.mli
@@ -1,5 +1,8 @@
 (* Parameters: P, Q *)
 
+(* This is in the [fancy] library, so it's compiled with
+[-open Fancy__ -open No_direct_access_to_fancy] *)
+
 type t
 
 val create : string -> t
