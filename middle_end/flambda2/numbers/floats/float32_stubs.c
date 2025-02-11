659393066554527d832c3395395bdfe3ef342965
dkalinichenko-js <118547217+dkalinichenko-js@users.noreply.github.com>
2025-02-11 14:20:53 -0500

    handle unsupported null constants like float32 constants (#3564)
    * handle unsupported null constants like float32 constants
    
    * rename + add CRs
    
    ---------
    
    Co-authored-by: Diana Kalinichenko <dkalinichenko@janestreet.com>

--- middle_end/flambda2/numbers/floats/float32_stubs.c
+++ middle_end/flambda2/numbers/floats/float32_stubs.c
@@ -339,7 +339,7 @@ CAMLprim value compiler_float32_format(value fmt, value arg)
 // They must be weak symbols with the same names as in runtime/ because the stage
 // two compiler will link against runtime/.
 
-CAMLweakdef value caml_float32_is_stage1(value v) {
+CAMLweakdef value caml_is_boot_compiler(value v) {
   (void)v;
   return Val_true;
 }
