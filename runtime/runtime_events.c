5a3ff8f1a4e146525d36484dff4b98883fe71dbf
Nick Barnes <Nick.Barnes@pobox.com>
2025-02-20 12:59:13 +0000

    A series of runtime-events fixes ported from upstream (#3596)
    * A series of runtime-events fixes, ported from #13297 and #13419 upstream.
    
    Data races on metadata, overflow in multiplication, failure with short
    message length, failure with corrupted rings, uninitialized data
    reads. Clean recovery from various initialisation failures.
    
    Bugs found by ASAN, -fsanitize=memory -fsanitize-memory-track-origins,
    AFL, and inspection, and mostly reproducible by included test-cases.
    
    Co-authored-by: Miod Vallat <118974489+dustanddreams@users.noreply.github.com>
    Co-authored-by: Edwin Török <edwin.torok@cloud.com>
    Co-authored-by: Nick Barnes <nick@tarides.com>
    
    (cherry picked from commit 53b9aaa6215b808067cb9e8fd74d7f3f7720190c)
    (cherry picked from commit b1756270a6b12b3dd9f03b275122c4fdf13e72e7)
    (cherry picked from commit cc326a18fa3bee10ef566823fbe19199fb6da7e8)
    (cherry picked from commit 6eee6e3cc53870022359c3a048d5f2aeb93a4178)
    (cherry picked from commit 6857e5ba171db351ac6e233dd588667eba5b72a2)
    (cherry picked from commit 130eae66c95de7bc81db585e6842d2fd9bf6aa43)
    (cherry picked from commit 3ca771eaafeb609b80c966a1981baa66e44e39b0)
    (cherry picked from commit 8d7995f53d06c1673b070d75acacc868d996670e)
    
    * Fix bug in caml_ml_runtime_events_path
    
    ---------
    
    Co-authored-by: Edwin Török <edwin.torok@cloud.com>
    Co-authored-by: Mark Shinwell <mshinwell@pm.me>

--- runtime/runtime_events.c
+++ runtime/runtime_events.c
@@ -205,11 +205,13 @@ void caml_runtime_events_post_fork(void) {
   }
 }
 
-/* Return the current location for the ring buffers of this process. This is
-  used in the consumer to read the ring buffers of the current process */
+/* Return the path of the ring buffers file of this process, or NULL
+   if runtime events are not enabled. This is used in the consumer to
+   read the ring buffers of the current process. Always returns a
+   freshly-allocated string. */
 char_os* caml_runtime_events_current_location(void) {
   if( atomic_load_acquire(&runtime_events_enabled) ) {
-    return current_ring_loc;
+    return caml_stat_strdup_noexc_os(current_ring_loc);
   } else {
     return NULL;
   }
@@ -278,9 +280,13 @@ static void runtime_events_create_from_stw_single(void) {
 
     if (ring_file_handle == INVALID_HANDLE_VALUE) {
       char* ring_loc_u8 = caml_stat_strdup_of_os(current_ring_loc);
-      caml_fatal_error("Couldn't open ring buffer loc: %s",
-                        ring_loc_u8);
-      caml_stat_free(ring_loc_u8);
+      if (ring_loc_u8) {
+        caml_fatal_error("Couldn't open ring buffer file: %s",
+                         ring_loc_u8);
+        caml_stat_free(ring_loc_u8);
+      } else {
+        caml_fatal_error("Couldn't open ring buffer file");
+      }
     }
 
     ring_handle = CreateFileMapping(
@@ -478,6 +484,29 @@ CAMLprim value caml_ml_runtime_events_pause(value vunit) {
 
 CAMLprim value caml_ml_runtime_events_resume(value vunit) {
   caml_runtime_events_resume(); return Val_unit;
+}
+
+CAMLprim value caml_ml_runtime_events_path(value vunit)
+{
+  CAMLparam0();
+  CAMLlocal2(res, str);
+
+  if (atomic_load_acquire(&runtime_events_enabled)) {
+    /* The allocation might GC, which could allow another domain to
+     * nuke current_ring_loc, so we snapshot it first. */
+
+    char_os* current_ring_loc_str = caml_stat_strdup_os(current_ring_loc);
+
+    res = caml_alloc(1, Tag_some);
+    str = caml_copy_string_of_os(current_ring_loc_str);
+    Store_field(res, 0, str);
+
+    caml_stat_free(current_ring_loc_str);
+  } else {
+    res = Val_none;
+  }
+
+  CAMLreturn(res);
 }
 
 CAMLprim value caml_ml_runtime_events_are_active(void) {
