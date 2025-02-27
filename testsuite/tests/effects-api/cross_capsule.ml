7fba4f65ba6234568aebb39affe289ef6bb4ca0b
Diana Kalinichenko <dkalinichenko@janestreet.com>
2025-02-26 22:15:03 -0500

    cross-capsule perform test

---
+++ testsuite/tests/effects-api/cross_capsule.ml
@@ -0,0 +1,47 @@
+(* TEST
+ include stdlib_alpha;
+ runtime5;
+ { bytecode; }
+ { native; }
+*)
+
+[@@@alert "-unsafe_parallelism"]
+
+open Stdlib_alpha
+
+type 'a op = 
+  | Set : int -> unit op
+  | Get : int op
+
+module Eff = Effect.Make(struct
+  type 'a t = 'a op
+end)
+
+let store = Atomic.Safe.make 0
+
+let rec handle = function
+  | Eff.Result.Contended.Value v -> v
+  | Exception e -> raise e
+  | Operation (Set n, k) ->
+    Atomic.Safe.set store n;
+    handle (Effect.continue k {portable = () } [])
+  | Operation (Get, k) ->
+    let n = Atomic.Safe.get store in
+    handle (Effect.continue k {portable = n} [])
+;;
+
+type 'a aliased = Aliased of 'a @@ aliased [@@unboxed]
+
+let () =
+  handle (Eff.Portable.run (fun h ->
+    let (P k) = Capsule.create () in
+    let Aliased x, k =
+      Capsule.Key.access k (fun a ->
+      Aliased (Capsule.Data.wrap a (ref 4)))
+  in
+  let (), _k = Capsule.Key.with_password k (fun p ->
+    Capsule.Data.iter p (fun a -> Eff.Portable.perform h (Set !a)) x [@nontail]);
+  in
+  Printf.printf "Get: %d\n" (Eff.Portable.perform h Get)))
+;;
+
