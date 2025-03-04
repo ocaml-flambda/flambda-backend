3d2028178ce9640e3cda29fafb2ba5ee2b0ecf00
Chris Casinghino <ccasinghino@janestreet.com>
2025-03-03 19:09:36 -0500

    failing test

--- testsuite/tests/compiler-libs/test_untypeast.ml
+++ testsuite/tests/compiler-libs/test_untypeast.ml
@@ -60,3 +60,34 @@ run {| fun x y z -> (function w -> x y z w) |};;
 [%%expect{|
 - : string = "fun x y z -> (function | w -> x y z w)"
 |}];;
+
+(***********************************)
+(* Untypeast/pprintast correctly handle value binding type annotations. *)
+
+run {| let foo : 'a. 'a -> 'a = fun x -> x in foo |}
+
+[%%expect{|
+- : string = "let (foo : ('a : value) . 'a -> 'a) = fun x -> x in foo"
+|}];;
+
+run {| let foo : type a . a -> a = fun x -> x in foo |}
+
+[%%expect{|
+- : string =
+"let (foo : ('a : value) . 'a -> 'a) =\n  fun (type a) -> ( (fun x -> x : a -> a)) in\nfoo"
+|}];;
+
+(* CR: untypeast/pprintast are totally busted on programs with modes in value
+   bindings. Fix this. *)
+run {| let foo : 'a -> 'a @@ portable = fun x -> x in foo |}
+
+[%%expect{|
+>> Fatal error: Unrecognized mode portable - should not parse
+Exception: Misc.Fatal_error.
+|}];;
+
+run {| let foo : 'a . 'a -> 'a @@ portable = fun x -> x in foo |}
+
+[%%expect{|
+- : string = "let (foo : ('a : value) . 'a -> 'a) = fun x -> x in foo"
+|}];;
