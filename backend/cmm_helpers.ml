bb2d88ec6f00cf50528a42878b57ff4748cd8eeb
Jacob Van Buren <jvanburen@janestreet.com>
2025-03-05 15:03:54 -0500

    reverted changes to mixed block alloc

--- backend/cmm_helpers.ml
+++ backend/cmm_helpers.ml
@@ -1751,9 +1751,8 @@ let alloc_generic_set_fn block ofs newval memory_chunk dbg =
     addr_array_initialize block ofs newval dbg
   | Word_int -> generic_case ()
   (* Generic cases that may differ under big endian archs *)
-  | Byte_unsigned | Byte_signed | Sixteen_unsigned | Sixteen_signed | Single _
-  | Double | Thirtytwo_unsigned | Thirtytwo_signed | Onetwentyeight_unaligned
-  | Onetwentyeight_aligned ->
+  | Single _ | Double | Thirtytwo_unsigned | Thirtytwo_signed
+  | Onetwentyeight_unaligned | Onetwentyeight_aligned ->
     if Arch.big_endian
     then
       Misc.fatal_errorf
@@ -1761,6 +1760,11 @@ let alloc_generic_set_fn block ofs newval memory_chunk dbg =
          architectures"
         (Printcmm.chunk memory_chunk);
     generic_case ()
+  (* Forbidden cases *)
+  | Byte_unsigned | Byte_signed | Sixteen_unsigned | Sixteen_signed ->
+    Misc.fatal_errorf
+      "Fields with memory_chunk %s are not supported in generic allocations"
+      (Printcmm.chunk memory_chunk)
 
 let make_alloc_generic ~block_kind ~mode ~alloc_block_kind dbg tag wordsize args
     args_memory_chunks =
@@ -1860,7 +1864,8 @@ let make_mixed_alloc ~mode dbg ~tag ~value_prefix_size args args_memory_chunks =
           (* regular scanned part of a block *)
           match memory_chunk with
           | Word_int | Word_val -> ok ()
-          | Byte_unsigned | Byte_signed | Sixteen_unsigned | Sixteen_signed
+          | Byte_unsigned | Byte_signed | Sixteen_unsigned | Sixteen_signed ->
+            error "mixed blocks"
           | Thirtytwo_unsigned | Thirtytwo_signed | Single _ | Double
           | Onetwentyeight_unaligned | Onetwentyeight_aligned ->
             error "the value prefix of a mixed block"
@@ -1868,9 +1873,10 @@ let make_mixed_alloc ~mode dbg ~tag ~value_prefix_size args args_memory_chunks =
           (* flat suffix part of the block *)
           match memory_chunk with
           | Word_int | Thirtytwo_unsigned | Thirtytwo_signed | Double
-          | Onetwentyeight_unaligned | Onetwentyeight_aligned | Single _
-          | Byte_unsigned | Byte_signed | Sixteen_unsigned | Sixteen_signed ->
+          | Onetwentyeight_unaligned | Onetwentyeight_aligned | Single _ ->
             ok ()
+          | Byte_unsigned | Byte_signed | Sixteen_unsigned | Sixteen_signed ->
+            error "mixed blocks"
           | Word_val -> error "the flat suffix of a mixed block")
       0 args_memory_chunks
   in
