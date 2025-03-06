1163afbbbe9f484a06627761afadfaf0992b24ec
Diana Kalinichenko <dkalinichenko@janestreet.com>
2025-03-06 09:50:26 -0500

    amend

--- testsuite/tests/typing-layouts-or-null/immediate.ml
+++ testsuite/tests/typing-layouts-or-null/immediate.ml
@@ -80,21 +80,9 @@ type int_or_null : immediate_or_null = int or_null
 type int_or_null = int or_null
 |}]
 
-(* CR layouts v2.8: this is a bug in principal inference with with-kinds. *)
-
 type should_work = int_or_null accept_immediate_or_null
 [%%expect{|
 type should_work = int_or_null accept_immediate_or_null
-|}, Principal{|
-Line 1, characters 19-30:
-1 | type should_work = int_or_null accept_immediate_or_null
-                       ^^^^^^^^^^^
-Error: This type "int_or_null" = "int or_null" should be an instance of type
-         "('a : immediate_or_null)"
-       The kind of int_or_null is immediate_or_null with int
-         because it is the primitive immediate_or_null type or_null.
-       But the kind of int_or_null must be a subkind of immediate_or_null
-         because of the definition of accept_immediate_or_null at line 1, characters 0-54.
 |}]
 
 type should_work = int or_null accept_immediate_or_null
