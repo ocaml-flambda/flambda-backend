54da8461601d396afb0cb109b943b86f21111a91
Mark Shinwell <mshinwell@pm.me>
2025-03-12 11:38:58 +0000

    Middle end wiring

--- lambda/mixed_block_shape.ml
+++ lambda/mixed_block_shape.ml
@@ -30,19 +30,17 @@ type path = int list
 
 type 'a shape = 'a Lambda.mixed_block_element array
 
-type 'a shape_with_paths = (('a Lambda.mixed_block_element) * path) array
+type 'a shape_with_paths = ('a Lambda.mixed_block_element * path) array
 
 type 'a tree =
-  | Leaf of {
-    element : 'a Lambda.mixed_block_element;
-    new_index : int;
-  }
-  | Node of {
-      children : 'a tree array;
-    }
+  | Leaf of
+      { element : 'a Lambda.mixed_block_element;
+        new_index : int
+      }
+  | Node of { children : 'a tree array }
 
 type 'a t =
-  { (* CR-soon xclerc for xclerc: once the nesting/flatenning work is done,
+  { (* CR-soon xclerc for xclerc: once the nesting/flattening work is done,
        revisit this record to only keep the fields actually needed. *)
     original_shape : 'a shape; (* OK *)
     prefix : 'a shape; (* invariant: no `Product` *)
@@ -51,99 +49,112 @@ type 'a t =
     new_index_to_old_path : path array;
     (* CR-soon xclerc for xclerc: consider building a tree rather than a hashtable. *)
     old_path_to_new_index : (path, int) Hashtbl.t;
-    forest : 'a tree array;
+    forest : 'a tree array
   }
+
+let rec flatten_tree_array arr =
+  Array.to_list arr
+  |> List.concat_map (fun tree ->
+         match tree with
+         | Leaf { new_index; _ } -> [new_index]
+         | Node { children } -> flatten_tree_array children)
+
+type new_indexes = int list
+
+let lookup_path { forest; _ } path =
+  match path with
+  | [] -> Misc.fatal_error "No path provided"
+  | index :: path ->
+    let tree = forest.(index) in
+    let rec lookup_path' path tree =
+      match path, tree with
+      | [], Leaf { new_index; _ } -> [new_index]
+      | index :: path, Node { children; _ } ->
+        lookup_path' path children.(index)
+      | [], Node { children } -> flatten_tree_array children
+      | _ :: _, Leaf _ -> Misc.fatal_error "Invalid path"
+    in
+    lookup_path' path tree
 
 let _XXX t = t.forest
 
-let concat_arrays
-  : 'a array array -> 'a array
-  = fun arrays ->
-    (* CR-soon xclerc for xclerc: should we simply use the following?
-       `arrays |> Array.to_list |> Array.concat` *)
-    let total_len = ref 0 in
-    let init = ref None in
+let concat_arrays : 'a array array -> 'a array =
+ fun arrays ->
+  (* CR-soon xclerc for xclerc: should we simply use the following?
+     `arrays |> Array.to_list |> Array.concat` *)
+  let total_len = ref 0 in
+  let init = ref None in
+  for i = 0 to pred (Array.length arrays) do
+    let array = Array.unsafe_get arrays i in
+    let len = Array.length array in
+    total_len := !total_len + len;
+    if len > 0 then init := Some (Array.unsafe_get array 0)
+  done;
+  match !total_len, !init with
+  | 0, None -> [||]
+  | 0, Some _ -> Misc.fatal_error "broken invariant"
+  | _, None -> Misc.fatal_error "broken invariant"
+  | _, Some init ->
+    let dst = Array.make !total_len init in
+    let dst_pos = ref 0 in
     for i = 0 to pred (Array.length arrays) do
       let array = Array.unsafe_get arrays i in
       let len = Array.length array in
-      total_len := !total_len + len;
-      if len > 0 then
-        init := Some (Array.unsafe_get array 0)
+      (* CR-soon xclerc for xclerc: use unsafe_blit? *)
+      ArrayLabels.blit ~src:array ~src_pos:0 ~dst ~dst_pos:!dst_pos ~len;
+      dst_pos := !dst_pos + len
     done;
-    match !total_len, !init with
-    | 0, None -> [||]
-    | 0, Some _ -> Misc.fatal_error "broken invariant"
-    | _, None -> Misc.fatal_error "broken invariant"
-    | _, Some init ->
-      let dst = Array.make !total_len init in
-      let dst_pos = ref 0 in
-      for i = 0 to pred (Array.length arrays) do
-        let array = Array.unsafe_get arrays i in
-        let len = Array.length array in
-        (* CR-soon xclerc for xclerc: use unsafe_blit? *)
-        ArrayLabels.blit ~src:array ~src_pos:0 ~dst ~dst_pos:!dst_pos ~len;
-        dst_pos := !dst_pos + len
-      done;
-      dst
+    dst
 
 (* CR-soon xclerc for xclerc: it is probably quite inefficient to map/concat repeatedly. *)
-let rec flatten_one
-  : int -> 'a Lambda.mixed_block_element -> 'a shape_with_paths
-  = fun index element ->
+let rec flatten_one :
+    int -> 'a Lambda.mixed_block_element -> 'a shape_with_paths =
+ fun index element ->
   match element with
-  | Value _
-  | Float_boxed _
-  | Float64
-  | Float32
-  | Bits32
-  | Bits64
-  | Vec128
-  | Word -> [|element, [index]|]
-  | Product sub_elements->
+  | Value _ | Float_boxed _ | Float64 | Float32 | Bits32 | Bits64 | Vec128
+  | Word ->
+    [| element, [index] |]
+  | Product sub_elements ->
     flatten_list sub_elements
-    |> Array.map (fun (sub_element, path) -> (sub_element, index :: path))
+    |> Array.map (fun (sub_element, path) -> sub_element, index :: path)
 
-and flatten_list
-  : 'a Lambda.mixed_block_element array -> 'a shape_with_paths
-  = fun sub_elements ->
-    Array.mapi flatten_one sub_elements
-    |> concat_arrays
+and flatten_list : 'a Lambda.mixed_block_element array -> 'a shape_with_paths =
+ fun sub_elements -> Array.mapi flatten_one sub_elements |> concat_arrays
 
 (* CR xclerc for xclerc: should/could be merged with the flattening. *)
-let rec build_tree_one
-  : (path, int) Hashtbl.t -> path -> int -> 'a Lambda.mixed_block_element -> 'a tree
-  = fun old_path_to_new_index path index element ->
-    match element with
-    | Value _
-    | Float_boxed _
-    | Float64
-    | Float32
-    | Bits32
-    | Bits64
-    | Vec128
-    | Word ->
-      let path = List.rev (index :: path) in
-      begin match Hashtbl.find_opt old_path_to_new_index path with
-      | None ->
-        Format.eprintf "XXX path=%s\n%!"
-          (String.concat ", " (List.map string_of_int path))
-        ;
-        assert false
-      | Some new_index -> Leaf { element; new_index }
-      end
-    | Product sub_elements ->
-      let children =
-        build_tree_list old_path_to_new_index (index :: path) sub_elements
-      in
-      Node { children; }
+let rec build_tree_one :
+    (path, int) Hashtbl.t ->
+    path ->
+    int ->
+    'a Lambda.mixed_block_element ->
+    'a tree =
+ fun old_path_to_new_index path index element ->
+  match element with
+  | Value _ | Float_boxed _ | Float64 | Float32 | Bits32 | Bits64 | Vec128
+  | Word -> (
+    let path = List.rev (index :: path) in
+    match Hashtbl.find_opt old_path_to_new_index path with
+    | None ->
+      Format.eprintf "XXX path=%s\n%!"
+        (String.concat ", " (List.map string_of_int path));
+      assert false
+    | Some new_index -> Leaf { element; new_index })
+  | Product sub_elements ->
+    let children =
+      build_tree_list old_path_to_new_index (index :: path) sub_elements
+    in
+    Node { children }
 
-and build_tree_list
-  : (path, int) Hashtbl.t -> path -> 'a Lambda.mixed_block_element array -> 'a tree array
-  = fun old_path_to_new_index path sub_elements ->
-    Array.mapi
-      (fun i sub_element ->
-         build_tree_one old_path_to_new_index path i sub_element)
-      sub_elements
+and build_tree_list :
+    (path, int) Hashtbl.t ->
+    path ->
+    'a Lambda.mixed_block_element array ->
+    'a tree array =
+ fun old_path_to_new_index path sub_elements ->
+  Array.mapi
+    (fun i sub_element ->
+      build_tree_one old_path_to_new_index path i sub_element)
+    sub_elements
 
 let print_indentation k =
   for _ = 1 to k do
@@ -168,20 +179,16 @@ let rec print_tree ~indent ~index tree =
     print_indentation indent;
     Format.eprintf "[%d] " index;
     print_element element new_index
-  | Node { children; } ->
+  | Node { children } ->
     print_indentation indent;
     Format.eprintf "[%d]\n%!" index;
     print_trees ~indent:(succ indent) children
 
 and print_trees ~indent trees =
-  Array.iteri
-    (fun index tree -> print_tree ~indent ~index tree)
-    trees
+  Array.iteri (fun index tree -> print_tree ~indent ~index tree) trees
 
 let of_mixed_block_elements (original_shape : 'a shape) : 'a t =
-  let flattened_shape_with_paths =
-    flatten_list original_shape
-  in
+  let flattened_shape_with_paths = flatten_list original_shape in
   let prefix = ref [] in
   let suffix = ref [] in
   for idx = Array.length flattened_shape_with_paths - 1 downto 0 do
@@ -199,32 +206,29 @@ let of_mixed_block_elements (original_shape : 'a shape) : 'a t =
   done;
   let prefix = Array.of_list !prefix in
   let suffix = Array.of_list !suffix in
-  let flattened_and_reordered_shape =
-    Array.append prefix suffix
-  in
+  let flattened_and_reordered_shape = Array.append prefix suffix in
   let new_index_to_old_path =
     Array.make (Array.length flattened_and_reordered_shape) []
   in
-  let old_path_to_new_index =
-    Hashtbl.create (Array.length original_shape)
-  in
+  let old_path_to_new_index = Hashtbl.create (Array.length original_shape) in
   Array.iteri
     (fun new_index (_elem, old_path) ->
-       new_index_to_old_path.(new_index) <- old_path;
-       Hashtbl.replace old_path_to_new_index old_path new_index)
+      new_index_to_old_path.(new_index) <- old_path;
+      Hashtbl.replace old_path_to_new_index old_path new_index)
     flattened_and_reordered_shape;
-  let forest =
-    build_tree_list old_path_to_new_index [] original_shape
-  in
+  let forest = build_tree_list old_path_to_new_index [] original_shape in
   Format.eprintf "new_index_to_old_path:\n%!";
   Array.iteri
     (fun index path ->
-       Format.eprintf "  - %d -> %s\n%!" index (String.concat ", " (List.map string_of_int path)))
+      Format.eprintf "  - %d -> %s\n%!" index
+        (String.concat ", " (List.map string_of_int path)))
     new_index_to_old_path;
   Format.eprintf "old_path_to_new_index:\n%!";
   Hashtbl.iter
     (fun path index ->
-       Format.eprintf "  - %s -> %d\n%!" (String.concat ", " (List.map string_of_int path)) index)
+      Format.eprintf "  - %s -> %d\n%!"
+        (String.concat ", " (List.map string_of_int path))
+        index)
     old_path_to_new_index;
   Format.eprintf "forest:\n%!";
   print_trees ~indent:0 forest;
@@ -235,7 +239,7 @@ let of_mixed_block_elements (original_shape : 'a shape) : 'a t =
     flattened_shape = Array.map fst flattened_and_reordered_shape;
     new_index_to_old_path;
     old_path_to_new_index;
-    forest;
+    forest
   }
 
 let value_prefix t = t.prefix
@@ -252,22 +256,22 @@ let flattened_shape t = t.flattened_shape
 
 let flattened_shape_unit t =
   Array.map
-    (fun (elt : _ Lambda.mixed_block_element) : (unit Lambda.mixed_block_element) ->
+    (fun (elt : _ Lambda.mixed_block_element) : unit Lambda.mixed_block_element ->
       match elt with
-        | Float_boxed _ -> Float_boxed ()
-        | Product _sub_elems -> Misc.fatal_error "broken invariant"
-        | (Value _ | Float64 | Float32 | Bits32 | Bits64 | Vec128 | Word) as elem
-          ->
-          elem)
+      | Float_boxed _ -> Float_boxed ()
+      | Product _sub_elems -> Misc.fatal_error "broken invariant"
+      | (Value _ | Float64 | Float32 | Bits32 | Bits64 | Vec128 | Word) as elem
+        ->
+        elem)
     t.flattened_shape
 
-let new_index_to_old_path t i =
-  t.new_index_to_old_path.(i)
+let new_index_to_old_path t i = t.new_index_to_old_path.(i)
 
 let old_path_to_new_index t p =
   match Hashtbl.find_opt t.old_path_to_new_index p with
   | Some i -> i
-  | None -> Misc.fatal_errorf "invalid path (%s)" (String.concat ", " (List.map string_of_int p))
+  | None ->
+    Misc.fatal_errorf "invalid path (%s)"
+      (String.concat ", " (List.map string_of_int p))
 
-let old_path_to_new_indices _t _p =
-  assert false
+let old_path_to_new_indices _t _p = assert false
