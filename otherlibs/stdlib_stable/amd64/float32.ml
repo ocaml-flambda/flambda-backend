4171c110aff2defee37c168d037dc760ba7c8b2b
Greta Yorsh <45005955+gretay-js@users.noreply.github.com>
2025-03-21 11:58:57 +0000

    Remove [caml_sse*] from Stdlib_stable.Float32

--- otherlibs/stdlib_stable/amd64/float32.ml
+++ otherlibs/stdlib_stable/amd64/float32.ml
@@ -281,19 +281,21 @@ external iround_current : t -> int64
   = "caml_simd_cast_float32_int64_bytecode" "caml_simd_cast_float32_int64"
   [@@noalloc] [@@unboxed] [@@builtin]
 
-external round_intrinsic : (int[@untagged]) -> (t[@unboxed]) -> (t[@unboxed])
-  = "caml_sse41_float32_round_bytecode" "caml_sse41_float32_round"
+external round_current : (t[@unboxed]) -> (t[@unboxed])
+  = "caml_simd_float32_round_current_bytecode" "caml_simd_float32_round_current"
   [@@noalloc] [@@builtin]
 
-(* On amd64, these constants also imply _MM_FROUND_NO_EXC (suppress exceptions). *)
-let round_neg_inf = 0x9
-let round_pos_inf = 0xA
-let round_zero = 0xB
-let round_current_mode = 0xC
-let[@inline] round_current x = round_intrinsic round_current_mode x
-let[@inline] round_down x = round_intrinsic round_neg_inf x
-let[@inline] round_up x = round_intrinsic round_pos_inf x
-let[@inline] round_towards_zero x = round_intrinsic round_zero x
+external round_down : (t[@unboxed]) -> (t[@unboxed])
+  = "caml_simd_float32_round_neg_inf_bytecode" "caml_simd_float32_round_neg_inf"
+  [@@noalloc] [@@builtin]
+
+external round_up : (t[@unboxed]) -> (t[@unboxed])
+  = "caml_simd_float32_round_pos_inf_bytecode" "caml_simd_float32_round_pos_inf"
+  [@@noalloc] [@@builtin]
+
+external round_towards_zero : (t[@unboxed]) -> (t[@unboxed])
+  = "caml_simd_float32_round_towards_zero_bytecode" "caml_simd_float32_round_towards_zero"
+  [@@noalloc] [@@builtin]
 
 external seeded_hash_param : int -> int -> int -> 'a -> int = "caml_hash_exn"
   [@@noalloc]
