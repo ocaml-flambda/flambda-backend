5f5b7065c1bac450b3f9dad925f41e952cbe66a4
Basile Clément <basile.clement@ocamlpro.com>
2025-03-24 16:20:40 +0100

    Add Meet_and_n_way_join as copy of Meet_and_join

---
+++ middle_end/flambda2/types/meet_and_n_way_join.mli
@@ -0,0 +1,44 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                 OCaml                                  *)
+(*                                                                        *)
+(*                       Pierre Chambart, OCamlPro                        *)
+(*           Mark Shinwell and Leo White, Jane Street Europe              *)
+(*                                                                        *)
+(*   Copyright 2013--2021 OCamlPro SAS                                    *)
+(*   Copyright 2014--2021 Jane Street Group LLC                           *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
+(*   special exception on linking described in the file LICENSE.          *)
+(*                                                                        *)
+(**************************************************************************)
+
+(** Greatest lower bound of two types. *)
+val meet :
+  Typing_env.t ->
+  Type_grammar.t ->
+  Type_grammar.t ->
+  (Type_grammar.t * Typing_env.t) Or_bottom.t
+
+(** Least upper bound of two types. *)
+val join :
+  ?bound_name:Name.t ->
+  Typing_env.Join_env.t ->
+  Type_grammar.t ->
+  Type_grammar.t ->
+  Type_grammar.t Or_unknown.t
+
+val meet_shape :
+  Typing_env.t ->
+  Type_grammar.t ->
+  shape:Type_grammar.t ->
+  Typing_env.t Or_bottom.t
+
+(* This function has a slightly different interface; it is meant to be used only
+   by functions in Typing_env *)
+val meet_type :
+  Typing_env.t ->
+  Type_grammar.t ->
+  Type_grammar.t ->
+  (Type_grammar.t Typing_env.meet_return_value * Typing_env.t) Or_bottom.t
