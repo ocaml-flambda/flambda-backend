a12a4332271316c12c155db13a6ba07dee92c4b8
Greta Yorsh <45005955+gretay-js@users.noreply.github.com>
2025-03-26 12:29:04 +0000

    Generate ops_u.ml

---
+++ flambda-backend/tests/simd/unbox_types.ml
@@ -0,0 +1,6 @@
+type nonrec int8x16 = int8x16#
+type nonrec int16x8 = int16x8#
+type nonrec int32x4 = int32x4#
+type nonrec int64x2 = int64x2#
+type nonrec float32x4 = float32x4#
+type nonrec float64x2 = float64x2#
