a908e156fe4cbac91479a02d0298b76b6b9aad2e
Xavier Clerc <xclerc@janestreet.com>
2025-03-31 13:18:44 +0100

    Merge `_utils` module (backend).

--- backend/cfg/cfg_cse.mli
+++ backend/cfg/cfg_cse.mli
@@ -1,9 +1,22 @@
 [@@@ocaml.warning "+a-30-40-41-42"]
 
+(* Classification of operations *)
+
+type op_class =
+  | Op_pure (* pure arithmetic, produce one or several result *)
+  | Op_load of Simple_operation.mutable_flag (* memory load *)
+  | Op_store of bool (* memory store, false = init, true = assign *)
+  | Op_other (* anything else that does not allocate nor store in memory *)
+
+(* We maintain sets of equations of the form valnums = operation(valnums) plus a
+   mapping from registers to valnums (value numbers). *)
+
+module type Operation = sig
+  type t
+end
+
 (* Common subexpression elimination by value numbering over extended basic
    blocks. *)
-
-open CSE_utils
 
 class cse_generic :
   object
