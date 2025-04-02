b731c0ca16e6b044b2f0c6692da131f7d53ff82d
Greta Yorsh <45005955+gretay-js@users.noreply.github.com>
2025-04-02 12:56:58 +0100

    Enable cfg invariants and dead trap handler elimination by default
    Fail if any handlers are dead

--- driver/flambda_backend_flags.ml
+++ driver/flambda_backend_flags.ml
@@ -15,7 +15,7 @@
 (**************************************************************************)
 let use_ocamlcfg = ref true             (* -[no-]ocamlcfg *)
 let dump_cfg = ref false                (* -dcfg *)
-let cfg_invariants = ref false          (* -dcfg-invariants *)
+let cfg_invariants = ref true          (* -dcfg-invariants *)
 let regalloc = ref ""                   (* -regalloc *)
 let regalloc_params = ref ([] : string list)  (* -regalloc-param *)
 let regalloc_validate = ref true        (* -[no-]regalloc-validate *)
@@ -32,7 +32,7 @@ let cfg_peephole_optimize = ref true    (* -[no-]cfg-peephole-optimize *)
 let cfg_stack_checks = ref true         (* -[no-]cfg-stack-check *)
 let cfg_stack_checks_threshold = ref 16384 (* -cfg-stack-threshold *)
 
-let cfg_eliminate_dead_trap_handlers = ref false  (* -cfg-eliminate-dead-trap-handlers *)
+let cfg_eliminate_dead_trap_handlers = ref true  (* -cfg-eliminate-dead-trap-handlers *)
 
 let reorder_blocks_random = ref None    (* -reorder-blocks-random seed *)
 let basic_block_sections = ref false    (* -basic-block-sections *)
