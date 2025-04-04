16c57d6543de701932caa4e2fbba9bb1cfd6c76c
Ryan Tjoa <rtjoa@janestreet.com>
2025-04-04 11:59:09 -0400

    x

--- testsuite/tests/typing-layouts-block-indices/block_indices_basics.ml
+++ testsuite/tests/typing-layouts-block-indices/block_indices_basics.ml
@@ -20,9 +20,52 @@ Uncaught exception: Misc.Fatal_error
 
 |}]
 
-let f a = idx_ .i .# blah
+type t1 = { mutable a : string; b : int }
+type t2 = { mutable a : string; b : int; c : string }
+
+let a () = idx_ .a
+let b () = idx_ .b
+[%%expect{|
+type t1 = { mutable a : string; b : int; }
+type t2 = { mutable a : string; b : int; c : string; }
+>> Fatal error: transl unimplemented
+Uncaught exception: Misc.Fatal_error
+
+|}]
+
+type ('c, 'b, 'a) mi = ('a, 'b) mut_idx
+type ('c, 'b, 'a) i = ('a, 'b) idx
+[%%expect{|
+type ('c, 'b, 'a) mi = ('a, 'b) mut_idx
+type ('c, 'b, 'a) i = ('a, 'b) idx
+|}]
+
+let a () : (t1, _) mut_idx = idx_ .a
+let b () : (float, int, t1) i = idx_ .b
 [%%expect{|
 >> Fatal error: transl unimplemented
+Uncaught exception: Misc.Fatal_error
+
+|}]
+
+let b () : (_, _, t1) i = idx_ .c
+[%%expect{|
+Line 38, characters 32-33:
+38 | let b () : (_, _, t1) i = idx_ .c
+                                     ^
+Error: This expression is a field index with base  type "t1"
+       There is no field "c" within type "t1"
+|}, Principal{|
+Line 51, characters 32-33:
+51 | let b () : (_, _, t1) i = idx_ .c
+                                     ^
+Error: This expression is a field index with base  type "t1"
+       There is no field "c" within type "t1"
+|}]
+
+let f a = idx_ .i .# blah
+[%%expect{|
+>> Fatal error: unimplemented: type unboxed accesses
 Uncaught exception: Misc.Fatal_error
 
 |}]
