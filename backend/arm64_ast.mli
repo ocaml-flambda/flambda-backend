c0d47b7a600be4788c745d2e5b14a9e8ff85aede
Simon Spies <sspies@janestreet.com>
2025-04-04 16:44:53 +0100

    Extend ARM DSL and use it for the emission of more instructions (Part 1) (#3794)
    * script to build a mutant compiler for comparison
    
    * start with emitting address; test by duplicating store instructions
    
    * avoid rebuilding the boot compiler accidentally
    
    * remove duplicate stores and replace more instructions
    
    * remove duplicate load instruction
    
    * different load alternatives
    
    * fix typos
    
    * fix printing to match the original
    
    * more loads
    
    * stack pointer
    
    * support for fixed register
    
    * move DSL
    
    * moves
    
    * ocamlformat
    
    * support for unconditional branches
    
    * more branching
    
    * move symbol emit into DSL
    
    * support for labels
    
    * add support for symbols besides labels to ARM Ast
    
    * symbol operands
    
    * support branch lo
    
    * replace several branch instructions with DSL
    
    * memory barriers
    
    * replace lots of standard instructions
    
    * move up DSL further
    
    * do not print tab for empty arguments
    
    * ocamlformat
    
    * remove mutant compiler script from this PR
    
    * mark changes for future PRs
    
    * note that record label should return to its original form

--- backend/arm64_ast.mli
+++ backend/arm64_ast.mli
@@ -82,6 +82,8 @@ module Instruction_name : sig
       | GT
       | LE
       | AL
+      (* CR sspies: Remove [LO], since it is an alias of [CC] *)
+      | LO
   end
 
   module Rounding_mode : sig
@@ -91,6 +93,27 @@ module Instruction_name : sig
       | Z
       | X
       | N
+  end
+
+  module Memory_barrier : sig
+    type t =
+      | SY
+        (* full system barrier operation; the default; use this for [dmb]/[dsb]
+           without arguments *)
+      | LD (* waits only for loads to complete *)
+      | ST (* waits only for stores to complete *)
+      | ISH (* waits only for the inner sharable domain *)
+      | ISHLD (* waits only for loads and only for the inner sharable domain *)
+      | ISHST (* waits only for stores and only for the inner sharable domain *)
+      | NSH (* only out to the point of unification *)
+      | NSHLD
+        (* waits only for loads and only out to the point of unification *)
+      | NSHST
+        (* only for stores to complete and only out to the point of
+           unification *)
+      | OSH (* only to the outer shareable domain *)
+      | OSHLD (* waits only for loads and only to the outer shareable domain *)
+      | OSHST (* waits only for stores and only to the outer shareable domain *)
   end
 
   (** mnemonic *)
@@ -112,9 +135,13 @@ module Instruction_name : sig
     | CNT
     | SMULH
     | UMULH
-    | B of Cond.t
+    | B
+    | BR
+    | B_cond of Cond.t
     | BL
     | BLR
+    | CMP
+    | CMN
     | CBNZ
     | CBZ
     | CSEL
@@ -124,6 +151,32 @@ module Instruction_name : sig
     | SXTW
     | UXTB
     | UXTH
+    | LDR
+    | LDRB
+    | LDRSB
+    | LDRH
+    | LDRSH
+    | LDRSW
+    | STR
+    | STRB
+    | STRH
+    | DMB of Memory_barrier.t
+    | DSB of Memory_barrier.t
+    | ISB
+    | SDIV
+    | MSUB
+    | MADD
+    | REV
+    | REV16
+    | UBFM
+    | SBFM
+    | TST
+    | TBNZ
+    | TBZ
+    | ADR
+    | STP
+    (* CR sspies: Remove [BCC], since it is an alias of [B_cond LO] *)
+    | BCC
     (* neon *)
     | MOV
     | MOVI
@@ -182,15 +235,30 @@ module DSL : sig
 
   val reg_w : int -> Operand.t
 
+  val sp : Operand.t
+
+  val xzr : Operand.t
+
   val imm : int -> Operand.t
 
   val mem : base:int -> offset:int -> Operand.t
+
+  val mem_symbol : base:int -> symbol:string -> offset:int -> Operand.t
+
+  val mem_sp_offset : int -> Operand.t
 
   val mem_pre : base:int -> offset:int -> Operand.t
 
   val mem_post : base:int -> offset:int -> Operand.t
 
-  val literal : string -> Operand.t
+  (* ARM symbol operand; string must be converted to the OS specific
+     representation first *)
+  (* An ARM symbol can be used for both labels and symbols from the symbol
+     table; the respective conversion must be applied first *)
+  val symbol : string -> Operand.t
+
+  (* access memory at a ARM symbol *)
+  val mem_literal : string -> Operand.t
 
   (* CR gyorsh: [print_*] functions below are exposed temporarily to use DSL for
      some but not all instructions in [emit.mlp]. They can eventually*)
