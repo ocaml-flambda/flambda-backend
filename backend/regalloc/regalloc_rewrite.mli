090844826595eb98f239e94a0eb9b919153e39b7
Xavier Clerc <xclerc@users.noreply.github.com>
2025-04-08 12:28:42 +0100

    Tweak logging code of register allocators (#3822)

--- backend/regalloc/regalloc_rewrite.mli
+++ backend/regalloc/regalloc_rewrite.mli
@@ -15,11 +15,13 @@ module type Utils = sig
 
   val invariants : bool Lazy.t
 
-  val log :
-    indent:int -> ?no_eol:unit -> ('a, Format.formatter, unit) format -> 'a
+  val log : ?no_eol:unit -> ('a, Format.formatter, unit) format -> 'a
+
+  val indent : unit -> unit
+
+  val dedent : unit -> unit
 
   val log_body_and_terminator :
-    indent:int ->
     Cfg.basic_instruction_list ->
     Cfg.terminator Cfg.instruction ->
     liveness ->
