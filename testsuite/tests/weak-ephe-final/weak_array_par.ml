ed9162a9c2e85f84bf1a412ab5453c5ed9fefdb4
Stephen Dolan <sdolan@janestreet.com>
2025-04-08 17:09:16 +0100

    Fix Weak.set on arm64 (and other relaxed architectures) (#3819)
    * Make weak_array_par more likely to fail on arm64
    
    * Add appropriate memory fences on ephemeron updates

--- testsuite/tests/weak-ephe-final/weak_array_par.ml
+++ testsuite/tests/weak-ephe-final/weak_array_par.ml
@@ -11,7 +11,7 @@ let () = Random.self_init ()
 
 let num_domains = 4
 let iters = 1_000_000
-let len = 10_000
+let len = 10
 let table = Weak.create len
 
 let go () =
@@ -20,9 +20,11 @@ let go () =
     let h = String.hash s mod len in
     begin match Weak.get table h with
     | None -> ()
-    | Some s' -> assert (String.hash s' mod len = h)
+    | Some (s1, s2) ->
+      assert (String.equal s1 s2);
+      assert (String.hash s1 mod len = h)
     end;
-    Weak.set table h (Some s)
+    Weak.set table h (Some (s,s))
   done
 
 let () =
