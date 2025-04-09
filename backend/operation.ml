21648c133b5673dca9785490f92b0553a3f1f06f
Xavier Clerc <xclerc@users.noreply.github.com>
2025-04-09 16:10:48 +0100

    Avoid polymorphic comparison in `backend` (#3649)

--- backend/operation.ml
+++ backend/operation.ml
@@ -26,6 +26,8 @@
 
 [@@@ocaml.warning "+a-4-9-40-41-42"]
 
+open! Int_replace_polymorphic_compare [@@ocaml.warning "-66"]
+
 type trap_stack =
   | Uncaught
   | Specific_trap of Cmm.trywith_shared_label * trap_stack
