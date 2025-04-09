07ec18d2db3a29fc2870ad13f5da760a8657e8c1
Xavier Clerc <xclerc@users.noreply.github.com>
2025-04-09 12:04:19 +0100

    Linscan: use doubly-linked lists for intervals (#3737)

--- backend/regalloc/regalloc_ls_utils.mli
+++ backend/regalloc/regalloc_ls_utils.mli
@@ -5,6 +5,8 @@ module DLL = Flambda_backend_utils.Doubly_linked_list
 
 val log : ?no_eol:unit -> ('a, Format.formatter, unit) format -> 'a
 
+val equal_list_dll : ('a -> 'a -> bool) -> 'a list -> 'a DLL.t -> bool
+
 val indent : unit -> unit
 
 val dedent : unit -> unit
@@ -57,6 +59,8 @@ module Interval : sig
       ranges : Range.t DLL.t
     }
 
+  val equal : t -> t -> bool
+
   val copy : t -> t
 
   val print : Format.formatter -> t -> unit
@@ -68,18 +72,31 @@ module Interval : sig
   val remove_expired : t -> pos:int -> unit
 
   module List : sig
+    val print : Format.formatter -> t list -> unit
+
     val release_expired_fixed : t list -> pos:int -> t list
 
     val insert_sorted : t list -> t -> t list
   end
+
+  module DLL : sig
+    val print : Format.formatter -> t DLL.t -> unit
+
+    val release_expired_fixed : t DLL.t -> pos:int -> unit
+
+    val insert_sorted : t DLL.t -> t -> unit
+  end
 end
 
 module ClassIntervals : sig
-  (* Similar to [Linscan.class_intervals] (in "backend/linscan.mln"). *)
+  (* Similar to [Linscan.class_intervals] (in "backend/linscan.ml"). *)
   type t =
-    { mutable fixed : Interval.t list;
-      mutable active : Interval.t list;
-      mutable inactive : Interval.t list
+    { mutable fixed_list : Interval.t list;
+      mutable active_list : Interval.t list;
+      mutable inactive_list : Interval.t list;
+      fixed_dll : Interval.t DLL.t;
+      active_dll : Interval.t DLL.t;
+      inactive_dll : Interval.t DLL.t
     }
 
   val make : unit -> t
@@ -91,8 +108,12 @@ module ClassIntervals : sig
   val clear : t -> unit
 
   val release_expired_intervals : t -> pos:int -> unit
+
+  val check_consistency : t -> string -> unit
 end
 
 val log_interval : kind:string -> Interval.t -> unit
 
-val log_intervals : kind:string -> Interval.t list -> unit
+val log_interval_list : kind:string -> Interval.t list -> unit
+
+val log_interval_dll : kind:string -> Interval.t DLL.t -> unit
