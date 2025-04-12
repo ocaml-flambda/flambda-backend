b82bb10b2ccd788d872e5e1c77ef42b7b2887b4c
Aspen Smith <aspsmith@janestreet.com>
2025-04-12 19:14:13 +0100

    Infer more precise with-bounds for static polymorphic variants
    In the case that a polymorphic variant is *static*, meaning it's closed and its
    row variable isn't bound, it's easy enough to infer a more precise kind for it
    than `value` - we just treat it like a regular (boxed) variant, adding all its
    mentioned types (given by Btype.fold_row) to the with-bounds.

--- typing/jkind.mli
+++ typing/jkind.mli
@@ -495,6 +495,9 @@ val for_boxed_variant : Types.constructor_declaration list -> Types.jkind_l
 (** Choose an appropriate jkind for a boxed tuple type. *)
 val for_boxed_tuple : (string option * Types.type_expr) list -> Types.jkind_l
 
+(** Choose an appropriate jkind for a (known to be non-immediate) row type *)
+val for_boxed_row : Types.row_desc -> Types.jkind_l
+
 (** The jkind of an arrow type. *)
 val for_arrow : Types.jkind_l
 
