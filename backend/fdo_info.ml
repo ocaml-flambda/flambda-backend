7ac5cbf1122db11aa663097ef125254b49028af5
Greta Yorsh <45005955+gretay-js@users.noreply.github.com>
2025-04-15 18:05:48 +0100

    same

--- backend/fdo_info.ml
+++ backend/fdo_info.ml
@@ -12,6 +12,8 @@
 (*                                                                        *)
 (**************************************************************************)
 
+[@@@ocaml.warning "+a-40-41-42"]
+
 open! Int_replace_polymorphic_compare
 
 type info =
