1edc31bbf48eba4bde18af5823193daea6d187b3
Greta Yorsh <45005955+gretay-js@users.noreply.github.com>
2025-04-15 18:32:37 +0100

    Enable more warnings

--- backend/operation.ml
+++ backend/operation.ml
@@ -24,7 +24,7 @@
  *                                                                                *
  **********************************************************************************)
 
-[@@@ocaml.warning "+a-4-9-40-41-42"]
+[@@@ocaml.warning "+a-40-41-42"]
 
 open! Int_replace_polymorphic_compare [@@ocaml.warning "-66"]
 
