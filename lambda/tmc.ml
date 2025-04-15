496a0c53193234755854a0ce6fea3c67782a5a95
Ryan Tjoa <rtjoa@janestreet.com>
2025-04-15 13:46:06 -0400

    wip offset access primitives

--- lambda/tmc.ml
+++ lambda/tmc.ml
@@ -980,7 +980,7 @@ let rec choice ctx t =
     | Psequand | Psequor
     | Ppoll
     | Ppeek _ | Ppoke _
-      ->
+    | Pread_offset _ | Pwrite_offset _ ->
         let primargs = traverse_list ctx primargs in
         Choice.lambda (Lprim (prim, primargs, loc))
 
