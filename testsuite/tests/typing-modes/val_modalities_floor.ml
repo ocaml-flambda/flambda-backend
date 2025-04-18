623ba4136359f7c3cab4942006418d3833be5e75
Zesen Qian <riaqn@users.noreply.github.com>
2025-04-18 10:53:50 +0100

    Revert "modalities zap to id for mli-less files (#3865)"
    This reverts commit ec6e24e2a0bc7418596b80aa0c0d88a625010df8.

--- testsuite/tests/typing-modes/val_modalities_floor.ml
+++ testsuite/tests/typing-modes/val_modalities_floor.ml
@@ -1,8 +1,12 @@
+(* We test that cmi generated without mli has the modalities pushed to the
+strongest instead of legacy *)
+
 (* TEST
  readonly_files = "\
    def_portable.ml \
-   use_portable.ml use_portable.reference \
-  ";
+   def_nonportable.ml \
+   use_portable.ml use_portable.bad.reference \
+ ";
 {
    setup-ocamlopt.byte-build-env;
    flags = "-extension mode";
@@ -17,15 +21,27 @@
     ocamlopt.byte;
 
     module = "use_portable.ml";
-    compiler_output = "use_portable.output";
+
+    ocamlopt_byte_exit_status = "0";
+    ocamlopt.byte;
+   }
+
+   {
+    src = "def_nonportable.ml";
+    dst = "maybe_portable.ml";
+    copy;
+
+    module = "maybe_portable.ml";
+    ocamlopt_byte_exit_status = "0";
+    ocamlopt.byte;
+
+    module = "use_portable.ml";
+    compiler_output = "use_portable.bad.output";
     ocamlopt_byte_exit_status = "2";
     ocamlopt.byte;
 
-    compiler_reference = "use_portable.reference";
+    compiler_reference = "use_portable.bad.reference";
     check-ocamlopt.byte-output;
     }
 }
 *)
-
-(* We test that cmi generated without mli has the modalities pushed to the
-legacy instead of strongest *)
