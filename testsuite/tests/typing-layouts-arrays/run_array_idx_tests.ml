b7a6e5f8d5ab778eabdc2e1aa58d8e15443ad14d
Ryan Tjoa <rtjoa@janestreet.com>
2025-05-01 10:10:55 -0400

    array_idx tests

--- testsuite/tests/typing-layouts-arrays/run_array_idx_tests.ml
+++ testsuite/tests/typing-layouts-arrays/run_array_idx_tests.ml
@@ -1,7 +1,7 @@
 (* TEST
- readonly_files = "generate_makearray_dynamic_tests.ml";
+ readonly_files = "generate_array_idx_tests.ml";
  (* Generate the bytecode/native code versions of
-    [generate_makearray_dynamic_tests.ml]. This doesn't actually run the test;
+    [generate_array_idx_tests.ml]. This doesn't actually run the test;
     it just updates the generated test program (which is separately
     run by the test harness).
   *)
@@ -10,7 +10,7 @@
    setup-ocamlopt.opt-build-env;
    stack-allocation;
    program = "${test_source_directory}/generate.out";
-   all_modules = "generate_makearray_dynamic_tests.ml";
+   all_modules = "generate_array_idx_tests.ml";
    include stdlib_stable;
    include stdlib_upstream_compatible;
    ocamlopt.opt;
