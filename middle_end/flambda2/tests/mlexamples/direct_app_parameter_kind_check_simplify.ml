9a4a9e123bc92c113d02120cf7fcde151a0f2420
Vincent Laviron <vincent.laviron@gmail.com>
2025-05-06 15:47:39 +0200

    Add tests for direct application kind mismatch

---
+++ middle_end/flambda2/tests/mlexamples/direct_app_parameter_kind_check_simplify.ml
@@ -0,0 +1,18 @@
+[@@@ocaml.flambda_o3]
+
+type _ arity = Unb : ( #('a * 'b) -> unit) arity
+let x : (#(int * int) -> unit) arity = Unb
+
+type _ tuple = Tup2 : {  arg1 : 'a ;  arg2 : 'b } -> ( 'a -> 'b -> unit) tuple
+
+type 'c t = 'c tuple option ref
+
+let send (type c) (t : c t) (c : c) : unit =
+  match !t with
+  | None -> ()
+  | Some (Tup2 {arg1; arg2}) -> c arg1 arg2
+
+let create (type c) (_arity : c arity) : c t =
+ ref None
+
+let () = send (create x) (fun _ -> ());
