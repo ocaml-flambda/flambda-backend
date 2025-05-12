6f8ecc59fd82c579a072ba2d499a215ab770d1bf
James Rayman <james@jamesrayman.com>
2025-05-12 17:44:45 -0400

    A

--- .github/workflows/80ch.sh
+++ .github/workflows/80ch.sh
@@ -19,9 +19,7 @@
 #          annoying, 80ch.sh never fails, but instead emits warnings, and only
 #          checks a subset of all files (See SKIP FILES).
 
-set -xu
-
-git log --oneline --decorate
+set -u
 
 feature_base="HEAD^1" # GitHub automatically makes us a merge commit (HEAD),
                       # so our feature base is just the first parent (HEAD^1)
