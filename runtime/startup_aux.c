ce056c9c1b71c9c84c3107a80c993d8ba2d25c07
Nick Barnes <Nick.Barnes@pobox.com>
2025-05-13 16:13:37 +0100

    Set a default for major_heap_increment.

--- runtime/startup_aux.c
+++ runtime/startup_aux.c
@@ -94,6 +94,7 @@ static void init_startup_params(void)
   params.init_custom_major_ratio = Custom_major_ratio_def;
   params.init_custom_minor_ratio = Custom_minor_ratio_def;
   params.init_custom_minor_max_bsz = Custom_minor_max_bsz_def;
+  params.init_major_heap_increment = Heap_chunk_def;
   params.init_max_stack_wsz = Max_stack_def;
   params.max_domains = Max_domains_def;
   params.runtime_events_log_wsize = Default_runtime_events_log_wsize;
