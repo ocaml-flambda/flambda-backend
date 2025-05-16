83809509546124fd77a81ce56b962b70ad3c9d2f
Diana Kalinichenko <dkalinichenko@janestreet.com>
2025-05-16 14:55:06 -0400

    compare with genarray

--- testsuite/tests/typing-layouts-or-null/non_float_array.ml
+++ testsuite/tests/typing-layouts-or-null/non_float_array.ml
@@ -3,23 +3,54 @@
  expect;
 *)
 
+(* Normal arrays are [genarray]s. *)
+
+let mk_gen (x : 'a) = [| x |]
+[%%expect{|
+(let
+  (mk_gen/283 =
+     (function {nlocal = 0} x/285 : genarray (makearray[gen] x/285)))
+  (apply (field_imm 1 (global Toploop!)) "mk_gen" mk_gen/283))
+val mk_gen : 'a -> 'a array = <fun>
+|}]
+
+let get_gen (xs : 'a array) i = xs.(i)
+[%%expect{|
+(let
+  (get_gen/286 =
+     (function {nlocal = 0} xs/288[genarray] i/289[int]
+       (array.get[gen indexed by int] xs/288 i/289)))
+  (apply (field_imm 1 (global Toploop!)) "get_gen" get_gen/286))
+val get_gen : 'a array -> int -> 'a = <fun>
+|}]
+
+let set_gen (xs : 'a array) x i = xs.(i) <- x
+[%%expect{|
+(let
+  (set_gen/340 =
+     (function {nlocal = 0} xs/342[genarray] x/343 i/344[int] : int
+       (array.set[gen indexed by int] xs/342 i/344 x/343)))
+  (apply (field_imm 1 (global Toploop!)) "set_gen" set_gen/340))
+val set_gen : 'a array -> 'a -> int -> unit = <fun>
+|}]
+
 (* [non_float] arrays are [addrarray]s. *)
 
 let mk (type t : value mod non_float) (x : t) = [| x |]
 [%%expect{|
 (let
-  (mk/283 = (function {nlocal = 0} x/286 : addrarray (makearray[addr] x/286)))
-  (apply (field_imm 1 (global Toploop!)) "mk" mk/283))
+  (mk/345 = (function {nlocal = 0} x/348 : addrarray (makearray[addr] x/348)))
+  (apply (field_imm 1 (global Toploop!)) "mk" mk/345))
 val mk : ('t : value mod non_float). 't -> 't array = <fun>
 |}]
 
 let get (type t : value mod non_float) (xs : t array) i = xs.(i)
 [%%expect{|
 (let
-  (get/287 =
-     (function {nlocal = 0} xs/290[addrarray] i/291[int]
-       (array.get[addr indexed by int] xs/290 i/291)))
-  (apply (field_imm 1 (global Toploop!)) "get" get/287))
+  (get/349 =
+     (function {nlocal = 0} xs/352[addrarray] i/353[int]
+       (array.get[addr indexed by int] xs/352 i/353)))
+  (apply (field_imm 1 (global Toploop!)) "get" get/349))
 val get : ('t : value mod non_float). 't array -> int -> 't = <fun>
 |}]
 
@@ -27,10 +58,10 @@ let set (type t : value mod non_float) (xs : t array) x i = xs.(i) <- x
 
 [%%expect{|
 (let
-  (set/342 =
-     (function {nlocal = 0} xs/345[addrarray] x/346 i/347[int] : int
-       (array.set[addr indexed by int] xs/345 i/347 x/346)))
-  (apply (field_imm 1 (global Toploop!)) "set" set/342))
+  (set/354 =
+     (function {nlocal = 0} xs/357[addrarray] x/358 i/359[int] : int
+       (array.set[addr indexed by int] xs/357 i/359 x/358)))
+  (apply (field_imm 1 (global Toploop!)) "set" set/354))
 val set : ('t : value mod non_float). 't array -> 't -> int -> unit = <fun>
 |}]
 
@@ -49,11 +80,11 @@ end = struct
 end
 
 [%%expect{|
-(apply (field_imm 1 (global Toploop!)) "X/358"
+(apply (field_imm 1 (global Toploop!)) "X/370"
   (let
-    (x1/353 =[(consts ()) (non_consts ([0: *, [int]]))] [0: "first" 1]
-     x2/354 =[(consts ()) (non_consts ([0: *, [int]]))] [0: "second" 2])
-    (makeblock 0 x1/353 x2/354)))
+    (x1/365 =[(consts ()) (non_consts ([0: *, [int]]))] [0: "first" 1]
+     x2/366 =[(consts ()) (non_consts ([0: *, [int]]))] [0: "second" 2])
+    (makeblock 0 x1/365 x2/366)))
 module X : sig type t : immutable_data val x1 : t val x2 : t end
 |}]
 
@@ -68,25 +99,25 @@ let () =
 
 [%%expect{|
 (let
-  (X/358 = (apply (field_imm 0 (global Toploop!)) "X/358")
-   *match*/363 =[int]
-     (let (xs/361 =[addrarray] (caml_make_vect 4 (field_imm 0 X/358)))
-       (seq (array.set[addr indexed by int] xs/361 1 (field_imm 1 X/358))
-         (array.set[addr indexed by int] xs/361 2 (field_imm 1 X/358))
+  (X/370 = (apply (field_imm 0 (global Toploop!)) "X/370")
+   *match*/375 =[int]
+     (let (xs/373 =[addrarray] (caml_make_vect 4 (field_imm 0 X/370)))
+       (seq (array.set[addr indexed by int] xs/373 1 (field_imm 1 X/370))
+         (array.set[addr indexed by int] xs/373 2 (field_imm 1 X/370))
          (if
-           (caml_equal (array.get[addr indexed by int] xs/361 0)
-             (array.get[addr indexed by int] xs/361 3))
+           (caml_equal (array.get[addr indexed by int] xs/373 0)
+             (array.get[addr indexed by int] xs/373 3))
            0
            (raise (makeblock 0 (getpredef Assert_failure/40!!) [0: "" 5 2])))
          (if
-           (caml_equal (array.get[addr indexed by int] xs/361 1)
-             (array.get[addr indexed by int] xs/361 2))
+           (caml_equal (array.get[addr indexed by int] xs/373 1)
+             (array.get[addr indexed by int] xs/373 2))
            0
            (raise (makeblock 0 (getpredef Assert_failure/40!!) [0: "" 6 2])))
          (if
            (not
-             (caml_equal (array.get[addr indexed by int] xs/361 0)
-               (array.get[addr indexed by int] xs/361 1)))
+             (caml_equal (array.get[addr indexed by int] xs/373 0)
+               (array.get[addr indexed by int] xs/373 1)))
            0
            (raise (makeblock 0 (getpredef Assert_failure/40!!) [0: "" 7 2]))))))
   0)
