39b54fc6bbd47b2cd9f791665d82d1ba5079d860
Ryan Tjoa <rtjoa@janestreet.com>
2025-05-16 11:09:18 -0400

    update stdlib

---
+++ otherlibs/stdlib_beta/idx_imm.ml
@@ -0,0 +1,21 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                 OCaml                                  *)
+(*                                                                        *)
+(*                    Ryan Tjoa, Jane Street, New York                    *)
+(*                                                                        *)
+(*   Copyright 2024 Jane Street Group LLC                                 *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
+(*   special exception on linking described in the file LICENSE.          *)
+(*                                                                        *)
+(**************************************************************************)
+
+[@@@ocaml.flambda_o3]
+
+type ('a, 'b : any) t : bits64 = ('a, 'b) idx_imm
+
+external[@layout_poly] unsafe_get :
+  'a ('b : any). ('a [@local_opt]) -> ('a, 'b) idx_imm -> ('b [@local_opt]) =
+  "%unsafe_get_idx_imm"
