9324f3b2caa77061e7a3dae4ab19682ee6178a85
Diana Kalinichenko <dkalinichenko@janestreet.com>
2025-05-19 18:53:14 -0400

    toggle separability

--- typing/typeopt.ml
+++ typing/typeopt.ml
@@ -243,14 +243,13 @@ let array_kind_of_elt ~elt_sort env loc ty =
     else
       Pgcscannableproductarray (scannable_product_array_kind loc sorts)
   in
-  (* CR dkalinichenko: layout, externality and separability should be
-     sufficient to determine the array kind, with no calls to [classify]
-     needed. But I'm leaving it here for now to make sure all existing
-     optimizations still work. *)
+  (* CR dkalinichenko: many checks in [classify] are redundant
+     with separability. *)
   match classify ~classify_product env loc ty elt_sort with
   | Any ->
     if Config.flat_float_array
-      && not (Ctype.check_type_separability env ty Non_float)
+      && not (Language_extension.is_at_least Separability ()
+          && Ctype.check_type_separability env ty Non_float)
     then Pgenarray
     else Paddrarray
   | Float -> if Config.flat_float_array then Pfloatarray else Paddrarray
