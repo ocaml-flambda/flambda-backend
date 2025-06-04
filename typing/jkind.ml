d5dce0834cb91256966e86c77c246e015d120509
Ryan Tjoa <rtjoa@janestreet.com>
2025-06-04 11:19:23 -0400

    create void-alpha
    This reverts commit 038bd5b1a6e11b8d8b43b1585559ba33f6e8cd93.

--- typing/jkind.ml
+++ typing/jkind.ml
@@ -1941,7 +1941,7 @@ module Const = struct
         List.fold_left
           (fun m l -> Language_extension.Maturity.max m (scan_layout l))
           Language_extension.Stable layouts
-      | Base Void, _ -> Alpha
+      | Base Void, _ -> Beta
     in
     scan_layout jkind.layout
 
